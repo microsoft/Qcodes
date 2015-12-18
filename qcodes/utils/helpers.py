4f64165c25a1fec1d103b371e3e6aa3ec0f57e9a
alexcjohnson <alex@plot.ly>
2015-12-18 22:50:25 +0100

    site flag in reload_code

--- qcodes/utils/helpers.py
+++ qcodes/utils/helpers.py
@@ -183,27 +183,28 @@ stdlib = tuple(p for p in syspaths
 # dependencies - this is mainly for reloading our own code.
 # could even whitelist site-packages items to allow, rather than to ignore?
 otherlib = ('jupyter', 'ipy', 'IPy', 'matplotlib', 'numpy', 'scipy', 'pyvisa',
-            'traitlets', 'zmq', 'tornado', 'dateutil', 'pkg-resources', 'six',
-            'pexpect')
+            'traitlets', 'zmq', 'tornado', 'dateutil', 'six', 'pexpect')
 otherpattern = tuple('site-packages/' + n for n in otherlib)
 
 
-def reload_code(pattern=None, lib=False):
+def reload_code(pattern=None, lib=False, site=False):
     '''
     reload all modules matching a given pattern
     or all (non-built-in) modules if pattern is omitted
     if lib is False (default), ignore the standard library and major packages
+    if site is False (default), ignore everything in site-packages, only reload
+    files in nonstandard paths
     '''
     reloaded_files = []
 
     for module in sys.modules.values():
         if (pattern is None or pattern in module.__name__):
-            reload_recurse(module, reloaded_files, lib)
+            reload_recurse(module, reloaded_files, lib, site)
 
     return reloaded_files
 
 
-def is_good_module(module, lib=False):
+def is_good_module(module, lib=False, site=False):
     '''
     is an object (module) a module we can reload?
     if lib is False (default), ignore the standard library and major packages
@@ -217,8 +218,12 @@ def is_good_module(module, lib=False):
     if name in sys.builtin_module_names or not hasattr(module, '__file__'):
         return False
 
+    path = os.path.abspath(module.__file__)
+
+    if 'site-packages' in path and not site:
+        return False
+
     if not lib:
-        path = os.path.abspath(module.__file__)
         if path.startswith(stdlib) and 'site-packages' not in path:
             return False
 
@@ -229,13 +234,14 @@ def is_good_module(module, lib=False):
     return True
 
 
-def reload_recurse(module, reloaded_files, lib):
+def reload_recurse(module, reloaded_files, lib, site):
     '''
     recursively search module for its own dependencies to reload,
     ignoring those already in reloaded_files
     if lib is False (default), ignore the standard library and major packages
     '''
-    if not is_good_module(module, lib) or module.__file__ in reloaded_files:
+    if (not is_good_module(module, lib, site) or
+            module.__file__ in reloaded_files):
         return
 
     reloaded_files.append(module.__file__)
@@ -243,7 +249,7 @@ def reload_recurse(module, reloaded_files, lib):
     try:
         for name in dir(module):
             module2 = getattr(module, name)
-            reload_recurse(module2, reloaded_files, lib)
+            reload_recurse(module2, reloaded_files, lib, site)
         imp.reload(module)
 
     except:
