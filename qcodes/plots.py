b6ad830fa923715b1c442c9a254b9718db3dda86
alexcjohnson <alex@plot.ly>
2016-01-13 12:14:47 +0100

    infer setpoint data and dimensionality in Plot calls

--- qcodes/plots.py
+++ qcodes/plots.py
@@ -16,7 +16,10 @@ from qcodes.widgets.widgets import HiddenUpdateWidget
 
 class Plot(object):
     '''
-    Plot x/y lines or x/y/z heatmap data
+    Plot x/y lines or x/y/z heatmap data. The first trace may be included
+    in the constructor, other traces can be added with Plot.add()
+
+    args: shortcut to provide the x/y/z data. See Plot.add
 
     figsize: (width, height) tuple to pass to plt.figure
         default (12, 5)
@@ -26,10 +29,11 @@ class Plot(object):
         plt.subplots. default is a single simple subplot (1, 1)
         you can use this to pass kwargs to the plt.figure constructor
 
-    kwargs: passed along to self.add() to add the first data trace
+    kwargs: passed along to Plot.add() to add the first data trace
     '''
-    def __init__(self, figsize=(12, 5), interval=1, subplots=(1, 1),
+    def __init__(self, *args, figsize=(12, 5), interval=1, subplots=(1, 1),
                  **kwargs):
+
         if isinstance(subplots, Mapping):
             self.fig, self.subplots = plt.subplots(**subplots)
         else:
@@ -40,15 +44,21 @@ class Plot(object):
         self.traces = []
         self.data_updaters = set()
 
-        if kwargs:
-            self.add(**kwargs)
+        if args or kwargs:
+            self.add(*args, **kwargs)
 
         self.update_widget = HiddenUpdateWidget(self.update, interval)
         display(self.update_widget)
 
-    def add(self, updater=None, **kwargs):
+    def add(self, *args, updater=None, **kwargs):
         '''
         adds one trace to this Plot.
+
+        args: a way to provide x/y/z data without keywords
+            The last one is the dependent data, and we look at its
+            dimensionality to determine if it's `y` or `z`.
+            If it's `y`, it may optionally be preceded by `x`.
+            If it's `z`, it may optionally be preceded by `x` and `y`.
 
         updater: a callable (with no args) that updates the data in this trace
             if omitted, we will look for DataSets referenced in this data, and
@@ -67,8 +77,14 @@ class Plot(object):
         '''
         # TODO some way to specify overlaid axes?
 
-        # TODO if we don't specify x (and/or y) take them from
-        # y (or z) setpoints
+        if args:
+            if hasattr(args[-1][0], '__len__'):
+                # 2D (or higher... but ignore this for now)
+                self._args_to_kwargs(args, kwargs, 'xyz', 'pcolormesh')
+            else:
+                # 1D
+                self._args_to_kwargs(args, kwargs, 'xy', 'plot')
+
         self.traces.append(kwargs)
 
         if updater is not None:
@@ -80,6 +96,18 @@ class Plot(object):
                     self.data_updaters.add(data_array.data_set.sync)
 
         self.draw()
+
+    def _args_to_kwargs(self, args, kwargs, axletters, plot_func_name):
+        if len(args) not in (1, len(axletters)):
+            raise ValueError('{} needs either 1 or {} unnamed args'.format(
+                plot_func_name, len(axletters)))
+
+        axletters = axletters[-len(args):]
+
+        for arg, axletter in zip(args, axletters):
+            if axletter in kwargs:
+                raise ValueError(axletter + ' data provided twice')
+            kwargs[axletter] = arg
 
     def update(self):
         any_updates = False
@@ -109,11 +137,18 @@ class Plot(object):
 
     def _draw_plot(self, y, x=None, fmt=None, subplot=1, **kwargs):
         ax = self.subplots[subplot - 1]
+        if x is None and hasattr(y, 'set_arrays'):
+            x = y.set_arrays[-1]
         args = [arg for arg in [x, y, fmt] if arg is not None]
         ax.plot(*args, **kwargs)
 
     def _draw_pcolormesh(self, z, x=None, y=None, subplot=1, **kwargs):
         ax = self.subplots[subplot - 1]
+        set_arrays = getattr(z, 'set_arrays', None)
+        if x is None and set_arrays:
+            x = set_arrays[-1]
+        if y is None and set_arrays:
+            y = set_arrays[-2]
 
         with warnings.catch_warnings():
             # we get a warning about masking NaNs right at the beginning here.
