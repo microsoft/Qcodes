35561e59751697050f67f00df3ceea910b9ffef0
alexcjohnson <alex@plot.ly>
2016-01-26 00:34:52 +0100

    StreamQueue knows if it's being watched

--- qcodes/utils/multiprocessing.py
+++ qcodes/utils/multiprocessing.py
@@ -1,6 +1,7 @@
 import multiprocessing as mp
 import sys
 from datetime import datetime
+import time
 
 
 def set_mp_method(method, force=False):
@@ -48,14 +49,12 @@ class QcodesProcess(mp.Process):
     def __init__(self, *args, name='QcodesProcess', queue_streams=True,
                  daemon=True, **kwargs):
         # make sure the singleton exists prior to launching a new process
-        self.queue_streams = queue_streams
-        if queue_streams:
-            get_stream_queue()
+        self.stream_queue = queue_streams and get_stream_queue()
         super().__init__(*args, name=name, daemon=daemon, **kwargs)
 
     def run(self):
-        if self.queue_streams:
-            get_stream_queue().connect(str(self.name))
+        if self.stream_queue:
+            self.stream_queue.connect(str(self.name))
         super().run()
 
     def __repr__(self):
@@ -77,6 +76,8 @@ def get_stream_queue():
 
 class StreamQueue(object):
     '''
+    Do not instantiate this directly: use get_stream_queue so we only make one.
+
     Redirect child process stdout and stderr to a queue
 
     One StreamQueue should be created in the consumer process, and passed
@@ -94,6 +95,7 @@ class StreamQueue(object):
 
     def __init__(self, *args, **kwargs):
         self.queue = mp.Queue(*args, **kwargs)
+        self.last_read_ts = mp.Value('d', time.time())
         self._last_stream = None
         self._on_new_line = True
 
@@ -108,8 +110,7 @@ class StreamQueue(object):
     def get(self):
         out = ''
         while not self.queue.empty():
-            msg_time, stream_name, msg = self.queue.get()
-            timestr = msg_time.strftime('%H:%M:%S.%f')[:-3]
+            timestr, stream_name, msg = self.queue.get()
             line_head = '[{} {}] '.format(timestr, stream_name)
 
             if self._on_new_line:
@@ -122,17 +123,38 @@ class StreamQueue(object):
             self._on_new_line = (msg[-1] == '\n')
             self._last_stream = stream_name
 
+        self.last_read_ts.value = time.time()
         return out
 
 
 class _SQWriter(object):
     def __init__(self, stream_queue, stream_name):
         self.queue = stream_queue.queue
+        self.last_read_ts = stream_queue.last_read_ts
         self.stream_name = stream_name
 
     def write(self, msg):
-        if msg:
-            self.queue.put((datetime.now(), self.stream_name, msg))
+        try:
+            if msg:
+                msgtuple = (datetime.now().strftime('%H:%M:%S.%f')[:-3],
+                            self.stream_name, msg)
+                self.queue.put(msgtuple)
+                if time.time() - self.last_read_ts.value > 10 and msg != '\n':
+                    # long time since the queue was read? maybe nobody is
+                    # watching it at all - send messages to the terminal too
+                    # but they'll still be in the queue if someone DOES look.
+                    termstr = '[{} {}] {}'.format(*msgtuple)
+                    # we always want a new line this way (so I don't use
+                    # end='' in the print) but we don't want an extra if the
+                    # caller already included a newline.
+                    if termstr[-1] == '\n':
+                        termstr = termstr[:-1]
+                    print(termstr, file=sys.__stdout__)
+        except:
+            # don't want to get an infinite loop if there's something wrong
+            # with the queue - put the regular streams back before handling
+            sys.stdout, sys.stderr = sys.__stdout__, sys.__stderr__
+            raise
 
     def flush(self):
         pass
