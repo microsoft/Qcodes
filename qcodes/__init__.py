e8ba1a5420fe0f6c4506ed82655637a7b2c090af
alexcjohnson <alex@plot.ly>
2016-02-12 00:39:42 +0100

    don't coverage test notebook imports - we'll test these separately

--- qcodes/__init__.py
+++ qcodes/__init__.py
@@ -11,7 +11,7 @@ from qcodes.utils.helpers import in_notebook
 # code that should only be imported into the main (notebook) thread
 # in particular, importing matplotlib in the side processes takes a long
 # time and spins up other processes in order to try and get a front end
-if in_notebook():
+if in_notebook():  # pragma: no cover
     try:
         from qcodes.plots.matplotlib import MatPlot
     except ImportError:
