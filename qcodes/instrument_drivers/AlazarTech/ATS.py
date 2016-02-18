522c3af4b3082e7c33db56659f668130e1d854c9
damazdejong <damazdejong@gmail.com>
2016-02-18 18:05:41 +0100

    started work on acquire function, not completed

--- qcodes/instrument_drivers/AlazarTech/ATS.py
+++ qcodes/instrument_drivers/AlazarTech/ATS.py
@@ -1,9 +1,17 @@
 import ctypes
+import logging
+import numpy as np
+import os
 
 from qcodes.instrument.base import Instrument
 from qcodes.instrument.parameter import Parameter
 
 # TODO logging
+
+# these items are important for generalizing this code to multiple alazar cards
+# TODO remove 8 bits per sample requirement
+# TODO some alazar cards have a different number of channels :(
+
 
 class AlazarTech_ATS(Instrument):
 
@@ -93,7 +101,6 @@ class AlazarTech_ATS(Instrument):
         self.parameters['clock_edge']._set_updated()
         self.parameters['decimation']._set_updated()
 
-        # TODO some alazar cards have a different number of channels :(
         for i in [1, 2]:
             return_code = self._ATS9870_dll.AlazarInputControl(self._handle,
                                                                i,
@@ -151,6 +158,132 @@ class AlazarTech_ATS(Instrument):
 
         # TODO config AUXIO
 
+    def acquire(self, mode=None, samples_per_record=None, records_per_buffer=None, buffers_per_acquisition=None,
+                channel_selection=None, transfer_offset=None, external_startcapture=None, enable_record_headers=None,
+                alloc_buffers=None, fifo_only_streaming=None, interleave_samples=None, get_processed_data=None):
+        # region set parameters from args
+        if mode is not None:
+            self.parameters['mode']._set(mode)
+        if samples_per_record is not None:
+            self.parameters['samples_per_record']._set(samples_per_record)
+        if records_per_buffer is not None:
+            self.parameters['records_per_buffer']._set(records_per_buffer)
+        if buffers_per_acquisition is not None:
+            self.parameters['buffers_per_acquisition']._set(buffers_per_acquisition)
+        if channel_selection is not None:
+            self.parameters['channel_selection']._set(channel_selection)
+        if transfer_offset is not None:
+            self.parameters['transfer_offset']._set(transfer_offset)
+        if external_startcapture is not None:
+            self.parameters['external_starcapture']._set(external_startcapture)
+        if enable_record_headers is not None:
+            self.parameters['enable_record_headers']._set(enable_record_headers)
+        if alloc_buffers is not None:
+            self.parameters['alloc_buffers']._set(alloc_buffers)
+        if fifo_only_streaming is not None:
+            self.parameters['fifo_only_streaming']._set(fifo_only_streaming)
+        if interleave_samples is not None:
+            self.parameters['interleave_samples']._set(interleave_samples)
+        if get_processed_data is not None:
+            self.parameters['get_processed_data']._set(get_processed_data)
+
+        # endregion
+
+        if not (self.parameters['mode'].get() == 'TS' or self.parameters['mode'].get() == 'NPT'):
+            raise Exception("Only the 'TS' and 'NPT' modes are implemented at this point")
+
+        return_code = self._ATS9870_dll.AlazarAbortAsyncRead(self._handle)
+        self._result_handler(error_code=return_code, error_source="AlazarAbortAsyncRead")
+
+        # get channel info
+        bps = np.array([0], dtype=np.uint8)  # bps bits per sample
+        max_s = np.array([0], dtype=np.uint32)  # max_s memory size in samples
+        return_code = self._ATS9870_dll.AlazarGetChannelInfo(self._handle, max_s.ctypes.data, bps.ctypes.data)
+        self._result_handler(error_code=return_code, error_source="AlazarGetChannelInfo")
+        bps = bps[0]
+        max_s = max_s[0]
+        if not bps == 8:
+            raise Exception("Only 8 bits per sample supported at this moment")
+
+        if self.parameters['mode'].get() == 'NPT':
+            pretriggersize = 0  # pretriggersize is 0 for NPT always
+            post_trigger_size = self.parameters['samples_per_record']._get_byte()
+            return_code = self._ATS9870_dll.AlazarSetRecordSize(self._handle, pretriggersize, post_trigger_size)
+            self._result_handler(error_code=return_code, error_source="AlazarSetRecordSize")
+
+        if self.parameters['channel_selection']._get_byte() == 3:
+            number_of_channels = 2
+        else:
+            number_of_channels = 1
+        samples_per_buffer = 0
+        acquire_flags = self.parameters['mode']._get_byte() | \
+                        self.parameters['external_startcapture']._get_byte() | \
+                        self.parameters['enable_record_headers']._get_byte() | \
+                        self.parameters['alloc_buffers']._get_byte() | \
+                        self.parameters['fifo_only_streaming']._get_byte() | \
+                        self.parameters['interleave_samples']._get_byte() | \
+                        self.parameters['get_processed_data']._get_byte()
+        if self.parameters['mode'].get() == 'NPT':
+            samples_per_record = self.parameters['samples_per_record']._get_byte()
+            records_per_buffer = self.parameters['records_per_buffer']._get_byte()
+            records_per_acquisition = records_per_buffer * self.parameters['buffers_per_acquisition']._get_byte()
+            samples_per_buffer = samples_per_record * records_per_buffer
+            return_code = self._ATS9870_dll.AlazarBeforeAsyncRead(self._handle,
+                                                                  self.parameters['channel_selection']._get_byte(),
+                                                                  self.parameters['transfer_offset']._get_byte(),
+                                                                  samples_per_record,
+                                                                  records_per_buffer,
+                                                                  records_per_acquisition,
+                                                                  acquire_flags)
+            self._result_handler(error_code=return_code, error_source="AlazarBeforeAsyncRead")
+        elif self.parameters['mode'].get() == 'TS':
+            if not self.parameters['samples_per_record']._get_byte() % self.parameters['buffers_per_acquisition'] == 0:
+                logging.warning("buffers_per_acquisition is not a divisor of samples per record which it should be in"
+                                " TS mode, rounding down in samples per buffer calculation")
+            samples_per_buffer = int(self.parameters['samples_per_record']._get_byte() / self.parameters['buffers_per_acquisition'])
+            buffers_per_acquisition = self.parameters['buffers_per_acquisition']._get_byte()
+            if not self.parameters['records_per_buffer']._get_byte() == 1:
+                logging.warning('records_per_buffer should be 1 in TS mode, defauling to 1')
+                self.parameters['records_per_buffer']._set(1)
+            records_per_buffer = self.parameters['records_per_buffer']._get_byte()
+            return_code = self._ATS9870_dll.AlazarBeforeAsyncRead(self._handle,
+                                                                  self.parameters['channel_selection']._get_byte(),
+                                                                  self.parameters['transfer_offset']._get_byte(),
+                                                                  samples_per_buffer,
+                                                                  records_per_buffer,
+                                                                  buffers_per_acquisition,
+                                                                  acquire_flags)
+            self._result_handler(error_code=return_code, error_source="AlazarBeforeAsyncRead")
+        self.parameters['samples_per_record']._set_updated()
+        self.parameters['records_per_buffer']._set_updated()
+        self.parameters['buffers_per_acquisition']._set_updated()
+        self.parameters['channel_selection']._set_updated()
+        self.parameters['transfer_offset']._set_updated()
+        self.parameters['mode']._set_updated()
+        self.parameters['external_starcapture']._set_updated()
+        self.parameters['enable_record_headers']._set_updated()
+        self.parameters['alloc_buffers']._set_updated()
+        self.parameters['fifo_only_streaming']._set_updated()
+        self.parameters['interleave_samples']._set_updated()
+        self.parameters['get_processed_data']._set_updated()
+
+        for buf in self.buflist:
+            return_code = self._ATS9870_dll.AlazarPostAsyncBuffer(self._handle, buf.addr, buf.size_bytes)
+            self._result_handler(error_code=return_code, error_source="AlazarPostAsyncBuffer")
+
+        return_code = self._ATS9870_dll.AlazarStartCapture(self._handle)
+        self._result_handler(error_code=return_code, error_source="AlazarStartCapture")
+
+        while BuffersCompleted < BuffersPerAcquisition:
+            return_code = self._ATS9870_dll.AlazarWaitAsyncBufferComplete(self._handle, buf.addr, self.waittimeout)
+            self._result_handler(error_code=return_code, error_source="AlazarWaitAsyncBufferComplete")
+
+            return_code = self._ATS9870_dll.AlazarPostAsyncBuffer(self._handle, buf.addr, buf.size_bytes)
+            self._result_handler(error_code=return_code, error_source="AlazarPostAsyncBuffer")
+
+        return_code = self._ATS9870_dll.AlazarAbortAsyncRead(self._handle)
+        self._result_handler(error_code=return_code, error_source="AlazarAbortAsyncRead")
+
     def _result_handler(self, error_code=0, error_source=""):
         # region error codes
         error_codes = {513: 'ApiFailed', 514: 'ApiAccessDenied', 515: 'ApiDmaChannelUnavailable',
@@ -202,8 +335,11 @@ class AlazarTech_ATS(Instrument):
                 raise KeyError(error_source+" raised unknown error "+str(error_code))
             raise Exception(error_source+" raised "+str(error_code)+": "+error_codes[error_code])
 
+
 class AlazarParameter(Parameter):
     def __init__(self, name=None, label=None, unit=None, value=None, byte_to_value_dict=None):
+        # TODO implement trivial dictionary
+        # TODO implement restrictions for trivial dictionary case
         super().__init__(name=name, label=label, unit=unit)
         self._byte = None
         self._uptodate_flag = True
@@ -250,3 +386,45 @@ class AlazarParameter(Parameter):
     def _set_updated(self):
         self._uptodate_flag = True
 
+
+class Buffer:
+    def __init__(self, bits_per_sample, samples_per_buffer, number_of_channels):
+        if not bits_per_sample == 8:
+            raise Exception("Buffer: only 8 bit per sample supported")
+        if not os.name == 'nt':
+            raise Exception("Buffer: only Windows supported at this moment")
+        self._allocated = True
+
+        # try to allocate memory
+        mem_commit = 0x1000
+        page_readwrite = 0x4
+
+        size_bytes = samples_per_buffer * number_of_channels
+
+        # please see https://msdn.microsoft.com/en-us/library/windows/desktop/aa366887(v=vs.85).aspx for documentation
+        ctypes.windll.kernel32.VirtualAlloc.argtypes = [ctypes.c_void_p, ctypes.c_long, ctypes.c_long, ctypes.c_long]
+        ctypes.windll.kernel32.VirtualAlloc.restype = ctypes.c_void_p
+        self.addr = ctypes.windll.kernel32.VirtualAlloc(0, ctypes.c_long(size_bytes), mem_commit, page_readwrite)
+        if self.addr is None:
+            self._allocated = False
+            e = ctypes.windll.kernel32.GetLastError()
+            raise Exception("Memory allocation error: " + str(e))
+
+        ctypes_array = (ctypes.c_uint8 * size_bytes).from_address(self.addr)
+        self.buffer = np.frombuffer(ctypes_array, dtype=np.uint8)
+        pointer, read_only_flag = self.buffer.__array_interface__['data']
+
+    def free_mem(self):
+        mem_release = 0x8000
+
+        # see https://msdn.microsoft.com/en-us/library/windows/desktop/aa366892(v=vs.85).aspx
+        ctypes.windll.kernel32.VirtualFree.argtypes = [ctypes.c_void_p, ctypes.c_long, ctypes.c_long]
+        ctypes.windll.kernel32.VirtualFree.restype = ctypes.c_int
+        ctypes.windll.kernel32.VirtualFree(ctypes.c_void_p(self.addr), 0, mem_release)
+        self._allocated = False
+
+    def __del__(self):
+        if self._allocated:
+            self.free_mem()
+            logging.warning("Buffer prevented memory leak; Memory released to Windows.\n"
+                            "Memory should have been released before buffer was deleted.")
