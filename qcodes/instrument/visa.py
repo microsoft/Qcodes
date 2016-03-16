0d762d1ad77dc899e31c6f5c72faf8c94bef3620
alexcjohnson <alex@plot.ly>
2016-03-16 23:24:14 +0100

    visa docstring

--- qcodes/instrument/visa.py
+++ qcodes/instrument/visa.py
@@ -5,7 +5,24 @@ from .server import ask_server, write_server
 
 
 class VisaInstrument(Instrument):
-    def __init__(self, name, address=None, server_name=None,
+    '''
+    Base class for all instruments using visa connections
+
+    name: what this instrument is called locally
+    address: the visa resource name. see eg:
+        http://pyvisa.readthedocs.org/en/stable/names.html
+        (can be changed later with set_address)
+    server_name: name of the InstrumentServer to use. By default
+        uses 'VisaServer', ie all visa instruments go on the same
+        server, but you can provide any other string, or None to
+        not use a server (not recommended, then it cannot be used
+        with subprocesses like background Loop's)
+    timeout: seconds to allow for responses (default 5)
+        (can be changed later with set_timeout)
+    terminator: the read termination character(s) to expect
+        (can be changed later with set_terminator)
+    '''
+    def __init__(self, name, address=None, server_name='',
                  timeout=5, terminator='', **kwargs):
         # only set the io routines if a subclass doesn't override EITHER
         # the sync or the async version, so we preserve the ability of
@@ -18,21 +35,23 @@ class VisaInstrument(Instrument):
                 self.ask_async.__func__ is Instrument.ask_async):
             self.ask = self._default_ask
 
-        if server_name is None:
-            server_name = self._default_server_name()
-        super().__init__(name, **kwargs)
+        self._address = address
+        self._timeout = timeout
+        self._terminator = terminator
 
-        self.connect(self, address, timeout, terminator)
+        if server_name == '':
+            server_name = self._default_server_name()
+        super().__init__(name, server_name=server_name, **kwargs)
 
     def _default_server_name(self):
         # TODO - figure out if we want multiple servers by default
         return 'VisaServer'
 
     @ask_server
-    def connect(self, address, timeout, terminator):
-        self.set_address(address)
-        self.set_timeout(timeout)
-        self.set_terminator(terminator)
+    def on_connect(self, address, timeout, terminator):
+        self.set_address(self._address)
+        self.set_timeout(self._timeout)
+        self.set_terminator(self._terminator)
 
     @write_server
     def set_address(self, address):
