2fd7bad595f0ff86bbfa326c13fdd881a758d6ff
alexcjohnson <alex@plot.ly>
2016-03-18 14:00:40 +0100

    more complete ip instrument driver

--- qcodes/instrument/ip.py
+++ qcodes/instrument/ip.py
@@ -1,38 +1,133 @@
 import socket
-import asyncio
 
 from .base import Instrument
+from .server import ask_server, write_server
 
 
 class IPInstrument(Instrument):
-    def __init__(self, name, address=None, port=None,
-                 timeout=5, terminator='\n', **kwargs):
+    '''
+    Bare socket ethernet instrument implementation
+
+    name: what this instrument is called locally
+    address: the IP address or domain name, as a string
+    port: the IP port, as an integer
+        (address and port can be set later with set_address)
+    timeout: seconds to allow for responses (default 5)
+        (can be set later with set_timeout)
+    terminator: character(s) to terminate each send with (default '\n')
+        (can be set later with set_terminator)
+    persistent: do we leave the socket open between calls? (default True)
+    write_confirmation: does the instrument acknowledge writes with some
+        response we can read? (default True)
+    '''
+    def __init__(self, name, address=None, port=None, timeout=5,
+                 terminator='\n', persistent=True, write_confirmation=True,
+                 **kwargs):
+        # only set the io routines if a subclass doesn't override EITHER
+        # the sync or the async version, so we preserve the ability of
+        # the base Instrument class to convert between sync and async
+        if not self._has_action('write'):
+            self._write_fn = self._default_write
+
+        if not self._has_action('ask'):
+            self._ask_fn = self._default_ask
+
+        self._address = address
+        self._port = port
+        self._timeout = timeout
+        self._terminator = terminator
+        self._persistent = persistent
+        self._confirmation = write_confirmation
+
+        self._socket = None
+
         super().__init__(name, **kwargs)
 
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((address, port))
+    @ask_server
+    def on_connect(self):
+        if self._persistent:
+            self._connect()
 
-        self.set_timeout(timeout)
-        self.terminator = terminator
+    @ask_server
+    def set_address(self, address=None, port=None):
+        if address is not None:
+            self._address = address
+        elif not hasattr(self, '_address'):
+            raise TypeError('This instrument doesn\'t have an address yet, '
+                            'you must provide one.')
+        if port is not None:
+            self._port = port
+        elif not hasattr(self, '_port'):
+            raise TypeError('This instrument doesn\'t have a port yet, '
+                            'you must provide one.')
 
-    def set_timeout(self, timeout):
-        self.socket.settimeout(float(timeout))
-        self._timeout = timeout
+        if self._persistent:
+            self._connect()
 
-    @asyncio.coroutine
-    def write_async(self, cmd):
-        data = cmd + self.terminator
-        self.socket.send(data.encode())
+    @ask_server
+    def _connect(self):
+        if self._socket is not None:
+            self._disconnect()
 
-    @asyncio.coroutine
-    def ask_async(self, cmd):
-        data = cmd + self.terminator
-        self.socket.send(data.encode())
-        # TODO: async? what's the 512?
-        # TODO: and why did athena *always* have a recv, even if there's
-        # no return?
-        # if this is an instrument-specific thing (like some instruments always
-        # reply "OK" to set commands) then these can override write_async
-        # to ask_async with response validation
-        # TODO: does this need a lock too? probably...
-        return self.socket.recv(512).decode()
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect((self._address, self._port))
+        self.set_timeout(self._timeout)
+
+    @ask_server
+    def _disconnect(self):
+        if getattr(self, '_socket', None) is None:
+            return
+
+        self._socket.shutdown(socket.SHUT_RDWR)
+        self._socket.close()
+        self._socket = None
+
+    @write_server
+    def set_timeout(self, timeout=None):
+        if timeout is not None:
+            self._timeout = timeout
+
+        if self._socket is not None:
+            self.socket.settimeout(float(self.timeout))
+
+    @write_server
+    def set_terminator(self, terminator):
+        self._terminator = terminator
+
+    @write_server
+    def _send(self, cmd):
+        data = cmd + self._terminator
+        self._socket.send(data.encode())
+
+    @ask_server
+    def _recv(self):
+        return self._socket.recv(512).decode()
+
+    def __del__(self):
+        self._disconnect()
+
+    @write_server
+    def _default_write(self, cmd):
+        with EnsureConnection(self):
+            self._send(cmd)
+            if self._confirmation:
+                self._recv()
+
+    @ask_server
+    def _default_ask(self, cmd):
+        with EnsureConnection(self):
+            self._send(cmd)
+            return self._recv()
+
+
+class EnsureConnection:
+    def __init__(self, instrument):
+        self._instrument = instrument
+
+    def __enter__(self):
+        if not self.instrument._persistent or self.instrument._socket is None:
+            self.instrument._connect()
+
+    def __exit__(self):
+        if not self.instrument._persistent:
+            self.instrument._disconnect()
