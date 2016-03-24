ae1d458ae4f9c126bb7b63d1389429b48c4b7710
damazdejong <damazdejong@gmail.com>
2016-03-24 22:45:16 +0100

    added preliminary list of parameters to add

--- qcodes/instrument_drivers/AlazarTech/ATS9870.py
+++ qcodes/instrument_drivers/AlazarTech/ATS9870.py
@@ -4,8 +4,44 @@ from .ATS import AlazarTech_ATS
 class AlazarTech_ATS9870(AlazarTech_ATS):
     def __init__(self, name):
         super().__init__(name)
-        # TODO check board kind
+        # TODO (M) check board kind
         # add parameters
-        # TODO make parameter for board type
+        # TODO (M) make parameter for board type
+
+# -----config-----
+# clock source
+#         sample_rate
+#         clock_edge
+#         decimation
+#
+# coupling{n}
+# range{n}
+# impedence{n}
+# bwlimit{n}
+#
+# trigger_operation
+# trigger_engine1
+# trigger_source1
+# trigger_slope1
+# trigger_level1
+# trigger_engine2
+# trigger_source2
+# trigger_slope2
+# trigger_level2
+#
+# external_trigger_coupling
+# trigger_range
+#
+# trigger_delay
+# timeout_ticks
+#
+# ----acquire-----
+# mode
+# samples_per_record
+# channel_selection
+#
+# more !?
+#
+# nbuffers
 
 
