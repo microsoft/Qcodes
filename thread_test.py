d7d66391da294b0db4c00d59765acb962bcfec23
alexcjohnson <alex@plot.ly>
2016-04-06 13:24:36 +0200

    thread test part 2

--- thread_test.py
+++ thread_test.py
@@ -2,18 +2,21 @@ import threading
 import time
 
 
-def sleeper(t, n, out):
+def sleeper(t, n, out, make_error):
     time.sleep(t)
     out[n] = n
+    if make_error:
+        raise RuntimeError('hello from # {}!'.format(n))
 
 
-def runmany(n, t):
+def runmany(n, t, error_nums):
     out = [None] * n
 
     t0 = time.perf_counter()
 
-    threads = [threading.Thread(target=sleeper, args=(t, i, out))
-               for i in range(n)]
+    threads = [
+        CatchingThread(target=sleeper, args=(t, i, out, i in error_nums))
+        for i in range(n)]
 
     # start threads backward
     [t.start() for t in reversed(threads)]
@@ -29,7 +32,24 @@ def runmany(n, t):
     if not out_ok:
         out_ok += ['all output correct']
 
-    print('{} parallel threads sleeping'.format(n) +
+    print('{} parallel threads sleeping\n'.format(n) +
           'given time: {}\n'.format(t) +
           'resulting time: {}\n'.format(t1 - t0) +
           '\n'.join(out_ok))
+
+
+class CatchingThread(threading.Thread):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.exception = None
+
+    def run(self):
+        try:
+            super().run()
+        except Exception as e:
+            self.exception = e
+
+    def join(self):
+        super().join()
+        if self.exception:
+            raise self.exception
