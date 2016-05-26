104382d0417bde3a9cf4e055bbaba16cd927ce45
Merlin <merlin@nbi.dk>
2016-05-26 21:16:21 +0200

    fix: more test

--- qcodes/tests/test_json.py
+++ qcodes/tests/test_json.py
@@ -1,19 +1,19 @@
 from unittest import TestCase
 import numpy as np
 import json
-from copy import deepcopy
 
 from qcodes.utils.helpers import json_encoder
 
 
 class TestNumpyJson(TestCase):
 
-    metadata = {'name': 'Rapunzel', 'age': 12, 'height': 112.234,
-                'scores': np.linspace(0,42)}
+    metadata = {'name': 'Rapunzel', 'age': np.int64(12), 'height': np.float64(112.234),
+                'scores': np.linspace(0,42,num=3)}
 
     def test_numpy(self):
-        metadata = deepcopy(self.metadata)
-        metadata['scores'] = list(range(10))
+
+        metadata = {'name': 'Rapunzel', 'age': 12, 'height': 112.234,
+                    'scores': list(range(10))}
         data = json.dumps(metadata, sort_keys=True, indent=4,
                           ensure_ascii=False)
         data_dict = json.loads(data)
@@ -24,12 +24,13 @@ class TestNumpyJson(TestCase):
         with self.assertRaises(TypeError):
             json.dumps(metadata, sort_keys=True, indent=4, ensure_ascii=False)
 
-
     def test_numpy_good(self):
         metadata = self.metadata
         data = json.dumps(metadata, sort_keys=True, indent=4,
                           ensure_ascii=False, cls=json_encoder)
         data_dict = json.loads(data)
 
-        # fails
-        # self.assertEqual(metadata, data_dict)
+        metadata = {'name': 'Rapunzel', 'age': 12, 'height': 112.234,
+                    'scores': [0, 21, 42]}
+
+        self.assertEqual(metadata, data_dict)
