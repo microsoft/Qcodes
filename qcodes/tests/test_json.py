456066e3e2c9d18b622b1aaf1a613fa0b14fecc4
alexcjohnson <alex@plot.ly>
2016-05-26 21:59:57 +0200

    small tweaks to numpy json encoder tests

--- qcodes/tests/test_json.py
+++ qcodes/tests/test_json.py
@@ -7,17 +7,18 @@ from qcodes.utils.helpers import json_encoder
 
 class TestNumpyJson(TestCase):
 
-    metadata = {'name': 'Rapunzel', 'age': np.int64(12), 'height': np.float64(112.234),
-                'scores': np.linspace(0,42,num=3)}
-
-    def test_numpy(self):
-
-        metadata = {'name': 'Rapunzel', 'age': 12, 'height': 112.234,
-                    'scores': list(range(10))}
-        data = json.dumps(metadata, sort_keys=True, indent=4,
-                          ensure_ascii=False)
-        data_dict = json.loads(data)
-        self.assertEqual(metadata, data_dict)
+    def setUp(self):
+        self.metadata = {
+            'name': 'Rapunzel',
+            'age': np.int64(12),
+            'height': np.float64(112.234),
+            'scores': np.linspace(0, 42, num=3),
+            # include some regular values to ensure they work right
+            # with our encoder
+            'weight': 19,
+            'length': 45.23,
+            'points': [12, 24, 48]
+        }
 
     def test_numpy_fail(self):
         metadata = self.metadata
@@ -30,7 +31,14 @@ class TestNumpyJson(TestCase):
                           ensure_ascii=False, cls=json_encoder)
         data_dict = json.loads(data)
 
-        metadata = {'name': 'Rapunzel', 'age': 12, 'height': 112.234,
-                    'scores': [0, 21, 42]}
+        metadata = {
+            'name': 'Rapunzel',
+            'age': 12,
+            'height': 112.234,
+            'scores': [0, 21, 42],
+            'weight': 19,
+            'length': 45.23,
+            'points': [12, 24, 48]
+        }
 
         self.assertEqual(metadata, data_dict)
