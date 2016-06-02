a6abd2e65485d5e27b2793ef5e04f666685b0a6b
euchas <adomas.valantinas@gmail.com>
2016-06-02 16:53:18 +0200

    driver: Fix style and parameter errors

--- qcodes/instrument_drivers/agilent/HP33210A.py
+++ qcodes/instrument_drivers/agilent/HP33210A.py
@@ -4,12 +4,8 @@ from qcodes import VisaInstrument, validators as vals
 class Agilent_HP33210A(VisaInstrument):
     """
     This is the code for Agilent HP33210A function/arbitrary waveform generator
-
-
-
-
-
-
+    Status: beta version
+    Includes the essential commands from the manual
     """
     def __init__(self, name, address, reset=False, **kwargs):
         super().__init__(name, address, terminator='\n', **kwargs)
@@ -19,26 +15,25 @@ class Agilent_HP33210A(VisaInstrument):
                            label='Frequency',
                            units='Hz',
                            get_cmd='FREQ?',
-                           set_cmd='FREQ {:.3f} Hz',
+                           set_cmd='FREQ {:.3f}',
                            get_parser=float,
                            vals=vals.Numbers(10e-3, 10e6))
         self.add_parameter(name='amplitude',
                            label='Amplitude',
                            units='V',
                            get_cmd='VOLT?',
-                           set_cmd='VOLT {:.3f} V',
+                           set_cmd='VOLT {:.3f}',
                            get_parser=float,
                            vals=vals.Numbers(10e-3, 10))
         self.add_parameter(name='offset',
                            label='Offset',
                            units='V',
                            get_cmd='VOLT:OFFS?',
-                           set_cmd='VOLT:OFFS {:.2f} V',
+                           set_cmd='VOLT:OFFS {:.2f}',
                            get_parser=float,
                            vals=vals.Numbers(-4.99, 4.99))
-    # burst phase
         self.add_parameter(name='phase',
-                           label='Phase',
+                           label='Burst phase',
                            units='deg',
                            get_cmd='BURS:PHAS?',
                            set_cmd='BURS:PHAS {:.2f}',
@@ -64,28 +59,29 @@ class Agilent_HP33210A(VisaInstrument):
                            vals=vals.Enum('SIN', 'SQU', 'RAMP',
                                           'PULS', 'NOIS', 'DC',
                                           'USER'))
-        self.add_parameter(name='output_connector',
+        self.add_parameter(name='status',
                            get_cmd='OUTP?',
                            set_cmd='OUTP {}',
                            val_mapping={'OFF': '0', 'ON': '1'})
-    # load impedence of the output
         self.add_parameter(name='load',
-                           label='Load',
+                           label='Output load impedance',
                            units='ohms',
                            get_cmd='OUTP:LOAD?',
                            set_cmd='OUTP:LOAD {:.1f}',
                            get_parser=float,
                            vals=vals.Numbers(1, 10e3))
-    # invert the waveform relative to the offset voltage
         self.add_parameter(name='polarity',
                            get_cmd='OUTP:POL?',
                            set_cmd='OUTP:POL {}',
-                           vals=vals.Enum('NORM', 'INV'))
-    # disable/enable front-panel Sync connector
-        self.add_parameter(name='sync_connector',
+                           vals=vals.Enum('NORM', 'INV'),
+                           docstring=('invert the waveform'
+                                      'relative to the offset voltage'))
+        self.add_parameter(name='sync_status',
                            get_cmd='OUTP:SYNC?',
                            set_cmd='OUTP:SYNC {}',
-                           val_mapping={'OFF': '0', 'ON': '1'})
+                           val_mapping={'OFF': '0', 'ON': '1'},
+                           docstring=('to disable/enable'
+                                      'font-panel sync connector'))
         self.add_parameter(name='amplitude_units',
                            get_cmd='VOLT:UNIT?',
                            set_cmd='VOLT:UNIT {}',
@@ -93,15 +89,17 @@ class Agilent_HP33210A(VisaInstrument):
         self.add_parameter(name='volt_range',
                            get_cmd='VOLT:RANG:AUTO?',
                            set_cmd='VOLT:RANG:AUTO {}',
-                           val_mapping={'OFF': '0', 'ON': '1'})
+                           val_mapping={'OFF': '0', 'ON': '1'},
+                           docstring=('does not change'
+                                      'volt_high and volt_low'))
         self.add_parameter(name='volt_high',
                            get_cmd='VOLT:HIGH?',
                            set_cmd='VOLT:HIGH {:.3f}',
                            get_parser=float,
-                           vals=vals.Numbers(10e-3, 10))
+                           vals=vals.Numbers(-5, 5))
         self.add_parameter(name='volt_low',
                            get_cmd='VOLT:LOW?',
                            set_cmd='VOLT:LOW {:.3f}',
                            get_parser=float,
-                           vals=vals.Numbers(10e-3, 10))
+                           vals=vals.Numbers(-5, 5))
         self.connect_message('IDN')
