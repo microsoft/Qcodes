1917dba9d52d0537e4d0ed63c32c79f98253a070
Merlin <merlin@nbi.dk>
2016-06-18 16:34:20 +0200

    fix: include automatic validator setting refactor: made those conversion tables (constant) class attributes

--- qcodes/instrument_drivers/stanford_research_systems/SR830.py
+++ qcodes/instrument_drivers/stanford_research_systems/SR830.py
@@ -6,12 +6,47 @@ class Stanford_Research_Systems_SR830(VisaInstrument):
     This is the qcodes driver for the Stanford Research Systems SR830
     Lock-in Amplifier
     """
+
+    _VOLT_TO_N = {2e-9:    0, 5e-9:    1, 10e-9:  2,
+                  20e-9:   3, 50e-9:   4, 100e-9: 5,
+                  200e-9:  6, 500e-9:  7, 1e-6:   8,
+                  2e-6:    9, 5e-6:   10, 10e-6:  11,
+                  20e-6:  12, 50e-6:  13, 100e-6: 14,
+                  200e-6: 15, 500e-6: 16, 1e-3:   17,
+                  2e-3:   18, 5e-3:   19, 10e-3:  20,
+                  20e-3:  21, 50e-3:  22, 100e-3: 23,
+                  200e-3: 24, 500e-3: 25, 1:      26}
+    _N_TO_VOLT = {v: k for k, v in _VOLT_TO_N.items()}
+
+    _CURR_TO_N = {2e-15:    0, 5e-15:    1, 10e-15:  2,
+                  20e-15:   3, 50e-15:   4, 100e-15: 5,
+                  200e-15:  6, 500e-15:  7, 1e-12:   8,
+                  2e-12:    9, 5e-12:   10, 10e-12:  11,
+                  20e-12:  12, 50e-12:  13, 100e-12: 14,
+                  200e-12: 15, 500e-12: 16, 1e-9:    17,
+                  2e-9:    18, 5e-9:    19, 10e-9:   20,
+                  20e-9:   21, 50e-9:   22, 100e-9:  23,
+                  200e-9:  24, 500e-9:  25, 1e-6:    26}
+    _N_TO_CURR = {v: k for k, v in _CURR_TO_N.items()}
+
+    _VOLT_ENUM = Enum(*_VOLT_TO_N.keys())
+    _CURR_ENUM = Enum(*_CURR_TO_N.keys())
+
+    _INPUT_CONFIG_TO_N = {
+        'a': 0,
+        'a-b': 1,
+        '1 mohm': 2,
+        '100 mohm': 3,
+    }
+
+    _N_TO_INPUT_CONFIG = {v: k for k, v in _INPUT_CONFIG_TO_N.items()}
+
     def __init__(self, name, address, **kwargs):
         super().__init__(name, address, **kwargs)
 
         # Reference and phase
         self.add_parameter('phase',
-                            label='Phase',
+                           label='Phase',
                            get_cmd='PHAS?',
                            get_parser=float,
                            set_cmd='PHAS {:.2f}',
@@ -61,21 +96,13 @@ class Stanford_Research_Systems_SR830(VisaInstrument):
                            vals=Numbers(min_value=0.004, max_value=5.000))
 
         # Input and filter
-        self._input_config_to_n = {
-            'a': 0,
-            'a-b': 1,
-            '1 mohm': 2,
-            '100 mohm': 3,
-        }
-        self._n_to_input_config = {v: k for k, v in self._input_config_to_n.items()}
-
         self.add_parameter('input_config',
                            label='Input configuration',
                            get_cmd='ISRC?',
                            get_parser=self._get_input_config,
                            set_cmd='ISRC {}',
                            set_parser=self._set_input_config,
-                           vals=Enum(*self._input_config_to_n.keys()))
+                           vals=Enum(*self._INPUT_CONFIG_TO_N.keys()))
 
         self.add_parameter('input_shield',
                            label='Input shield',
@@ -106,37 +133,14 @@ class Stanford_Research_Systems_SR830(VisaInstrument):
                                'both': 3,
                            })
 
-        self._volt_to_n = {2e-9:    0, 5e-9:    1, 10e-9:  2, 
-                           20e-9:   3, 50e-9:   4, 100e-9: 5, 
-                           200e-9:  6, 500e-9:  7, 1e-6:   8,
-                           2e-6:    9, 5e-6:   10, 10e-6:  11,
-                           20e-6:  12, 50e-6:  13, 100e-6: 14,
-                           200e-6: 15, 500e-6: 16, 1e-3:   17,
-                           2e-3:   18, 5e-3:   19, 10e-3:  20,
-                           20e-3:  21, 50e-3:  22, 100e-3: 23,
-                           200e-3: 24, 500e-3: 25, 1:      26}
-        self._n_to_volt = {v: k for k, v in self._volt_to_n.items()}
-
-        self._curr_to_n = {2e-15:    0, 5e-15:    1, 10e-15:  2, 
-                           20e-15:   3, 50e-15:   4, 100e-15: 5, 
-                           200e-15:  6, 500e-15:  7, 1e-12:   8,
-                           2e-12:    9, 5e-12:   10, 10e-12:  11,
-                           20e-12:  12, 50e-12:  13, 100e-12: 14,
-                           200e-12: 15, 500e-12: 16, 1e-9:    17,
-                           2e-9:    18, 5e-9:    19, 10e-9:   20,
-                           20e-9:   21, 50e-9:   22, 100e-9:  23,
-                           200e-9:  24, 500e-9:  25, 1e-6:    26}
-        self._n_to_curr = {v: k for k, v in self._curr_to_n.items()}
-
         # Gain and time constant
         self.add_parameter(name='sensitivity',
                            label='Sensitivity',
                            get_cmd='SENS?',
                            set_cmd='SENS {:d}',
                            get_parser=self._get_sensitivity,
-                           set_parser=self._set_sensitivity,
-                           vals=MultiType(Enum(*self._volt_to_n.keys()),
-                                          Enum(*self._curr_to_n.keys())))
+                           set_parser=self._set_sensitivity
+                           )
 
         self.add_parameter('reserve',
                            label='Reserve',
@@ -189,7 +193,7 @@ class Stanford_Research_Systems_SR830(VisaInstrument):
 
         def parse_offset_get(s):
             parts = s.split(',')
-            
+
             return float(parts[0]), int(parts[1])
 
         # TODO: Parameters that can be set with multiple arguments
@@ -238,24 +242,24 @@ class Stanford_Research_Systems_SR830(VisaInstrument):
         self.add_function('auto_offset', call_cmd='AOFF {0}', args=[Enum(1, 2, 3, 4)])
 
         # Data transfer
-        self.add_parameter('X', 
-                           get_cmd='OUTP? 1', 
-                           get_parser=float, 
+        self.add_parameter('X',
+                           get_cmd='OUTP? 1',
+                           get_parser=float,
                            units='V')
 
-        self.add_parameter('Y', 
-                           get_cmd='OUTP? 2', 
-                           get_parser=float, 
+        self.add_parameter('Y',
+                           get_cmd='OUTP? 2',
+                           get_parser=float,
                            units='V')
 
-        self.add_parameter('R', 
-                           get_cmd='OUTP? 3', 
-                           get_parser=float, 
+        self.add_parameter('R',
+                           get_cmd='OUTP? 3',
+                           get_parser=float,
                            units='V')
 
-        self.add_parameter('P', 
-                           get_cmd='OUTP? 4', 
-                           get_parser=float, 
+        self.add_parameter('P',
+                           get_cmd='OUTP? 4',
+                           get_parser=float,
                            units='deg')
 
         # Interface
@@ -265,33 +269,46 @@ class Stanford_Research_Systems_SR830(VisaInstrument):
         self.add_function('enable_front_panel', call_cmd='OVRM 1')
 
         # Initialize the proper units of the outputs and sensitivities
-        self.input_config(self.input_config())
+        self.input_config()
 
         self.connect_message()
 
     def _set_units(self, units):
+        # TODO:
+        # make a public parameter function that allows to change the units
         for param in [self.X, self.Y, self.R, self.sensitivity]:
             param.units = units
 
     def _get_input_config(self, s):
-        return self._n_to_input_config[int(s)]
+        mode = self._N_TO_INPUT_CONFIG[int(s)]
+
+        if mode in ['a', 'a-b']:
+            self.sensitivity.set_validator(self._VOLT_ENUM)
+            self._set_units('V')
+        else:
+            self.sensitivity.set_validator(self._CURR_ENUM)
+            self._set_units('A')
+
+        return mode
 
     def _set_input_config(self, s):
         if s in ['a', 'a-b']:
+            self.sensitivity.set_validator(self._VOLT_ENUM)
             self._set_units('V')
         else:
+            self.sensitivity.set_validator(self._CURR_ENUM)
             self._set_units('A')
 
-        return self._input_config_to_n[s]
+        return self._INPUT_CONFIG_TO_N[s]
 
     def _get_sensitivity(self, s):
         if self.input_config() in ['a', 'a-b']:
-            return self._n_to_volt[int(s)]
+            return self._N_TO_VOLT[int(s)]
         else:
-            return self._n_to_curr[int(s)]
+            return self._N_TO_CURR[int(s)]
 
     def _set_sensitivity(self, s):
         if self.input_config() in ['a', 'a-b']:
-            return self._volt_to_n[s]
+            return self._VOLT_TO_N[s]
         else:
-            return self._curr_to_n[s]
+            return self._CURR_TO_N[s]
