999bd683ea014af8037852025efd3939089361c1
Natalie Pearson <natalie@connectedhomes.co.uk>
2016-06-20 12:12:24 +0200

    feature: update RS_ZNB20 driver and create example for testing

--- qcodes/instrument_drivers/rohde_schwarz/ZNB20.py
+++ qcodes/instrument_drivers/rohde_schwarz/ZNB20.py
@@ -35,7 +35,7 @@ class RohdeSchwarz_ZNB20(VisaInstrument):
                            label='Averages',
                            units='',
                            get_cmd='AVER:COUN?',
-                           set_cmd='AVER:COUN? {:d}',
+                           set_cmd='AVER:COUN {:d}',
                            get_parser=int,
                            vals=vals.Numbers(1,5000))
 
@@ -55,37 +55,59 @@ class RohdeSchwarz_ZNB20(VisaInstrument):
                            get_parser=int,
                            vals=vals.Numbers(1,2e10))
 
+        # TODO: add params (trigger source, sweep time auto, )
+
+        self.add_function('reset', call_cmd='*RST')
+        self.add_function('turn_on_error_tooltip', call_cmd='SYST:ERR:DISP:REM ON')
+        self.add_function('turn_off_error_tooltip', call_cmd='SYST:ERR:DISP:REM OFF')
+        self.add_function('turn_off_cont_meas', call_cmd='INIT:CONT:ALL OFF')
+
         self.initialise()
         self.connect_message()
 
     def initialise(self):
         # TODO: set input and output buffer size (its in the matlab)?
+        self.write('*RST')
         self.write('SENS1:SWE:TYPE LIN')
         self.write('SENS1:SWE:TIME:AUTO ON')
         self.write('TRIG1:SEQ:SOUR IMM')
         # need to set averages?
         self.write('SENS1:AVER:STAT ON')
 
-    def getTrace(self):
-      self.write('SENS:SWE:POIN'+points)
-      self.write('INIT:CONT OFF') 
-      self.write('AVER:CLE')
+    # TODO: use *WAI or *OPC?
+    # TODO: what does repeating and decreasing avgnumber do?
 
-      avgnum = self.avg
-      while (avgnum > 0):
-          self.write('INIT')
-          self.ask('*OPC?')
+    def getTraceSimple(self,points):
+        self.write('SENS1:AVER:STAT OFF')
+        self.write('INIT:CONT:ALL OFF') 
+        self.write('SENS:SWE:POIN %d' % points)
+        data = self.ask('CALC:DATA? SDAT')
+        self.write('SYST:DISPl:UPD ONCE')
+        return data
 
-      data_str = self.ask('CALC:DATA? SDAT')
-      self.write('INIT:CONT ON')
+    def getTrace(self, points):
+        self.write('SENS1:AVER:STAT ON')
+        self.write('AVER:CLE')
+        self.write('SENS:SWE:POIN %d' %points)
+        self.write('INIT:CONT:ALL OFF') 
+        self.write('INIT:IMM; *WAI')
+        data_str = self.ask('CALC:DATA? SDAT')
+        self.write('SYST:DISPl:UPD ONCE')
 
-      data_list = list(map(float, data_str.split()))
-      data_mat = list(zip(data_list[0::2], data_list[1::2]))
 
-      traces = []
-      for (re,im) in data_mat:
-          mag1 = abs(complex(re, im))
-          phase1 = phase(complex(re, im))
-          print(mag1, phase1)
-          traces.append([mag1, phase1])
+        # while (self.get('avg') > 0):
+        #     self.write('INIT:IMM; *WAI')
+        #     self.set('avg') = self.get('avg')-1
+
+        # data_str = self.ask('CALC:DATA? SDAT')
+        # self.write('INIT:CONT ON')
+
+        data_list = list(map(float, data_str.split(',')))
+        data_mat = list(zip(data_list[0::2], data_list[1::2]))
+
+        traces = []
+        for (re,im) in data_mat:
+            mag1 = abs(complex(re, im))
+            phase1 = phase(complex(re, im))
+            traces.append([mag1, phase1])
         return traces
