b035aa6fd459f0537b1eb3abf5284dd5449c0180
Natalie Pearson <natalie@connectedhomes.co.uk>
2016-07-04 19:39:13 +0200

    feature: Trace as parameter feature added

--- qcodes/instrument_drivers/rohde_schwarz/ZNB20.py
+++ qcodes/instrument_drivers/rohde_schwarz/ZNB20.py
@@ -1,16 +1,44 @@
 from qcodes import VisaInstrument
 from qcodes.utils import validators as vals
 from cmath import phase
+import numpy as np
+from qcodes import Parameter
 
 '''
     TODO: 
-        better error messages
+        better error messages (eg do I have enough to do a sweep?)
         add functionality (ability to set start and stop freq as well as centre and span)
         needs on/off status?
-        write string to int and do it
 '''
 
-class RohdeSchwarz_ZNB20(VisaInstrument):
+class FrequencySweep(Parameter):
+    def __init__(self, instrument, start, stop, npts):
+        self._instrument = instrument
+        self.set_sweep(start, stop, npts)
+        self.names = ('magnitude', 'phase')
+        self.units = ('dBm', 'degrees')
+        self.setpoint_names = (('frequency',), ('frequency',))
+ 
+    def set_sweep(self, start, stop, npts):
+        # update the config of the software parameter
+        f = np.linspace(start, stop, num=npts)
+        self.setpoints = ((f,), (f,))
+        self.shapes = ((npts,), (npts,))
+ 
+    def get(self):      
+        self.write('INIT:IMM; *WAI')
+        data_list = [float(v) for v in self._instrument.ask('CALC:DATA? SDAT').split(',')]
+        data_arr = data_arr = np.array(data_list).reshape(len(data_list)/2,2)
+        mag_array=[]
+        phase_array=[]
+        for comp in data_arr:
+            complex_num = complex(comp[0],comp[1])
+            mag_array.append(abs(complex_num))
+            phase_array.append(phase(complex_num))
+        return mag_array, phase_array
+
+
+class ZNB20(VisaInstrument):
 
     def __init__(self, name, address, **kwargs):
         super().__init__(name=name, address=address, **kwargs)
@@ -39,31 +67,66 @@ class RohdeSchwarz_ZNB20(VisaInstrument):
                            get_parser=int,
                            vals=vals.Numbers(1,5000))
 
-        self.add_parameter(name='centFreq',
-                           label='Central Frequency',
-                           units='Hz',
-                           get_cmd='SENS:FREQ:CENT?',
-                           set_cmd='SENS:FREQ:CENT {:d}',
-                           get_parser=int,
-                           vals=vals.Numbers(1,2e10))
+        # self.add_parameter(name='centFreq',
+        #                    label='Central Frequency',
+        #                    units='Hz',
+        #                    get_cmd='SENS:FREQ:CENT?',
+        #                    set_cmd='SENS:FREQ:CENT {:d}',
+        #                    get_parser=int,
+        #                    vals=vals.Numbers(1,2e10))
 
-        self.add_parameter(name='spanFreq',
-                           label='Span Frequency',
-                           units='Hz',
-                           get_cmd='SENS:FREQ:SPAN?',
-                           set_cmd='SENS:FREQ:SPAN {:d}',
-                           get_parser=int,
-                           vals=vals.Numbers(1,2e10))
+        # self.add_parameter(name='spanFreq',
+        #                    label='Span Frequency',
+        #                    units='Hz',
+        #                    get_cmd='SENS:FREQ:SPAN?',
+        #                    set_cmd='SENS:FREQ:SPAN {:d}',
+        #                    get_parser=int,
+        #                    vals=vals.Numbers(1,2e10))
 
-        # TODO: add params (trigger source, sweep time auto, )
+        self.add_parameter(name='start', 
+                           get_cmd='SENS:FREQ:START?',
+                           set_cmd=self._set_start,
+                           vals=Numbers(1e3, 1e10))
+
+        self.add_parameter(name='stop', 
+                           get_cmd='SENS:FREQ:STOP?',
+                           set_cmd=self._set_stop,
+                           vals=Numbers(1e3, 1e10))
+
+        self.add_parameter(name='npts',
+                           get_cmd='SENS:SWE:POIN?',
+                           set_cmd=self._set_npts,
+                           get_parser=int,
+                           vals=vals.Numbers(1,1e3))
+
+        self.add_parameter(name='read',
+                           start=self.start(),
+                           stop=self.stop(),
+                           npts=self.npts(),
+                           parameter_class=FrequencySweep)       
 
         self.add_function('reset', call_cmd='*RST')
         self.add_function('turn_on_error_tooltip', call_cmd='SYST:ERR:DISP:REM ON')
         self.add_function('turn_off_error_tooltip', call_cmd='SYST:ERR:DISP:REM OFF')
+        self.add_function('turn_on_cont_meas', call_cmd='INIT:CONT:ALL ON')
         self.add_function('turn_off_cont_meas', call_cmd='INIT:CONT:ALL OFF')
+        self.add_function('update_display_once', call_cmd='SYST:DISP:UPD ONCE')
 
         self.initialise()
         self.connect_message()
+
+
+    def _set_start(self, val):
+        self.write('SENS:FREQ:START {:.4f}'.format(val))
+        self.read.set_sweep(val, self.stop(), self.npts())
+
+    def _set_stop(self, val):
+        self.write('SENS:FREQ:STOP {:.4f}'.format(val))
+        self.read.set_sweep(self.start(), val, self.npts())
+ 
+    def _set_npts(self, val):
+        self.write('SENS:SWE:POIN {:.4f}'.format(val))
+        self.read.set_sweep(self.start(), self.stop(), val)
 
     def initialise(self):
         # TODO: set input and output buffer size (its in the matlab)?
@@ -71,52 +134,31 @@ class RohdeSchwarz_ZNB20(VisaInstrument):
         self.write('SENS1:SWE:TYPE LIN')
         self.write('SENS1:SWE:TIME:AUTO ON')
         self.write('TRIG1:SEQ:SOUR IMM')
-        # need to set averages?
         self.write('SENS1:AVER:STAT ON')
 
-    # TODO: use *WAI or *OPC?
-    # TODO: what does repeating and decreasing avgnumber do?
-
-    def getTraceSimple(self,points):
-        self.write('SENS1:AVER:STAT OFF')
-        self.write('INIT:CONT:ALL OFF') 
-        self.write('SENS:SWE:POIN %d' % points)
-        self.write('INIT:IMM; *WAI')
-        data = self.ask('CALC:DATA? SDAT')
-        self.write('SYST:DISPl:UPD ONCE')
-        return data
-
-    def getTraceAve(self,points):
-        self.write('SENS1:AVER:STAT ON')
-        self.write('AVER:CLE')
-        self.write('INIT:CONT:ALL OFF') 
-        self.write('SENS:SWE:POIN %d' % points)
-        self.write('INIT:IMM; *WAI')
-        data = self.ask('CALC:DATA? SDAT')
-        self.write('SYST:DISPl:UPD ONCE')
-        return data
-
+    #TODO: get rid of this
     def getTrace(self, points):
         self.write('SENS1:AVER:STAT ON')
         self.write('AVER:CLE')
         self.write('SENS:SWE:POIN %d' %points)
-        self.write('INIT:CONT:ALL OFF') 
+        self.turn_off_cont_meas()
 
         avgnum = self.get('avg')
-        while avgnum > 0
+        while avgnum > 0:
             self.write('INIT:IMM; *WAI')
             avgnum-=1
 
         data_str = self.ask('CALC:DATA? SDAT')
-        self.write('SYST:DISPl:UPD ONCE')
+        self.update_display_once()
         self.write('INIT:CONT ON')
 
         data_list = list(map(float, data_str.split(',')))
-        data_mat = list(zip(data_list[0::2], data_list[1::2]))
+        data_arr = np.array(data_list).reshape(len(data_list)/2,2)
 
-        traces = []
-        for (re,im) in data_mat:
-            mag1 = abs(complex(re, im))
-            phase1 = phase(complex(re, im))
-            traces.append([mag1, phase1])
-        return traces
+        #TODO: better python here
+        mag_array=[]
+        phase_array=[]
+        for complex_num in data_arr:
+            mag_array.append(abs(complex(cpl[0],cpl[1])))
+            phase_array.append(phase(complex(cpl[0],cpl[1])))
+        return [np.array(mag_array), np.array(phase_array)]
