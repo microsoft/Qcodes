b1281c2a39f197131a8761e7e11f7690101fd402
Ruben van Gulik <rubenvangulik@gmail.com>
2016-07-08 17:01:54 +0200

    feature: Initial commit of AMI430 driver

--- qcodes/instrument_drivers/american_magnetics/AMI430.py
+++ qcodes/instrument_drivers/american_magnetics/AMI430.py
@@ -1,136 +1,83 @@
+import time
+
 from qcodes import VisaInstrument
 from qcodes.utils.validators import Numbers, Ints, Enum, MultiType
 
-from functools import partial
-
-def parse_output_string(s):
-    """ Parses and cleans string outputs of the Keithley """
-    # Remove surrounding whitespace and newline characters
-    s = s.strip()
-
-    # Remove surrounding quotes
-    if (s[0] == s[-1]) and s.startswith(("'", '"')):
-        s = s[1:-1]
-
-    s = s.lower()
-
-    # Convert some results to a better readable version
-    conversions = {
-        'mov': 'moving',
-        'rep': 'repeat',
-    }
-
-    if s in conversions.keys():
-        s = conversions[s]
-
-    return s
-
-def parse_output_bool(value):
-    return 'on' if int(value) == 1 else 'off'
-
+class AMI430(VisaInstrument):
     """
+    Driver for the American Magnetics Model 430 magnet power supply programmer
     """
-    def __init__(self, name, address, reset=False, **kwargs):
+    def __init__(self, name, address,
+                 coil_constant, current_rating, current_ramp_limit, persistent_switch=True,
+                 terminator='\n', reset=False, **kwargs):
         super().__init__(name, address, **kwargs)
 
-        self._trigger_sent = False
+        self._coil_constant = coil_constant
+        self._current_rating = current_rating
+        self._current_ramp_limit = current_ramp_limit
+        self._persistent_switch = persistent_switch
 
-        self.add_parameter('mode',
-                           get_cmd='SENS:FUNC?',
-                           set_cmd="SENS:FUNC {}",
-                           val_mapping={
-                               'ac current': '"CURR:AC"\n',
-                               'dc current': '"CURR:DC"\n',
-                               'ac voltage': '"VOLT:AC"\n',
-                               'dc voltage': '"VOLT:DC"\n',
-                               '2w resistance': '"RES"\n',
-                               '4w resistance': '"FRES"\n',
-                               'temperature': '"TEMP"\n',
-                               'frequency': '"FREQ"\n',
-                           })
+        self._field_rating = coil_constant * current_rating
 
-        # Mode specific parameters
-        self.add_parameter('nplc',
-                           get_cmd=partial(self._get_mode_param, 'NPLC', float),
-                           set_cmd=partial(self._set_mode_param, 'NPLC'),
-                           vals=Numbers(min_value=0.01, max_value=10))
+        self.add_parameter('field',
+                           get_cmd='FIELD:MAG?',
+                           get_parser=float,
+                           set_cmd=self._set_field,
+                           units='T',
+                           vals=Numbers(-self._field_rating, self._field_rating))
 
-        # TODO: validator, this one is more difficult since different modes
-        # require different validation ranges
-        self.add_parameter('range',
-                           get_cmd=partial(self._get_mode_param, 'RANG', float),
-                           set_cmd=partial(self._set_mode_param, 'RANG'),
+        self.add_function('ramp_to',
+                          call_cmd=self._ramp_to,
+                          args=[Numbers(-self._field_rating, self._field_rating)])
+
+        self.add_parameter('ramp_rate',
+                           get_cmd=self._get_ramp_rate,
+                           set_cmd=self._set_ramp_rate,
+                           units='T/s',
                            vals=Numbers())
 
-        self.add_parameter('auto_range',
-                           get_cmd=partial(self._get_mode_param, 'RANG:AUTO', parse_output_bool),
-                           set_cmd=partial(self._set_mode_param, 'RANG:AUTO'),
-                           vals=Enum('on', 'off'))
+        self.add_parameter('setpoint',
+                           get_cmd='FIELD:TARG?',
+                           get_parser=float,
+                           units='T')
 
-        self.add_parameter('digits',
-                           get_cmd=partial(self._get_mode_param, 'DIG', int),
-                           set_cmd=partial(self._set_mode_param, 'DIG'),
-                           vals=Ints(min_value=4, max_value=7))
+        if persistent_switch:
+            self.add_parameter('persistent_switch_heater_enabled',
+                               get_cmd='PS?',
+                               set_cmd=self._set_persistent_switch_heater,
+                               val_mapping={False: '0', True: '1'})
 
-        self.add_parameter('averaging_type',
-                           get_cmd=partial(self._get_mode_param, 'AVER:TCON', parse_output_string),
-                           set_cmd=partial(self._set_mode_param, 'AVER:TCON'),
-                           vals=Enum('moving', 'repeat'))
+            self.add_parameter('in_persistent_mode',
+                               get_cmd='PERS?',
+                               val_mapping={False: '0', True: '1'})
 
-        self.add_parameter('averaging_count',
-                           get_cmd=partial(self._get_mode_param, 'AVER:COUN', int),
-                           set_cmd=partial(self._set_mode_param, 'AVER:COUN'),
-                           vals=Ints(min_value=1, max_value=100))
+        self.add_parameter('is_quenched',
+                           get_cmd='QU?',
+                           val_mapping={False: '0', True: '1'})
 
-        self.add_parameter('averaging',
-                           get_cmd=partial(self._get_mode_param, 'AVER:STAT', parse_output_bool),
-                           set_cmd=partial(self._set_mode_param, 'AVER:STAT'),
-                           vals=Enum('on', 'off'))
+        self.add_function('reset_quench', call_cmd='QU 0')
+        self.add_function('set_quenched', call_cmd='QU 1')
 
-        # Global parameters
-        self.add_parameter('display',
-                           get_cmd='DISP:ENAB?',
-                           get_parser=parse_output_bool,
-                           set_cmd='DISP:ENAB {}',
-                           vals=Enum('on', 'off'))
-
-        self.add_parameter('trigger_continuous',
-                           get_cmd='INIT:CONT?',
-                           get_parser=parse_output_bool,
-                           set_cmd='INIT:CONT {}',
-                           vals=Enum('on', 'off'))
-
-        self.add_parameter('trigger_count',
-                           get_cmd='TRIG:COUN?',
-                           set_cmd='TRIG:COUN {}',
-                           vals=MultiType(Ints(min_value=1, max_value=9999), Enum('inf')))
-
-        self.add_parameter('trigger_delay',
-                           get_cmd='TRIG:DEL?',
-                           set_cmd='TRIG:DEL {}',
-                           units='s',
-                           vals=Numbers(min_value=0, max_value=999999.999))
-
-        self.add_parameter('trigger_source',
-                           get_cmd='TRIG:SOUR?',
-                           set_cmd='TRIG:SOUR {}',
+        self.add_parameter('ramping_state',
+                           get_cmd='STATE?',
                            val_mapping={
-                               'immediate': 'IMM\n',
-                               'timer': 'TIM\n',
-                               'manual': 'MAN\n',
-                               'bus': 'BUS\n',
-                               'external': 'EXT\n',
+                               'ramping': 1,
+                               'holding': 2,
+                               'paused': 3,
+                               'manual up': 4,
+                               'manual down': 5,
+                               'zeroing current': 6,
+                               'quench detected': 7,
+                               'at zero current': 8,
+                               'heating switch': 9,
+                               'cooling switch': 10,
                            })
 
-        self.add_parameter('trigger_timer',
-                           get_cmd='TRIG:TIM?',
-                           set_cmd='TRIG:TIM {}',
-                           units='s',
-                           vals=Numbers(min_value=0.001, max_value=999999.999))
+        self.add_function('get_error', get_cmd='SYST:ERR?')
 
-        self.add_parameter('amplitude',
-                           units='arb.unit',
-                           get_cmd=self._read_next_value)
+        self.add_function('ramp', call_cmd='RAMP')
+        self.add_function('pause', call_cmd='PAUSE')
+        self.add_function('zero', call_cmd='ZERO')
 
         self.add_function('reset', call_cmd='*RST')
 
@@ -139,26 +86,107 @@ def parse_output_bool(value):
 
         self.connect_message()
 
-    def trigger(self):
-        if self.trigger_continuous() == 'off':
-            self.write('INIT')
-            self._trigger_sent = True
+    def _can_start_ramping(self):
+        """
+        Check the current state of the magnet to see if we
+        can start ramping
+        """
+        if self.is_quenched():
+            return False
 
-    def _read_next_value(self):
-        # Prevent a timeout when no trigger has been sent
-        if self.trigger_continuous() == 'off' and not self._trigger_sent:
-            return 0.0
+        if self._persistent_switch and self.in_persistent_mode():
+            return False
 
-        self._trigger_sent = False
+        state = self.ramping_state()
 
-        return float(self.ask('DATA:FRESH?'))
+        if state == 'ramping':
+            if not self._persistent_switch:
+                return True
+            elif self.persistent_switch_heater_enabled():
+                return True
+            else:
+                return False
+        elif state in ['holding', 'paused', 'at zero current']:
+            return True
+        else:
+            return False
 
-    def _get_mode_param(self, parameter, parser):
-        cmd = '{}:{}?'.format(self.mode(), parameter)
+        return False
 
-        return parser(self.ask(cmd))
+    def _set_field(self, value):
+        """ BLocking method to ramp to a certain field """
+        if self._can_start_ramping():
+            self.pause()
 
-    def _set_mode_param(self, parameter, value):
-        cmd = '{}:{} {}'.format(self.mode(), parameter, value)
+            # Set the ramp target
+            self.write('CONF:FIELD:TARG {}'.format(value))
+
+            # If we have a persistent switch, make sure it is enabled
+            if self._persistent_switch:
+                if not self.persistent_switch_heater_enabled():
+                    self.persistent_switch_heater_enabled(True)
+
+            self.ramp()
+
+            time.sleep(0.5)
+
+            # Wait until no longer ramping
+            while self.ramping_state() == 'ramping':
+                time.sleep(0.3)
+
+            time.sleep(2.0)
+
+            # If we are now holding, it was succesful
+            if self.ramping_state() == 'holding':
+                self.pause()
+            else:
+                pass # ramp ended
+
+    def _ramp_to(self, value):
+        """ Non-blocking method to ramp to a certain field """
+        if self._can_start_ramping():
+            self.pause()
+
+            # Set the ramp target
+            self.write('CONF:FIELD:TARG {}'.format(value))
+
+            # If we have a persistent switch, make sure it is enabled
+            if self._persistent_switch:
+                if not self.persistent_switch_heater_enabled():
+                    self.persistent_switch_heater_enabled(True)
+
+            self.ramp()
+
+    def _get_ramp_rate(self):
+        results = self.ask('RAMP:RATE:FIELD:1?').split(',')
+
+        return float(results[0])
+
+    def _set_ramp_rate(self, rate):
+        cmd = 'CONF:RAMP:RATE:FIELD 1,{}{}'.format(rate, self._field_rating)
 
         self.write(cmd)
+
+    def _set_persistent_switch_heater(self, value):
+        """
+        This function sets the persistent switch heater state and blocks until
+        it has finished either heating or cooling
+
+        value: False/True
+        """
+        if value:
+            self.write('PS 1')
+
+            time.sleep(0.5)
+
+            # Wait until heating is finished
+            while self.ramping_state() == 'heating switch':
+                time.sleep(0.3)
+        else:
+            self.write('PS 0')
+
+            time.sleep(0.5)
+
+            # Wait until cooling is finished
+            while self.ramping_state() == 'cooling switch':
+                time.sleep(0.3)
