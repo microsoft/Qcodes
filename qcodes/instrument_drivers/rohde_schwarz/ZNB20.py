f9cd472bab6266c4d94486108d8faa79378f3bab
Natalie Pearson <natalie@connectedhomes.co.uk>
2016-07-22 15:09:08 +0200

    style: pep8ing

--- qcodes/instrument_drivers/rohde_schwarz/ZNB20.py
+++ qcodes/instrument_drivers/rohde_schwarz/ZNB20.py
@@ -3,16 +3,16 @@ from qcodes.utils import validators as vals
 from cmath import phase
 import numpy as np
 from qcodes import Parameter
-from math import log
+
 
 class FrequencySweep(Parameter):
-    '''Composite parameter class for a frequency sweep done with the Rohde Schwarz RSZNB20.
+    '''Hardware controlled parameter class for Rohde Schwarz RSZNB20 trace.
 
-    Allows for 'fast redout' where the instrument returns an list of transmission data in the form 
-    of a complex numbers taken from a frequency sweep.
+    Instrument returns an list of transmission data in the form of a list of
+    complex numbers taken from a frequency sweep.
 
-    TODO: 
-      - ability to choose for abs or db in magnitude return (important?)
+    TODO:
+      - ability to choose for abs or db in magnitude return
     '''
     def __init__(self, name, instrument, start, stop, npts):
         super().__init__(name)
@@ -21,28 +21,31 @@ class FrequencySweep(Parameter):
         self.names = ('magnitude', 'phase')
         self.units = ('dBm', 'rad')
         self.setpoint_names = (('frequency',), ('frequency',))
- 
+
     def set_sweep(self, start, stop, npts):
-    # neccesary to update the config of the software parameter when sweep is chaged 
-        f = tuple(np.linspace(int(start), int(stop), num=npts)) # tuple as it needs to be hashable for look up
+        #  needed to update config of the software parameter on sweep chage
+        # freq setpoints tuple as needs to be hashable for look up
+        f = tuple(np.linspace(int(start), int(stop), num=npts))
         self.setpoints = ((f,), (f,))
         self.shapes = ((npts,), (npts,))
- 
+
     def get(self):
         self._instrument.write('SENS1:AVER:STAT ON')
         self._instrument.write('AVER:CLE')
         self._instrument.cont_meas_off()
 
-    # instrument averages over its last 'avg' number of sweeps need to ensure averaged result is returned
+    # instrument averages over its last 'avg' number of sweeps
+    # need to ensure averaged result is returned
         for avgcount in range(self._instrument.avg()):
             self._instrument.write('INIT:IMM; *WAI')
-        data_list = [float(v) for v in self._instrument.ask('CALC:DATA? SDAT').split(',')] 
-    
+        data_str = self._instrument.ask('CALC:DATA? SDAT').split(',')
+        data_list = [float(v) for v in data_str]
+
     # data_list of complex numbers [re1,im1,re2,im2...]
-        data_arr = data_arr = np.array(data_list).reshape(int(len(data_list)/2),2)
+        data_arr = np.array(data_list).reshape(int(len(data_list) / 2), 2)
         mag_array, phase_array = [], []
         for comp in data_arr:
-            complex_num = complex(comp[0],comp[1])
+            complex_num = complex(comp[0], comp[1])
             mag_array.append(abs(complex_num))
             phase_array.append(phase(complex_num))
         self._instrument.cont_meas_on()
@@ -50,17 +53,18 @@ class FrequencySweep(Parameter):
 
 
 class ZNB20(VisaInstrument):
-    ''' This is the qcodes driver for the Rohde & Schwarz ZNB20 virtual network analyser
+    ''' qcodes driver for the Rohde & Schwarz ZNB20 virtual network analyser
 
-    Requires FrequencySweep parameter for taking a trace     
+    Requires FrequencySweep parameter for taking a trace
 
     TODO:
     - centre/span settable for frequwncy sweep
     - check initialisation settings and test functions
     '''
     def __init__(self, name, address, **kwargs):
+
         super().__init__(name=name, address=address, **kwargs)
-        
+
         self.add_parameter(name='power',
                            label='Power',
                            units='dBm',
@@ -71,11 +75,11 @@ class ZNB20(VisaInstrument):
 
         self.add_parameter(name='bandwidth',
                            label='Bandwidth',
-                           units='Hz', 
+                           units='Hz',
                            get_cmd='SENS:BAND?',
                            set_cmd='SENS:BAND {:.4f}',
                            get_parser=int,
-                           vals=vals.Numbers(1,1e6))
+                           vals=vals.Numbers(1, 1e6))
 
         self.add_parameter(name='avg',
                            label='Averages',
@@ -83,14 +87,14 @@ class ZNB20(VisaInstrument):
                            get_cmd='AVER:COUN?',
                            set_cmd='AVER:COUN {:.4f}',
                            get_parser=int,
-                           vals=vals.Numbers(1,5000))
+                           vals=vals.Numbers(1, 5000))
 
-        self.add_parameter(name='start', 
+        self.add_parameter(name='start',
                            get_cmd='SENS:FREQ:START?',
                            set_cmd=self._set_start,
                            get_parser=int)
 
-        self.add_parameter(name='stop', 
+        self.add_parameter(name='stop',
                            get_cmd='SENS:FREQ:STOP?',
                            set_cmd=self._set_stop,
                            get_parser=int)
@@ -100,11 +104,11 @@ class ZNB20(VisaInstrument):
                            set_cmd=self._set_npts,
                            get_parser=int)
 
-        self.add_parameter(name = 'trace',
+        self.add_parameter(name='trace',
                            start=self.start(),
                            stop=self.stop(),
                            npts=self.npts(),
-                           parameter_class=FrequencySweep)       
+                           parameter_class=FrequencySweep)
 
         self.add_function('reset', call_cmd='*RST')
         self.add_function('tooltip_on', call_cmd='SYST:ERR:DISP ON')
@@ -122,15 +126,18 @@ class ZNB20(VisaInstrument):
 
     def _set_start(self, val):
         self.write('SENS:FREQ:START {:.4f}'.format(val))
-        self.trace.set_sweep(val, self.stop(), self.npts()) # update setpoints for FrequencySweep param
+        # update setpoints for FrequencySweep param
+        self.trace.set_sweep(val, self.stop(), self.npts())
 
     def _set_stop(self, val):
         self.write('SENS:FREQ:STOP {:.4f}'.format(val))
-        self.trace.set_sweep(self.start(), val, self.npts()) # update setpoints for FrequencySweep param
- 
+        # update setpoints for FrequencySweep param
+        self.trace.set_sweep(self.start(), val, self.npts())
+
     def _set_npts(self, val):
         self.write('SENS:SWE:POIN {:.4f}'.format(val))
-        self.trace.set_sweep(self.start(), self.stop(), val) # update setpoints for FrequencySweep param
+        # update setpoints for FrequencySweep param
+        self.trace.set_sweep(self.start(), self.stop(), val)
 
     def initialise(self):
         self.write('*RST')
