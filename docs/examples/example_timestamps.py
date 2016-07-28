923c3477fa020a8498da9c3be57a33d35a79837d
Pieter <pieter.eendebak@gmail.com>
2016-07-28 09:32:57 +0200

    update example

--- docs/examples/example_timestamps.py
+++ docs/examples/example_timestamps.py
@@ -10,7 +10,7 @@ import qcodes.instrument_drivers.QuTech.TimeStamp
 
 #%% Create dummy instruments and model
 
-from toymodel import AModel, MockGates, MockSource, MockMeter
+from toymodel import AModel, MockGates, MockMeter
 
 # now create this "experiment"
 model = AModel()
@@ -18,9 +18,6 @@ gates = MockGates('gates', model=model)
 meter = MockMeter('meter', model=model)
 
 station = qc.Station(gates, meter)
-
-c0, c1, c2 = gates.chan0, gates.chan1, gates.chan2
-
 ts = qcodes.instrument_drivers.QuTech.TimeStamp.TimeStampInstrument(name='TimeStamp')
 
 # could measure any number of things by adding arguments to this
@@ -36,7 +33,7 @@ station.measure()
 def progress(data):
     ''' Simpe progress meter, should be integrated with either loop or data object '''
     data.sync()
-    tt=data.arrays['timestamp']
+    tt=data.arrays['TimeStamp_timestamp']
     vv=~np.isnan(tt)        
     ttx=tt[vv]
     if ttx.size==0:
@@ -46,14 +43,14 @@ def progress(data):
     
     logging.info('t0 %f t1 %f' % (t0, t1))
 
-    fraction = ttx.size / tt.size[0]
+    fraction = ttx.size / tt.shape[0]
     remaining = (t1-t0)*(1-fraction)/fraction
     return fraction, remaining
 
 
 #%% Go!
     
-data = qc.Loop(c0[-20:20:0.1], 0.1).run(location='testsweep', overwrite=True)
+data = qc.Loop(gates.chan0[-20:20:0.1], 0.1).run(location='testsweep', overwrite=True)
 
 for ii in range(10):
     print('progress: fraction %.2f, %.1f seconds remaining' % progress(data) )
