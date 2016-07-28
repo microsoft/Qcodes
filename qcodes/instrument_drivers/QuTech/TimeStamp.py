3788837519a936092bc9432de4f0e701de1cd7f4
Pieter <pieter.eendebak@gmail.com>
2016-07-28 09:32:30 +0200

    added string timestamp

--- qcodes/instrument_drivers/QuTech/TimeStamp.py
+++ qcodes/instrument_drivers/QuTech/TimeStamp.py
@@ -2,17 +2,28 @@
 #
 
 import time
+import datetime
 from qcodes import Instrument
 
 
 class TimeStampInstrument(Instrument):
     '''
-    Instrument that generates a timestamp
+    Instrument that generates timestamps
+    
+    Currently available are:
+    
+        * timestamp: Time in seconds sich epoch
+        * timestr: String representation of the current time
     '''
     def __init__(self, name, **kwargs):
         super().__init__(name, **kwargs)
-        # we need this to be a parameter (a function does not work with measure)
         self.add_parameter('timestamp', units='s', get_cmd=time.time, docstring='Timestamp based on number of seconds since epoch.')
-        _ = self.timestamp.get()
+        self.add_parameter('timestring', get_cmd=self.get_timestring, docstring='Time string based on number of seconds since epoch.')
         
-    
+        _ = self.timestamp.get()
+        _ = self.timestring.get()
+        
+
+    def get_timestring(self):
+        """ Return string representation of current time """
+        return str(datetime.datetime.now())
