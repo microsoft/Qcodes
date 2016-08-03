56bdc4401da7acc3063ede3e1affb2a7465ded65
Pieter <pieter.eendebak@gmail.com>
2016-08-03 09:53:56 +0200

    autopep8

--- qcodes/process/heartbeat.py
+++ qcodes/process/heartbeat.py
@@ -5,42 +5,45 @@ from qcodes import config
 
 
 def makeHeartBeatFile(bfile, reinit=False):
-    if (not os.path.exists(bfile) ) or reinit:
+    if (not os.path.exists(bfile)) or reinit:
         f = open(bfile, 'wb')
-        f.write(bytes([99,1])) # heartbeat on
+        f.write(bytes([99, 1]))  # heartbeat on
         f.close()
+
 
 def initHeartBeat(bfile, reinit=False):
     ''' Initialize connection to heartbeat for writing, use with setHeartBeat '''
-    if (not os.path.exists(bfile) ) or reinit:
+    if (not os.path.exists(bfile)) or reinit:
         f = open(bfile, 'wb')
-        f.write(bytes([99,1])) # heartbeat on
+        f.write(bytes([99, 1]))  # heartbeat on
         f.close()
-    
+
     f = open(bfile, 'a+b')
     f.seek(0)
     m = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_WRITE)
     m.flush()
-    #print(m[0])
-    #print(m[1])
-    #print('size %d' % m.size() )
+    # print(m[0])
+    # print(m[1])
+    # print('size %d' % m.size() )
 
     return m
+
 
 def openHeartBeat(bfile):
     f = open(bfile, 'r')
     m = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
     return m
 
+
 def setHeartBeat(m, value):
     ''' Set heartbeat value '''
-    m[1]=value
+    m[1] = value
+
 
 def readHeartBeat(m):
     ''' Return heartbeat value '''
-    return m[1]    
-    
-    
-_bfile=config.heartbeatfile 
+    return m[1]
+
+
+_bfile = config.heartbeatfile
 makeHeartBeatFile(_bfile)
-    
--- qcodes/utils/loggingGUI.py
+++ qcodes/utils/loggingGUI.py
@@ -10,10 +10,7 @@ Pieter Eendebak <pieter.eendebak@tno.nl>
 #%% Import packages
 import logging
 import os
-import random
-import sys
 import time
-import logging
 import argparse
 
 from qtpy import QtCore
@@ -26,7 +23,8 @@ from zmq.log.handlers import PUBHandler
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--verbose', default=1, help="verbosity level")
-parser.add_argument('-l', '--level', default=logging.DEBUG, help="logging level")
+parser.add_argument(
+    '-l', '--level', default=logging.DEBUG, help="logging level")
 parser.add_argument('-p', '--port', type=int, default=5800, help="zmq port")
 parser.add_argument('-g', '--gui', type=int, default=1, help="show gui")
 args = parser.parse_args()
@@ -64,22 +62,24 @@ def tprint(string, dt=1, output=False):
 
 import zmq.log.handlers
 
+
 def removeZMQlogger(name=None):
-    logger=logging.getLogger(name)
-    
+    logger = logging.getLogger(name)
+
     for h in logger.handlers:
         if isinstance(h, zmq.log.handlers.PUBHandler):
             print('removing handler %s' % h)
             logger.removeHandler(h)
-        
+
+
 def installZMQlogger(port=5800, name=None, clear=True, level=logging.INFO):
     if clear:
         removeZMQlogger(name)
-        
+
     ctx = zmq.Context()
     pub = ctx.socket(zmq.PUB)
     pub.setsockopt(zmq.RCVHWM, 10)
-    
+
     pub.connect('tcp://127.0.0.1:%i' % port)
 
     if name is None:
@@ -89,48 +89,48 @@ def installZMQlogger(port=5800, name=None, clear=True, level=logging.INFO):
     if level is not None:
         rootlogger.setLevel(level)
     handler = PUBHandler(pub)
-    pid=os.getpid()
-    pstr='pid %d: ' % pid
+    pid = os.getpid()
+    pstr = 'pid %d: ' % pid
     handler.formatters = {
         logging.DEBUG: logging.Formatter(pstr +
-        "%(levelname)s %(filename)s:%(lineno)d - %(message)s\n"),
+                                         "%(levelname)s %(filename)s:%(lineno)d - %(message)s\n"),
         logging.INFO: logging.Formatter(pstr + "%(message)s\n"),
         logging.WARN: logging.Formatter(pstr +
-        "%(levelname)s %(filename)s:%(lineno)d - %(message)s\n"),
+                                        "%(levelname)s %(filename)s:%(lineno)d - %(message)s\n"),
         logging.ERROR: logging.Formatter(pstr +
-        "%(levelname)s %(filename)s:%(lineno)d - %(message)s - %(exc_info)s\n"),
+                                         "%(levelname)s %(filename)s:%(lineno)d - %(message)s - %(exc_info)s\n"),
         logging.CRITICAL: logging.Formatter(pstr +
-        "%(levelname)s %(filename)s:%(lineno)d - %(message)s\n")}
-        
+                                            "%(levelname)s %(filename)s:%(lineno)d - %(message)s\n")}
+
     rootlogger.addHandler(handler)
-    logging.debug('installZMQlogger: handler installed') # first message always is discarded
+    logging.debug('installZMQlogger: handler installed')
+                  # first message always is discarded
     return rootlogger
 
 #%%
 
-        
+
 class zmqLoggingGUI(QtWidgets.QDialog):
-    
+
     LOG_LEVELS = dict({logging.DEBUG: 'debug', logging.INFO: 'info',
-              logging.WARN: 'warning', logging.ERROR: 'error', logging.CRITICAL: 'critical'})
-    
-    
-    def __init__( self, parent = None ):
+                       logging.WARN: 'warning', logging.ERROR: 'error', logging.CRITICAL: 'critical'})
+
+    def __init__(self, parent=None):
         super(zmqLoggingGUI, self).__init__(parent)
 
         self.setWindowTitle('ZMQ logger')
 
-        self.imap=dict((v, k) for k, v in self.LOG_LEVELS.items())
+        self.imap = dict((v, k) for k, v in self.LOG_LEVELS.items())
 
         self._console = QtWidgets.QPlainTextEdit(self)
         self._console.setMaximumBlockCount(2000)
-        
-        self._button  = QtWidgets.QPushButton(self)
+
+        self._button = QtWidgets.QPushButton(self)
         self._button.setText('Clear')
-        self._levelBox  = QtWidgets.QComboBox(self)
+        self._levelBox = QtWidgets.QComboBox(self)
         for k in sorted(self.LOG_LEVELS.keys()):
             print('item %s' % k)
-            val=self.LOG_LEVELS[k]            
+            val = self.LOG_LEVELS[k]
             self._levelBox.insertItem(k, val)
 
         blayout = QtWidgets.QHBoxLayout()
@@ -144,28 +144,28 @@ class zmqLoggingGUI(QtWidgets.QDialog):
         layout.addLayout(blayout)
         self.setLayout(layout)
 
-        self.addMessage('logging started...'+'\n')
-        
+        self.addMessage('logging started...' + '\n')
+
         self._levelBox.setCurrentIndex(1)
-        self.loglevel=logging.INFO
-        
+        self.loglevel = logging.INFO
+
     def setLevel(self, boxidx):
         name = self._levelBox.itemText(boxidx)
-        lvl=self.imap.get(name, None)
-        print('set level to %s: %d' % (name, lvl) )
+        lvl = self.imap.get(name, None)
+        print('set level to %s: %d' % (name, lvl))
         if lvl is not None:
-            self.loglevel=lvl
-            
-    def addMessage(self, msg, level=None):    
+            self.loglevel = lvl
+
+    def addMessage(self, msg, level=None):
         print(level)
         if level is not None:
-            if level<self.loglevel:
+            if level < self.loglevel:
                 return
-        self._console.moveCursor (QtGui.QTextCursor.End);
-        self._console.insertPlainText (msg);
-        self._console.moveCursor (QtGui.QTextCursor.End);
+        self._console.moveCursor(QtGui.QTextCursor.End)
+        self._console.insertPlainText(msg)
+        self._console.moveCursor(QtGui.QTextCursor.End)
 
-    def clearMessages( self ):
+    def clearMessages(self):
         ''' Clear the messages in the logging window '''
         dlg._console.clear()
         self.addMessage('cleared messages...\n')
@@ -176,69 +176,69 @@ def qt_logger(port, dlg, level=logging.INFO, verbose=1):
     sub = ctx.socket(zmq.SUB)
     sub.bind('tcp://127.0.0.1:%i' % port)
     sub.setsockopt(zmq.SUBSCRIBE, b"")
-    sub.setsockopt(zmq.RCVHWM, 10)   
-    
+    sub.setsockopt(zmq.RCVHWM, 10)
+
     logging.basicConfig(level=level)
-    app=QtWidgets.QApplication.instance()
+    app = QtWidgets.QApplication.instance()
     app.processEvents()
 
     print('connected to port %s' % port)
     while True:
         tprint('ZMQ logger: logging...', dt=5)
         try:
-            level, message  = sub.recv_multipart(zmq.NOBLOCK)      
-            #level, message = sub.recv_multipart()
+            level, message = sub.recv_multipart(zmq.NOBLOCK)
+            # level, message = sub.recv_multipart()
             message = message.decode('ascii')
             if message.endswith('\n'):
                 # trim trailing newline, which will get appended again
                 message = message[:-1]
-            level=level.lower().decode('ascii')
+            level = level.lower().decode('ascii')
             log = getattr(logging, level)
-            lvlvalue=dlg.imap.get(level, None)
+            lvlvalue = dlg.imap.get(level, None)
 
-            #print(lvlvalue)
+            # print(lvlvalue)
             log(message)
-            dlg.addMessage(message+'\n', lvlvalue)
-            
+            dlg.addMessage(message + '\n', lvlvalue)
+
             app.processEvents()
-            
-            if verbose>=2:
+
+            if verbose >= 2:
                 print('message: %s (level %s)' % (message, level))
         except:
             # no messages in system....
             app.processEvents()
             time.sleep(.06)
-            message=''
-            level=None
+            message = ''
+            level = None
             pass
-        
+
 #%%
 if __name__ == '__main__':
 
-    port=args.port
-    verbose=args.verbose
-    
+    port = args.port
+    verbose = args.verbose
+
     app = None
-    if ( not QtWidgets.QApplication.instance() ):
+    if (not QtWidgets.QApplication.instance()):
         app = QtWidgets.QApplication([])
     dlg = HorseLoggingGUI()
-    dlg.resize( 800, 400)
+    dlg.resize(800, 400)
     dlg.show()
 
     # start the log watcher
     try:
-        #sub_logger(port, level=args.level, verbose=verbose)
+        # sub_logger(port, level=args.level, verbose=verbose)
         qt_logger(port, level=args.level, verbose=verbose, dlg=dlg)
         pass
     except KeyboardInterrupt:
         pass
 
-    if ( app ):
+    if (app):
         app.exec_()
 
 #%% Send message to logger
 if 0:
-    port=5800
+    port = 5800
     installZMQlogger(port=port, level=None)
     logging.warning('test')
-    #log_worker(port=5700, interval=1)
+    # log_worker(port=5700, interval=1)
