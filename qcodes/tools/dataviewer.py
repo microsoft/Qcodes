ca07dc5a8d7dcd71044b8386d6f1f9fd29c46fe3
Pieter <pieter.eendebak@gmail.com>
2016-08-03 23:38:35 +0200

    clean up code

--- qcodes/tools/dataviewer.py
+++ qcodes/tools/dataviewer.py
@@ -1,14 +1,27 @@
 #%% Load packages
 
-import sys,os
+import os
 import re
 import logging
 
-import qcodes
-import qcodes as qc
-import qtpy.QtCore as QtCore
 import qtpy.QtGui as QtGui
+import qtpy.QtWidgets as QtWidgets
 import pyqtgraph as pg
+import argparse
+
+import qcodes
+from qcodes.plots.pyqtgraph import QtPlot
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbose', default=1, help="verbosity level")
+parser.add_argument(
+    '-d', '--datadir', type=str, default=None, help="data directory")
+args = parser.parse_args()
+verbose = args.verbose
+datadir = args.datadir
+
+#%% Helper functions
+
 
 def findfilesR(p, patt):
     """ Get a list of files (recursive)
@@ -22,136 +35,159 @@ def findfilesR(p, patt):
     """
     lst = []
     rr = re.compile(patt)
-    for root, dirs, files in os.walk(p, topdown=False):
+    for root, _, files in os.walk(p, topdown=False):
         lst += [os.path.join(root, f) for f in files if re.match(rr, f)]
     return lst
 
 #%% Main class
-class DataViewer(QtGui.QWidget):
+
+
+class DataViewer(QtWidgets.QWidget):
 
     ''' Simple viewer for Qcodes data
-    
+
     Arugments
     ---------
-    
+
         default_parameter : string
             name of default parameter to plot
     '''
-    def __init__(self, window_title='Log Viewer', default_parameter='amlitude', debugdict=dict()):
+
+    def __init__(self, datadir=None, window_title='Log Viewer', default_parameter='amlitude'):
         super(DataViewer, self).__init__()
 
         self.default_parameter = default_parameter
-        
+
+        self.datadir = datadir
+        if self.datadir is None:
+            self.datadir = qcodes.DataSet.default_io.base_location
+
+        qcodes.DataSet.default_io = qcodes.DiskIO(datadir)
+
         # setup GUI
-        self.text= QtGui.QLabel()
-        self.text.setText('Log files at %s' %  qcodes.DataSet.default_io.base_location)
-        self.logtree= QtGui.QTreeView() # QTreeWidget
-        self.logtree.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
+        self.text = QtWidgets.QLabel()
+        self.text.setText('Log files at %s' %
+                          self.datadir)
+        self.logtree = QtWidgets.QTreeView()  # QTreeWidget
+        self.logtree.setSelectionBehavior(
+            QtWidgets.QAbstractItemView.SelectRows)
         self._treemodel = QtGui.QStandardItemModel()
         self.logtree.setModel(self._treemodel)
-        self.__debug = debugdict
-        self.qplot= qc.QtPlot(remote=False)
-        self.plotwindow= self.qplot.win
-        #self.plotwindow = pg.GraphicsWindow(title='dummy')
+        self.__debug = dict()
+        self.qplot = QtPlot(remote=False)
+        self.plotwindow = self.qplot.win
 
-        vertLayout = QtGui.QVBoxLayout()
+        vertLayout = QtWidgets.QVBoxLayout()
         vertLayout.addWidget(self.text)
         vertLayout.addWidget(self.logtree)
         vertLayout.addWidget(self.plotwindow)
         self.setLayout(vertLayout)
 
         self._treemodel.setHorizontalHeaderLabels(['Log', 'Comments'])
-        self.setWindowTitle(window_title)        
+        self.setWindowTitle(window_title)
         self.logtree.header().resizeSection(0, 240)
 
-
         # disable edit
-        self.logtree.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
+        self.logtree.setEditTriggers(
+            QtWidgets.QAbstractItemView.NoEditTriggers)
         self.logtree.doubleClicked.connect(self.logCallback)
 
-        # get logs from disk             
+        # get logs from disk
         self.updateLogs()
-        
-    def updateLogs(self):
-        pass
 
-        model=self._treemodel
-        dd=findfilesR(qcodes.DataSet.default_io.base_location, '.*dat')        
+    def updateLogs(self):
+        model = self._treemodel
+        dd = findfilesR(self.datadir, '.*dat')
         print(dd)
-        
-        logs=dict()        
+
+        logs = dict()
         for i, d in enumerate(dd):
-            tag= os.path.basename(d)
-            datetag, logtag=d.split('/')[-3:-1]
+            datetag, logtag = d.split('/')[-3:-1]
             if not datetag in logs:
-                logs[datetag]=dict()
-            logs[datetag][logtag]=d
+                logs[datetag] = dict()
+            logs[datetag][logtag] = d
 
         self.logs = logs
-        
-        for i, datetag in enumerate(sorted(logs.keys())[::-1]):             
+
+        for i, datetag in enumerate(sorted(logs.keys())[::-1]):
             parent1 = QtGui.QStandardItem(datetag)
-            for j, logtag in enumerate(logs[datetag]):
+            for j, logtag in enumerate(sorted(logs[datetag])):
                 child1 = QtGui.QStandardItem(logtag)
                 child2 = QtGui.QStandardItem('info about plot')
-                child3 = QtGui.QStandardItem(os.path.join(datetag, logtag) )
+                child3 = QtGui.QStandardItem(os.path.join(datetag, logtag))
                 parent1.appendRow([child1, child2, child3])
             model.appendRow(parent1)
             # span container columns
-            self.logtree.setFirstColumnSpanned(i, self.logtree.rootIndex(), True)
+            self.logtree.setFirstColumnSpanned(
+                i, self.logtree.rootIndex(), True)
 
-    ''' Return parameter to be plotted '''
     def plot_parameter(self, data):
-        
-        if self.default_parameter in data.arrays.keys():
+        ''' Return parameter to be plotted '''
+        arraynames = data.arrays.keys()
+        if self.default_parameter in arraynames:
             return self.default_parameter
+        vv = [v for v in arraynames if v.endswith('default_parameter')]
+        if (len(vv) > 0):
+            return vv[0]
+        vv = [v for v in arraynames if v.endswith('amplitude')]
+        if (len(vv) > 0):
+            return vv[0]
+
         if 'amplitude' in data.arrays.keys():
             return 'amplitude'
 
         try:
-            key= next(iter (data.arrays.keys()))
+            key = next(iter(data.arrays.keys()))
             return key
-        except:
+        except Exception:
             return None
-            
-    def logCallback(self, index):
-        print('logCallback!')
-        logging.debug('index %s'% str(index))
-        self.__debug['last']=index
-        pp=index.parent()
-        row=index.row()
 
-        
-        tag=pp.child(row,2).data()
-        
+    def logCallback(self, index):
+        ''' Function called when a log entry is selected '''
+        logging.info('logCallback!')
+        logging.debug('logCallback: index %s' % str(index))
+        self.__debug['last'] = index
+        pp = index.parent()
+        row = index.row()
+
+        tag = pp.child(row, 2).data()
+
         # load data
         if tag is not None:
             print('logCallback! tag %s' % tag)
             try:
-                logging.debug('load tag %s' % tag) 
-                data=qc.load_data(tag)
-        
-                self.qplot.clear(); 
- 
-                param_name=self.plot_parameter(data)
-                
-                if param_name is not None:                    
-                    logging.info('using parameter %s for plotting' % param_name)
-                    self.qplot.add( getattr(data, param_name) ); 
+                logging.debug('load tag %s' % tag)
+                data = qcodes.load_data(tag)
+
+                self.qplot.clear()
+
+                infotxt = 'arrays: ' + ', '.join(list(data.arrays.keys()))
+                q = pp.child(row, 1).model()
+                q.setData(pp.child(row, 1), infotxt)
+
+                param_name = self.plot_parameter(data)
+
+                if param_name is not None:
+                    logging.info(
+                        'using parameter %s for plotting' % param_name)
+                    self.qplot.add(getattr(data, param_name))
                 else:
                     logging.info('could not find parameter for DataSet')
             except Exception as e:
-                print('logCallback! error ...' )
-                logging.debug(e)
+                print('logCallback! error ...')
+                print(e)
+                logging.warning(e)
                 pass
         pass
 
 
 #%% Testing
 
-if __name__=='__main__':
-    logviewer = DataViewer()
-    logviewer.setGeometry(1920+1280,60, 700,800)
-    logviewer.qplot.win.setMaximumHeight(400)
-    logviewer.show()
-    self=logviewer
+if __name__ == '__main__':
+    app = pg.mkQApp()
+
+    dataviewer = DataViewer(datadir=datadir)
+    dataviewer.setGeometry(1920 + 1280, 60, 700, 800)
+    dataviewer.qplot.win.setMaximumHeight(400)
+    dataviewer.show()
+    self = dataviewer
