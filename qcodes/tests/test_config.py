f7bd4ddc00d6b0e319278ab3ee81a73b5571fc8e
Giulio Ungaretti <giulioungaretti@me.com>
2016-10-03 00:35:20 +0200

    feat: Add user schema validation

--- qcodes/tests/test_config.py
+++ qcodes/tests/test_config.py
@@ -11,23 +11,23 @@ ENV_KEY = "/dev/random"
 BAD_JSON = "{}"
 
 # example configs
-GOOD_CONFIG_MAP = {Config.default_file_name : {"z":1, "a": 1, "b":0},
-        ENV_KEY: {"z":3, "h": 2, "user": "foo"},
-        Config.home_file_name : {"z":3, "b": 2, "user": "foo"},
-        Config.cwd_file_name: {"z":4, "c": 3, "bar": True}}
+GOOD_CONFIG_MAP = {Config.default_file_name: {"z": 1, "a": 1, "b": 0},
+                   ENV_KEY: {"z": 3, "h": 2, "user": {"foo":  "1"}},
+                   Config.home_file_name: {"z": 3, "b": 2, "user": "foo"},
+                   Config.cwd_file_name: {"z": 4, "c": 3, "bar": True}}
 
 # in this case the home config is messging up a type
-BAD_CONFIG_MAP = {Config.default_file_name : {"z":1, "a": 1, "b":0},
-        ENV_KEY: {"z":3, "h": 2, "user": "foo"},
-        Config.home_file_name : {"z":3, "b":"2", "user": "foo"},
-        Config.cwd_file_name: {"z":4, "c": 3, "bar": True}}
+BAD_CONFIG_MAP = {Config.default_file_name: {"z": 1, "a": 1, "b": 0},
+                  ENV_KEY: {"z": 3, "h": 2, "user": {"foo":  1}},
+                  Config.home_file_name: {"z": 3, "b": "2", "user": "foo"},
+                  Config.cwd_file_name: {"z": 4, "c": 3, "bar": True}}
 
 # expected config after succesfful loading of config files
-CONFIG = {"a":1, "b":2, "h":2, "user":"foo", "c":3, "bar": True, "z":4}
+CONFIG = {"a": 1, "b": 2, "h": 2, "user": {"foo":  "1"}, "c": 3, "bar": True, "z": 4}
 
 # the schema does not cover extra fields, so users can pass
 # wathever they want
-SCHEMA= {
+SCHEMA = {
         "$schema": "http://json-schema.org/draft-04/schema#",
         "type": "object",
         "properties": {
@@ -36,9 +36,6 @@ SCHEMA= {
                 },
             "b": {
                 "type": "integer"
-                },
-            "user": {
-                "type": "string"
                 },
             "z": {
                 "type": "integer"
@@ -55,8 +52,25 @@ SCHEMA= {
             ]
         }
 
+USER_SCHEMA = """ {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "object",
+        "properties": {
+            "user": {
+            "properties": {
+                       "foo":
+                       {
+                           "type": "string"
+                           }
+                       }
+                   }
+               }
+        } """
+
+
 def side_effect(map, name):
     return map[name]
+
 
 class TestConfig(TestCase):
     def setUp(self):
@@ -71,10 +85,14 @@ class TestConfig(TestCase):
     @patch.object(Config, 'load_config')
     @patch('os.path.isfile')
     def test_default_config_files(self, isfile, load_config, env, schema):
+        # don't try to load custom schemas
+        self.conf.schema_cwd_file_name = None
+        self.conf.schema_home_file_name = None
+        self.conf.schema_env_file_name = None
         schema.return_value = SCHEMA
         env.return_value = ENV_KEY
         isfile.return_value = True
-        load_config.side_effect = partial(side_effect, GOOD_CONFIG_MAP) 
+        load_config.side_effect = partial(side_effect, GOOD_CONFIG_MAP)
         config = self.conf.load_default()
         self.assertEqual(config, CONFIG)
 
@@ -83,9 +101,40 @@ class TestConfig(TestCase):
     @patch.object(Config, 'load_config')
     @patch('os.path.isfile')
     def test_bad_config_files(self, isfile, load_config, env, schema):
+        # don't try to load custom schemas
+        self.conf.schema_cwd_file_name = None
+        self.conf.schema_home_file_name = None
+        self.conf.schema_env_file_name = None
         schema.return_value = SCHEMA
         env.return_value = ENV_KEY
         isfile.return_value = True
         load_config.side_effect = partial(side_effect, BAD_CONFIG_MAP) 
         with self.assertRaises(jsonschema.exceptions.ValidationError):
-            _ = self.conf.load_default()
+                self.conf.load_default()
+    
+    @patch.object(Config, 'schema', new_callable=PropertyMock)
+    @patch.object(Config, 'env_file_name', new_callable=PropertyMock)
+    @patch.object(Config, 'load_config')
+    @patch('os.path.isfile')
+    @patch("builtins.open", mock_open(read_data=USER_SCHEMA))
+    def test_user_schema(self, isfile, load_config, env, schema):
+        schema.return_value = SCHEMA
+        env.return_value = ENV_KEY
+        isfile.return_value = True
+        load_config.side_effect = partial(side_effect, GOOD_CONFIG_MAP)
+        config = self.conf.load_default()
+        self.assertEqual(config, CONFIG)
+
+    @patch.object(Config, 'schema', new_callable=PropertyMock)
+    @patch.object(Config, 'env_file_name', new_callable=PropertyMock)
+    @patch.object(Config, 'load_config')
+    @patch('os.path.isfile')
+    @patch("builtins.open", mock_open(read_data=USER_SCHEMA))
+    def test_bad_user_schema(self, isfile, load_config, env, schema):
+        schema.return_value = SCHEMA
+        env.return_value = ENV_KEY
+        isfile.return_value = True
+        load_config.side_effect = partial(side_effect, BAD_CONFIG_MAP)
+        with self.assertRaises(jsonschema.exceptions.ValidationError):
+            self.conf.load_default()
+
