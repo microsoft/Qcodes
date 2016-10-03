9cc174de69ee16f893724746a3cd544e957aa60f
Giulio Ungaretti <giulioungaretti@me.com>
2016-10-03 19:26:38 +0200

    fix: Bugs in user specified schema validation

--- qcodes/tests/test_config.py
+++ qcodes/tests/test_config.py
@@ -13,7 +13,7 @@ BAD_JSON = "{}"
 # example configs
 GOOD_CONFIG_MAP = {Config.default_file_name: {"z": 1, "a": 1, "b": 0},
                    ENV_KEY: {"z": 3, "h": 2, "user": {"foo":  "1"}},
-                   Config.home_file_name: {"z": 3, "b": 2, "user": "foo"},
+                   Config.home_file_name: {"z": 3, "b": 2},
                    Config.cwd_file_name: {"z": 4, "c": 3, "bar": True}}
 
 # in this case the home config is messging up a type
@@ -22,8 +22,15 @@ BAD_CONFIG_MAP = {Config.default_file_name: {"z": 1, "a": 1, "b": 0},
                   Config.home_file_name: {"z": 3, "b": "2", "user": "foo"},
                   Config.cwd_file_name: {"z": 4, "c": 3, "bar": True}}
 
-# expected config after succesfful loading of config files
-CONFIG = {"a": 1, "b": 2, "h": 2, "user": {"foo":  "1"}, "c": 3, "bar": True, "z": 4}
+# expected config after successful loading of config files
+CONFIG = {"a": 1, "b": 2, "h": 2,
+          "user": {"foo":  "1"},
+          "c": 3, "bar": True, "z": 4}
+
+# expected config after updade by user
+UPDATED_CONFIG = {"a": 1, "b": 2, "h": 2,
+                  "user": {"foo":  "bar"},
+                  "c": 3, "bar": True, "z": 4}
 
 # the schema does not cover extra fields, so users can pass
 # wathever they want
@@ -45,6 +52,10 @@ SCHEMA = {
                 },
             "bar": {
                 "type": "boolean"
+                },
+            "user": {
+                "type": "object",
+                "properties": {}
                 }
             },
         "required": [
@@ -57,10 +68,13 @@ USER_SCHEMA = """ {
         "type": "object",
         "properties": {
             "user": {
-            "properties": {
-                       "foo":
-                       {
-                           "type": "string"
+                "type": "object",
+                "properties": {
+                           "foo":
+                           {
+                               "type": "string",
+                               "description": "foo"
+                               }
                            }
                        }
                    }
@@ -108,17 +122,17 @@ class TestConfig(TestCase):
         schema.return_value = SCHEMA
         env.return_value = ENV_KEY
         isfile.return_value = True
-        load_config.side_effect = partial(side_effect, BAD_CONFIG_MAP) 
+        load_config.side_effect = partial(side_effect, BAD_CONFIG_MAP)
         with self.assertRaises(jsonschema.exceptions.ValidationError):
                 self.conf.load_default()
-    
+
     @patch.object(Config, 'schema', new_callable=PropertyMock)
     @patch.object(Config, 'env_file_name', new_callable=PropertyMock)
     @patch.object(Config, 'load_config')
     @patch('os.path.isfile')
     @patch("builtins.open", mock_open(read_data=USER_SCHEMA))
     def test_user_schema(self, isfile, load_config, env, schema):
-        schema.return_value = SCHEMA
+        schema.return_value = copy.deepcopy(SCHEMA)
         env.return_value = ENV_KEY
         isfile.return_value = True
         load_config.side_effect = partial(side_effect, GOOD_CONFIG_MAP)
@@ -131,7 +145,7 @@ class TestConfig(TestCase):
     @patch('os.path.isfile')
     @patch("builtins.open", mock_open(read_data=USER_SCHEMA))
     def test_bad_user_schema(self, isfile, load_config, env, schema):
-        schema.return_value = SCHEMA
+        schema.return_value = copy.deepcopy(SCHEMA)
         env.return_value = ENV_KEY
         isfile.return_value = True
         load_config.side_effect = partial(side_effect, BAD_CONFIG_MAP)
