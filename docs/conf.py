6ff7a9214ffeb8aedfdbcd4de15461a44c7261c8
Jens Hedegaard Nielsen <j.h.nielsen@nbi.ku.dk>
2016-12-20 12:41:37 +0100

    docs: Dont include templates as regular rst

--- docs/conf.py
+++ docs/conf.py
@@ -93,7 +93,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_templates']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -383,4 +383,4 @@ try:
 except ImportError as e:
         print(e)
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['../_templates']
+templates_path = []
