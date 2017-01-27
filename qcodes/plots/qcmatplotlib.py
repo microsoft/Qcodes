046d2001fe4cbfc7adb29163c807c699501c8e82
Jens Hedegaard Nielsen <j.h.nielsen@nbi.ku.dk>
2017-01-27 11:44:04 +0100

    Add simple click widget, still very much wip

--- qcodes/plots/qcmatplotlib.py
+++ qcodes/plots/qcmatplotlib.py
@@ -6,6 +6,8 @@ from collections import Mapping
 
 import matplotlib.pyplot as plt
 from matplotlib.transforms import Bbox
+from matplotlib.widgets import Cursor
+
 import numpy as np
 from numpy.ma import masked_invalid, getmask
 
@@ -217,3 +219,45 @@ class MatPlot(BasePlot):
         default = "{}.png".format(self.get_default_title())
         filename = filename or default
         self.fig.savefig(filename)
+
+
+class ClickWidget:
+    def __init__(self, dataset):
+        self._data = {}
+        BasePlot.expand_trace(args=[dataset], kwargs=self._data)
+        self.fig, self.ax = plt.subplots(2, 2)
+        self.ax[0, 0].pcolormesh(self._data['x'],
+                                 self._data['y'],
+                                 self._data['z'])
+        self._data['xaxis'] = self._data['x'].ndarray[0, :]
+        self._data['yaxis'] = self._data['y'].ndarray
+        self.ax[0, 0].set_xlabel("x")
+        self.ax[0, 0].set_ylabel("y")
+        self.ax[1, 0].set_xlabel("x")
+        self.ax[1, 0].set_ylabel("z")
+        self.ax[1, 0].set_xlim(min(self._data['xaxis']), max(self._data['xaxis']))
+        self.ax[1, 0].set_ylim(0, self._data['z'].max() * 1.05)
+        self.ax[0, 1].set_xlabel("z")
+        self.ax[0, 1].set_ylabel("y")
+        self.ax[0, 1].set_ylim(min(self._data['yaxis']), max(self._data['yaxis']))
+        self.ax[0, 1].set_xlim(0, self._data['z'].max() * 1.05)
+        self._xline = None
+        self._yline = None
+        self._cursor = Cursor(self.ax[0, 0], useblit=True, color='black')
+        self.fig.tight_layout()
+        self.fig.canvas.mpl_connect('button_press_event', self._click)
+
+    def _click(self, event):
+
+        if event.inaxes == self.ax[0, 0]:
+            xpos = (abs(self._data['xaxis'] - event.xdata)).argmin()
+            ypos = (abs(self._data['yaxis'] - event.ydata)).argmin()
+            print("X: {} Y: {}".format(event.xdata, event.ydata))
+            print("Clicked on number {},{}".format(xpos, ypos))
+            if self._xline:
+                self._xline[0].remove()
+            if self._yline:
+                self._yline[0].remove()
+            self._yline = self.ax[0, 1].plot(self._data['z'][:, xpos], self._data['yaxis'], color='C0', marker='.')
+            self._xline = self.ax[1, 0].plot(self._data['xaxis'], self._data['z'][ypos, :], color='C0', marker='.')
+            self.fig.canvas.draw()
