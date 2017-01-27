a46d7cd6bbe3e33e6c4650cfa240adc0a91d0a24
Jens Hedegaard Nielsen <j.h.nielsen@nbi.ku.dk>
2017-01-27 11:20:44 +0100

    Fix: expand_trace is a static method

--- qcodes/plots/base.py
+++ qcodes/plots/base.py
@@ -174,7 +174,8 @@ class BasePlot:
         unit = getattr(data_array, 'unit', '')
         return  name, unit
 
-    def expand_trace(self, args, kwargs):
+    @staticmethod
+    def expand_trace(args, kwargs):
         """
         Complete the x, y (and possibly z) data definition for a trace.
 
