42ea28823c258e2dcfdd6556a90d4f34accfff16
Jens Hedegaard Nielsen <j.h.nielsen@nbi.ku.dk>
2017-02-14 09:37:33 +0100

    fix: acq_helpers correct doc syntax

--- qcodes/instrument_drivers/AlazarTech/acq_helpers.py
+++ qcodes/instrument_drivers/AlazarTech/acq_helpers.py
@@ -7,12 +7,12 @@ def filter_win(rec, cutoff, sample_rate, numtaps, axis=-1):
     low pass filter, returns filtered signal using FIR window
     filter
 
-    inputs:
-        record to filter
-        cutoff frequency
-        sampling rate
-        number of frequency comppnents to use in the filer
-        axis of record to apply filter along
+    Args:
+        rec: record to filter
+        cutoff: cutoff frequency
+        sample_rate: sampling rate
+        numtaps: number of frequency comppnents to use in the filer
+        axis: axis of record to apply filter along
     """
     nyq_rate = sample_rate / 2.
     fir_coef = signal.firwin(numtaps, cutoff / nyq_rate)
@@ -25,12 +25,12 @@ def filter_ls(rec, cutoff, sample_rate, numtaps, axis=-1):
     low pass filter, returns filtered signal using FIR
     least squared filter
 
-    inputs:
-        record to filter
-        cutoff frequency
-        sampling rate
-        number of frequency comppnents to use in the filer
-        axis of record to apply filter along
+    Args:
+        rec: record to filter
+        cufoff: cutoff frequency
+        sample_rate: sampling rate
+        numtaps: number of frequency comppnents to use in the filer
+        axis: axis of record to apply filter along
     """
     raise NotImplementedError
 
@@ -39,6 +39,7 @@ def sample_to_volt_u12(raw_samples, bps):
     """
     Applies volts conversion for 12 bit sample data stored
     in 2 bytes
+
     return:
         samples_magnitude_array
         samples_phase_array
@@ -64,11 +65,12 @@ def roundup(num, to_nearest):
     """
     Rounds up the 'num' to the nearest multiple of 'to_nearest', all int
 
-    inputs:
-        num to be rounded up
-        to_nearest value to be rounded to int multiple of
+    Args:
+        num: to be rounded up
+        to_nearest: value to be rounded to int multiple of
     return:
         rounded up value
+
     """
     remainder = num % to_nearest
     return int(num if remainder == 0 else num + to_nearest - remainder)
