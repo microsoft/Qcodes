c1a58def29412534a4bdd526c1cce69c39aa98b1
Natalie Pearson <npearson@phys.ethz.ch>
2017-03-09 12:11:33 +0100

    pep8 and making reset optional)

--- qcodes/instrument_drivers/tektronix/Keithley_2400.py
+++ qcodes/instrument_drivers/tektronix/Keithley_2400.py
@@ -1,22 +1,29 @@
 from qcodes import VisaInstrument
-from qcodes.utils.validators import Numbers, Ints, Enum, MultiType, Bool
+from qcodes.utils.validators import Numbers
 
 
 class Keithley_2400(VisaInstrument):
     """
     Driver for the Keithley 2400 multimeter.
+
+    Args:
+      name
+      address
+      reset (bool): optional
     """
+
     def __init__(self, name, address, reset=False, **kwargs):
         super().__init__(name, address, terminator='\n', **kwargs)
 
-        self.add_parameter(name = 'voltage',
+        self.add_parameter(name='voltage',
                            set_cmd='SOURCE:VOLTAGE:LEVEL {:.4f}',
                            get_cmd='SOURCE:VOLTAGE:LEVEL?',
                            get_parser=float,
                            vals=Numbers(-10, 10)
                            )
 
-        self.initialise()
+        if reset:
+            self.initialise()
         self.connect_message()
 
     def initialise(self):
