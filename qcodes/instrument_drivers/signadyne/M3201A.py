6b4e04fa0db5fd3aa3dd019e3d96d62421908049
lgnjanssen <L.G.N.Janssen@student.tudelft.nl>
2017-03-10 14:44:07 +1100

    feat: add waveform and awg related functions
    Add waveform and awg related functions to the M3201A driver. Also add clock and trigger related functions.

--- qcodes/instrument_drivers/signadyne/M3201A.py
+++ qcodes/instrument_drivers/signadyne/M3201A.py
@@ -109,7 +109,9 @@ class Signadyne_M3201A(Instrument):
         self.add_parameter('trigger_io',
                            label='trigger io',
                            get_cmd=self.get_trigger_io,
-                           docstring='The trigger input value, 0 (OFF) or 1 (ON)')
+                           set_cmd=self.set_trigger_io,
+                           docstring='The trigger input value, 0 (OFF) or 1 (ON)',
+                           vals=validator.Enum(0, 1))
         self.add_parameter('clock_frequency',
                            label='clock frequency',
                            unit='Hz',
@@ -366,6 +368,16 @@ class Signadyne_M3201A(Instrument):
         """
         self.awg.PXItriggerWrite(pxi_trigger, value)
 
+    def set_trigger_io(self, value):
+        """
+        Sets the trigger output. The trigger must be configured as output using
+        config_trigger_io
+
+        Args:
+            value (int): Tigger output value: 0 (OFF), 1 (ON)
+        """
+        self.awg.triggerIOwrite(value)
+
     #
     # The methods below are useful for controlling the device, but are not used for setting or getting parameters
     #
@@ -376,7 +388,7 @@ class Signadyne_M3201A(Instrument):
         self.awg.close()
         super().close()
 
-    # only closes the hardware device, not the current instrument object
+    # only closes the hardware device, does not delete the current instrument object
     def close_soft(self):
         self.awg.close()
 
@@ -404,6 +416,410 @@ class Signadyne_M3201A(Instrument):
     def run_self_test(self):
         value = self.awg.runSelfTest()
         print('Did self test and got result: {}'.format(value))
+
+    def reset_clock_phase(self, trigger_behaviour, trigger_source):
+        """
+        Sets the module in a sync state, waiting for the first trigger to reset the phase
+        of the internal clocks CLKsync and CLKsys
+
+        Args:
+            trigger_behaviour (int): value indicating the trigger behaviour
+                Active High     :   1
+                Active Low      :   2
+                Rising Edge     :   3
+                Falling Edge    :   4
+
+            trigger_source (int): value indicating external trigger source
+                External I/O Trigger    :   0
+                PXI Trigger [0..n]      :   4000+n
+        """
+        self.awg.clockResetPhase(trigger_behaviour, trigger_source)
+
+    def reset_channel_phase(self, channel_number):
+        """
+        Resets the accumulated phase of the selected channel. This accumulated phase is the
+        result of the phase continuous operation of the product.
+
+        Args:
+            channel_number (int): the number of the channel to reset
+        """
+        self.awg.channelPhaseReset(channel_number)
+
+    def reset_multiple_channel_phase(self, channel_mask):
+        """
+        Resets the accumulated phase of the selected channels simultaneously.
+
+        Args:
+            channel_mask (int): Mask to select the channel to reset (LSB is channel 0, bit 1 is channel 1 etc.)
+
+        Example:
+            reset_multiple_channel_phase(5) would reset the phase of channel 0 and 2
+        """
+        self.awg.channelPhaseResetMultiple(channel_mask)
+
+    def config_angle_modulation(self, channel_number, modulation_type, deviation_gain):
+        """
+        Configures the modulation in frequency/phase for the selected channel
+
+        Args:
+            channel_number (int): the number of the channel to configure
+            modulation_type (int): the modulation type the AWG is used for
+                No Modulation           :   0
+                Frequency Modulation    :   1
+                Phase Modulation        :   2
+            deviation_gain (int): gain for the modulating signal
+        """
+        self.awg.modulationAngleConfig(channel_number, modulation_type, deviation_gain)
+
+    def config_amplitude_modulation(self, channel_number, modulation_type, deviation_gain):
+        """
+        Configures the modulation in amplitude/offset for the selected channel
+
+        Args:
+            channel_number (int): the number of the channel to configure
+            modulation_type (int): the modulation type the AWG is used for
+                No Modulation           :   0
+                Amplitude Modulation    :   1
+                Offset Modulation       :   2
+            deviation_gain (int): gain for the modulating signal
+        """
+        self.awg.modulationAmplitudeConfig(channel_number, modulation_type, deviation_gain)
+
+    def set_iq_modulation(self, channel_number, enable):
+        """
+        Sets the IQ modulation for the selected channel
+
+        Args:
+            channel_number (int): the number of the channel to configure
+            enable (int): Enable (1) or Disabled (0) the IQ modulation
+        """
+        self.awg.modulationIQconfig(channel_number, enable)
+
+    def config_clock_io(self, clock_config):
+        """
+        Sets the IQ modulation for the selected channel
+
+        Args:
+            clock_config (int): clock connector function
+                Disable         :   0   (The CLK connector is disabled)
+                CLKref Output   :   1   (A copy of the reference clock is available at the CLK connector)
+        """
+        self.awg.clockIOconfig(clock_config)
+
+    def config_trigger_io(self, direction, sync_mode):
+        """
+        Configures the trigger connector/line direction and synchronization/sampling method
+
+        Args:
+            direction (int): input (1) or output (0)
+            sync_mode (int): sampling/synchronization mode
+                Non-synchronized mode   :   0   (trigger is sampled with internal 100 Mhz clock)
+                Synchronized mode       :   1   (trigger is sampled using CLK10)
+        """
+        self.awg.triggerIOconfig(direction, sync_mode)
+
+    #
+    # Waveform related functions
+    #
+
+    def load_wave_form(self, waveform_object, waveform_number, verbose=False):
+        """
+        Loads the specified waveform into the module onboard RAM.
+        Waveforms must be created first as an instance of the SD_Wave class.
+
+        Args:
+            waveform_object (SD_Wave): pointer to the waveform object
+            waveform_number (int): waveform number to identify the waveform
+                in subsequent related function calls.
+
+        Returns:
+            availableRAM (int): available onboard RAM in waveform points,
+            or negative numbers for errors
+        """
+        value = self.awg.waveformLoad(waveform_object, waveform_number)
+        value_name = 'Loaded waveform. available_RAM'
+        return result_parser(value, value_name, verbose)
+
+    def load_wave_form_int16(self, waveform_type, data_raw, waveform_number, verbose=False):
+        """
+        Loads the specified waveform into the module onboard RAM.
+        Waveforms must be created first as an instance of the SD_Wave class.
+
+        Args:
+            waveform_type (int): waveform type
+            data_raw (array): array with waveform points
+            waveform_number (int): waveform number to identify the waveform
+                in subsequent related function calls.
+
+        Returns:
+            availableRAM (int): available onboard RAM in waveform points,
+            or negative numbers for errors
+        """
+        value = self.awg.waveformLoadInt16(waveform_type, data_raw, waveform_number)
+        value_name = 'Loaded waveform. available_RAM'
+        return result_parser(value, value_name, verbose)
+
+    def reload_wave_form(self, waveform_object, waveform_number, padding_mode=0, verbose=False):
+        """
+        Replaces a waveform located in the module onboard RAM.
+        The size of the new waveform must be smaller than or
+        equal to the existing waveform.
+
+        Args:
+            waveform_object (SD_Wave): pointer to the waveform object
+            waveform_number (int): waveform number to identify the waveform
+                in subsequent related function calls.
+            padding_mode (int):
+                0:  the waveform is loaded as it is, zeros are added at the
+                    end if the number of points is not a multiple of the number
+                    required by the AWG.
+                1:  the waveform is loaded n times (using DMA) until the total
+                    number of points is multiple of the number required by the
+                    AWG. (only works for waveforms with even number of points)
+
+        Returns:
+            availableRAM (int): available onboard RAM in waveform points,
+            or negative numbers for errors
+        """
+        value = self.awg.waveformReLoad(waveform_object, waveform_number, padding_mode)
+        value_name = 'Reloaded waveform. available_RAM'
+        return result_parser(value, value_name, verbose)
+
+    def reload_wave_form_int16(self, waveform_type, data_raw, waveform_number, padding_mode=0, verbose=False):
+        """
+        Replaces a waveform located in the module onboard RAM.
+        The size of the new waveform must be smaller than or
+        equal to the existing waveform.
+
+        Args:
+            waveform_type (int): waveform type
+            data_raw (array): array with waveform points
+            waveform_number (int): waveform number to identify the waveform
+                in subsequent related function calls.
+            padding_mode (int):
+                0:  the waveform is loaded as it is, zeros are added at the
+                    end if the number of points is not a multiple of the number
+                    required by the AWG.
+                1:  the waveform is loaded n times (using DMA) until the total
+                    number of points is multiple of the number required by the
+                    AWG. (only works for waveforms with even number of points)
+
+        Returns:
+            availableRAM (int): available onboard RAM in waveform points,
+            or negative numbers for errors
+        """
+        value = self.awg.waveformReLoadArrayInt16(waveform_type, data_raw, waveform_number, padding_mode)
+        value_name = 'Reloaded waveform. available_RAM'
+        return result_parser(value, value_name, verbose)
+
+    def flush_wave_form(self, verbose=False):
+        """
+        Deletes all waveforms from the module onboard RAM and flushes all the AWG queues.
+        """
+        value = self.awg.waveformFlush()
+        value_name = 'flushed AWG queue and RAM'
+        return result_parser(value, value_name, verbose)
+
+    #
+    # AWG related functions
+    #
+
+    def awg_from_file(self, awg_number, waveform_file, trigger_mode, start_delay, cycles, prescaler, padding_mode=0,
+                      verbose=False):
+        """
+        Provides a one-step method to load, queue and start a single waveform
+        in one of the module AWGs.
+
+        Loads a waveform from file.
+
+        Args:
+            awg_number (int): awg number where the waveform is queued
+            waveform_file (str): file containing the waveform points
+            trigger_mode (int): trigger method to launch the waveform
+                Auto                        :   0
+                Software/HVI                :   1
+                Software/HVI (per cycle)    :   5
+                External trigger            :   2
+                External trigger (per cycle):   6
+            start_delay (int): defines the delay between trigger and wf launch
+                given in multiples of 10ns.
+            cycles (int): number of times the waveform is repeated once launched
+                negative = infinite repeats
+            prescaler (int): waveform prescaler value, to reduce eff. sampling rate
+
+        Returns:
+            availableRAM (int): available onboard RAM in waveform points,
+            or negative numbers for errors
+        """
+        value = self.awg.AWGFromFile(awg_number, waveform_file, trigger_mode, start_delay, cycles, prescaler,
+                                     padding_mode)
+        value_name = 'AWG from file. available_RAM'
+        return result_parser(value, value_name, verbose)
+
+    def awg_from_array(self, awg_number, trigger_mode, start_delay, cycles, prescaler, waveform_type, waveform_data_a,
+                       waveform_data_b=None, padding_mode=0, verbose=False):
+        """
+        Provides a one-step method to load, queue and start a single waveform
+        in one of the module AWGs.
+
+        Loads a waveform from array.
+
+        Args:
+            awg_number (int): awg number where the waveform is queued
+            trigger_mode (int): trigger method to launch the waveform
+                Auto                        :   0
+                Software/HVI                :   1
+                Software/HVI (per cycle)    :   5
+                External trigger            :   2
+                External trigger (per cycle):   6
+            start_delay (int): defines the delay between trigger and wf launch
+                given in multiples of 10ns.
+            cycles (int): number of times the waveform is repeated once launched
+                negative = infinite repeats
+            prescaler (int): waveform prescaler value, to reduce eff. sampling rate
+            waveform_type (int): waveform type
+            waveform_data_a (array): array with waveform points
+            waveform_data_b (array): array with waveform points, only for the waveforms
+                                        which have a second component
+
+        Returns:
+            availableRAM (int): available onboard RAM in waveform points,
+            or negative numbers for errors
+        """
+        value = self.awg.AWGfromArray(awg_number, trigger_mode, start_delay, cycles, prescaler,
+                                      waveform_type, waveform_data_a, waveform_data_b, padding_mode)
+        value_name = 'AWG from file. available_RAM'
+        return result_parser(value, value_name, verbose)
+
+    def awg_queue_waveform(self, awg_number, waveform_number, trigger_mode, start_delay, cycles, prescaler):
+        """
+        Queues the specified waveform in one of the AWGs of the module.
+        The waveform must be already loaded in the module onboard RAM.
+        """
+        self.awg.AWGqueueWaveform(awg_number, waveform_number, trigger_mode, start_delay, cycles, prescaler)
+
+    def awg_flush(self, awg_number):
+        """
+        Empties the queue of the selected AWG.
+        Waveforms are not removed from the onboard RAM.
+        """
+        self.awg.AWGflush(awg_number)
+
+    def awg_start(self, awg_number):
+        """
+        Starts the selected AWG from the beginning of its queue.
+        The generation will start immediately or when a trigger is received,
+        depending on the trigger selection of the first waveform in the queue
+        and provided that at least one waveform is queued in the AWG.
+        """
+        self.awg.AWGstart(awg_number)
+
+    def awg_start_multiple(self, awg_mask):
+        """
+        Starts the selected AWGs from the beginning of their queues.
+        The generation will start immediately or when a trigger is received,
+        depending on the trigger selection of the first waveform in their queues
+        and provided that at least one waveform is queued in these AWGs.
+
+        Args:
+            awg_mask (int): Mask to select the awgs to start (LSB is awg 0, bit 1 is awg 1 etc.)
+        """
+        self.awg.AWGstartMultiple(awg_mask)
+
+    def awg_pause(self, awg_number):
+        """
+        Pauses the selected AWG, leaving the last waveform point at the output,
+        and ignoring all incoming triggers.
+        The waveform generation can be resumed calling awg_resume
+        """
+        self.awg.AWGpause(awg_number)
+
+    def awg_pause_multiple(self, awg_mask):
+        """
+        Pauses the selected AWGs, leaving the last waveform point at the output,
+        and ignoring all incoming triggers.
+        The waveform generation can be resumed calling awg_resume_multiple
+
+        Args:
+            awg_mask (int): Mask to select the awgs to pause (LSB is awg 0, bit 1 is awg 1 etc.)
+        """
+        self.awg.AWGpauseMultiple(awg_mask)
+
+    def awg_resume(self, awg_number):
+        """
+        Resumes the selected AWG, from the current position of the queue.
+        """
+        self.awg.AWGresume(awg_number)
+
+    def awg_resume_multiple(self, awg_mask):
+        """
+        Resumes the selected AWGs, from the current positions of their respective queue.
+
+        Args:
+            awg_mask (int): Mask to select the awgs to resume (LSB is awg 0, bit 1 is awg 1 etc.)
+        """
+        self.awg.AWGresumeMultiple(awg_mask)
+
+    def awg_stop(self, awg_number):
+        """
+        Stops the selected AWG, setting the output to zero and resetting the AWG queue to its initial position.
+        All following incoming triggers are ignored.
+        """
+        self.awg.AWGstop(awg_number)
+
+    def awg_stop_multiple(self, awg_mask):
+        """
+        Stops the selected AWGs, setting their output to zero and resetting their AWG queues to the initial positions.
+        All following incoming triggers are ignored.
+
+        Args:
+            awg_mask (int): Mask to select the awgs to stop (LSB is awg 0, bit 1 is awg 1 etc.)
+        """
+        self.awg.AWGstopMultiple(awg_mask)
+
+    def awg_jump_next_wave_form(self, awg_number):
+        """
+        Forces a jump to the next waveform in the awg queue.
+        The jump is executed once the current waveform has finished a complete cycle.
+        """
+        self.awg.AWGjumpNextWaveform(awg_number)
+
+    def awg_config_external_trigger(self, awg_number, external_source, trigger_behaviour):
+        """
+        Configures the external triggers for the selected awg.
+        The external trigger is used in case the waveform is queued with th external trigger mode option.
+
+        Args:
+            awg_number (int): awg number
+            external_source (int): value indicating external trigger source
+                External I/O Trigger    :   0
+                PXI Trigger [0..n]      :   4000+n
+            trigger_behaviour (int): value indicating the trigger behaviour
+                Active High     :   1
+                Active Low      :   2
+                Rising Edge     :   3
+                Falling Edge    :   4
+        """
+        self.awg.AWGtriggerExternalConfig(awg_number, external_source, trigger_behaviour)
+
+    def awg_trigger(self, awg_number):
+        """
+        Triggers the selected AWG.
+        The waveform waiting in the current position of the queue is launched,
+        provided it is configured with VI/HVI Trigger.
+        """
+        self.awg.AWGtrigger(awg_number)
+
+    def awg_trigger_multiple(self, awg_mask):
+        """
+        Triggers the selected AWGs.
+        The waveform waiting in the current position of the queue is launched,
+        provided it is configured with VI/HVI Trigger.
+
+        Args:
+            awg_mask (int): Mask to select the awgs to stop (LSB is awg 0, bit 1 is awg 1 etc.)
+        """
+        self.awg.AWGtriggerMultiple(awg_mask)
 
     #
     # The methods below are not used for setting or getting parameters, but can be used in the test functions of the
