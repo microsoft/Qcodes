eacbd6098b7ca571899343317c362a043cd2be70
Mark Johnson <mark.johnson.ai@gmail.com>
2017-03-20 10:11:41 +1100

    fix: indentation in the M3300 card and change library reference to use relative addressing.

--- qcodes/instrument_drivers/keysight/M3300A.py
+++ qcodes/instrument_drivers/keysight/M3300A.py
@@ -10,34 +10,34 @@
 ###################################################################################
 
 try:
-    from SD_common.SD_AWG import SD_AWG
-    from SD_common.SD_DIG import SD_DIG
+    from .SD_common.SD_AWG import SD_AWG
+    from .SD_common.SD_DIG import SD_DIG
 except ImportError:
     raise ImportError('To use the M3300A driver, install the keysight module')
 
 class M3300A_AWG(SD_AWG):
-    def __init__(self, name, chassis=1, slot=8, **kwargs):
-        """ Driver for the AWG of the Keysight M3300A card.
+    """ Driver for the AWG of the Keysight M3300A card.
 
-        Args:
-            name (str)    : name for this instrument, passed to the base instrument
-            chassis (int) : chassis number where the device is located
-            slot (int)    : slot number where the device is plugged in
-        Example:
-                AWG = AWG('M3300A')
-        """
-        super().__init__(name, chassis=1, slot=8, channels=8, triggers=8, **kwargs)
+    Args:
+        name (str)    : name for this instrument, passed to the base instrument
+        chassis (int) : chassis number where the device is located
+        slot (int)    : slot number where the device is plugged in
+    Example:
+        AWG = AWG('M3300A')
+    """
+    def __init__(self, name, chassis=1, slot=8, **kwargs):
+        super().__init__(name, chassis=1, slot=8, channels=4, triggers=8, **kwargs)
 
 class M3300A_DIG(SD_DIG):
-        """ Driver for the digitizer of the keysight M3300A card.
+    """ Driver for the digitizer of the keysight M3300A card.
 
-        Args:
-            name (str)    : name for this instrument, passed to the base instrument
-            chassis (int) : chassis number where the device is located
-            slot (int)    : slot number where the device is plugged in
+    Args:
+        name (str)    : name for this instrument, passed to the base instrument
+        chassis (int) : chassis number where the device is located
+        slot (int)    : slot number where the device is plugged in
 
-        Example:
-                DIG  = DIG('M3300A')
-        """
+    Example:
+        DIG  = DIG('M3300A')
+    """
     def __init__(self, name, chassis=1, slot=8, **kwargs):
         super().__init__(name, chassis, slot, channels=8, triggers=8, **kwargs)
