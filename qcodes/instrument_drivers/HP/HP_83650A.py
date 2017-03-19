3ca5483709a8496dac6f23fddae2d66f33718792
Pieter Eendebak <pieter.eendebak@gmail.com>
2017-03-19 17:49:11 +0100

    fix changes made in (e1d9ece)

--- qcodes/instrument_drivers/HP/HP_83650A.py
+++ qcodes/instrument_drivers/HP/HP_83650A.py
@@ -7,6 +7,8 @@ import logging
 from qcodes import VisaInstrument
 from qcodes import validators as vals
 
+log = logging.getLogger(__name__)
+
 
 def parsestr(v):
     return v.strip().strip('"')
@@ -19,7 +21,7 @@ class HP_83650A(VisaInstrument):
 
         """
         self.verbose = verbose
-        logging.debug(__name__ + ' : Initializing instrument')
+        log.debug(__name__ + ' : Initializing instrument')
         super().__init__(name, address, **kwargs)
 
         self.add_parameter('frequency',
@@ -29,7 +31,7 @@ class HP_83650A(VisaInstrument):
                            vals=vals.Numbers(10e6, 40e9),
                            docstring='Microwave frequency, ....',
                            get_parser=float,
-                           units='Hz')
+                           unit='Hz')
 
         self.add_parameter('freqmode',
                            label='Frequency mode',
@@ -45,7 +47,7 @@ class HP_83650A(VisaInstrument):
                            set_cmd='SOUR:POW {}',
                            vals=vals.Numbers(-20, 20),
                            get_parser=float,
-                           units='dBm',
+                           unit='dBm',
                            docstring='Microwave power, ....')
 
         self.add_parameter('rfstatus',
@@ -101,12 +103,12 @@ class HP_83650A(VisaInstrument):
                            docstring='Pulse source, ....')
 
     def reset(self):
-        logging.debug(__name__ + ' : Resetting instrument')
+        log.debug(__name__ + ' : Resetting instrument')
         self.write('*RST')
         self.print_all()
 
     def print_all(self):
-        logging.debug(__name__ + ' : reading all settings from instrument')
+        log.debug(__name__ + ' : reading all settings from instrument')
         print(self.rfstatus.label + ':', self.rfstatus.get())
         print(self.power.label + ':', self.power.get(), self.power.units)
         print(self.frequency.label +
