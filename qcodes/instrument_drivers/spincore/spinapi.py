248aa35bc14dda8fcb3a55c0d30f545307000166
Mark Johnson <mark.johnson.ai@gmail.com>
2017-04-08 10:33:19 +1000

    refactor: modify spacing in spinapi.py to be PEP8 compliant.

--- qcodes/instrument_drivers/spincore/spinapi.py
+++ qcodes/instrument_drivers/spincore/spinapi.py
@@ -20,19 +20,22 @@
 # Modified by Mark Johnson
 
 import ctypes
-c_float_p = ctypes.POINTER(ctypes.c_float), # create a float pointer type
+
+c_float_p = ctypes.POINTER(ctypes.c_float),  # create a float pointer type
 
 try:
-        spinapi = ctypes.CDLL("spinapi64")
+    spinapi = ctypes.CDLL("spinapi64")
 except:
-	try:
-		spinapi = ctypes.CDLL("spinapi")
-	except:
-		raise("Failed to load spinapi library.")
-	
+    try:
+        spinapi = ctypes.CDLL("spinapi")
+    except:
+        raise ("Failed to load spinapi library.")
+
+
 def enum(**enums):
     return type('Enum', (), enums)
-		
+
+
 ns = 1.0
 us = 1000.0
 ms = 1000000.0
@@ -40,19 +43,19 @@ ms = 1000000.0
 MHz = 1.0
 kHz = 0.001
 Hz = 0.000001
-		
-#Instruction enum
+
+# Instruction enum
 Inst = enum(
-	CONTINUE = 0,
-	STOP = 1,
-	LOOP = 2,
-	END_LOOP = 3,
-	JSR = 4,
-	RTS = 5,
-	BRANCH = 6,
-	LONG_DELAY = 7,
-	WAIT = 8,
-	RTI = 9
+    CONTINUE=0,
+    STOP=1,
+    LOOP=2,
+    END_LOOP=3,
+    JSR=4,
+    RTS=5,
+    BRANCH=6,
+    LONG_DELAY=7,
+    WAIT=8,
+    RTI=9
 )
 spinapi.pb_get_version.restype = (ctypes.c_char_p)
 spinapi.pb_get_error.restype = (ctypes.c_char_p)
@@ -86,20 +89,20 @@ spinapi.pb_reset.restype = (ctypes.c_int)
 spinapi.pb_close.restype = (ctypes.c_int)
 
 spinapi.pb_inst_dds2.argtype = (
-	ctypes.c_int, #Frequency register DDS0
-	ctypes.c_int, #Phase register DDS0
-	ctypes.c_int, #Amplitude register DDS0
-	ctypes.c_int, #Output enable DDS0
-	ctypes.c_int, #Phase reset DDS0
-	ctypes.c_int, #Frequency register DDS1
-	ctypes.c_int, #Phase register DDS1
-	ctypes.c_int, #Amplitude register DDS1
-	ctypes.c_int, #Output enable DDS1,
-	ctypes.c_int, #Phase reset DDS1,
-	ctypes.c_int, #Flags
-	ctypes.c_int, #inst
-	ctypes.c_int, #inst data
-	ctypes.c_double, #timing value (double)
+	ctypes.c_int,  # Frequency register DDS0
+    ctypes.c_int,  # Phase register DDS0
+    ctypes.c_int,  # Amplitude register DDS0
+    ctypes.c_int,  # Output enable DDS0
+    ctypes.c_int,  # Phase reset DDS0
+    ctypes.c_int,  # Frequency register DDS1
+    ctypes.c_int,  # Phase register DDS1
+    ctypes.c_int,  # Amplitude register DDS1
+    ctypes.c_int,  # Output enable DDS1,
+    ctypes.c_int,  # Phase reset DDS1,
+    ctypes.c_int,  # Flags
+    ctypes.c_int,  # inst
+    ctypes.c_int,  # inst data
+    ctypes.c_double,  # timing value (double)
 )
 spinapi.pb_inst_dds2.restype = (ctypes.c_int)
 
@@ -117,114 +120,134 @@ spinapi.pb_select_dds.argtype = (ctypes.c_int)
 
 spinapi.pb_dds_load.restype = (ctypes.c_int)
 spinapi.pb_dds_load.argtype = (
-        c_float_p, # *data
-        ctypes.c_int # device number
+    c_float_p,  # *data
+    ctypes.c_int  # device number
 )
 
 spinapi.pb_dds_set_envelope_freq.restype = (ctypes.c_int)
 spinapi.pb_dds_set_envelope_freq.argtype = (
-        ctypes.c_float, # envelope frequency
-        ctypes.c_int    # the frequency register
+    ctypes.c_float,  # envelope frequency
+    ctypes.c_int  # the frequency register
 )
 
 spinapi.pb_set_amp.restype = (ctypes.c_int)
 spinapi.pb_set_amp.argtype = (
-        ctypes.c_float, # amplitude 
-        ctypes.c_int,   # address of amplitude register
+    ctypes.c_float,  # amplitude
+    ctypes.c_int,  # address of amplitude register
 )
 
 
 def pb_get_version():
-	"""Return library version as UTF-8 encoded string."""
-	ret = spinapi.pb_get_version()
-	return str(ctypes.c_char_p(ret).value.decode("utf-8"))
+    """Return library version as UTF-8 encoded string."""
+    ret = spinapi.pb_get_version()
+    return str(ctypes.c_char_p(ret).value.decode("utf-8"))
+
 
 def pb_get_error():
-	"""Return library error as UTF-8 encoded string."""
-	ret = spinapi.pb_get_error()
-	return str(ctypes.c_char_p(ret).value.decode("utf-8"))
-	
+    """Return library error as UTF-8 encoded string."""
+    ret = spinapi.pb_get_error()
+    return str(ctypes.c_char_p(ret).value.decode("utf-8"))
+
+
 def pb_count_boards():
-	"""Return the number of boards detected in the system."""
-	return spinapi.pb_count_boards()
-	
+    """Return the number of boards detected in the system."""
+    return spinapi.pb_count_boards()
+
+
 def pb_init():
-	"""Initialize currently selected board."""
-	return spinapi.pb_init()
-	
+    """Initialize currently selected board."""
+    return spinapi.pb_init()
+
+
 def pb_set_debug(debug):
-	return spinapi.pb_set_debug(debug)
-	
+    return spinapi.pb_set_debug(debug)
+
+
 def pb_select_board(board_number):
-	"""Select a specific board number"""
-	return spinapi.pb_select_board(board_number)
-	
+    """Select a specific board number"""
+    return spinapi.pb_select_board(board_number)
+
+
 def pb_set_defaults():
-	"""Set board defaults. Must be called before using any other board functions."""
-	return spinapi.pb_set_defaults()
-	
+    """Set board defaults. Must be called before using any other board functions."""
+    return spinapi.pb_set_defaults()
+
+
 def pb_core_clock(clock):
-	return spinapi.pb_core_clock(ctypes.c_double(clock))
-	
+    return spinapi.pb_core_clock(ctypes.c_double(clock))
+
+
 def pb_write_register(address, value):
-	return spinapi.pb_write_register(address, value)
-	
+    return spinapi.pb_write_register(address, value)
+
+
 def pb_start_programming(target):
-	return spinapi.pb_start_programming(target)
+    return spinapi.pb_start_programming(target)
+
 
 def pb_stop_programming():
-	return spinapi.pb_stop_programming()
-	
+    return spinapi.pb_stop_programming()
+
+
 def pb_start():
-	return spinapi.pb_start()
-	
+    return spinapi.pb_start()
+
+
 def pb_stop():
-	return spinapi.pb_stop()
-	
-def pb_reset(): 
-	return spinapi.pb_reset()
-	
+    return spinapi.pb_stop()
+
+
+def pb_reset():
+    return spinapi.pb_reset()
+
+
 def pb_close():
-	return spinapi.pb_close()
-	
-	
-	
+    return spinapi.pb_close()
+
+
 ################################################################################
 ###                                 DDS.h                                    ###
 ################################################################################
 
 def pb_set_freq(freq):
-        return spinapi.pb_set_freq(ctypes.c_double(freq))
+    return spinapi.pb_set_freq(ctypes.c_double(freq))
+
 
 def pb_set_phase(phase):
-        return spinapi.pb_set_phase(ctypes.c_double(phase))
+    return spinapi.pb_set_phase(ctypes.c_double(phase))
+
 
 def pb_inst_dds2(*args):
-	t = list(args)
-	#Argument 13 must be a double
-	t[13] = ctypes.c_double(t[13])
-	args = tuple(t)
-	return spinapi.pb_inst_dds2(*args)
+    t = list(args)
+    # Argument 13 must be a double
+    t[13] = ctypes.c_double(t[13])
+    args = tuple(t)
+    return spinapi.pb_inst_dds2(*args)
 
 
 def pb_inst_dds2_shape(*args):
-	t = list(args)
-	# Final argument must be a double
-	t[-1] = ctypes.c_double(t[-1])
-	args = tuple(t)
-        return spinapi.pb_inst_dds2_shape(*args)
+    t = list(args)
+    # Final argument must be a double
+    t[-1] = ctypes.c_double(t[-1])
+    args = tuple(t)
+    return spinapi.pb_inst_dds2_shape(*args)
+
 
 def pb_set_shape_defaults():
-        return spinapi.pb_set_shape_defaults()
+    return spinapi.pb_set_shape_defaults()
+
 
 def pb_select_dds(dds_num):
-        return spinapi.pb_select_dds(dds_num)
+    return spinapi.pb_select_dds(dds_num)
+
 
 def pb_dds_load(data, device):
-        return spinapi.pb_dds_load(c_float_p(data), device)
+    return spinapi.pb_dds_load(c_float_p(data), device)
+
 
 def pb_dds_set_envelope_freq(freq, n):
-        return spinapi.pb_dds_set_envelope_freq(ctypes.c_double(freq), n)
+    return spinapi.pb_dds_set_envelope_freq(ctypes.c_double(freq), n)
+
 
 def pb_set_amp(amp, addr):
-        return spinapi.pb_set_amp(ctypes.c_float(amp), addr)
+    return spinapi.pb_set_amp(ctypes.c_float(amp), addr)
