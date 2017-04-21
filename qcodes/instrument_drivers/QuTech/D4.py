068fdf6942581829696bded060bd89b59819f79d
Ruben van Gulik <rubenvangulik@gmail.com>
2017-04-21 15:44:24 +0200

    updated D4 module to low-level code

--- qcodes/instrument_drivers/QuTech/D4.py
+++ qcodes/instrument_drivers/QuTech/D4.py
@@ -1,7 +1,8 @@
 from qcodes import Instrument
-from qcodes.utils.validators import Numbers
 
 from .D4_module import D4_module
+
+from functools import partial
 
 
 class D4(Instrument):
@@ -13,5 +14,29 @@ class D4(Instrument):
 
         self.d4 = D4_module(spi_rack, module)
 
+        self.add_parameter('mode',
+                           label='Mode',
+                           get_cmd=self.get_mode)
+
+        self.add_parameter('filter_value',
+                           label='Filter value',
+                           get_cmd=self.get_filter_value)
+
+        self.add_parameter('buffers_enabled',
+                           label='Buffers enabled',
+                           get_cmd=self.get_buffers_enabled)
+
         for i in range(2):
-            pass
+            self.add_parameter('adc{}'.format(i + 1),
+                               label='ADC {}'.format(i + 1),
+                               get_cmd=partial(self.d4.singleConversion, i),
+                               units='V')
+
+    def get_mode(self):
+        return self.d4.mode
+
+    def get_filter_value(self):
+        return self.d4.filter_val
+
+    def get_buffers_enabled(self):
+        return self.d4.buf_en
