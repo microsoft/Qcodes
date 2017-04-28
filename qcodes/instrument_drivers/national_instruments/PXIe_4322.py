825d8aadcae6e7fe9dc0bd2e619deafe972ce406
lgnjanssen <L.G.N.Janssen@student.tudelft.nl>
2017-04-28 14:34:18 +1000

    feat: Make setcmd store voltage in file.
    Make the set command store the last-set voltages in a json file on disk. Also try to read from this file when creating the instrument. This tries to keep the actual current output voltage in sync with the value stored in the qcodes driver.

--- qcodes/instrument_drivers/national_instruments/PXIe_4322.py
+++ qcodes/instrument_drivers/national_instruments/PXIe_4322.py
@@ -2,6 +2,8 @@ from qcodes.instrument.base import Instrument
 from qcodes import validators as validator
 
 from functools import partial
+import warnings
+import json
 
 try:
     import nidaqmx
@@ -20,30 +22,43 @@ class PXIe_4322(Instrument):
     the nidaqmx package need to be installed in order to use this QCoDeS driver.
     """
 
-    def __init__(self, name, chassis, slot, **kwargs):
+    def __init__(self, name, device_name, **kwargs):
         super().__init__(name, **kwargs)
 
-        self.chassis = chassis
-        self.slot = slot
+        self.device_name = device_name
 
         self.channels = 8
 
-        self.__voltage = [0] * self.channels
+        try:
+            with open('NI_voltages_{}.json'.format(device_name)) as data_file:
+                self.__voltage = json.load(data_file)
+        except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
+            self.__voltage = [0] * self.channels
+
+        print('Please read the following warning message:')
+
+        warnings.warn('The last known output values are: {} Please check these values and make sure they correspond '
+                      'to the actual output of the PXIe-4322 module. Any difference between stored value and actual '
+                      'value WILL cause sudden jumps in output.'.format(self.__voltage), UserWarning)
 
         for i in range(self.channels):
             self.add_parameter('voltage_channel_{}'.format(i),
                                label='voltage channel {}'.format(i),
-                               unit='Hz',
+                               unit='V',
                                set_cmd=partial(self.set_voltage, channel=i),
                                get_cmd=partial(self.get_voltage, channel=i),
                                docstring='The DC output voltage of channel {}'.format(i),
                                vals=validator.Numbers(-16, 16))
 
-    def set_voltage(self, voltage, channel):
+    def set_voltage(self, voltage, channel, save_to_file=True):
         with nidaqmx.Task() as task:
-            task.ao_channels.add_ao_voltage_chan('PXI{}Slot{}/ao{}'.format(self.chassis, self.slot, channel))
+            task.ao_channels.add_ao_voltage_chan('{}/ao{}'.format(self.device_name, channel),
+                                                 min_val=-16.0, max_val=16.0)
             task.write(voltage, auto_start=True)
             self.__voltage[channel] = voltage
+            if save_to_file:
+                with open('NI_voltages_{}.json'.format(self.device_name), 'w') as output_file:
+                    json.dump(self.__voltage, output_file, ensure_ascii=False)
 
     def get_voltage(self, channel):
         return self.__voltage[channel]
