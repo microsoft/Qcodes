a5e069674703552ad3619fd8ec5ad1a66cfe71c0
Jens Hedegaard Nielsen <j.h.nielsen@nbi.ku.dk>
2017-05-04 16:20:53 +0200

    adapt toy cliclient to removed ping/pong

--- qcodes/instrument_drivers/oxford/wsclient.py
+++ qcodes/instrument_drivers/oxford/wsclient.py
@@ -4,14 +4,9 @@ import asyncio
 import websockets
 
 async def hello():
-    async with websockets.connect('ws://localhost:8000/monitor') as websocket:
+    async with websockets.connect('ws://localhost:8000/ws') as websocket:
         i = 0
         while True:
             data = await websocket.recv()
             print(data)
-            print("sending ping")
-            pong = await websocket.ping()
-            print("send ping")
-            await pong
-            print("got pong")
 asyncio.get_event_loop().run_until_complete(hello())
