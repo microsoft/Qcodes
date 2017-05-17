7ce11b304f529b9ac9729f799e640ef792d860a6
Jens Hedegaard Nielsen <j.h.nielsen@nbi.ku.dk>
2017-05-17 10:42:25 +0200

    Revert "Fix relative import of py_header"
    This reverts commit 71ce11a8ed55f495f01170f66f5c587add75f446.

--- qcodes/instrument_drivers/Spectrum/pyspcm.py
+++ qcodes/instrument_drivers/Spectrum/pyspcm.py
@@ -4,12 +4,10 @@ import sys
 from ctypes import *
 
 # load registers for easier access
-# QCoDeS change to relative import
-from .py_header.regs import *
+from py_header.regs import *
 
 # load registers for easier access
-# QCoDeS change to relative import
-from .py_header.spcerr import *
+from py_header.spcerr import *
 
 SPCM_DIR_PCTOCARD = 0
 SPCM_DIR_CARDTOPC = 1
