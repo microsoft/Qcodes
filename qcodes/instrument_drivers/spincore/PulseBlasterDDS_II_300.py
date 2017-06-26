8ed950e1c948458c76aea99589579a972c21e8fe
FQT <serwan.asaad@gmail.com>
2017-06-26 12:18:40 +1000

    Initial changes to DDS
    Removed import all from spinapi
    Turned error_parse into a decorator
    Fixed bug in dds_load

--- qcodes/instrument_drivers/spincore/PulseBlasterDDS_II_300.py
+++ qcodes/instrument_drivers/spincore/PulseBlasterDDS_II_300.py
@@ -1,10 +1,27 @@
+from functools import partial, wraps
+import logging
+
 from qcodes.instrument.base import Instrument
 from qcodes.utils.validators import Numbers, Enum, Ints, Strings, Anything
-from functools import partial
+
 try:
-    from .spinapi import *
+    from . import spinapi as api
 except:
     raise ImportError('To use a SpinCore PulseBlaster, install the Python SpinAPI')
+
+
+logger = logging.getLogger(__name__)
+
+
+def error_parse(f):
+    @wraps(f)
+    def error_wrapper(*args, **kwargs):
+        value = f(*args, **kwargs)
+        if not isinstance(value, str) and value < 0:
+            raise IOError('{}: {}'.format(value, api.pb_get_error()))
+        return value
+    return error_wrapper
+
 
 class PB_KEYWORDS():
     ns = 1.0
@@ -35,138 +52,136 @@ class PB_KEYWORDS():
 
 
 
-class PB_DDS(Instrument):
+class PulseBlaster_DDS(Instrument):
     """
     This is the qcodes driver for the SpinCore PulseBlasterDDS-II-300
 
     Args:
         name (str): name for this instrument, passed to the base instrument
     """
-    def error_parse(self, value):
-        if not isinstance(value, str) and value < 0:
-            raise IOError('{}:'.format(value) + self.get_error())
-        return value
 
-    def __init__(self, name, **kwargs):
-        if 'Werror' in kwargs:
-            self.werror = kwargs.pop('Werror')
-        else:   
-            self.werror = False
+    def __init__(self, name,  raise_errors=False, **kwargs):
+        self.raise_errors = raise_errors
 
         super().__init__(name, **kwargs)
 
-        self.N_CHANNELS        = 2
-        self.N_FREQ_REGS       = 16
-        self.N_PHASE_REGS      = 8
-        self.N_AMPLITUDE_REGS  = 4
+        self.N_CHANNELS = 2
+        self.N_FREQ_REGS = 16
+        self.N_PHASE_REGS = 8
+        self.N_AMPLITUDE_REGS = 4
 
         
-        self.DEFAULT_DDS_INST      = (0, 0, 0, 0, 0)
+        self.DEFAULT_DDS_INST = (0, 0, 0, 0, 0)
             
         # Initialise the DDS
-        # NOTE: Hard coded value for board may want to be part of instrument init
+        # NOTE: Hard coded value for board may want to be part of initalize
         if self.count_boards() > 0:
-            self.init()
+            self.initialize()
             self.select_board(0)
-            # Call set defaults as part of init to give board a well defined state
-            pb_set_defaults()
+            # Call set defaults to give board a well defined state
+            api.pb_set_defaults()
         else:
-            if self.werror:
+            if self.raise_errors:
                 raise IOError('PB Error: Can\'t find board')
             else:
-                print('Error: Can\'t find board, will continue anyway')
+                logger.error('Error: Can\'t find board, will continue anyway')
 
-        # Internal State Variables, used when setting individual registers when others
-        # may be undefined
-        self.__frequency = [[0.0]*self.N_FREQ_REGS     , [0.0]*self.N_FREQ_REGS ]
-        self.__phase     = [[0.0]*self.N_PHASE_REGS    , [0.0]*self.N_PHASE_REGS]
+        # Internal State Variables, used when setting individual registers when
+        # others may be undefined
+        self._frequency = [[0.0] * self.N_FREQ_REGS,  [0.0] * self.N_FREQ_REGS]
+        self._phase     = [[0.0] * self.N_PHASE_REGS, [0.0] * self.N_PHASE_REGS]
+
         # Create an empty list of lists of instructions [[], [], ...]
-        self.__chan_instructions = []
-        for n in range(self.N_CHANNELS):
-            self.__chan_instructions.append([])
+        self.instructions = [[] for _ in range(self.N_CHANNELS)]
 
-        ###########################################################################
-        ###                              Parameters                             ###
-        ###########################################################################
 
+        ########################################################################
+        ###                              Parameters                          ###
+        ########################################################################
         self.add_parameter(
             'core_clock',
-            label='The core clock of the PulseBlasterDDS',
+            label='Core clock',
             set_cmd=self.set_core_clock,
             vals=Numbers(),
-            docstring=''
-        )
+            docstring='The core clock of the PulseBlasterDDS')
 
         for n in range(self.N_CHANNELS):
             # DDS Register Bank
             for r in range(self.N_FREQ_REGS):
                 self.add_parameter(
                     'frequency_n{}_r{}'.format(n, r),
-                    label='DDS Frequency for channel {}, register {}'.format(n, r),
-                    set_cmd=partial(self.set_frequency_register, channel=n, register=r),
+                    label='DDS Frequency for channel {}, '
+                          'register {}'.format(n, r),
+                    set_cmd=partial(self.set_frequency_register,
+                                    channel=n, register=r),
                     vals=Numbers(),
-                    docstring=''
-                )
+                    docstring='')
 
             for r in range(self.N_PHASE_REGS):
                 self.add_parameter(
                     'phase_n{}_r{}'.format(n, r),
-                    label='DDS Phase for channel {}, register {}'.format(n, r),
-                    set_cmd=partial(self.set_phase_register, channel=n, register=r),
+                    label='DDS Phase for channel {}, '
+                          'register {}'.format(n, r),
+                    set_cmd=partial(self.set_phase_register,
+                                    channel=n, register=r),
                     vals=Numbers(),
-                    docstring=''
-                )
+                    docstring='')
 
             for r in range(self.N_AMPLITUDE_REGS):
                 self.add_parameter(
                     'amplitude_n{}_r{}'.format(n, r),
-                    label='DDS Amplitude for channel {}, register {}'.format(n, r),
-                    set_cmd=partial(self.set_amplitude_register, channel=n, register=r),
+                    label='DDS Amplitude for channel {}, '
+                          'register {}'.format(n, r),
+                    set_cmd=partial(self.set_amplitude_register,
+                                    channel=n, register=r),
                     vals=Numbers(),
-                    docstring=''
-                )
+                    docstring='')
 
     ###########################################################################
     ###                         DDS Board commands                          ###
     ###########################################################################
     # Just wrapped from spinapi.py #
-    
+
+    @error_parse
     def get_version(self):
         """ Return the current version of the spinapi library being used. """
-        return self.error_parse(pb_get_version())
+        return api.pb_get_version()
 
-    def get_error(self):
+    @staticmethod
+    def get_error():
         """ Print library error as UTF-8 encoded string. """
-        return str(pb_get_error())
-        
+        return str(api.pb_get_error())
+
+    @error_parse
     def count_boards(self):
         """ Print the number of boards detected in the system. """
-        return self.error_parse(pb_count_boards())
-        
-    def init(self):
+        return api.pb_count_boards()
+
+    @error_parse
+    def initialize(self):
         """ Initialize currently selected board. """
-        return self.error_parse(pb_init())
-        
+        return api.pb_init()
+
+    @error_parse
     def set_debug(self, debug):
         """ Enables logging to a log.txt file in the current directory """
-        return self.error_parse(pb_set_debug(debug))
-        
-    def select_board(self, board_number):
-        """ Select a specific board number """
-        return self.error_parse(pb_select_board(board_number))
-        
+        return api.pb_set_debug(debug)
+
+    @error_parse
     def set_defaults(self):
-        """ Set board defaults. Must be called before using any other board functions."""
-        return self.error_parse(pb_set_defaults())
-        
+        """ Set board defaults. Must be called before using any other board 
+        functions."""
+        return api.pb_set_defaults()
+
     def set_core_clock(self, clock):
         """ Sets the core clock reference value
         
-        Note: This does not change anything in the device, it just provides the library
-              with the value given
+        Note: This does not change anything in the device, it just provides 
+        the library with the value given
         """
-        pb_core_clock(clock)
-        
+        api.pb_core_clock(clock)
+
+    @error_parse
     def write_register(self, address, value):
         """ Write to one of two core registers in the DDS
         
@@ -174,38 +189,41 @@ class PB_DDS(Instrument):
             address (int) : the address of the register
             value   (int) : the value to be written to the register
         """
-        return self.error_parse(pb_write_register(address, value))
+        return api.pb_write_register(address, value)
 
     ###########################################################################
     ###                        DDS Control commands                         ###
     ###########################################################################
 
+    @error_parse
     def start(self):
-        return self.error_parse(pb_start())
+        return api.pb_start()
 
+    @error_parse
     def reset(self):
-        return self.error_parse(pb_reset())
+        return api.pb_reset()
 
+    @error_parse
     def stop(self):
-        return self.error_parse(pb_stop())
+        return api.pb_stop()
 
+    @error_parse
     def close(self):
-        return self.error_parse(pb_close())
+        return api.pb_close()
 
-    def add_inst(self, inst, channel):
-        self.__chan_instructions[channel].append(inst)
+    def add_instruction(self, inst, channel):
+        self.instructions[channel].append(inst)
 
-    def get_inst_list(self, channel):
-        return self.__chan_instructions[channel]
-
+    @error_parse
     def start_programming(self, mode):
         """ Start a programming sequence 
         
         Args:
             mode    (int) : one of (PULSE_PROGRAM, FREQ_REGS, etc.)
         """
-        return self.error_parse(pb_start_programming(mode))
+        return api.pb_start_programming(mode)
 
+    @error_parse
     def inst_dds2(self, inst):
         """ During start_programming(PULSE_PROGRAM) 
             add an unshaped sine pulse to program
@@ -215,8 +233,9 @@ class PB_DDS(Instrument):
                             (FREQ0, PHASE0, ...)
         """
         inst = inst[:-1] + (inst[-1] * PB_KEYWORDS.s,)
-        return self.error_parse(pb_inst_dds2(*inst))
+        return api.pb_inst_dds2(*inst)
 
+    @error_parse
     def inst_dds2_shape(self, inst):
         """ During start_programming(PULSE_PROGRAM) 
             add a shaped pulse to program, if desired
@@ -226,8 +245,9 @@ class PB_DDS(Instrument):
                             (FREQ0, PHASE0, ...)
         """
         inst = inst[:-1] + (inst[-1] * PB_KEYWORDS.s,)
-        return self.error_parse(pb_inst_dds2_shape(*inst))
+        return api.pb_inst_dds2_shape(*inst)
 
+    @error_parse
     def dds_load(self, data, device):
         """ Load a 1024 point waveform into one of two devices
 
@@ -237,12 +257,16 @@ class PB_DDS(Instrument):
             device (int) : Either DEVICE_DDS (the default shape for all output)
                                or DEVICE_SHAPE (an alternative shape register)
         """
-        return self.error_parse(pb_dds_load(), device)
+        return api.pb_dds_load(device)
 
+    @error_parse
     def stop_programming(self):
         """ End a programming sequence """
-        return self.error_parse(pb_stop_programming())
+        return api.pb_stop_programming()
 
+    @error_parse
+    def select_dds(self, channel):
+        return api.pb_select_dds(channel)
 
     def program_pulse_sequence(self, pulse_sequence):
         """ An all-in-one function to send a pulse sequence to the board
@@ -252,14 +276,13 @@ class PB_DDS(Instrument):
                                     the instructions should be tuples i.e.
                                     (FREQ0, PHASE0, ...)
         """
-        pb_start_programming(PB_KEYWORDS.PULSE_PROGRAM)
+        self.start_programming(PB_KEYWORDS.PULSE_PROGRAM)
         for p in pulse_sequence:
             # convert last element from seconds to ns
             p = p[:-1] + (p[-1]*PB_KEYWORDS.s,)
             # * breaks tuple into args, 
-            pb_inst_dds2(*p)
-        pb_stop_programming()
-    
+            self.inst_dds2(*p)
+        self.stop_programming()
 
     def program_inst(self):
         """ Send the current instruction list to the board
@@ -269,39 +292,41 @@ class PB_DDS(Instrument):
         """
         raise NotImplementedError('Warning: Unimplemented function')
         pass
-        #pb_start_programming()
-        #n_inst = 0
-        ## Find the longest set of instructions defined
-        #for n in range(self.N_CHANNELS):
+        # pb_start_programming()
+        # n_inst = 0
+        # # Find the longest set of instructions defined
+        # for n in range(self.N_CHANNELS):
         #    n_inst = max(n_inst, len(self.__chan_instructions[n]))
-        #for i in range(n_inst):
+        # for i in range(n_inst):
         #    # TODO: use lambdas here for ease
         #    #if (self.__chan_instructions[
         #    pass
-        #pb_stop_programming()
+        # pb_stop_programming()
 
+    @error_parse
     def set_shape_defaults(self, channel):
         """ Resets the shape for the specified channel
         
         Args:
             channel      (int) : Either DDS0 (0) or DDS1 (1)
         """
-        pb_select_dds(channel)
-        return self.error_parse(pb_set_shape_defaults())
+        self.select_dds(channel)
+        return api.pb_set_shape_defaults()
 
     def load_waveform(self, data, device, channel):
         """ Loads a waveform onto the DDS
 
         Args:
-            data       (float) : an array of 1024 points representing a single 
+            data         (arr) : an array of 1024 points representing a single 
                                  period of the waveform. Points range between 
                                  -1.0 and 1.0
             device       (int) : Either DEVICE_DDS or DEVICE_SHAPE
             channel      (int) : Either DDS0 (0) or DDS1 (1)
         """
-        pb_select_dds(channel)
-        return self.error_parse(pb_dds_load(data, device))
+        self.select_dds(channel)
+        return self.dds_load(data, device)
 
+    @error_parse
     def set_envelope_freq(self, freq, register, channel):
         """ Sets the frequency for an envelope register
         
@@ -312,8 +337,8 @@ class PB_DDS(Instrument):
         """
         # Scale the frequency to Hertz, as the underlying api assumes MHz
         freq *= PB_KEYWORDS.Hz 
-        pb_select_dds(channel)
-        return self.error_parse(pb_dds_set_envelope_freq(freq, register))
+        self.select_dds(channel)
+        return api.pb_dds_set_envelope_freq(freq, register)
 
     ###########################################################################
     ###                  Set/Get commands for parameters                    ###
@@ -329,11 +354,11 @@ class PB_DDS(Instrument):
         """
         # Scale the frequency to Hertz, as the underlying api assumes MHz
         frequency *= PB_KEYWORDS.Hz 
-        self.__frequency[channel][register] = frequency
-        pb_select_dds(channel)
+        self._frequency[channel][register] = frequency
+        self.select_dds(channel)
         self.start_programming(PB_KEYWORDS.FREQ_REGS)
         for r in range(self.N_FREQ_REGS):
-            self.error_parse(pb_set_freq(self.__frequency[channel][r]))
+            error_parse(api.pb_set_freq(self._frequency[channel][r]))
         self.stop_programming()
 
     def set_phase_register(self, phase, channel, register):
@@ -344,13 +369,14 @@ class PB_DDS(Instrument):
             register     (int) : the register number
             channel      (int) : Either DDS0 (0) or DDS1 (1)
         """
-        self.__phase[channel][register] = phase
-        pb_select_dds(channel)
+        self._phase[channel][register] = phase
+        self.select_dds(channel)
         self.start_programming(PB_KEYWORDS.TX_PHASE_REGS)
         for r in range(self.N_PHASE_REGS):
-            self.error_parse(pb_set_phase(self.__phase[channel][register]))
+            error_parse(api.pb_set_phase(self._phase[channel][register]))
         self.stop_programming()
 
+    @error_parse
     def set_amplitude_register(self, amplitude, channel, register):
         """ Sets the DDS amplitude for the specified channel and register
         
@@ -359,6 +385,6 @@ class PB_DDS(Instrument):
             register     (int) : the register number
             channel      (int) : Either DDS0 (0) or DDS1 (1)
         """
-        pb_select_dds(channel)
-        return self.error_parse(pb_set_amp(amplitude, register))
+        self.select_dds(channel)
+        return api.pb_set_amp(amplitude, register)
 
