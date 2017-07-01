ceedfaadee91060cf335adf97f677aa608f4a011
Giulio Ungaretti <giulioungaretti@me.com>
2017-07-01 19:06:52 +0200

    fix: properly escape and pass table_name to sql

--- new_exp.py
+++ new_exp.py
@@ -248,9 +248,9 @@ def insert_meta_data(conn: sqlite3.Connection, row_id: int, table_name: str,
         - metadata: the metadata to add
     """
     for key, value in metadata.items():
-        insert_column(conn, "runs", key)
+        insert_column(conn, '{table_name}', key)
         sql = f"""
-            UPDATE {table_name} set '{key}'=? WHERE rowid=?;
+            UPDATE '{table_name}' set '{key}'=? WHERE rowid=?;
         """
         transaction(conn, sql, value, row_id)
 
