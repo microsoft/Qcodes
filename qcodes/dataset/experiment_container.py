5698f2e227f0aa659ff61605c9a2a2f8ba1fbf17
Giulio Ungaretti <giulioungaretti@me.com>
2017-07-11 15:47:17 +0200

    fix: Remove leftover todo

--- qcodes/dataset/experiment_container.py
+++ qcodes/dataset/experiment_container.py
@@ -13,8 +13,6 @@ DB = qcodes.config["core"]["db_location"]
 
 class Experiment(Sized):
     def __init__(self, path_to_db: str) -> None:
-        # TODO: handle fail here by defaulting to
-        # a standard db
         self.path_to_db = path_to_db
         self.conn = connect(self.path_to_db)
         self._debug = False
@@ -138,7 +136,7 @@ def new_experiment(name: str,
                     sample_name: str,
                     format_string: Optional[str] = "{}-{}-{}") -> Experiment:
     """ Create a new experiment
-    
+
     Args:
         name: the name of the experiment
         sample_name: the name of the current sample
