8746109e7e2855cd92fe867226cd558dbe9f9a91
nataliejpg <npearson@phys.ethz.ch>
2017-09-01 17:52:31 +0200

    make show_num_do plotting optional

--- qcodes/utils/natalie_wrappers/show_num.py
+++ qcodes/utils/natalie_wrappers/show_num.py
@@ -4,7 +4,7 @@ from qcodes.plots.pyqtgraph import QtPlot
 from qcodes.plots.qcmatplotlib import MatPlot
 
 
-def show_num(id, useQT=False):
+def show_num(id, useQT=False, do_plot=True):
     """
     Show  and return plot and data for id in current instrument.
     Args:
@@ -21,22 +21,25 @@ def show_num(id, useQT=False):
     t = qc.DataSet.location_provider.fmt.format(counter=str_id)
     data = qc.load_data(t)
 
-    plots = []
-    for value in data.arrays.keys():
-        if "set" not in value:
-            if useQT:
-                plot = QtPlot(
-                    getattr(data, value),
-                    fig_x_position=CURRENT_EXPERIMENT['plot_x_position'])
-                title = "{} #{}".format(CURRENT_EXPERIMENT["sample_name"],
-                                        str_id)
-                plot.subplots[0].setTitle(title)
-                plot.subplots[0].showGrid(True, True)
-            else:
-                plot = MatPlot(getattr(data, value))
-                title = "{} #{}".format(CURRENT_EXPERIMENT["sample_name"],
-                                        str_id)
-                plot.subplots[0].set_title(title)
-                plot.subplots[0].grid()
-            plots.append(plot)
+    if do_plot:
+        plots = []
+        for value in data.arrays.keys():
+            if "set" not in value:
+                if useQT:
+                    plot = QtPlot(
+                        getattr(data, value),
+                        fig_x_position=CURRENT_EXPERIMENT['plot_x_position'])
+                    title = "{} #{}".format(CURRENT_EXPERIMENT["sample_name"],
+                                            str_id)
+                    plot.subplots[0].setTitle(title)
+                    plot.subplots[0].showGrid(True, True)
+                else:
+                    plot = MatPlot(getattr(data, value))
+                    title = "{} #{}".format(CURRENT_EXPERIMENT["sample_name"],
+                                            str_id)
+                    plot.subplots[0].set_title(title)
+                    plot.subplots[0].grid()
+                plots.append(plot)
+    else:
+        plots = None
     return data, plots
