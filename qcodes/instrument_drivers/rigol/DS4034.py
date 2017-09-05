fdd607b6534616d9dc882b3600f1e45eb614de2f
sohailc <a-sochat@microsoft.com>
2017-09-05 17:36:07 +0200

    Made a get_wave_form function which also returns times.

--- qcodes/instrument_drivers/rigol/DS4034.py
+++ qcodes/instrument_drivers/rigol/DS4034.py
@@ -1,23 +1,9 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 
-from qcodes import VisaInstrument, ArrayParameter
-
-
-class WaveForm(ArrayParameter):
-    def __init__(self, name, shape, instrument, **kwargs):
-        super().__init__(name, shape, **kwargs)
-        self._instrument = instrument
-
-    def get(self):
-        """
-        Get a wave form reading from the scope
-        """
-        data = self._instrument.visa_handle.query_ascii_values(":wav:data?", converter="s")
-        # We want to get the data as ascii strings. Leaving query_ascii_values with a default converter=f will make
-        # it prone to exceptions as the internal string to float function is not smart enough to deal with empty
-        # strings.
-        return [float(d) for d in data if d != ""]
+from qcodes import VisaInstrument
+from qcodes.utils.validators import Ints
 
 
 class Rigol_DS4035(VisaInstrument):
@@ -36,23 +22,51 @@ class Rigol_DS4035(VisaInstrument):
                 'bin': 'binary'}
         )
 
-    def get_wave_form(self):
-        self.data_format("asc")  # Make sure we get outputs in ascii format
-        return self.visa_handle.query_ascii_values(":wav:data?", converter="s")  # We want to get the data as ascii
-        # strings. Leaving query_ascii_values with a default converter=f will make it prone to exceptions as the
-        # internal string to float function is not smart enough to deal with empty strings.
+        self.add_parameter(
+            "time_base",
+            get_cmd=":tim:scal?",
+            get_parser=float,
+            set_cmd=":tim:scal {}",
+            unit="s/sample"
+        )
+
+        self.add_parameter(
+            "sample_point_count",
+            get_cmd=":wav:poin?",
+            get_parser=int,
+            set_cmd=":wav:poin {}",
+            vals=Ints(min_value=0, max_value=1400)
+        )
+
+        self.add_function(
+            "get_wave_form",
+            call_cmd=self._get_wave_form,
+            args=[Ints(min_value=0, max_value=1400)]
+        )
+
+    def _get_wave_form(self, n_samples):
+
+        self.sample_point_count(n_samples)
+        data = self.visa_handle.query_ascii_values(":wav:data?", converter="s")
+        # We want to get the data as ascii strings. Leaving query_ascii_values with a default converter=f will make
+        # it prone to exceptions as the internal string to float function is not smart enough to deal with empty
+        # strings.
+        data = [float(d) for d in data if d != ""]
+        times = self.time_base() * np.arange(0, len(data))
+
+        return np.vstack([times, data])
 
 
 def main():
 
     visa_address = "TCPIP::169.254.180.17::INSTR"
     dev = Rigol_DS4035("rigol_scope", visa_address)
-    data = [float(d) for d in dev.get_wave_form() if d != ""]
+
+    wave = dev.get_wave_form(1400)
 
     fig, ax = plt.subplots()
-    ax.plot(data)
+    ax.plot(*wave)
     plt.show()
-    pass
 
 if __name__ == "__main__":
     main()
