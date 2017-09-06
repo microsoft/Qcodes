45b9fde39cba506c9d4ecec2f6c8b717034b3634
sohailc <a-sochat@microsoft.com>
2017-09-06 15:23:54 +0200

    added parameters to measure amplitude

--- qcodes/instrument_drivers/rigol/DS4034.py
+++ qcodes/instrument_drivers/rigol/DS4034.py
@@ -1,3 +1,11 @@
+"""
+The QCodes driver of the oscilloscope Rigol DS4034. Please see
+
+http://int.rigol.com/File/TechDoc/20160831/DS4000E_ProgrammingGuide_EN.pdf
+
+for the programming manual for this device
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -38,20 +46,53 @@ class Rigol_DS4035(VisaInstrument):
             vals=Ints(min_value=0, max_value=1400)
         )
 
+        self.add_parameter(
+            "source_channel",
+            set_cmd="WAV:SOUR CHAN{}",
+            vals=Ints(min_value=0, max_value=4)
+        )
+
+        self.add_parameter(
+            "mode",
+            set_cmd=":wav:mode {}",
+            val_mapping={
+                'normal': 'norm'}
+        )
+
+        for channel_number in range(1, 5):  # It is really unfortunate that a get parameter does not except
+            # arguments.
+            # TODO: Improve the design of the Parameter class
+            self.add_parameter(
+                "measure_amplitude_channel{}".format(channel_number),
+                get_cmd=":meas:vamp? chan{}".format(channel_number)
+            )
+
+            self.add_parameter(
+                "vertical_scale_channel{}".format(channel_number),
+                get_cmd="chan{}:scale?".format(channel_number),
+                set_cmd="chan{}:scale ".format(channel_number) + "{}",
+                get_parser=float
+            )
+
         self.add_function(
             "get_wave_form",
             call_cmd=self._get_wave_form,
-            args=[Ints(min_value=0, max_value=1400)]
+            args=[Ints(min_value=0, max_value=1400), Ints(min_value=0, max_value=4)]
         )
 
-    def _get_wave_form(self, n_samples):
+    def _get_wave_form(self, n_samples, channel):
 
+        self.source_channel(channel)
+        self.mode("normal")
+        self.data_format("asc")
         self.sample_point_count(n_samples)
+
         data = self.visa_handle.query_ascii_values(":wav:data?", converter="s")
         # We want to get the data as ascii strings. Leaving query_ascii_values with a default converter=f will make
         # it prone to exceptions as the internal string to float function is not smart enough to deal with empty
         # strings.
         data = [float(d) for d in data if d != ""]
+
         times = self.time_base() * np.arange(0, len(data))
 
         return np.vstack([times, data])
@@ -62,7 +103,10 @@ def main():
     visa_address = "TCPIP::169.254.180.17::INSTR"
     dev = Rigol_DS4035("rigol_scope", visa_address)
 
-    wave = dev.get_wave_form(1400)
+    amp = dev.measure_amplitude_channel2()
+    print("The amplitude of the signal is {}".format(amp))
+
+    wave = dev.get_wave_form(1400, 2)
 
     fig, ax = plt.subplots()
     ax.plot(*wave)
