e9776fc0c5b05fc059ef1ecccb0bff8f1d45fef3
nulinspiratie <serwan.asaad@gmail.com>
2017-09-06 13:51:27 +1000

    fix: cleanup iPython check

--- qcodes/__init__.py
+++ qcodes/__init__.py
@@ -61,12 +61,8 @@ from qcodes.utils import validators
 
 from qcodes.instrument_drivers.test import test_instruments, test_instrument
 
-try:
-    if __IPYTHON__: # Test if we are in iPython
-        from qcodes.utils.magic import register_magic_class
-        register_magic = config.core.get('register_magic', False)
-        if register_magic is not False:
-            register_magic_class(magic_commands=register_magic)
-except NameError:
-    # Not in iPython
-    pass
+if hasattr(__builtins__, '__IPYTHON__'):
+    from qcodes.utils.magic import register_magic_class
+    register_magic = config.core.get('register_magic', False)
+    if register_magic is not False:
+        register_magic_class(magic_commands=register_magic)
