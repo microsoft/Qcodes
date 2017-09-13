b67949b372bfbf4211e82e8bf2f47b1fe5c17d99
WilliamHPNielsen <william@nbi.ku.dk>
2017-09-13 15:54:40 +0200

    feat: Channelise the four channels

--- qcodes/instrument_drivers/rohde_schwarz/RTO1024.py
+++ qcodes/instrument_drivers/rohde_schwarz/RTO1024.py
@@ -1,9 +1,129 @@
 
 import numpy as np
 from time import sleep
-from qcodes.instrument import visa
+from qcodes.instrument import visa, Instrument
+from qcodes.instrument.channel import InstrumentChannel, ChannelList
 from qcodes.utils import validators as vals
 from qcodes.instrument.parameter import ManualParameter
+
+
+class ScopeChannel(InstrumentChannel):
+    """
+    Class to hold an input channel of the scope.
+
+    Exposes: state, coupling, ground, scale, range, position, offset,
+    invert, bandwidth, impedance, overload
+    """
+
+    def __init__(self, parent: Instrument, name: str, channum: int) -> None:
+        """
+        Args:
+            parent: The instrument to which the channel is attached
+            name: The name of the channel
+            channum: The number of the channel in question. Must match the
+                actual number as used by the instrument (1..4)
+        """
+
+        if channum not in [1, 2, 3, 4]:
+            raise ValueError('Invalid channel number! Must be 1, 2, 3, or 4.')
+
+        self.channum = channum
+
+        super().__init__(parent, name)
+
+        # Add the parameters
+        # Note (WilliamHPNielsen): I just slavishly typed in pp 1176-1180
+        # of the manual
+        self.add_parameter('state',
+                           label='Channel {} state'.format(channum),
+                           get_cmd='CHANnel{}:STATe?'.format(channum),
+                           set_cmd='CHANnel{}:STATE {{}}'.format(channum),
+                           vals=vals.Enum('ON', 'OFF'),
+                           docstring='Switches the channel on or off')
+
+        self.add_parameter('coupling',
+                           label='Channel {} coupling'.format(channum),
+                           get_cmd='CHANnel{}:COUPling?'.format(channum),
+                           set_cmd='CHANnel{}:COUPling {{}}'.format(channum),
+                           vals=vals.Enum('DC', 'DCLimit', 'AC'),
+                           docstring=('Selects the connection of the channel'
+                                      'signal. DC: 50 Ohm, DCLimit 1 MOhm, '
+                                      'AC: Con. through DC capacitor'))
+
+        self.add_parameter('ground',
+                           label='Channel {} ground'.format(channum),
+                           get_cmd='CHANnel{}:GND?'.format(channum),
+                           set_cmd='CHANnel{}:GND {{}}'.format(channum),
+                           vals=vals.Enum('ON', 'OFF'),
+                           docstring=('Connects/disconnects the signal to/from'
+                                      'the ground.'))
+
+        # NOTE (WilliamHPNielsen): This parameter depends on other parameters and
+        # should be dynamically updated accordingly. Cf. p 1178 of the manual
+        self.add_parameter('scale',
+                           label='Channel {} Y scale'.format(channum),
+                           unit='V/div'
+                           get_cmd='CHANnel{}:SCALe?'.format(channum),
+                           set_cmd='CHANnel{}:SCALe {{}}'.format(channum),
+                           )
+
+        # NOTE (WilliamHPNielsen):
+        # This parameter competes with the `scale` parameter, and they should
+        # be interlinked
+        self.add_parameter('range',
+                           label='Channel {} Y range'.format(channum),
+                           unit='V/div',
+                           get_cmd='CHANnel{}:RANGe?'.format(channum),
+                           set_cmd='CHANnel{}:RANGe {{}}'.format(channum),
+                           )
+
+        # TODO (WilliamHPNielsen): would it be better to recast this in terms
+        # of Volts?
+        self.add_parameter('position',
+                           label='Channel {} vert. pos.'.format(channum),
+                           unit='div',
+                           get_cmd='CHANnel{}:POSition?'.format(channum),
+                           set_cmd='CHANnel{}:POSition {{}}'.format(channum),
+                           vals=vals.Numbers(-5, 5),
+                           docstring=('Positive values move the waveform up,'
+                                      ' negative values move it down.'))
+
+        self.add_parameter('offset',
+                           label='Channel {} offset'.format(channum),
+                           unit='V',
+                           get_cmd='CHANnel{}:OFFSet?'.format(channum),
+                           set_cmd='CHANnel{}:OFFSet {{}}'.format(channum),
+                           )
+
+        self.add_parameter('invert',
+                           label='Channel {} inverted'.format(channum),
+                           get_cmd='CHANnel{}:INVert?'.format(channum),
+                           set_cmd='CHANnel{}:INVert {{}}'.format(channum),
+                           vals=vals.Enum('ON', 'OFF'))
+
+        # TODO (WilliamHPNielsen): This parameter should be dynamically
+        # validated since 800 MHz BW is only available for 50 Ohm coupling
+        self.add_parameter('bandwidth',
+                           label='Channel {} bandwidth'.format(channum),
+                           get_cmd='CHANnel{}:BANDwidth?'.format(channum),
+                           set_cmd='CHANnel{}:BANDwidth {{}}'.format(channum),
+                           vals=vals.Enum('FULL', 'B800', 'B200', 'B20')
+                           )
+
+        self.add_parameter('impedance',
+                           label='Channel {} impedance'.format(channum),
+                           unit='Ohm',
+                           get_cmd='CHANnel{}:IMPedance?'.format(channum),
+                           set_cmd='CHANnel{}:IMPedance {{}}'.format(channum),
+                           vals=vals.Ints(1, 100000),
+                           docstring=('Sets the impedance of the channel '
+                                      'for power calculations and '
+                                      'measurements.'))
+
+        self.add_parameter('overload',
+                           label='Channel {} overload'.format(channum),
+                           get_cmd='CHANnel{}:OVERload?'.format(channum)
+                           )
 
 
 class RTO1024_scope_test(visa.VisaInstrument):
@@ -85,25 +205,6 @@ class RTO1024_scope_test(visa.VisaInstrument):
                            vals=vals.Numbers(2, 20e12),
                            get_parser=float)
 
-        # needs get set methods as channel no. needs to be satisfied
-        self.add_parameter('y_range',
-                           unit='Volt/div',
-                           get_cmd='CHANnel1:RANGe'+'?',
-                           set_cmd=self.set_y_range,
-                           docstring='voltage range 10 vertical divisions',
-                           # todo:something fishy  here
-                           vals=vals.Numbers(.01, 10),
-                           get_parser=float)
-
-        self.add_parameter('y_scale',
-                           label='CH 1 y scale',
-                           unit='Volt/div',
-                           get_cmd='CHANnel1:SCALe'+'?',
-                           set_cmd=self.set_y_scale,
-                           docstring='Scale value',
-                           vals=vals.Numbers(.001, 1),
-                           get_parser=float)
-
         self.add_parameter('t_start',
                            label='Waveform start time',
                            unit='s',
@@ -122,6 +223,17 @@ class RTO1024_scope_test(visa.VisaInstrument):
                            vals=vals.Numbers(-100E+24, 100E+24),
                            get_parser=float)
 
+        # Add the channels to the instrument
+
+        channels = ChannelList(self, 'channels', ScopeChannel)
+
+        for ch in range(1, 5):
+            chan = ScopeChannel(self, 'channel{}'.format(ch), ch)
+            channels.append(chan)
+            self.add_submodule('ch{}'.format(ch), chan)
+        channels.lock()
+        self.add_submodule('channels', channels)
+
         self.add_function('reset', call_cmd='*RST')
         self.add_function('opc', call_cmd='*OPC?')
         self.add_function('stop_opc', call_cmd='*STOP;OPC?')
@@ -135,6 +247,9 @@ class RTO1024_scope_test(visa.VisaInstrument):
 
         # this function is for quick debugging, will be removed in the beta
         # version
+
+        # NOTE (WilliamHPNielsen): I broke this function with the
+        # channelisation
 
         self.write('*RST')
         self.time_step(1E-8)
