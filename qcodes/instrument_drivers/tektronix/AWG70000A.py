e9315fa4081711f6b777681fd35cebd823bd872f
WilliamHPNielsen <william@nbi.ku.dk>
2017-09-19 13:58:37 +0200

    refactor: Make AWG70002A subclass of AWG70001A

--- qcodes/instrument_drivers/tektronix/AWG70000A.py
+++ qcodes/instrument_drivers/tektronix/AWG70000A.py
@@ -1,4 +1,4 @@
-from qcodes import VisaInstrument, validators as vals
+from qcodes import Instrument, VisaInstrument, validators as vals
 from qcodes.instrument.channel import ChannelList, InstrumentChannel
 
 
@@ -7,26 +7,39 @@ class AWGChannel(InstrumentChannel):
     Class to hold a channel of the AWG.
     """
 
-    def __init(args):
-        passg
+    def __init__(self,  parent: Instrument, name: str, channel: int) -> None:
+        """
+        Args:
+            parent: The Instrument instance to which the channel is
+                to be attached.
+            name: The name used in the DataSet
+            channel: The channel number, either 1 or 2.
+        """
 
+        super().__init__(parent, name)
+
+        num_channels = self._parent.num_channels
+
+        if channel not in list(range(1, num_channels+1)):
+            raise ValueError('Illegal channel value.')
 
 
 class AWG70000A(VisaInstrument):
     """
     The QCoDeS driver for Tektronix AWG70000A series AWG's.
 
-    So far only tested with AWG70002A, but the AWG70001A should be
-    very similar [citation needed].
+    The drivers for AWG70001A and AWG70002A should be subclasses of this
+    general class.
     """
 
-    def __init__(self, name: str, address: str, timeout: float=10,
-                 **kwargs) -> None:
+    def __init__(self, name: str, address: str, num_channels: int,
+                 timeout: float=10, **kwargs) -> None:
         """
         Args:
             name: The name used internally by QCoDeS in the DataSet
             address: The VISA resource name of the instrument
             timeout: The VISA timeout time (in seconds).
+
         """
 
         super().__init__(name, address, timeout=timeout, **kwargs)
