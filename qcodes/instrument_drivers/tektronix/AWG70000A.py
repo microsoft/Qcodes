f4abe10e66b66e4a4b19025d16080e85592fd930
WilliamHPNielsen <william@nbi.ku.dk>
2017-09-20 16:27:08 +0200

    Add loadWFMXfile function

--- qcodes/instrument_drivers/tektronix/AWG70000A.py
+++ qcodes/instrument_drivers/tektronix/AWG70000A.py
@@ -81,16 +81,27 @@ class AWG70000A(VisaInstrument):
 
         super().__init__(name, address, timeout=timeout, **kwargs)
 
+        self.add_parameter('current_directory',
+                           label='Current file system directory',
+                           set_cmd='MMEMory:CDIRectory "{}"',
+                           get_cmd='MMEMory:CDIRectory?',
+                           vals=vals.Strings())
+
         for ch_num in range(1, num_channels+1):
             ch_name = 'ch{}'.format(ch_num)
             channel = AWGChannel(self, ch_name, ch_num)
             self.add_submodule(ch_name, channel)
 
+        # Folder on the AWG where to files are uplaoded by default
+        self.wfmxFileFolder = "\\Users\\OEM\\Documents"
+
+        self.current_directory(self.wfmxFileFolder)
+
         self.connect_message()
 
     # Idea: two low-level parsers, one for the header and one for the data.
 
-    def makeWFXFile(self, data: np.ndarray) -> bytes:
+    def makeWFMXFile(self, data: np.ndarray) -> bytes:
         """
         Compose a WFMX file
 
@@ -98,20 +109,73 @@ class AWG70000A(VisaInstrument):
             data: A numpy array holding the data. Markers can be included.
         """
 
-        N = np.shape(data)[1]
-        layers = np.shape(data)[0]
-
-        if layers > 1:
+        shape = np.shape(data)
+        if len(shape) == 1:
+            N = shape[0]
+            markers_included = False
+        elif len(shape) == 2:
+            N = shape[1]
             markers_included = True
+        else:
+            raise ValueError('Input data has too many dimensions!')
 
         wfmx_hdr = self._makeWFMXFileHeader(num_samples=N,
                                             markers_included=markers_included)
         wfmx_hdr = bytes(wfmx_hdr, 'utf8')
         wfmx_data = self._makeWFMXFileBinaryData(data)
 
-        wfmx = wfmx_hdr + wfmx_data
+        wfmx = wfmx_hdr  # + wfmx_data
 
         return wfmx
+
+    def sendWFMXFile(self, wfmx: bytes, filename: str,
+                     path: str=None) -> None:
+        """
+        Send a binary wfmx file to the AWG's memory
+
+        Args:
+            wfmx: The binary wfmx file, preferably the output of
+                makeWFMXFile.
+            filename: The name of the file on the AWG disk
+            path: The path to the directory where the file should be saved. If
+        """
+
+        name_str = 'MMEMory:DATA "{}"'.format(filename).encode('utf8')
+        len_file = len(wfmx)
+        len_str = len(str(len_file))  # No. of digits needed to write length
+        size_str = (',#{}{}'.format(len_str, len_file)).encode('utf8')
+
+        msg = name_str + size_str + wfmx
+
+        # IEEE 488.2 limit on a single write is 999,999,999 bytes
+        # TODO: If this happens, we should split the file
+        if len(msg) > 1e9-1:
+            raise ValueError('File too large to transfer')
+
+        if path:
+            self.current_directory(path)
+        else:
+            self.current_directory(self.wfmxFileFolder)
+
+        self.visa_handle.write_raw(msg)
+
+    def loadWFMXFile(self, filename: str, path: str=None) -> None:
+        """
+        Loads a wfmx from memory into the waveform list
+        Only loading from the C: drive is supported
+
+        Args:
+            filename: Name of the file (with extension)
+            path: Path to load from. If omitted, the default path
+                (self.wfmxFileFolder) is used.
+        """
+
+        if not path:
+            path = self.wfmxFileFolder
+
+        pathstr = 'C:' + path + '\\' + filename
+
+        self.write('MMEMory:OPEN "{}"'.format(pathstr))
 
     def _makeWFMXFileHeader(self,
                             num_samples: int,
@@ -147,7 +211,7 @@ class AWG70000A(VisaInstrument):
         datasets = ET.SubElement(_, 'DataSets', attrib={'version': '1'})
 
         # Description of the data
-        datadesc = ET.SubElement(datagsets, 'DataDescription')
+        datadesc = ET.SubElement(datasets, 'DataDescription')
         _ = ET.SubElement(datadesc, 'NumberSamples')
         _.text = '{:d}'.format(num_samples)
         _ = ET.SubElement(datadesc, 'SamplesType')
@@ -178,7 +242,7 @@ class AWG70000A(VisaInstrument):
 
         return xmlstr
 
-    def _makeWFMXFileBinaryData(self) -> bytes:
+    def _makeWFMXFileBinaryData(self, data: np.ndarray) -> bytes:
         """
         For the binary part
         """
