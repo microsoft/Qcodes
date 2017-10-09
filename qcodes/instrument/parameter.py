b52cacb6196a4e36221087ba25331995cbb704dd
Jens H Nielsen <j.h.nielsen@nbi.ku.dk>
2017-10-09 12:34:06 +0200

    set private variable in init
    This is how the official documentation does it and avoids a number of linter warnings

--- qcodes/instrument/parameter.py
+++ qcodes/instrument/parameter.py
@@ -174,11 +174,11 @@ class _BaseParameter(Metadatable, DeferredOperations):
             vals = Enum(*val_mapping.keys())
         self.vals = vals
 
-        self.step = step
+        self._step = step
         self.scale = scale
         self.raw_value = None
-        self.inter_delay = inter_delay
-        self.post_delay = post_delay
+        self._inter_delay = inter_delay
+        self._post_delay = post_delay
 
         self.val_mapping = val_mapping
         if val_mapping is None:
