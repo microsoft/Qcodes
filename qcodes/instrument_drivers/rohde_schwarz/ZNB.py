a96d38213727f63826b50e2313e7e6ffa784b4b9
Dominik Vogel <a-dovoge@microsoft.com>
2017-11-01 11:31:37 +0100

    removed T3 specific modifications, now in wrappers pr #1

--- qcodes/instrument_drivers/rohde_schwarz/ZNB.py
+++ qcodes/instrument_drivers/rohde_schwarz/ZNB.py
@@ -451,88 +451,9 @@ class ZNB(VisaInstrument):
     def add_channel(self, vna_parameter: str):
         n_channels = len(self.channels)
         channel = ZNBChannel(self, vna_parameter, n_channels + 1)
-        self.write(
-            'SOUR{}:FREQ1:CONV:ARB:IFR 1, 1, 0, SWE'.format(n_channels + 1))
-        self.write(
-            'SOUR{}:FREQ2:CONV:ARB:IFR 1, 1, 0, SWE'.format(n_channels + 1))
-        self.write('SOUR{}:POW1:OFFS 0, CPAD'.format(n_channels + 1))
-        self.write('SOUR{}:POW2:OFFS 0, CPAD'.format(n_channels + 1))
-        self.write('SOUR{}:POW1:PERM OFF'.format(n_channels + 1))
-        self.write('SOUR{}:POW:GEN1:PERM OFF'.format(n_channels + 1))
-        self.write('SOUR{}:POW:GEN1:STAT OFF'.format(n_channels + 1))
-        self.write('SOUR{}:POW2:STAT ON'.format(n_channels + 1))
         self.channels.append(channel)
         if n_channels == 0:
             self.display_single_window()
-
-    def count_external_generators(self):
-        num = self.ask('SYST:COMM:RDEV:GEN:COUN?').strip()
-        return int(num)
-
-    def set_external_generator(self, address, gen=1, gen_name="ext gen 1",
-                               driver="SGS100A", interface="VXI-11"):
-        self.write('SYST:COMM:RDEV:GEN{:.0f}:DEF "{}", "{}", "{}",  "{}", OFF, ON'.format(
-            gen, gen_name, driver, interface, address))
-
-    def get_external_generator_setup(self, num=1):
-        setup = self.ask(
-            'SYSTem:COMMunicate:RDEVice:GEN{:.0f}:DEF?'.format(num)).strip()
-        return setup
-
-    def clear_external_generator(self, num=1):
-        self.write('SYST:COMM:RDEV:GEN{:.0f}:DEL'.format(num))
-
-    def get_external_generator_numbers(self):
-        cat = self.ask('SYST:COMM:RDEV:GEN1:CAT?').strip()
-        return cat
-
-    def add_spectroscopy_channel(self, generator_address,
-                                 vna_parameter="B2G1SAM"):
-        """
-        Adds a generator and uses it to generate a fixed frequency tone, the
-        response at this frequency is read out at port 2 which is also set to
-        be fixed freq. Port 1 is set as the port for sweeping etc"""
-        self.set_external_generator(generator_address)
-        self.add_channel(vna_parameter)
-        chan_num = len(self.channels)
-        self.write('SOUR{}:POW2:STAT OFF'.format(chan_num))
-        time.sleep(0.2)
-        self.write('SOUR{}:POW:GEN1:PERM ON'.format(chan_num))
-        time.sleep(0.2)
-        self.write('SOUR{}:POW1:PERM ON'.format(chan_num))
-        time.sleep(0.2)
-        self.write('SOUR{}:POW:GEN1:STAT ON'.format(chan_num))
-        time.sleep(0.2)
-        self.write('ROSC EXT')
-        self.add_parameter(
-            'readout_freq',
-            set_cmd=partial(self._set_readout_freq, chan_num),
-            get_cmd=partial(self._get_readout_freq, chan_num),
-            get_parser=float,
-            vals=vals.Numbers(self._min_freq, self._max_freq))
-        self.add_parameter(
-            'readout_power',
-            set_cmd=partial(self._set_readout_pow, chan_num),
-            get_cmd=partial(self._get_readout_pow, chan_num),
-            get_parser=int,
-            vals=vals.Numbers(-150, 25))
-
-
-    def _set_readout_freq(self, chan_num, freq):
-        self.write(
-            'SOUR{}:FREQ:CONV:ARB:EFR1 ON, 0, 1, {:.6f}, CW'.format(chan_num, freq))
-        self.write(
-            'SOUR{}:FREQ2:CONV:ARB:IFR 0, 1, {:.6f}, CW'.format(chan_num, freq))
-
-    def _get_readout_freq(self, chan_num):
-        return self.ask('SOUR:FREQ:CONV:ARB:EFR1?').split(',')[3]
-
-    def _set_readout_pow(self, chan_num, pow):
-        self.write('SOUR{}:POW:GEN1:OFFS {:.3f}, ONLY'.format(chan_num, pow))
-        self.write('SOUR{}:POW2:OFFS {:.3f}, ONLY'.format(chan_num, pow))
-
-    def _get_readout_pow(self, chan_num):
-        return self.ask('SOUR{}:POW:GEN1:OFFS?'.format(chan_num)).split(',')[0]
 
     def _set_default_values(self):
         for channel in self.channels:
