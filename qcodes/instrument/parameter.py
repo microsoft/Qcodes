a590a965939a17114afe5391b506e937806841a2
FQT <serwan.asaad@gmail.com>
2017-11-06 16:54:08 +1100

    feat: add option to not wrap get/set

--- qcodes/instrument/parameter.py
+++ qcodes/instrument/parameter.py
@@ -100,6 +100,14 @@ class _BaseParameter(Metadatable, DeferredOperations):
         snapshot_value (Optional[bool]): False prevents parameter value to be
             stored in the snapshot. Useful if the value is large.
 
+        wrap_get (Optional[bool]): Wrap get method, adding features such as 
+            saving latest value, `val_mapping`, `get_parser`.  
+            Default True
+            
+        wrap_get (Optional[bool]): Wrap set method, adding features such as 
+            saving latest value, `val_mapping`, `set_parser`.  
+            Default True
+
         step (Optional[Union[int, float]]): max increment of parameter value.
             Larger changes are broken into multiple steps this size.
             When combined with delays, this acts as a ramp.
@@ -152,6 +160,8 @@ class _BaseParameter(Metadatable, DeferredOperations):
                  instrument: Optional['Instrument'],
                  snapshot_get: bool=True,
                  metadata: Optional[dict]=None,
+                 wrap_get: bool=True,
+                 wrap_set: bool=True,
                  step: Optional[Union[int, float]]=None,
                  scale: Optional[Union[int, float]]=None,
                  inter_delay: Union[int, float]=0,
@@ -201,16 +211,16 @@ class _BaseParameter(Metadatable, DeferredOperations):
         self._latest = {'value': None, 'ts': None, 'raw_value': None}
         self.get_latest = GetLatest(self, max_val_age=max_val_age)
 
-        if hasattr(self, 'get_raw'):
+        if hasattr(self, 'get_raw') and wrap_get:
             self.get = self._wrap_get(self.get_raw)
-        elif hasattr(self, 'get'):
+        elif hasattr(self, 'get') and wrap_get:
             warnings.warn('Wrapping get method, original get method will not '
                           'be directly accessible. It is recommended to '
                           'define get_raw in your subclass instead.' )
             self.get = self._wrap_get(self.get)
-        if hasattr(self, 'set_raw'):
+        if hasattr(self, 'set_raw') and wrap_set:
             self.set = self._wrap_set(self.set_raw)
-        elif hasattr(self, 'set'):
+        elif hasattr(self, 'set') and wrap_set:
             warnings.warn('Wrapping set method, original set method will not '
                           'be directly accessible. It is recommended to '
                           'define set_raw in your subclass instead.' )
