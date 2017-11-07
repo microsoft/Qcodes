8b258ada006cd35e8518a8ceb91f68b3b700eba5
William H.P. Nielsen <whpn@mailbox.org>
2017-11-07 10:26:46 +0100

    Validators/permissive multiples (#852)
    * add a PermissiveMultiples validator
    
    * Add a new validator for non-integer multiples + a test
    
    * Match call signatures
    
    * Now actually match them :)
    
    * Make Multiples and PermissiveMultiples numeric
    
    * add the promised precision test
    
    * add a __repr__ of PermissiveMultiples

--- qcodes/tests/test_validators.py
+++ qcodes/tests/test_validators.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from qcodes.utils.validators import (Validator, Anything, Bool, Strings,
                                      Numbers, Ints, PermissiveInts,
-                                     Enum, MultiType,
+                                     Enum, MultiType, PermissiveMultiples,
                                      Arrays, Multiples, Lists, Callable, Dict)
 
 
@@ -449,6 +449,43 @@ class TestMultiples(TestCase):
         n = Multiples(divisor=3, min_value=1, max_value=10)
         self.assertEqual(
             repr(n), '<Ints 1<=v<=10, Multiples of 3>')
+
+
+class TestPermissiveMultiples(TestCase):
+    divisors = [40e-9, -1, 0.2225, 1/3, np.pi/2]
+
+    multiples = [[800e-9, -40e-9, 0, 1],
+                 [3, -4, 0, -1, 1],
+                 [1.5575, -167.9875, 0],
+                 [2/3, 3, 1, 0, -5/3, 1e4],
+                 [np.pi, 5*np.pi/2, 0, -np.pi/2]]
+
+    not_multiples = [[801e-9, 4.002e-5],
+                     [1.5, 0.9999999],
+                     [0.2226],
+                     [0.6667, 28/9],
+                     [3*np.pi/4]]
+
+    def test_passing(self):
+        for divind, div in enumerate(self.divisors):
+            val = PermissiveMultiples(div)
+            for mult in self.multiples[divind]:
+                val.validate(mult)
+
+    def test_not_passing(self):
+        for divind, div in enumerate(self.divisors):
+            val = PermissiveMultiples(div)
+            for mult in self.not_multiples[divind]:
+                with self.assertRaises(ValueError):
+                    val.validate(mult)
+
+    # finally, a quick test that the precision is indeed setable
+    def test_precision(self):
+        pm_lax = PermissiveMultiples(35e-9, precision=3e-9)
+        pm_lax.validate(72e-9)
+        pm_strict = PermissiveMultiples(35e-9, precision=1e-10)
+        with self.assertRaises(ValueError):
+            pm_strict.validate(70.2e-9)
 
 
 class TestMultiType(TestCase):
