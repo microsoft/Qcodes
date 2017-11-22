7ca59bdb9f6183f0e636bcadd56d51aa73641298
nulinspiratie <serwan.asaad@gmail.com>
2017-11-22 13:46:01 +1100

    feat: add toc widget

--- qcodes/widgets/toc_widget/toc_widget.py
+++ qcodes/widgets/toc_widget/toc_widget.py
@@ -5,4 +5,7 @@ from traitlets import Unicode, List, Bool
 
 
 class TOCWidget(DOMWidget):
-    pass
+    _view_name = Unicode('TOCView').tag(sync=True)
+    _view_module = Unicode('toc').tag(sync=True)
+    _view_module_version = Unicode('0.1.0').tag(sync=True)
+    _widget_name = Unicode('none').tag(sync=True)
