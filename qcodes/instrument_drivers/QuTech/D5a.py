0d1cf5b71bfd3b25238782e292d17a120da4d907
layeoh <lareine.yeoh@gmail.com>
2017-11-28 16:45:15 +0100

    add update for D5mux

--- qcodes/instrument_drivers/QuTech/D5a.py
+++ qcodes/instrument_drivers/QuTech/D5a.py
@@ -27,7 +27,7 @@ class D5a(Instrument):
     """
 
     def __init__(self, name, spi_rack, module, inter_delay=0.1, dac_step=10e-3,
-                 reset_voltages=False, mV=False, **kwargs):
+                 reset_voltages=False, mV=False, number_dacs=16, **kwargs):
         """ Create instrument for the D5a module.
 
         The D5a module works with volts as units. For backward compatibility
@@ -45,12 +45,14 @@ class D5a(Instrument):
             dac_step (float): max step size (V or mV), passed to dac parameters of the object
             reset_voltages (bool): passed to D5a_module constructor
             mV (bool): if True, then use mV as units in the dac parameters
+            number_dacs (int): number of DACs available. This is 8 for the D5mux
         """
         super().__init__(name, **kwargs)
 
         self.d5a = D5a_module(spi_rack, module, reset_voltages=reset_voltages)
         self._mV = mV
-
+        self._number_dacs = number_dacs
+        
         self._span_set_map = {
             '4v uni': 0,
             '4v bi': 2,
@@ -68,7 +70,7 @@ class D5a(Instrument):
             self._gain = 1
             unit = 'V'
 
-        for i in range(16):
+        for i in range(self._number_dacs):
             validator = self._get_validator(i)
 
             self.add_parameter('dac{}'.format(i + 1),
@@ -90,7 +92,7 @@ class D5a(Instrument):
                                vals=Enum(*self._span_set_map.keys()))
 
     def _set_dacs_zero(self):
-        for i in range(16):
+        for i in range(self._number_dacs):
             self._set_dac(i, 0.0)
 
     def _set_dac(self, dac, value):
