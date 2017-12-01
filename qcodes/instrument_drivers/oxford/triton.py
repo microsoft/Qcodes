8b86be8d0ea0ff81f5ee9a35073f0ec58aef0bc1
Pieter Eendebak <pieter.eendebak@gmail.com>
2017-12-01 12:55:51 +0100

    add magnet control to oxford driver

--- qcodes/instrument_drivers/oxford/triton.py
+++ qcodes/instrument_drivers/oxford/triton.py
@@ -5,7 +5,9 @@ import logging
 from traceback import format_exc
 
 from qcodes import IPInstrument
-from qcodes.utils.validators import Enum, Ints
+from qcodes.utils.validators import Enum
+
+from time import sleep
 
 
 class Triton(IPInstrument):
@@ -13,10 +15,10 @@ class Triton(IPInstrument):
     Triton Driver
 
     Args:
-        tmpfile: Optional: an exported windows registry file from the registry
+        tmpfile: Expects an exported windows registry file from the registry
             path:
             `[HKEY_CURRENT_USER\Software\Oxford Instruments\Triton System Control\Thermometry]`
-            It is used to extract the names of temperature channels if set.
+            and is used to extract the available temperature channels.
 
 
     Status: beta-version.
@@ -31,8 +33,8 @@ class Triton(IPInstrument):
 
         self._heater_range_auto = False
         self._heater_range_temp = [0.03, 0.1, 0.3, 1, 12, 40]
-        self._heater_range_curr = [0.316, 1, 3.16, 10, 31.6, 100]
 
+        self._heater_range_curr = [0.316, 1, 3.16, 10, 31.6, 100]
         self._control_channel = 5
 
         self.add_parameter(name='time',
@@ -53,8 +55,7 @@ class Triton(IPInstrument):
         self.add_parameter(name='pid_control_channel',
                            label='PID control channel',
                            get_cmd=self._get_control_channel,
-                           set_cmd=self._set_control_channel,
-                           vals=Ints(1,16))
+                           set_cmd=self._set_control_channel)
 
         self.add_parameter(name='pid_mode',
                            label='PID Mode',
@@ -93,20 +94,80 @@ class Triton(IPInstrument):
                            set_cmd=partial(self._set_control_param, 'RANGE'),
                            vals=Enum(*self._heater_range_curr))
 
+        self.add_parameter(name='magnet_status',
+                           label='Magnet status',
+                           unit='',
+                           get_cmd=partial(self._get_control_B_param, 'ACTN'))
+
+        self.add_parameter(name='magnet_sweeprate',
+                           label='Magnet sweep rate',
+                           unit='T/min',
+                           get_cmd=partial(
+                               self._get_control_B_param, 'RVST:RATE'),
+                           set_cmd=partial(self._set_control_magnet_sweeprate_param))
+
+        self.add_parameter(name='magnet_sweeprate_insta',
+                           label='Instantaneous magnet sweep rate',
+                           unit='T/min',
+                           get_cmd=partial(self._get_control_B_param, 'RFST'))
+
+        self.add_parameter(name='B',
+                           label='Magnetic field',
+                           unit='T',
+                           get_cmd=partial(self._get_control_B_param, 'VECT'))
+
+        self.add_parameter(name='Bx',
+                           label='Magnetic field x-component',
+                           unit='T',
+                           get_cmd=partial(
+                               self._get_control_Bcomp_param, 'VECTBx'),
+                           set_cmd=partial(self._set_control_Bx_param))
+
+        self.add_parameter(name='By',
+                           label='Magnetic field y-component',
+                           unit='T',
+                           get_cmd=partial(
+                               self._get_control_Bcomp_param, 'VECTBy'),
+                           set_cmd=partial(self._set_control_By_param))
+
+        self.add_parameter(name='Bz',
+                           label='Magnetic field z-component',
+                           unit='T',
+                           get_cmd=partial(
+                               self._get_control_Bcomp_param, 'VECTBz'),
+                           set_cmd=partial(self._set_control_Bz_param))
+
+        self.add_parameter(name='magnet_sweep_time',
+                           label='Magnet sweep time',
+                           unit='T/min',
+                           get_cmd=partial(self._get_control_B_param, 'RVST:TIME'))
+
         self.chan_alias = {}
-        self.chan_temp_names = {}
+        self.chan_temps = {}
+
         if tmpfile is not None:
-            self._get_temp_channel_names(tmpfile)
-        self._get_temp_channels()
+            self._get_temp_channels(tmpfile)
         self._get_pressure_channels()
 
         try:
             self._get_named_channels()
         except:
-            logging.warning('Ignored an error in _get_named_channels\n' +
-                            format_exc())
+            logging.warn('Ignored an error in _get_named_channels\n' +
+                         format_exc())
 
         self.connect_message()
+
+    def set_B(self, x, y, z, s):
+        if 0 < s <= 0.2:
+            self.write('SET:SYS:VRM:COO:CART:RVST:MODE:RATE:RATE:' + str(s) +
+                       ':VSET:[' + str(x) + ' ' + str(y) + ' ' + str(z) + ']\r\n')
+            self.write('SET:SYS:VRM:ACTN:RTOS\r\n')
+            t_wait = self.magnet_sweep_time() * 60 + 10
+            print('Please wait ' + str(t_wait) +
+                  ' seconds for the field sweep...')
+            sleep(t_wait)
+        else:
+            print('Warning: set magnet sweep rate in range (0 , 0.2] T/min')
 
     def _get_response(self, msg):
         return msg.split(':')[-1]
@@ -115,6 +176,18 @@ class Triton(IPInstrument):
         msg = self._get_response(msg)
         if msg.endswith('NOT_FOUND'):
             return None
+        elif msg.endswith('IDLE'):
+            return 'IDLE'
+        elif msg.endswith('RTOS'):
+            return 'RTOS'
+        elif msg.endswith('Bx'):
+            return float(re.findall("[-+]?\d*\.\d+|\d+", msg)[0])
+        elif msg.endswith('By'):
+            return float(re.findall("[-+]?\d*\.\d+|\d+", msg)[1])
+        elif msg.endswith('Bz'):
+            return float(re.findall("[-+]?\d*\.\d+|\d+", msg)[2])
+        elif len(re.findall("[-+]?\d*\.\d+|\d+", msg)) > 1:
+            return [float(re.findall("[-+]?\d*\.\d+|\d+", msg)[0]), float(re.findall("[-+]?\d*\.\d+|\d+", msg)[1]), float(re.findall("[-+]?\d*\.\d+|\d+", msg)[2])]
         try:
             return float(re.findall("[-+]?\d*\.\d+|\d+", msg)[0])
         except Exception:
@@ -123,39 +196,87 @@ class Triton(IPInstrument):
     def get_idn(self):
         idstr = self.ask('*IDN?')
         idparts = [p.strip() for p in idstr.split(':', 4)][1:]
-
         return dict(zip(('vendor', 'model', 'serial', 'firmware'), idparts))
 
-    def _get_control_channel(self):
-
-        # verify current channel
-        if self._control_channel:
-            tempval = self.ask('READ:DEV:T{}:TEMP:LOOP:MODE'.format(self._control_channel))
-            if not tempval.endswith('NOT_FOUND'):
-                return self._control_channel
-
-        # either _control_channel is not set or wrong
-        for i in range(1,17):
-            tempval = self.ask('READ:DEV:T{}:TEMP:LOOP:MODE'.format(i))
-            if not tempval.endswith('NOT_FOUND'):
-                self._control_channel = i
-                break
-
+    def _get_control_channel(self, force_get=False):
+        if force_get or (not self._control_channel):
+            for i in range(20):
+                tempval = self.ask('READ:DEV:T%s:TEMP:LOOP:MODE' % (i))
+                if not tempval.endswith('NOT_FOUND'):
+                    self._control_channel = i
         return self._control_channel
 
     def _set_control_channel(self, channel):
         self._control_channel = channel
-        self.write('SET:DEV:T{}:TEMP:LOOP:HTR:H1'.format(channel))
+        self.write('SET:DEV:T%s:TEMP:LOOP:HTR:H1' %
+                   self._get_control_channel())
 
     def _get_control_param(self, param):
         chan = self._get_control_channel()
         cmd = 'READ:DEV:T{}:TEMP:LOOP:{}'.format(chan, param)
         return self._get_response_value(self.ask(cmd))
 
+    def _get_control_B_param(self, param):
+        cmd = 'READ:SYS:VRM:{}'.format(param)
+        return self._get_response_value(self.ask(cmd))
+
+    def _get_control_Bcomp_param(self, param):
+        cmd = 'READ:SYS:VRM:{}'.format(param)
+        return self._get_response_value(self.ask(cmd[:-2]) + cmd[-2:])
+
     def _set_control_param(self, param, value):
         chan = self._get_control_channel()
         cmd = 'SET:DEV:T{}:TEMP:LOOP:{}:{}'.format(chan, param, value)
         self.write(cmd)
+
+    def _set_control_magnet_sweeprate_param(self, s):
+        if 0 < s <= 0.2:
+            x = round(self.Bx(), 4)
+            y = round(self.By(), 4)
+            z = round(self.Bz(), 4)
+            self.write('SET:SYS:VRM:COO:CART:RVST:MODE:RATE:RATE:' + str(s) +
+                       ':VSET:[' + str(x) + ' ' + str(y) + ' ' + str(z) + ']\r\n')
+        else:
+            print('Warning: set sweeprate in range (0 , 0.2] T/min')
+
+    def _set_control_Bx_param(self, x):
+        s = self.magnet_sweeprate()
+        y = round(self.By(), 4)
+        z = round(self.Bz(), 4)
+        self.write('SET:SYS:VRM:COO:CART:RVST:MODE:RATE:RATE:' + str(s) +
+                   ':VSET:[' + str(x) + ' ' + str(y) + ' ' + str(z) + ']\r\n')
+        self.write('SET:SYS:VRM:ACTN:RTOS\r\n')
+        # just to give an time estimate, +10s for overhead
+        t_wait = self.magnet_sweep_time() * 60 + 10
+        print('Please wait ' + str(t_wait) + ' seconds for the field sweep...')
+        while self.magnet_status() != 'IDLE':
+            pass
+
+    def _set_control_By_param(self, y):
+        s = self.magnet_sweeprate()
+        x = round(self.Bx(), 4)
+        z = round(self.Bz(), 4)
+        self.write('SET:SYS:VRM:COO:CART:RVST:MODE:RATE:RATE:' + str(s) +
+                   ':VSET:[' + str(x) + ' ' + str(y) + ' ' + str(z) + ']\r\n')
+        self.write('SET:SYS:VRM:ACTN:RTOS\r\n')
+        # just to give an time estimate, +10s for overhead
+        t_wait = self.magnet_sweep_time() * 60 + 10
+        print('Please wait ' + str(t_wait) + ' seconds for the field sweep...')
+        while self.magnet_status() != 'IDLE':
+            pass
+
+    def _set_control_Bz_param(self, z):
+        s = self.magnet_sweeprate()
+        x = round(self.Bx(), 4)
+        y = round(self.By(), 4)
+        self.write('SET:SYS:VRM:COO:CART:RVST:MODE:RATE:RATE:' + str(s) +
+                   ':VSET:[' + str(x) + ' ' + str(y) + ' ' + str(z) + ']\r\n')
+        self.write('SET:SYS:VRM:ACTN:RTOS\r\n')
+        # just to give an time estimate, +10s for overhead
+        t_wait = self.magnet_sweep_time() * 60 + 10
+        print('Please wait ' + str(t_wait) + ' seconds for the field sweep...')
+        while self.magnet_status() != 'IDLE':
+            pass
 
     def _get_named_channels(self):
         allchans = self.ask('READ:SYS:DR:CHAN')
@@ -163,7 +284,7 @@ class Triton(IPInstrument):
         for ch in allchans:
             msg = 'READ:SYS:DR:CHAN:%s' % ch
             rep = self.ask(msg)
-            if 'INVALID' not in rep and 'NONE' not in rep:
+            if 'INVALID' not in rep:
                 alias, chan = rep.split(':')[-2:]
                 self.chan_alias[alias] = chan
                 self.add_parameter(name=alias,
@@ -172,19 +293,16 @@ class Triton(IPInstrument):
                                    get_parser=self._parse_temp)
 
     def _get_pressure_channels(self):
-        self.chan_pressure = []
         for i in range(1, 7):
             chan = 'P%d' % i
-            self.chan_pressure.append(chan)
             self.add_parameter(name=chan,
                                unit='bar',
                                get_cmd='READ:DEV:%s:PRES:SIG:PRES' % chan,
                                get_parser=self._parse_pres)
-        self.chan_pressure = set(self.chan_pressure)
 
-    def _get_temp_channel_names(self, file):
+    def _get_temp_channels(self, file):
         config = configparser.ConfigParser()
-        with open(file, 'r', encoding='utf16') as f:
+        with open(file, 'r') as f:
             next(f)
             config.read_file(f)
 
@@ -192,23 +310,13 @@ class Triton(IPInstrument):
             options = config.options(section)
             namestr = '"m_lpszname"'
             if namestr in options:
-                chan_number = int(section.split('\\')[-1].split('[')[-1]) + 1
-                # the names used in the register file are base 0 but the api and the gui
-                # uses base one names so add one
-                chan = 'T'+ str(chan_number)
+                chan = 'T' + section.split('\\')[-1].split('[')[-1]
                 name = config.get(section, '"m_lpszname"').strip("\"")
-                self.chan_temp_names[chan] = {'name': name, 'value': None}
-
-    def _get_temp_channels(self):
-        self.chan_temps = []
-        for i in range(1, 17):
-            chan = 'T%d' % i
-            self.chan_temps.append(chan)
-            self.add_parameter(name=chan,
-                               unit = 'K',
-                               get_cmd = 'READ:DEV:%s:TEMP:SIG:TEMP' % chan,
-                               get_parser = self._parse_temp)
-        self.chan_temps = set(self.chan_temps)
+                self.chan_temps[chan] = {'name': name, 'value': None}
+                self.add_parameter(name=chan,
+                                   unit='K',
+                                   get_cmd='READ:DEV:%s:TEMP:SIG:TEMP' % chan,
+                                   get_parser=self._parse_temp)
 
     def _parse_action(self, msg):
         action = msg[17:]
@@ -243,7 +351,7 @@ class Triton(IPInstrument):
     def _parse_pres(self, msg):
         if 'NOT_FOUND' in msg:
             return None
-        return float(msg.split('SIG:PRES:')[-1].strip('mB'))*1e-3
+        return float(msg.split('SIG:PRES:')[-1].strip('mB')) * 1e3
 
     def _recv(self):
         return super()._recv().rstrip()
