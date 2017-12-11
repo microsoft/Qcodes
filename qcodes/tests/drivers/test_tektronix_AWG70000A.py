872d910a547115aababfe62f9b1939de106dc752
WilliamHPNielsen <william@nbi.ku.dk>
2017-12-11 11:27:40 +0100

    tests: add a test for our previous SML-file truncation

--- qcodes/tests/drivers/test_tektronix_AWG70000A.py
+++ qcodes/tests/drivers/test_tektronix_AWG70000A.py
@@ -1,6 +1,12 @@
+from io import StringIO
+
 import pytest
+from hypothesis import given, settings
+import hypothesis.strategies as hst
+from lxml import etree
 
 from qcodes.instrument_drivers.tektronix.AWG70002A import AWG70002A
+from qcodes.instrument_drivers.tektronix.AWG70000A import AWG70000A
 import qcodes.instrument.sims as sims
 visalib = sims.__file__.replace('__init__.py', 'Tektronix_AWG70000A.yaml@sim')
 
@@ -20,3 +26,26 @@ def test_init_awg2(awg2):
     idn_dict = awg2.IDN()
 
     assert idn_dict['vendor'] == 'QCoDeS'
+
+
+@settings(deadline=600, max_examples=25)
+@given(N=hst.integers(1, 1000))
+def test_SML_successful_generation_vary_length(N):
+
+    tw = [0]*N
+    nreps = [1]*N
+    ejs = [0]*N
+    ejt = [0]*N
+    goto = [0]*N
+    wfm_names = [['ch{}pos{}'.format(ch, pos)
+                  for pos in range(N)] for ch in range(1, 3)]
+
+    seqname = 'seq'
+
+    smlstring = AWG70000A._makeSMLFile(tw, nreps, ejs, ejt, goto,
+                                       wfm_names, seqname)
+
+    # This line will raise an exception if the XML is not valid
+    parsed_xml = etree.parse(StringIO(smlstring))
+
+# TODO: Add some failing tests for inproper input
