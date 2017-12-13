0ee8e50f2a5b2040f860b9b685c6734708d00878
Dominik Vogel <a-dovoge@microsoft.com>
2017-12-13 10:45:06 +0100

    pep8

--- qcodes/instrument_drivers/Kepco/KepcoMagnet.py
+++ qcodes/instrument_drivers/Kepco/KepcoMagnet.py
@@ -4,16 +4,24 @@ import qcodes as qc
 
 from typing import Union
 
+
 class KepcoMagnet(Instrument):
     MAX_AMP = 20
-
     """
     This is the qcodes driver for controlling the field using the Kepco BOP 20-20M.
 
     This is a virtual driver only and will not talk to your instrument.
     """
 
-    def __init__(self, name, parameter, max_field, volt_to_amp, field_to_amp,axis,rate=0.2, **kwargs):
+    def __init__(self,
+                 name,
+                 parameter,
+                 max_field,
+                 volt_to_amp,
+                 field_to_amp,
+                 axis,
+                 rate=0.2,
+                 **kwargs):
         super().__init__(name, **kwargs)
 
         self.v1 = parameter
@@ -21,27 +29,27 @@ class KepcoMagnet(Instrument):
         self.max_field = max_field
         self.volt_to_amp = volt_to_amp
         self.field_to_amp = field_to_amp
-        self.axis=axis
-        self.rate=rate
+        self.axis = axis
+        self.rate = rate
 
         delay = 0.1
         self.v1.inter_delay = delay
-        self.v1.step = (self.rate * self.field_to_amp / self.volt_to_amp) * delay / 60
+        self.v1.step = (
+            self.rate * self.field_to_amp / self.volt_to_amp) * delay / 60
 
         self.v1.vals = Numbers(-self.MAX_AMP / volt_to_amp,
                                self.MAX_AMP / volt_to_amp)
 
-        self.add_parameter('minmax',
-                           label='Maximum field value',
-                           unit='T',
-                           vals=Numbers())
+        self.add_parameter(
+            'minmax', label='Maximum field value', unit='T', vals=Numbers())
 
-        self.add_parameter('{}_BField'.format(axis),
-                           label='{} magnetic field'.format(axis),
-                           unit='T',
-                           get_cmd=self.get_field,
-                           set_cmd=self.set_field,
-                           vals=Numbers(-max_field, max_field))
+        self.add_parameter(
+            '{}_BField'.format(axis),
+            label='{} magnetic field'.format(axis),
+            unit='T',
+            get_cmd=self.get_field,
+            set_cmd=self.set_field,
+            vals=Numbers(-max_field, max_field))
 
     def set_field(self, value: Union[int, float]) -> None:
         instrument_value = value * self.field_to_amp / self.volt_to_amp
@@ -60,5 +68,9 @@ class KepcoMagnet(Instrument):
         model = '1211'
         serial = None
         firmware = None
-        return {'vendor': vendor, 'model': model,
-                'serial': serial, 'firmware': firmware}
+        return {
+            'vendor': vendor,
+            'model': model,
+            'serial': serial,
+            'firmware': firmware
+        }
