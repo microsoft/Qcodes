5efb853ce90fc85f68be40089543a6fde0d91d91
WilliamHPNielsen <william@nbi.ku.dk>
2018-01-11 10:44:30 +0100

    fully cover unregister_parameter in test

--- qcodes/tests/dataset/test_measurement_context_manager.py
+++ qcodes/tests/dataset/test_measurement_context_manager.py
@@ -211,6 +211,16 @@ def test_unregister_parameter(DAC, DMM):
     meas.unregister_parameter(DAC.ch2)
     assert list(meas.parameters.keys()) == [str(DMM.v1), str(DMM.v2)]
 
+    not_parameters = [DAC, DMM, 0.0, 1]
+    for notparam in not_parameters:
+        with pytest.raises(ValueError):
+            meas.unregister_parameter(notparam)
+
+    # unregistering something not registered should silently "succeed"
+    meas.unregister_parameter('totes_not_registered')
+    meas.unregister_parameter(DAC.ch2)
+    meas.unregister_parameter(DAC.ch2)
+
 
 @given(wp=hst.one_of(hst.integers(), hst.floats(allow_nan=False),
                      hst.text()))
