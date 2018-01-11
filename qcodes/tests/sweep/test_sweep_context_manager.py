01f6479bade62874ef0f3da2640450879f492f02
sohail chatoor <schatoor@gmail.com>
2018-01-11 16:09:23 +0100

    intermediate work

--- qcodes/tests/sweep/test_sweep_context_manager.py
+++ qcodes/tests/sweep/test_sweep_context_manager.py
@@ -36,12 +36,12 @@ def test_simple():
 
 
 def test_nest():
+    n = 100
     x = ManualParameter("x")
-    sweep_values_x = np.linspace(-1, 1, 10)  # We have only 10 data points along each axis because of the bug described
-    # in https://github.com/QCoDeS/Qcodes/issues/942
+    sweep_values_x = np.linspace(-1, 1, n) 
 
     y = ManualParameter("y")
-    sweep_values_y = np.linspace(-1, 1, 10)
+    sweep_values_y = np.linspace(-1, 1, n)
 
     m = ManualParameter("m")
     m.get = lambda: np.sin(x())
@@ -69,13 +69,13 @@ def test_nest():
     data_x = data_set.get_data('x')
     data_y = data_set.get_data('y')
 
-    assert data_x[::11] == [[xi] for xi in sweep_values_x]
-    assert data_y[::11] == [[None] for _ in sweep_values_x]
+    assert data_x[::n + 1] == [[xi] for xi in sweep_values_x]
+    assert data_y[::n + 1] == [[None] for _ in sweep_values_x]
 
     coordinate_layout = itertools.product(sweep_values_x, sweep_values_y)
     expected_x, expected_y = zip(*coordinate_layout)
-    assert [ix for c, ix in enumerate(data_x) if c % 11] == [[xi] for xi in expected_x]
-    assert [iy for c, iy in enumerate(data_y) if c % 11] == [[yi] for yi in expected_y]
+    assert [ix for c, ix in enumerate(data_x) if c % (n + 1)] == [[xi] for xi in expected_x]
+    assert [iy for c, iy in enumerate(data_y) if c % (n + 1)] == [[yi] for yi in expected_y]
 
 
 
