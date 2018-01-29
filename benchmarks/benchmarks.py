436499107f086c50576a369d37946ccb63ab79db
sohail chatoor <schatoor@gmail.com>
2018-01-29 16:10:15 +0100

    Added a time benchmark

---
+++ benchmarks/benchmarks.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from qcodes import ParamSpec, new_data_set, new_experiment
+
+
+class TimeSuite:
+
+    def __init__(self):
+        self._data_set = None
+
+    def setup(self):
+        self._data_set = new_data_set("stress_test_simple")
+        t1 = ParamSpec('t', 'numeric', label='time', unit='s')
+        x = ParamSpec('x', 'numeric', label='voltage', unit='v', depends_on=[t1])
+
+        self._data_set.add_parameter(t1)
+        self._data_set.add_parameter(x)
+
+    def time_range(self):
+        t_values = np.linspace(-1, 1, 2000)
+
+        for _ in range(1000):
+            results = [{"t": t, "x": 2 * t ** 2 + 1} for t in t_values]
+            self._data_set.add_results(results)
