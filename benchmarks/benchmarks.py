1edc83d5c84a5fb5652a522e9810fa3bc2cf816c
sohail chatoor <schatoor@gmail.com>
2018-01-30 12:54:42 +0100

    1) Decided to not do data retrieval benchmark for now 2) Added more data insert benchmarks

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -8,18 +8,15 @@ asv run --python=same --quick --show-stderr --dry-run
 in the command prompt. Make sure the environment you are running this in has the current Master installed as a package
 
 """
-
-import os
-from shutil import copyfile
 import numpy as np
 
-from qcodes import ParamSpec, new_data_set, new_experiment, load_last_experiment, DataSet
-from qcodes.dataset.experiment_container import Experiment
+from qcodes import ParamSpec, new_data_set, new_experiment
 
 
-class TimeSuiteInsertion:
+class TimeSuiteAddResults:
     """
-    Make a moderately large data set and investigate insertion time.
+    Make a moderately large data set and investigate insertion time. We make a single large results list and insert
+    in one SQL command. Notice the plural in "Results"
     """
 
     def __init__(self):
@@ -36,10 +33,6 @@ class TimeSuiteInsertion:
         self._data_set.add_parameter(t1)
         self._data_set.add_parameter(x)
 
-    def teardown(self):
-        if not os.path.exists("experiments_persistent.db"):
-            copyfile("experiments.db", "experiments_persistent.db")
-
     def time_range(self):
         t_values = np.linspace(-1, 1, self._insertion_size)
 
@@ -48,15 +41,58 @@ class TimeSuiteInsertion:
             self._data_set.add_results(results)
 
 
-class TimeSuiteExtraction:
+class TimeSuiteAddResult:
     """
-    Monitor how the data retrieval time evolves in time. Specifically, we do not want the retrieval time to increase
-    unacceptably when the data set becomes really large.
+    Make a moderately large data set and investigate insertion time. We make a lot of single results and insert one by
+    one. Notice the singular in "Result"
+    """
 
-    TODO: It is known that this suite fails.
-    """
+    def __init__(self):
+        self._data_set = None
+        self._insertion_size = 200
+
+    def setup(self):
+        new_experiment("profile", "profile")
+        self._data_set = new_data_set("stress_test_simple")
+
+        t1 = ParamSpec('t', 'numeric', label='time', unit='s')
+        x = ParamSpec('x', 'numeric', label='voltage', unit='v', depends_on=[t1])
+
+        self._data_set.add_parameter(t1)
+        self._data_set.add_parameter(x)
 
     def time_range(self):
-        exp = load_last_experiment()
-        data = exp.data_set(1)
-        x = data.get_values("x")
+        t_values = np.linspace(-1, 1, self._insertion_size)
+
+        for _ in range(10):  # Adding results one by one is much more time consuming.
+            results = [{"t": t, "x": 2 * t ** 2 + 1} for t in t_values]
+            for result in results:
+                self._data_set.add_result(result)
+
+
+class TimeSuiteAddArrayResults:
+    """
+    Make a moderately large data set and investigate insertion time. We make a single large results list and insert
+    in one SQL command. Notice the plural in "Results". The dependent parameters shall be arrays
+    """
+
+    def __init__(self):
+        self._data_set = None
+        self._insertion_size = 2000
+
+    def setup(self):
+        new_experiment("profile", "profile")
+        self._data_set = new_data_set("stress_test_simple")
+
+        t1 = ParamSpec('t', 'numeric', label='time', unit='s')
+        x = ParamSpec('x', 'array', label='voltage', unit='v', depends_on=[t1])
+
+        self._data_set.add_parameter(t1)
+        self._data_set.add_parameter(x)
+
+    def time_range(self):
+        t_values = np.linspace(-1, 1, self._insertion_size)
+
+        for _ in range(10):
+            results = [{"t": t, "x": np.array([2 * t**2 + 1, t**3 - 1])} for t in t_values]
+            self._data_set.add_results(results)
