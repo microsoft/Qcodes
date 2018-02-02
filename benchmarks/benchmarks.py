a71c9228eeb4acf12b3b487642c1dad18f11a950
sohail chatoor <schatoor@gmail.com>
2018-02-02 11:29:15 +0100

    Added additional benchmarks for the context manager

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -43,14 +43,52 @@ class TimeSuiteAddResults:
         self._data_set.add_parameter(x)
 
     def time_range(self, insertion_size):
+        """
+        We test the insertion time as a function of the number of results we generate. Add all results in one sql
+        command using the "add_results" method (notice the plural "s")
+        """
         t_values = np.linspace(-1, 1, insertion_size)
         results = [{"t": t, "x": 2 * t ** 2 + 1} for t in t_values]
         self._data_set.add_results(results)
 
 
-class TimeSuiteAddResultContext:
-    params = [200, 500, 1000, 1500, 2000]
+class TimeSuiteAddResult:
+    """
+    Make a moderately large data set and investigate insertion time. We make a lot of single results and insert one by
+    one. Notice the singular in "Result".
+    """
+    params = [20, 50, 100, 150, 200]
     repeats = 10
+
+    def __init__(self, insertion_size):
+        self._data_set = None
+
+    def setup(self, insertion_size):
+        new_experiment("profile", "profile")
+        self._data_set = new_data_set("stress_test_simple")
+
+        t1 = ParamSpec('t', 'numeric', label='time', unit='s')
+        x = ParamSpec('x', 'numeric', label='voltage', unit='v', depends_on=[t1])
+
+        self._data_set.add_parameter(t1)
+        self._data_set.add_parameter(x)
+
+    def time_range(self, insertion_size):
+        """
+        We test the insertion time as a function of the number of results we generate. Then, add the results in one by
+        one on a loop by calling "add_result". Contrast this with the plot "TimeSuiteAddResults"; we see that this
+        method is ~200 times slower!
+        """
+        t_values = np.linspace(-1, 1, insertion_size)
+        results = [{"t": t, "x": 2 * t ** 2 + 1} for t in t_values]
+
+        for result in results:
+            self._data_set.add_result(result)
+
+
+class TimeSuiteAddResultContext:
+    params = [20, 50, 100, 150, 200]
+    repeats = 100
 
     def __init__(self, insertion_size):
         self._meas = None
@@ -70,41 +108,15 @@ class TimeSuiteAddResultContext:
         self._m = m
 
     def time_range(self, insertion_size):
-        self._x(0)
-        self._m.get = lambda: np.arange(insertion_size)
-
+        """
+        Use the context manager to add results in a data set. Compare this result with the "TimeSuiteAddResult" and
+        "TimeSuiteAddResults". We see that although it is not as slow as the former, it is still much slower then the
+        latter.
+        TODO: We should find out why this is so much slower.
+        """
         with self._meas.run() as datasaver:
-            datasaver.add_result((self._x, self._x()), (self._m, self._m()))
-
-
-class TimeSuiteAddResult:
-    """
-    Make a moderately large data set and investigate insertion time. We make a lot of single results and insert one by
-    one. Notice the singular in "Result"
-    """
-    params = [20, 50, 100, 150, 200]
-    repeats = 10
-
-    def __init__(self, insertion_size):
-        self._data_set = None
-
-    def setup(self, insertion_size):
-        new_experiment("profile", "profile")
-        self._data_set = new_data_set("stress_test_simple")
-
-        t1 = ParamSpec('t', 'numeric', label='time', unit='s')
-        x = ParamSpec('x', 'numeric', label='voltage', unit='v', depends_on=[t1])
-
-        self._data_set.add_parameter(t1)
-        self._data_set.add_parameter(x)
-
-    def time_range(self, insertion_size):
-
-        t_values = np.linspace(-1, 1, insertion_size)
-        results = [{"t": t, "x": 2 * t ** 2 + 1} for t in t_values]
-
-        for result in results:
-            self._data_set.add_result(result)
+            for ix, im in zip(range(insertion_size), range(insertion_size)):
+                datasaver.add_result((self._x, ix), (self._m, im))
 
 
 class TimeSuiteAddArrayResults:
@@ -113,7 +125,7 @@ class TimeSuiteAddArrayResults:
     in one SQL command. Notice the plural in "Results". The dependent parameters shall be arrays
     """
     params = [200, 500, 1000, 1500, 2000]
-    repeats = 10
+    repeats = 100
 
     def __init__(self, insertion_size):
         self._data_set = None
@@ -129,9 +141,75 @@ class TimeSuiteAddArrayResults:
         self._data_set.add_parameter(x)
 
     def time_range(self, insertion_size):
+        """
+        Insert arrayed valued values. Each result contains a 1x2 array. Again we see that this is much slower then
+        inserting single valued results.
+        """
         t_values = np.linspace(-1, 1, insertion_size)
         results = [{"t": t, "x": np.array([2 * t**2 + 1, t**3 - 1])} for t in t_values]
         self._data_set.add_results(results)
+
+
+class TimeSuiteAddArrayResultsII:
+    """
+    Make a moderately large data set and investigate insertion time. We make a single large results list and insert
+    in one SQL command. Notice the plural in "Results". The dependent parameters shall be arrays
+    """
+    params = [200, 500, 1000, 1500, 2000]
+    repeats = 100
+
+    def __init__(self, insertion_size):
+        self._data_set = None
+
+    def setup(self, insertion_size):
+        new_experiment("profile", "profile")
+        self._data_set = new_data_set("stress_test_simple")
+
+        t1 = ParamSpec('t', 'numeric', label='time', unit='s')
+        x = ParamSpec('x', 'array', label='voltage', unit='v', depends_on=[t1])
+
+        self._data_set.add_parameter(t1)
+        self._data_set.add_parameter(x)
+
+    def time_range(self, insertion_size):
+        """
+        Insert arrayed valued values. The dimensionality of the array increases along the x-axis.
+        """
+        t_values = np.linspace(-1, 1, 1000)
+        results = [{"t": t, "x": np.random.uniform(0, 1, (1, insertion_size))} for t in t_values]
+        self._data_set.add_results(results)
+
+
+class TimeSuiteAddArrayResultsContext:
+    params = [200, 500, 1000, 1500, 2000]
+    repeats = 100
+
+    def __init__(self, insertion_size):
+        self._meas = None
+        self._x = None
+        self._m = None
+
+    def setup(self, insertion_size):
+        new_experiment("profile", "profile")
+        self._meas = Measurement()
+        x = ManualParameter("x")
+        m = ManualParameter("m")
+
+        self._meas.register_parameter(x)
+        self._meas.register_parameter(m, setpoints=(x,))
+
+        self._x = x
+        self._m = m
+
+    def time_range(self, insertion_size):
+        """
+        Add array valued results with the context manager.
+        """
+        self._x(0)
+        self._m.get = lambda: np.arange(insertion_size)
+
+        with self._meas.run() as datasaver:
+            datasaver.add_result((self._x, self._x()), (self._m, self._m()))
 
 
 def run_suite(suite_cls):
@@ -170,14 +248,12 @@ def make_report():
 
     report = "Benchmark results \n=================\n"
 
-    suites = [TimeSuiteAddResultContext, TimeSuiteAddResults, TimeSuiteAddResult, TimeSuiteAddArrayResults]
+    suites = [TimeSuiteAddResults, TimeSuiteAddResult, TimeSuiteAddResultContext,
+              TimeSuiteAddArrayResults, TimeSuiteAddArrayResultsII, TimeSuiteAddArrayResultsContext]
 
     for suite in suites:
-        #result_plot_file_name = run_suite(suite)
-        result_plot_file_name = "{}.png".format(suite.__name__)
-
+        result_plot_file_name = run_suite(suite)
         code = inspect.getsource(suite.time_range)
-
         report += f".. image:: {result_plot_file_name}\n\t:width: 1000px\n\t:align: center\n\t:height: 800px"
         report += f"\n\n.. code-block:: python\n\n{code}"
 
