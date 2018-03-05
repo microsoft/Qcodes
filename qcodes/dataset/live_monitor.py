44770bbfb005d7d8e5307da82dbe6ed656b06fa5
sohail chatoor <schatoor@gmail.com>
2018-03-05 16:39:18 +0100

    added formatting to live monitor

--- qcodes/dataset/live_monitor.py
+++ qcodes/dataset/live_monitor.py
@@ -7,7 +7,7 @@ from typing import Callable
 import sys
 
 
-def printer(names: list) ->Callable:
+def printer(names: list) -> Callable:
     """
     A simple function that will print inserted data to stdout in such a way
     that single line on the output prompt is refreshed at every call. This
@@ -19,7 +19,7 @@ def printer(names: list) ->Callable:
     >>> from qcodes.dataset.data_set import new_data_set, ParamSpec
     >>> specs=[ParamSpec("x", "numeric", unit='Hz'), ParamSpec("y", "numeric", unit='V')]
     >>> data_set = new_data_set("test", specs=specs)
-    >>> sub_id = data_set.subscribe(printer(["x", "y"]), state=[])
+    >>> sub_id = data_set.subscribe(printer(["x:.2", "y:.2e"]), state=[])
     >>> for x in np.linspace(100, 200, 150):
     >>>     y = np.random.randn()
     >>>     data_set.add_result({"x": x, "y": y})
@@ -37,7 +37,20 @@ def printer(names: list) ->Callable:
     ----
     Find out how we can make this multi line!
     """
-    s_template = ", ".join(["{} = {{}}".format(name) for name in names])
+
+    def split_format(txt, splitter):
+        if splitter not in txt:
+            return txt, "{}"
+
+        splits = txt.split(splitter)
+        if len(splits) > 2:
+            raise ValueError(f"'{txt}' has invalid format")
+
+        splits[1] = "{:" + splits[1] + "}"
+        return splits
+
+    fmt = [split_format(name, ":") for name in names]
+    s_template = ", ".join(["{} = {}".format(*f) for f in fmt])
 
     def subscriber(results, length, state):
         s = s_template.format(*results[-1])
