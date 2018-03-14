14f344853bf2df78d25ed4ad2c32fba5e90e1014
WilliamHPNielsen <william@nbi.ku.dk>
2018-03-14 12:47:07 +0100

    Add seqx-from-forged-sequence builder (+test)
    Update _makeSMLFile to support subsequences
    
    squash_me
    
    squash_me
    
    Delete blank line
    
    test

--- qcodes/tests/drivers/test_tektronix_AWG70000A.py
+++ qcodes/tests/drivers/test_tektronix_AWG70000A.py
@@ -1,6 +1,7 @@
 from io import StringIO
 
 import pytest
+import numpy as np
 from hypothesis import given, settings
 import hypothesis.strategies as hst
 from lxml import etree
@@ -8,6 +9,9 @@ from lxml import etree
 from qcodes.instrument_drivers.tektronix.AWG70002A import AWG70002A
 from qcodes.instrument_drivers.tektronix.AWG70000A import AWG70000A
 import qcodes.instrument.sims as sims
+
+from broadbean.broadbean import InvalidForgedSequenceError
+
 visalib = sims.__file__.replace('__init__.py', 'Tektronix_AWG70000A.yaml@sim')
 
 
@@ -21,6 +25,49 @@ def awg2():
     awg2_sim.close()
 
 
+@pytest.fixture(scope='module')
+def forged_sequence():
+    """
+    Return an example forged sequence containing a
+    subsequence
+    """
+
+    N = 5
+    num_chans = 3
+    types = ['element']*(N-1) + ['subsequence']
+
+    def random_element(num_chans):
+        """
+        Return an element with random values
+
+        200 points long
+        """
+        data = {n: {} for n in range(1, 1 + num_chans)}
+        for key in data.keys():
+            data[key] = {'wfm': np.random.randn(200),
+                         'm1': np.random.randint(0, 2, 200),
+                         'm2': np.random.randint(0, 2, 200)}
+
+        return data
+
+    seq = {i: {} for i in range(1, 1 + N)}
+    for pos1, typ in zip(seq.keys(), types):
+        seq[pos1] = {'type': typ,
+                     'content': {},
+                     'sequencing': {}}
+
+    for pos1 in range(1, N):
+        seq[pos1]['content'] = {1: {'data': random_element(num_chans)}}
+
+    # and finally add the subsequence
+    seq[N]['content'] = {1: {'data': random_element(num_chans),
+                             'sequencing': {'nreps': 2}},
+                         2: {'data': random_element(num_chans),
+                             'sequencing': {'nreps': 2}}}
+
+    return seq
+
+
 def test_init_awg2(awg2):
 
     idn_dict = awg2.IDN()
@@ -28,8 +75,8 @@ def test_init_awg2(awg2):
     assert idn_dict['vendor'] == 'QCoDeS'
 
 
-@settings(deadline=1500, max_examples=25)
-@given(N=hst.integers(1, 1000))
+@settings(deadline=1500, max_examples=7)
+@given(N=hst.integers(1, 100))
 def test_SML_successful_generation_vary_length(N):
 
     tw = [0]*N
@@ -37,8 +84,8 @@ def test_SML_successful_generation_vary_length(N):
     ejs = [0]*N
     ejt = [0]*N
     goto = [0]*N
-    wfm_names = [['ch{}pos{}'.format(ch, pos)
-                  for pos in range(N)] for ch in range(1, 3)]
+    wfm_names = [['pos{}ch{}'.format(pos, ch)
+                  for ch in range(1, 3)] for pos in range(N)]
 
     seqname = 'seq'
 
@@ -63,5 +110,33 @@ def test_WFMXHeader_failing(num_samples, markers_included):
     with pytest.raises(ValueError):
         AWG70000A._makeWFMXFileHeader(num_samples, markers_included)
 
+
+def test_seqxfilefromfs_failing(forged_sequence):
+
+    # typing convenience
+    make_seqx = AWG70000A.makeSEQXFileFromForgedSequence
+
+    # TODO: the number of channels is defined in the
+    # forged_sequence fixture but used here
+    chan_map = {n: n for n in range(1, 4)}
+
+    # the input dict (first argument) is not a valid forged
+    # sequence dict
+    with pytest.raises(InvalidForgedSequenceError):
+        make_seqx({}, [], {})
+
+    # wrong number of channel amplitudes
+    with pytest.raises(ValueError):
+        make_seqx(forged_sequence, amplitudes=[1, 1], channel_mapping=chan_map)
+
+    # wrong channel mapping keys
+    with pytest.raises(ValueError):
+        make_seqx(forged_sequence, [1, 1, 1],
+                  channel_mapping={1: None, 3: None})
+
+    # wrong channel mapping values
+    with pytest.raises(ValueError):
+        make_seqx(forged_sequence, [1, 1, 1],
+                  channel_mapping={1: 10, 2: 8, 3: -1})
 
 # TODO: Add some failing tests for inproper input
