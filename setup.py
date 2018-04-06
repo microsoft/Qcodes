fde3e580bd9c280a0fd4ec7fa6bdd924ec65caac
Jens Hedegaard Nielsen <jenshnielsen@gmail.com>
2018-04-06 10:33:47 +0200

    Correct typo

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(name='qcodes',
           'numpy>=1.10,<1.14',
           'pyvisa>=1.8',
           'h5py>=2.6',
-          'websockets>=3.2,
+          'websockets>=3.2',
           'jsonschema',
           'pymongo',
           'pyzmq'
