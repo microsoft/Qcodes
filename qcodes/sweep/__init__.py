8fd4ae09bceebc82eb08d2c58bb57ba4677869f9
sohail chatoor <schatoor@gmail.com>
2018-04-09 11:44:09 +0200

    Restructed example folder

--- qcodes/sweep/__init__.py
+++ qcodes/sweep/__init__.py
@@ -1,3 +1,3 @@
-from .sweep import sweep, nest, chain, szip
-from .getter_setter import  getter, setter
+from .sweep import sweep, nest, chain, szip, time_trace, sweep_while
+from .getter_setter import getter, setter
 from .sweep_measurement import SweepMeasurement, run
