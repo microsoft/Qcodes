4b75c5c7f67e1412c940e48e19789d519394674c
malte schmidt <mltschmdt@gmail.com>
2018-04-12 13:08:02 +0200

    catch and raise error

--- qcodes/tests/test_wrong_address.py
+++ qcodes/tests/test_wrong_address.py
@@ -1,4 +1,4 @@
-import pytest
+import logging
 
 import qcodes.instrument.sims as sims
 from qcodes.instrument_drivers.Keysight.Keysight_34465A import Keysight_34465A
@@ -7,13 +7,18 @@ visalib = sims.__file__.replace('__init__.py', 'Keysight_34465A.yaml@sim')
 
 def test_wrong_address():
     try:
-        # wrong address
+        try:
+            # wrong address
+            keysight_sim = Keysight_34465A('keysight_34465A_sim',
+                                           address='GPIB::1::6553512341234::INSTR',
+                                           visalib=visalib)
+        except:
+            pass
+        # right address
         keysight_sim = Keysight_34465A('keysight_34465A_sim',
-                                       address='GPIB::1::6553512341234::INSTR',
+                                       address='GPIB::1::65535::INSTR',
                                        visalib=visalib)
-    except:
-        pass
-    # right address
-    keysight_sim = Keysight_34465A('keysight_34465A_sim',
-                                   address='GPIB::1::65535::INSTR',
-                                   visalib=visalib)
+    except Exception as e:
+        logging.warning(e)
+        logging.warning('not good')
+        raise e
