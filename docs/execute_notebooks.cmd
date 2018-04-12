8d689f151826c7f4b3ee38b00e2f037f127829b7
WilliamHPNielsen <william@nbi.ku.dk>
2018-04-12 11:16:02 +0200

    Only execute DataSet example notebook
    The examples notebook contain errors... we should fix that

--- docs/execute_notebooks.cmd
+++ docs/execute_notebooks.cmd
@@ -1,4 +1,4 @@
 echo "Executing the notebooks..."
-jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=600 --execute "examples\*.ipynb"
+jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=600 --execute "examples\DataSet\*.ipynb"
 echo "Cleaning up the generated output..."
 rm "examples\*nbconvert.ipynb"
