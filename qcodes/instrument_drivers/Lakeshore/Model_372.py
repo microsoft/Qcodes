6c8c7a6682869e2a557cf573d8083a03bcd8f5d0
Dominik Vogel <a-dovoge@microsoft.com>
2018-05-11 15:27:13 +0200

    added extended channel for 372

--- qcodes/instrument_drivers/Lakeshore/Model_372.py
+++ qcodes/instrument_drivers/Lakeshore/Model_372.py
@@ -1,4 +1,7 @@
-from .lakeshore_base import LakeshoreBase
+from .lakeshore_base import LakeshoreBase, BaseSensorChannel
+
+class Model_372_Channel(BaseSensorChannel):
+    pass
 
 
 class Model_372(LakeshoreBase):
@@ -6,5 +9,6 @@ class Model_372(LakeshoreBase):
     Lakeshore Model 372 Temperature Controller Driver
     Controlled via sockets
     """
+    CHANNEL_CLASS = Model_372_Channel
     channel_name_command: Dict[str,str] = {'ch{:02}'.format(i): str(i) for i in range(1,17)}
   
