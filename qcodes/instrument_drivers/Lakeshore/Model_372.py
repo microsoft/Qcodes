b5ddabfde0a2ce0ff94cdd191ccb6c1d06d0b1f5
Dominik Vogel <a-dovoge@microsoft.com>
2018-05-17 17:38:36 +0200

    implement group parameter

--- qcodes/instrument_drivers/Lakeshore/Model_372.py
+++ qcodes/instrument_drivers/Lakeshore/Model_372.py
@@ -1,35 +1,64 @@
 from typing import Dict
+from collections import OrderedDict
 from qcodes.instrument.channel import InstrumentChannel
 from qcodes.instrument_drivers.Lakeshore.lakeshore_base import LakeshoreBase, BaseSensorChannel
 from qcodes.instrument.parameter import Parameter
 from qcodes.utils.command import Command
 
 
-# class GroupParameter(Parameter):
-#     def __init__(self, name, instrument, *, group=None, **kwargs):
-#         self.group = group
-#         self.group.add(self)
-#         super().__init__(name, instrument, **kwargs)
+class GroupParameter(Parameter):
+    def __init__(self, name, instrument, **kwargs):
+        self.group = None
+        super().__init__(name, instrument, **kwargs)
 
-#     def set_raw(self, value, **kwargs):
-#         self.group.update()
+    def set_raw(self, value, **kwargs):
+        self.group.set(self, value)
 
-#     def get_raw(self):
-#         return self.group.get(self.name)
+    def get_raw(self, result=None):
+        if not result:
+            self.group.update()
+            return self.get_latest()
+        else:
+            return result
 
 
-# class Group():
-#     def __init__(self, set_cmd=None, get_cmd=None):
-#         self.parameters = []
-
-#     def add(self, parameter: GroupParameter)
-#         # assert they are all of the same instrument
-#         self.parameters.append(parameter)
+class Group():
+    def __init__(self, parameters, set_cmd=None, get_cmd=None, get_parser=None, separator=','):
+        self.parameters = OrderedDict((p.name, p) for p in parameters)
+        self.instrument = parameters[0].root_instrument
+        for p in parameters:
+            p.group = self
+        self.set_cmd = set_cmd
+        self.get_cmd = get_cmd
+        if get_parser:
+            self.get_parser = get_parser
+        else:
+            self.get_parser = self._separator_parser(separator)
     
-#     def update(self):
-#         calling_dict = {p.name: p.get_latest() for p in self.parameters}
-#         command_str = self.instrument.set_cmd.format()
-#         self.set_cmd(wrapping)
+    def _separator_parser(self, separator):
+        def parser(ret_str):
+            print(f'retr string is {ret_str}')
+            return dict(zip(self.parameters.keys(),
+                        (float(arg) for arg in ret_str.split(separator))))
+        return parser
+    
+    def set(self, set_parameter, value):
+        if any((p.get_latest() is None) for p in self.parameters.values()):
+            self.update()
+        calling_dict = {name: p.get_latest() for name, p in self.parameters.items()}
+        calling_dict[set_parameter.name] = value
+        command_str = self.set_cmd.format(**calling_dict)
+        set_parameter.root_instrument.write(command_str)
+
+    def update(self):
+        ret = self.get_parser(self.instrument.ask(self.get_cmd))
+        # if any(p.get_latest() != ret[] for name, p in self.parameters if p is not get_parameter):
+        #     log.warn('a value has changed on the device')
+        # TODO(DV): this is odd, but the only way to call the wrapper accordingly
+        for name, p in self.parameters.items():
+            p.get(result=ret[name])
+        
+        
 
 
 class Model_372_Channel(BaseSensorChannel):
@@ -58,34 +87,53 @@ class Model_372(LakeshoreBase):
     def __init__(self, name: str, address: str, **kwargs) -> None:
         super().__init__(name, address, **kwargs)
 
-        self.add_parameter('P', set_cmd=self._set_p, get_cmd=self._get_p)
-        self.add_parameter('I', set_cmd=self._set_i, get_cmd=self._get_i)
-        self.add_parameter('D', set_cmd=self._set_d, get_cmd=self._get_d)
+        self.add_parameter('P', parameter_class=GroupParameter)
+        self.add_parameter('I', parameter_class=GroupParameter)
+        self.add_parameter('D', parameter_class=GroupParameter)
+        self.pid_group = Group([self.P, self.I, self.D],
+                               set_cmd="PID 1, {P}, {I}, {D}",
+                               get_cmd='PID? 1')
 
-        # self.add_parameter('selected_heater', set_cmd=None, val_mapping={'SAMPLE_HEATER': 0, 'OUTPUT_HEATER': 1})
-        self.add_parameter('output', set_cmd=None, val_mapping={'SAMPLE_HEATER': 0, 'OUTPUT_HEATER': 1})
 
 
-    def selected_heater(self):
-        return '0'
+# class Model_372(LakeshoreBase):
+#     """
+#     Lakeshore Model 372 Temperature Controller Driver
+#     Controlled via sockets
+#     """
+#     CHANNEL_CLASS = Model_372_Channel
+#     channel_name_command: Dict[str,str] = {'ch{:02}'.format(i): str(i) for i in range(1,17)}
+#     def __init__(self, name: str, address: str, **kwargs) -> None:
+#         super().__init__(name, address, **kwargs)
 
-    def _get_pid(self):
-        return [float(num) for num in self.ask(f"PID?").split(',')]
+#         self.add_parameter('P', set_cmd=self._set_p, get_cmd=self._get_p)
+#         self.add_parameter('I', set_cmd=self._set_i, get_cmd=self._get_i)
+#         self.add_parameter('D', set_cmd=self._set_d, get_cmd=self._get_d)
 
-    def _set_p(self, P):
-        self.write(f"PID {self.selected_heater()}, {P}, {self.I()}, {self.D()}")
+#         # self.add_parameter('selected_heater', set_cmd=None, val_mapping={'SAMPLE_HEATER': 0, 'OUTPUT_HEATER': 1})
+#         self.add_parameter('output', set_cmd=None, val_mapping={'SAMPLE_HEATER': 0, 'OUTPUT_HEATER': 1})
 
-    def _get_p(self):
-        return self._get_pid()[0]
 
-    def _set_i(self, i):
-        self.write(f"PID {self.selected_heater()}, {self.P()}, {i}, {self.D()}")
+#     def selected_heater(self):
+#         return '0'
 
-    def _get_i(self):
-        return self._get_pid()[1]
+#     def _get_pid(self):
+#         return [float(num) for num in self.ask(f"PID? {self.selected_heater()}").split(',')]
 
-    def _set_d(self, d):
-        self.write(f"PID {self.selected_heater()}, {self.P()}, {self.I()}, {d}")
+#     def _set_p(self, P):
+#         self.write(f"PID {self.selected_heater()}, {P}, {self.I()}, {self.D()}")
 
-    def _get_d(self):
-        return self._get_pid()[2]
+#     def _get_p(self):
+#         return self._get_pid()[0]
+
+#     def _set_i(self, i):
+#         self.write(f"PID {self.selected_heater()}, {self.P()}, {i}, {self.D()}")
+
+#     def _get_i(self):
+#         return self._get_pid()[1]
+
+#     def _set_d(self, d):
+#         self.write(f"PID {self.selected_heater()}, {self.P()}, {self.I()}, {d}")
+
+#     def _get_d(self):
+#         return self._get_pid()[2]
--- qcodes/tests/drivers/test_lakeshore.py
+++ qcodes/tests/drivers/test_lakeshore.py
@@ -1,6 +1,7 @@
 import pytest
 import logging
 from collections import namedtuple
+from functools import wraps
 
 import qcodes
 from qcodes.instrument_drivers.Lakeshore.Model_336 import Model_336
@@ -43,13 +44,15 @@ class MockVisaInstrument():
         else:
             super().ask_raw(cmd)
 
+
 def split_args(split_char=','):
-    def decorator(func):
-        def decorated_func(string_arg):
+    def wrapper(func):
+        @wraps(func)
+        def decorated_func(self, string_arg):
             args = string_arg.split(split_char)
-            return func(args)
+            return func(self, *args)
         return decorated_func
-    return decorator
+    return wrapper
 
 Heater = namedtuple('Heater', ('P', 'I', 'D'))    
 
@@ -61,6 +64,7 @@ class Model_372_Mock(MockVisaInstrument, Model_372):
         self.cmds['PID'] = self.pid
         self.heaters = {}
         self.heaters['0'] = Heater(P=1, I=2, D=3)
+        self.heaters['1'] = Heater(P=4, I=5, D=6)
     
     def pidq(self, arg):
         print('reached pidq')
@@ -69,11 +73,9 @@ class Model_372_Mock(MockVisaInstrument, Model_372):
 
     @split_args()
     def pid(self, output, P, I, D):
-        heater = self.heaters[output]
-        heater.P = P
-        heater.I = I
-        heater.D = D
+        self.heaters[output] = Heater(P=P, I=I, D=D)
         print('reached pids')
+
 
 def test_instantiation_model_336():
     visalib = sims.__file__.replace('__init__.py', 'lakeshore_model336.yaml@sim')
@@ -84,3 +86,4 @@ def test_instantiation_model_372():
     visalib = sims.__file__.replace('__init__.py', 'lakeshore_model372.yaml@sim')
     ls = Model_372_Mock('lakeshore_372', 'GPIB::3::65535::INSTR', visalib=visalib, device_clear=False)
     ls.P(1)
+    assert ls.P() == 1
