6920ee97d799d64e2953c38bc982ff60eff103b0
Dominik Vogel <a-dovoge@microsoft.com>
2018-05-18 16:57:55 +0200

    Added parameters  and fixed Group

--- qcodes/instrument_drivers/Lakeshore/Model_372.py
+++ qcodes/instrument_drivers/Lakeshore/Model_372.py
@@ -1,9 +1,11 @@
 from typing import Dict
+from enum import Enum
 from collections import OrderedDict
 from qcodes.instrument.channel import InstrumentChannel
 from qcodes.instrument_drivers.Lakeshore.lakeshore_base import LakeshoreBase, BaseSensorChannel
 from qcodes.instrument.parameter import Parameter
 from qcodes.utils.command import Command
+import qcodes.utils.validators as vals
 
 
 class GroupParameter(Parameter):
@@ -17,14 +19,14 @@ class GroupParameter(Parameter):
     def get_raw(self, result=None):
         if not result:
             self.group.update()
-            return self.get_latest()
+            return self.raw_value
         else:
             return result
 
 
 class Group():
     def __init__(self, parameters, set_cmd=None, get_cmd=None,
-                 get_parser=None, separator=','):
+                 get_parser=None, separator=',', types=None):
         self.parameters = OrderedDict((p.name, p) for p in parameters)
         self.instrument = parameters[0].root_instrument
         for p in parameters:
@@ -34,19 +36,20 @@ class Group():
         if get_parser:
             self.get_parser = get_parser
         else:
-            self.get_parser = self._separator_parser(separator)
+            self.get_parser = self._separator_parser(separator, types)
     
-    def _separator_parser(self, separator):
+    def _separator_parser(self, separator, types):
         def parser(ret_str):
             print(f'retr string is {ret_str}')
-            return dict(zip(self.parameters.keys(),
-                        (float(arg) for arg in ret_str.split(separator))))
+            keys = self.parameters.keys()
+            values = ret_str.split(separator)
+            return dict(zip(keys, values))
         return parser
     
     def set(self, set_parameter, value):
         if any((p.get_latest() is None) for p in self.parameters.values()):
             self.update()
-        calling_dict = {name: p.get_latest() for name, p in self.parameters.items()}
+        calling_dict = {name: p.raw_value for name, p in self.parameters.items()}
         calling_dict[set_parameter.name] = value
         command_str = self.set_cmd.format(**calling_dict)
         set_parameter.root_instrument.write(command_str)
@@ -56,11 +59,34 @@ class Group():
         # if any(p.get_latest() != ret[] for name, p in self.parameters if p is not get_parameter):
         #     log.warn('a value has changed on the device')
         # TODO(DV): this is odd, but the only way to call the wrapper accordingly
-        for name, p in self.parameters.items():
+        for name, p in list(self.parameters.items()):
             p.get(result=ret[name])
         
         
 
+class Mode(Enum):
+    off = 0
+    monitor_out = 1
+    open_loop = 2
+    zone = 3
+    still = 4
+    closed_loop = 5
+    warm_up = 6
+
+class Polarity(Enum):
+    unipolar = 0
+    bipolar = 1
+
+class Range(Enum):
+    off = 0
+    max_31_6μA = 1
+    max_100μA = 2
+    max_316μA = 3
+    max_1mA = 4
+    max_3_16mA = 5
+    max_10mA = 6
+    max_31_6mA = 7
+    max_100mA = 8
 
 class Model_372_Channel(BaseSensorChannel):
     pass
@@ -69,26 +95,43 @@ class Heater(InstrumentChannel):
      def __init__(self, parent, heater_name, heater_index):
         super().__init__(parent, heater_name)
         self.heater_index = heater_index
-        self.add_parameter('mode', parameter_class=GroupParameter)
-        self.add_parameter('input_channel', parameter_class=GroupParameter)
-        self.add_parameter('powerup_enable', parameter_class=GroupParameter)
-        self.add_parameter('polarity', parameter_class=GroupParameter)
-        self.add_parameter('filter', parameter_class=GroupParameter)
-        self.add_parameter('delay', parameter_class=GroupParameter)
+        self.add_parameter('mode', get_parser=lambda x: Mode(int(x)),
+                           vals=vals.Enum(*[i for i in Mode]),
+                           set_parser=(lambda enum_value: enum_value.value),
+                           parameter_class=GroupParameter)
+        self.add_parameter('input_channel', get_parser=int, parameter_class=GroupParameter)
+        self.add_parameter('powerup_enable',
+                           val_mapping={True: 1, False: 0},
+                           parameter_class=GroupParameter)
+        self.add_parameter('polarity', get_parser=lambda x: Polarity(int(x)),
+                           vals=vals.Enum(*[i for i in Polarity]),
+                           set_parser=(lambda enum_value: enum_value.value),
+                           parameter_class=GroupParameter)
+        self.add_parameter('filter',
+                           val_mapping={True: 1, False: 0},
+                           parameter_class=GroupParameter)
+        self.add_parameter('delay', vals=vals.Ints(0, 255),
+                           get_parser=int,
+                           parameter_class=GroupParameter)
         self.pid_group = Group([self.mode, self.input_channel,
                                self.powerup_enable, self.polarity,
                                self.filter, self.delay],
-                               set_cmd=f"OUTMODE {heater_index}, {{mode}}, {{input_channel}}, {{poweup_enable}}, {{polarity}}, {{filter}}, {{delay}}",
+                               set_cmd=f"OUTMODE {heater_index}, {{mode}}, {{input_channel}}, {{powerup_enable}}, {{polarity}}, {{filter}}, {{delay}}",
                                get_cmd=f'OUTMODE? {heater_index}')
 
-        self.add_parameter('P', parameter_class=GroupParameter)
-        self.add_parameter('I', parameter_class=GroupParameter)
-        self.add_parameter('D', parameter_class=GroupParameter)
+        self.add_parameter('P', vals=vals.Ints(0, 1000),
+                           get_parser=int, parameter_class=GroupParameter)
+        self.add_parameter('I', vals=vals.Ints(0, 1000),
+                           get_parser=int, parameter_class=GroupParameter)
+        self.add_parameter('D', vals=vals.Ints(0, 2500),
+                           get_parser=int, parameter_class=GroupParameter)
         self.pid_group = Group([self.P, self.I, self.D],
                                set_cmd=f"PID {heater_index}, {{P}}, {{I}}, {{D}}",
                                get_cmd=f'PID? {heater_index}')
         
-        self.add_parameter('range',
+        self.add_parameter('range', get_parser=lambda x: Range(int(x)),
+                           vals=vals.Enum(*[i for i in Range]),
+                           set_parser=(lambda enum_value: enum_value.value),
                            set_cmd=f'RANGE {heater_index}, {{}}',
                            get_cmd=f'RANGE? {heater_index}')
 
--- qcodes/tests/drivers/test_lakeshore.py
+++ qcodes/tests/drivers/test_lakeshore.py
@@ -2,10 +2,11 @@ import pytest
 import logging
 from collections import namedtuple
 from functools import wraps
+from contextlib import suppress
 
 import qcodes
 from qcodes.instrument_drivers.Lakeshore.Model_336 import Model_336
-from qcodes.instrument_drivers.Lakeshore.Model_372 import Model_372
+from qcodes.instrument_drivers.Lakeshore.Model_372 import Model_372, Mode, Polarity, Range
 import qcodes.instrument.sims as sims
 
 
@@ -16,33 +17,52 @@ class MockVisaInstrument():
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        # This base class mixin holds two dictionaries associated with the 
+        # pyvisa_instrument.write()
         self.cmds = {}
+        # and pyvisa_instrument.query() functions
+        self.queries = {}
+        # the keys are the issued VISA commands like '*IDN?' or '*OPC'
+        # the values are the corresponding methods to be called on the mock
+        # instrument.
+
+        # to facilitate the definition there are the decorators `@query' and `@command`
+        # these attach an attribute to the method, so that the dictionaries can be 
+        # filled here in the constructor.
+        # (This is borderline abusive, but makes a it easy to define mocks)
+        func_names = dir(self)
+        # cycle through all methods
+        for func_name in func_names:
+            f = getattr(self, func_name)
+            # only add for methods that have such an attribue
+            with suppress(AttributeError):
+                self.queries[getattr(f, 'query_name')] = f
+            with suppress(AttributeError):
+                self.cmds[getattr(f, 'command_name')] = f
+
 
     def write_raw(self, cmd):
         cmd_parts = cmd.split(' ')
         cmd_str = cmd_parts[0].upper()
-        print(f'command string is {cmd_str} and cmd: {cmd}, and len(cmd_parts):{len(cmd_parts)}')
         if cmd_str in self.cmds:
             args = ''.join(cmd_parts[1:])
             log.debug("Calling query on instrument Mock {}: {} for command {} with args {}".format(self.name, cmd, cmd_str, args))
-            print("Calling write on instrument Mock {}: {} for command {} with args {}".format(self.name, cmd, cmd_str, args))
             self.cmds[cmd_str](args)
         else:
             super().write_raw(cmd)
 
-    def ask_raw(self, cmd):
-        cmd_parts = cmd.split(' ')
-        cmd_str = cmd_parts[0].upper()
-        print(f'command string is {cmd_str} and cmd: {cmd}, and len(cmd_parts):{len(cmd_parts)}')
-        if cmd_str in self.cmds:
-            args = ''.join(cmd_parts[1:])
-            log.debug("Calling query on instrument Mock {}: {} for command {} with args {}".format(self.name, cmd, cmd_str, args))
-            print("Calling query on instrument Mock {}: {} for command {} with args {}".format(self.name, cmd, cmd_str, args))
-            response = self.cmds[cmd_str](args)
+    def ask_raw(self, query):
+        query_parts = query.split(' ')
+        query_str = query_parts[0].upper()
+        if query_str in self.queries:
+            args = ''.join(query_parts[1:])
+            log.debug("Calling query on instrument Mock {}: {} for command {} with args {}".format(self.name, query, query_str, args))
+            print("Calling query on instrument Mock {}: {} for command {} with args {}".format(self.name, query, query_str, args))
+            response = self.queries[query_str](args)
             log.debug(f"Got mock instrument response: {response}")
             return response
         else:
-            super().ask_raw(cmd)
+            super().ask_raw(query)
 
 
 def split_args(split_char=','):
@@ -54,36 +74,137 @@ def split_args(split_char=','):
         return decorated_func
     return wrapper
 
-Heater = namedtuple('Heater', ('P', 'I', 'D'))    
 
+def query(name=None):
+    def wrapper(func):
+        func.query_name = name.upper()
+        return func
+    return wrapper
+
+
+def command(name=None):
+    def wrapper(func):
+        func.command_name = name.upper()
+        return func
+    return wrapper
+
+
+class Heater:
+    def __init__(self, **kwargs):
+        for kwarg, value in kwargs.items():
+            setattr(self, kwarg, value)
 
 class Model_372_Mock(MockVisaInstrument, Model_372):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.cmds['PID?'] = self.pidq
-        self.cmds['PID'] = self.pid
         self.heaters = {}
-        self.heaters['0'] = Heater(P=1, I=2, D=3)
-        self.heaters['1'] = Heater(P=4, I=5, D=6)
-    
+        # initial values
+        self.heaters['0'] = Heater(P=1, I=2, D=3,
+                                   mode=5, input_channel=2,
+                                   powerup_enable=0, polarity=0,
+                                   filter=0, delay=1,
+                                   range=0)
+        self.heaters['1'] = Heater(P=1, I=2, D=3,
+                                   mode=5, input_channel=2,
+                                   powerup_enable=0, polarity=0,
+                                   filter=0, delay=1,
+                                   range=0)
+        self.heaters['2'] = Heater(P=1, I=2, D=3,
+                                   mode=5, input_channel=2,
+                                   powerup_enable=0, polarity=0,
+                                   filter=0, delay=1,
+                                   range=0)
+    @query('PID?')    
     def pidq(self, arg):
-        print('reached pidq')
         heater = self.heaters[arg]
         return f'{heater.P}, {heater.I}, {heater.D}'
 
+    @command('PID')
     @split_args()
     def pid(self, output, P, I, D):
         self.heaters[output] = Heater(P=P, I=I, D=D)
-        print('reached pids')
+
+    @query('OUTMODE?')    
+    def outmodeq(self, arg):
+        heater = self.heaters[arg]
+        return f'{heater.mode}, {heater.input_channel}, {heater.powerup_enable}, {heater.polarity}, {heater.filter}, {heater.delay}'
+
+    @command('OUTMODE')
+    @split_args()
+    def outputmode(self, output, mode, input_channel, powerup_enable,
+                   polarity, filter, delay):
+        h = self.heaters[output]
+        h.output = output
+        h.mode = mode
+        h.input_channel = input_channel
+        h.powerup_enable = powerup_enable
+        h.polarity = polarity
+        h.filter = filter
+        h.delay = delay
+        print(f'setting outputmode to {h.mode}, {input_channel}, {powerup_enable}, {polarity}, {filter}, {delay}')
+
+    @query('RANGE?')    
+    def rangeq(self, arg):
+        heater = self.heaters[arg]
+        return f'{heater.range}'
+
+    @command('RANGE')
+    @split_args()
+    def heater_range(self, output, heater_range):
+        h = self.heaters[output]
+        h.range = heater_range
+        print(f'setting range to {h.range}')
+
+visalib = sims.__file__.replace('__init__.py', 'lakeshore_model372.yaml@sim')
+# def test_instantiation_model_336():
+#     ls = Model_336('lakeshore_336', 'GPIB::2::65535::INSTR', visalib=visalib, device_clear=False)
+#     ls.close()
+
+# def test_instantiation_model_372():
+#     ls = Model_372_Mock('lakeshore_372', 'GPIB::3::65535::INSTR', visalib=visalib, device_clear=False)
+#     ls.close()
 
 
-def test_instantiation_model_336():
-    visalib = sims.__file__.replace('__init__.py', 'lakeshore_model336.yaml@sim')
-    ls = Model_336('lakeshore_336', 'GPIB::2::65535::INSTR', visalib=visalib, device_clear=False)
+@pytest.fixture(scope='function')
+def lakeshore_372():
+    ls = Model_372_Mock('lakeshore_372_fixture', 'GPIB::3::65535::INSTR', visalib=visalib, device_clear=False)
+    yield ls
+    ls.close()
 
+def test_pid_set(lakeshore_372):
+    ls = lakeshore_372
+    P, I, D = 1, 2, 3
+    for h in (ls.warmup_heater, ls.analog_heater, ls.sample_heater):
+        h.P(P)
+        h.I(I)
+        h.D(D)
+        assert (h.P(), h.I(), h.D()) == (P, I, D)
 
-def test_instantiation_model_372():
-    visalib = sims.__file__.replace('__init__.py', 'lakeshore_model372.yaml@sim')
-    ls = Model_372_Mock('lakeshore_372', 'GPIB::3::65535::INSTR', visalib=visalib, device_clear=False)
-    ls.warmup_heater.P(1)
-    assert ls.warmup_heater.P() == 1
+def test_output_mode(lakeshore_372):
+    ls = lakeshore_372
+    mode = Mode.closed_loop
+    input_channel = 1
+    powerup_enable = True
+    polarity = Polarity.unipolar
+    filter = True
+    delay = 1
+    # for h in (ls.warmup_heater, ls.analog_heater, ls.sample_heater):
+    h = ls.warmup_heater
+    h.mode(mode)
+    h.input_channel(input_channel)
+    h.powerup_enable(powerup_enable)
+    h.polarity(polarity)
+    h.filter(filter)
+    h.delay(delay)
+    h.mode()
+    assert (h.mode(), h.input_channel()) == (mode, input_channel)
+    assert (h.powerup_enable(), h.polarity()) == (powerup_enable, polarity)
+    assert (h.filter(), h.delay()) == (filter, delay)
+
+def test_range(lakeshore_372):
+    ls = lakeshore_372
+    heater_range = Range.max_10mA
+    # for h in (ls.warmup_heater, ls.analog_heater, ls.sample_heater):
+    h = ls.warmup_heater
+    h.range(heater_range)
+    assert h.range() == heater_range
