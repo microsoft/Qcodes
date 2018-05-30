bea9ffafec28533f5df65f5e05d1d4fa4b430427
WilliamHPNielsen <whpn@mailbox.org>
2018-05-30 13:42:06 +0200

    Change mark_run to mark_run_complete

--- qcodes/dataset/data_set.py
+++ qcodes/dataset/data_set.py
@@ -25,7 +25,7 @@ from qcodes.dataset.sqlite_base import (atomic, atomic_transaction,
                                         get_experiments,
                                         get_last_experiment, select_one_where,
                                         length, modify_values,
-                                        add_meta_data, mark_run,
+                                        add_meta_data, mark_run_complete,
                                         modify_many_values, insert_values,
                                         insert_many_values,
                                         VALUE, VALUES, get_data,
@@ -165,7 +165,7 @@ class Subscriber(Thread):
 
 
 class DataSet(Sized):
-    def __init__(self, path_to_db: str, conn=None) -> None:
+    def __init__(self, path_to_db: str, run_id: int, conn=None) -> None:
         # TODO: handle fail here by defaulting to
         # a standard db
         self.path_to_db = path_to_db
@@ -174,7 +174,10 @@ class DataSet(Sized):
         else:
             self.conn = conn
 
+        self.run_id = run_id
         self._debug = False
+        self.subscribers: Dict[str, Subscriber] = {}
+        self._completed = self._is_completed()
 
     def _new(self, name, exp_id, specs: SPECS = None, values=None,
              metadata=None) -> None:
@@ -187,7 +190,6 @@ class DataSet(Sized):
 
         # this is really the UUID (an ever increasing count in the db)
         self.run_id = run_id
-        self.subscribers: Dict[str, Subscriber] = {}
         self._completed = False
 
     @property
@@ -228,6 +230,11 @@ class DataSet(Sized):
     def exp_id(self):
         return select_one_where(self.conn, "runs",
                                 "exp_id", "run_id", self.run_id)
+
+    def _is_completed(self) -> bool:
+        comp_time = select_one_where(self.conn, "runs", "completed_timestamp",
+                                     "run_id", self.run_id)
+        return False if comp_time is None else True
 
     def toggle_debug(self):
         """
@@ -308,7 +315,8 @@ class DataSet(Sized):
     @completed.setter
     def completed(self, value):
         self._completed = value
-        mark_run(self.conn, self.run_id, value)
+        if value:
+            mark_run_complete(self.conn, self.run_id)
 
     def mark_complete(self) -> None:
         """Mark dataset as complete and thus read only and notify the
