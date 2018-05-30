b57fac15be82921b93e9ecc52df2c7394abf8b57
WilliamHPNielsen <whpn@mailbox.org>
2018-05-30 16:41:26 +0200

    Add test for load_by_counter

--- qcodes/tests/dataset/test_dataset_loading.py
+++ qcodes/tests/dataset/test_dataset_loading.py
@@ -6,7 +6,7 @@ import tempfile
 import qcodes as qc
 from qcodes.dataset.database import initialise_database
 from qcodes.dataset.experiment_container import new_experiment
-from qcodes.dataset.data_set import new_data_set, load_by_id
+from qcodes.dataset.data_set import new_data_set, load_by_id, load_by_counter
 
 
 @pytest.fixture(scope="function")
@@ -32,13 +32,26 @@ def test_load_by_id(experiment):
     ds.mark_complete()
 
     loaded_ds = load_by_id(run_id)
-    assert loaded_ds.completed == True
+    assert loaded_ds.completed is True
     assert loaded_ds.exp_id == 1
 
     ds = new_data_set("test-dataset-unfinished")
     run_id = ds.run_id
 
     loaded_ds = load_by_id(run_id)
-    assert loaded_ds.completed == False
+    assert loaded_ds.completed is False
     assert loaded_ds.exp_id == 1
 
+
+def test_load_by_counter(empty_temp_db):
+    exp = new_experiment(name="for_loading", sample_name="no_sample")
+    ds = new_data_set("my_first_ds")
+
+    loaded_ds = load_by_counter(exp.exp_id, 1)
+
+    assert loaded_ds.completed is False
+
+    ds.mark_complete()
+    loaded_ds = load_by_counter(exp.exp_id, 1)
+
+    assert loaded_ds.completed is True
