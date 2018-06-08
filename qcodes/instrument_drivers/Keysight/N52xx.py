a8136cb8b603f868e4d5485c9b4926f9bfb758dc
Sebastian Pauka <spauka@gmail.com>
2018-06-08 12:26:49 +1000

    Make driver generic, extend with traces and ports

--- qcodes/instrument_drivers/Keysight/N52xx.py
+++ qcodes/instrument_drivers/Keysight/N52xx.py
@@ -1,18 +1,10 @@
 from cmath import phase
 import math
 import numpy as np
-from qcodes import VisaInstrument, MultiParameter
+from qcodes import VisaInstrument, MultiParameter, InstrumentChannel
 from qcodes.utils.validators import Numbers
 import time
-
-
-def _Sparam(string):
-    """
-    Converting S-parameter of active trace to a number for qcodes loop
-    """
-    x = string.strip('"')
-    idx = x.find('_S')
-    return int(x[idx+2:idx+4])
+import re
 
 class RawSweep(MultiParameter):
     """
@@ -116,14 +108,7 @@ class FormattedSweep(ArrayParameter):
         self._instrument = instrument
         self.format = format
         
-    def set_sweep(self, start, stop, npts):
-        f = np.linspace(int(start), int(stop), num=npts)
-        self.setpoints = (f,)
-        self.shape = (npts,)
-        
     def get_raw(self):
-        npts = self._instrument.points()
-        self.set_sweep(self._instrument.start(), self._instrument.stop(), npts)
         
         # Take instrument out of continuous mode, and send triggers equal to the number of averages
         self._instrument.write('SENS:AVER:CLE')
@@ -141,34 +126,155 @@ class FormattedSweep(ArrayParameter):
         # Ask for magnitude
         self._instrument.write('CALC:FORM %s' % self.format)
         data_str = self._instrument.ask('CALC:DATA? FDATA').split(',')
-        data_list = np.fromiter((float(v) for v in data_str), float, count=npts)
+        data_list = np.fromiter((float(v) for v in data_str), float)
 
         # Return the instrument state
         self._instrument.write('SENS:SWE:MODE CONT')
                       
         return data_list
 
-class N5245A(VisaInstrument):
+    @property
+    def shape(self):
+        return (self._instrument.root_instrument.points(),)
+    @property
+    def setpoints(self):
+        start = self._instrument.root_instrument.start()
+        stop = self._instrument.root_instrument.stop()
+        return (np.linspace(start, stop, self.shape),)
+    
+
+class PNAPort(InstrumentChannel):
     """
-    qcodes driver for Agilent N5245A Network Analyzer. Command list at
+    Allow operations on individual PNA ports.
+    Note: This can be expanded to include a large number of extra parameters...
+    """
+
+    def __init__(self, parent, name, port,
+                 min_power, max_power):
+        super().__init__(parent, name)
+
+        self.port = int(port)
+        if self.port < 1 or self.port > 4:
+            raise ValueError("Port must be between 1 and 4.")
+
+        pow_cmd = f"SOUR:POW{self.port}"
+        self.add_parameter("source_power",
+                           label="power",
+                           unit="dBm",
+                           get_cmd=pow_cmd + "?",
+                           set_cmd=pow_cmd + "{}",
+                           get_parser=float)
+
+class PNATrace(InstrumentChannel):
+    """
+    Allow operations on individual PNA traces.
+    """
+
+    def __init__(self, parent, name, trace):
+        super().__init__(parent, name)
+        self.trace = trace
+
+        # Name of parameter
+        self.add_parameter('trace',
+                           label='Trace',
+                           get_cmd='CALC:PAR:SEL?'.format(self.trace),
+                           get_parser=_Sparam
+                           )
+
+        # And a list of individual formats
+        self.add_parameter('magnitude',
+                           format='MLOG',
+                           label='Magnitude',
+                           unit='dB',
+                           start=start,
+                           stop=stop,
+                           npts=npts,
+                           parameter_class=FormattedSweep)
+        self.add_parameter('phase',
+                           format='PHAS',
+                           label='Phase',
+                           unit='deg',
+                           start=start,
+                           stop=stop,
+                           npts=npts,
+                           parameter_class=FormattedSweep)
+        self.add_parameter('real',
+                           format='REAL',
+                           label='Real',
+                           unit='LinMag',
+                           start=start,
+                           stop=stop,
+                           npts=npts,
+                           parameter_class=FormattedSweep)
+        self.add_parameter('imaginary',
+                           format='IMAG',
+                           label='Imaginary',
+                           unit='LinMag',
+                           start=start,
+                           stop=stop,
+                           npts=npts,
+                           parameter_class=FormattedSweep)
+
+    def write(self, cmd):
+        """
+        Select correct trace before querying
+        """
+        super().write("CALC:PAR:MNUM {}".format(self.trace))
+        super().write(cmd)
+    def ask(self, cmd):
+        """
+        Select correct trace before querying
+        """
+        super().write("CALC:PAR:MNUM {}".format(self.trace))
+        return super().ask(cmd)
+
+    @staticmethod
+    def parse_paramstring(paramspec):
+        """
+        Parse parameter specification from PNA
+        """
+        paramspec = paramspec.strip('"')
+        ch, param, trnum = re.findall(r"CH(\d+)_(S\d+)_(\d+)", paramspec)[0]
+        return ch, param, trnum
+
+    def _Sparam(self, paramspec):
+        """
+        Extrace S_parameter from returned PNA format
+        """
+        return self.parse_paramstring(paramspec)[1]
+
+class PNABase(VisaInstrument):
+    """
+    Base qcodes driver for Agilent/Keysight series PNAs
     http://na.support.keysight.com/pna/help/latest/Programming/GP-IB_Command_Finder/SCPI_Command_Tree.htm
+
+    Note: Currently this driver only expects a single channel on the PNA. We can handle multiple
+          traces, but using traces across multiple channels may have unexpected results.
     """
 
-    def __init__(self, name, address, **kwargs):
+    def __init__(self, name, address, 
+                 min_freq, max_freq, # Set frequency ranges
+                 min_power, max_power, # Set power ranges
+                 nports, # Number of ports on the PNA
+                 **kwargs):
         super().__init__(name, address, terminator='\n', **kwargs)
-        
-        # A default measurement and output format on initialisation
-        self.write('FORM ASCii,0')
-        self.write('FORM:BORD SWAP')
-        self.write('CALC:PAR:DEF:EXT "S21", S21')
-        self.write('CALC:FORM MLOG')
 
-        # Query the instrument for what options are installed
-        options = self.ask('*OPT?').strip('"').split(',')
+        #Ports
+        ports = ChannelList(self, "PNAPorts", PNAPort)
+        for port in range(1,nports+1):
+            port = PNAPort(self, f"Port{port}", port)
+            ports.append(port)
+            self.add_submodule(port)
+        ports.lock()
+        self.add_submodule("ports", ports)
+
+        # Traces
+        # Note: These will be accessed through the traces property which updates
+        # the channellist to include only active trace numbers
+        self._traces = ChannelList(self, "PNATraces", PNATrace)
+        self.add_submodule("traces", self._traces)
         
-        '''
-        Parameters
-        '''
+        # Parameters
         # The active trace (i.e. what is pulled from the PNA), and its assigned S parameter.
         # To extract multiple S-parameters, set the trace first, then use one of the sweeps.
         self.add_parameter('trace_number',
@@ -177,21 +283,15 @@ class N5245A(VisaInstrument):
                            set_cmd='CALC:PAR:MNUM {}',
                            get_parser=int
                            )
-        self.add_parameter('trace',
-                           label='Trace',
-                           get_cmd='CALC:PAR:SEL?',
-                           get_parser=_Sparam
-                           )
-        
+
         # Drive power
-        min_power = -90 if '419' in options or '219' in options else -30
         self.add_parameter('power',
                            label='Power',
                            get_cmd='SOUR:POW?',
                            get_parser=float,
                            set_cmd='SOUR:POW {:.2f}',
                            unit='dBm',
-                           vals=Numbers(min_value=min_power,max_value=10))
+                           vals=Numbers(min_value=min_power,max_value=max_power))
         self.add_parameter('sweep_time',
                            label='Time',
                            get_cmd='SENS:SWE:TIME?',
@@ -257,39 +357,8 @@ class N5245A(VisaInstrument):
                            vals=Numbers(min_value=0, max_value=100000))
         
         '''
-        PNA-x units with two sources have an enormous list of functions & configurations.
-        In practice, most of this will be set up manually on the unit, with power and frequency
-        varied.
-        '''
-        if '400' in options or '224' in options:
-            ports = ['1','2','3','4'] if '400' in options else ['1','2']
-            for p in ports:
-                power_cmd = 'SOUR:POW{}'.format(p)
-                self.add_parameter('aux_power{}'.format(p),
-                                   label='Aux Power',
-                                   get_cmd=power_cmd + '?',
-                                   get_parser=float,
-                                   set_cmd=power_cmd+ ' {:.2f}',
-                                   unit='dBm',
-                                   vals=Numbers(min_value=min_power, max_value=10))
-            self.add_parameter('aux_frequency',
-                               label='Aux Frequency',
-                               get_cmd='SENS:FOM:RANG4:FREQ:CW?',
-                               get_parser=float,
-                               set_cmd='SENS:FOM:RANG4:FREQ:CW {:.2f}',
-                               unit='Hz',
-                               vals=Numbers(min_value=10e6,max_value=50e9))
-        
-        '''
         Sweeps
         '''
-        # An immediate collection of data from the instrument
-        self.add_parameter('raw_trace',
-                           start=self.start(),
-                           stop=self.stop(),
-                           npts=self.points(),
-                           parameter_class=RawSweep)
-        
         # Wait until the sweep (and averages) are completed first
         self.add_parameter('vector_trace',
                            start = self.start(),
@@ -303,25 +372,43 @@ class N5245A(VisaInstrument):
         # Clear averages
         self.add_function('reset_averages',
                           call_cmd='SENS:AVER:CLE')
-        
         # Averages ON
         self.add_function('averages_on',
                           call_cmd='SENS:AVER ON')
-        
         # Averages OFF
         self.add_function('averages_off',
                           call_cmd='SENS:AVER OFF')
-                
+
+        # A default output format on initialisation
+        self.write('FORM ASCii,0')
+        self.write('FORM:BORD SWAP')
+
         self.connect_message()
 
+    @property
+    def traces(self):
+        """
+        Update channel list with active traces and return the new list
+        """
+        parlist = self.ask("CALC:PAR:CAT:EXT?").strip('"').split(",")
+        self._traces.clear()
+        for trace in parlist[::2]:
+            trnum = PNATrace.parse_paramstring(trace)[2]
+            trace = PNATrace(self, "tr{}".format(trnum), int(trnum))
+            self._traces.add(trace)
+        return self._traces
+    
 
-    def get_idn(self):
-        IDN = self.ask_raw('*IDN?')
-        vendor, model, serial, firmware = map(str.strip, IDN.split(','))
-        IDN = {'vendor': vendor, 'model': model,
-               'serial': serial, 'firmware': firmware}
-        return IDN
-        
+    def get_options(self):
+        # Query the instrument for what options are installed
+        return self.ask('*OPT?').strip('"').split(',')
+
+    def _set_power_limits(self, min_power, max_power):
+        """
+        Set port power limits
+        """
+        self.power.vals = Numbers(min_value=min_power,max_value=max_power)
+
     # Adjusting the sweep requires an adjustment to the sweep parameters
     def _set_start(self, val):
         self.write('SENS:FREQ:STAR {:.2f}'.format(val))
@@ -344,3 +431,29 @@ class N5245A(VisaInstrument):
             npts = self.points()
         for trace in self.traces:
             trace.set_sweep(start, stop, npts)
+
+class PNAxBase(PNABase):
+    def __init__(self, name, address, 
+                 min_freq, max_freq, # Set frequency ranges
+                 min_power, max_power, # Set power ranges
+                 nports, # Number of ports on the PNA
+                 **kwargs):
+
+        super.__init__(self, name, address, 
+                       min_freq, max_freq,
+                       min_power, max_power,
+                       **kwargs)
+
+    def _enable_fom(self):
+        '''
+        PNA-x units with two sources have an enormous list of functions & configurations.
+        In practice, most of this will be set up manually on the unit, with power and frequency
+        varied.
+        '''
+        self.add_parameter('aux_frequency',
+                           label='Aux Frequency',
+                           get_cmd='SENS:FOM:RANG4:FREQ:CW?',
+                           get_parser=float,
+                           set_cmd='SENS:FOM:RANG4:FREQ:CW {:.2f}',
+                           unit='Hz',
+                           vals=Numbers(min_value=10e6,max_value=50e9))
