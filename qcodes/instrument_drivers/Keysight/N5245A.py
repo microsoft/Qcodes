9dcbda94ae7b0d96c6a30aab94b57b3deb9f1906
Sebastian Pauka <spauka@gmail.com>
2018-06-08 12:27:22 +1000

    Add initializer for N5245A PNA

---
+++ qcodes/instrument_drivers/Keysight/N5245A.py
@@ -0,0 +1,12 @@
+from . import N52xx
+
+class N5245A(N52xx.PNAxBase):
+    def __init__(self, name, address, **kwargs):
+        super().__init__(name, address, 
+                         min_freq=10e6, max_freq=50e9, 
+                         min_power=-30, max_power=13,
+                         **kwargs)
+
+        options = self.get_options()
+        if "419" in options:
+            self._set_power_limits(min_power=-90, max_power=13)
