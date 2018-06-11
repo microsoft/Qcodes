1b964bc35fed979385d1da2d8d78648fc4293002
Luka Drmić <13066652+kNalj@users.noreply.github.com>
2018-06-11 13:49:01 +0200

    Update loops.py

--- qcodes/loops.py
+++ qcodes/loops.py
@@ -831,7 +831,7 @@ class ActiveLoop(Metadatable):
                 tprint('loop %s: %d/%d (%.1f [s])' % (
                     self.sweep_values.name, i, imax, time.time() - t0),
                     dt=self.progress_interval, tag='outerloop')
-                  if i:
+                if i:
                     tprint("Estimated finish time: %s" % (
                         time.asctime(time.localtime(t0 + ((time.time() - t0) * imax / i)))),
                            dt=self.progress_interval, tag="finish")
