93fd68cc7e4b23d11db8bec18495a300181578d1
Luka Drmić <13066652+kNalj@users.noreply.github.com>
2018-06-13 16:38:41 +0200

    Update IVVI.py

--- qcodes/instrument_drivers/QuTech/IVVI.py
+++ qcodes/instrument_drivers/QuTech/IVVI.py
@@ -434,7 +434,7 @@ class IVVI(VisaInstrument):
         for ch in channels:
             self.pol_num[ch - 1] = val
             name = "dac" + str(ch)
-            self.set_parameter_bounds(name, val, 
+            self.set_parameter_bounds(name, val,
                                       val + self.Fullrange)
 
         if get_all:
@@ -460,7 +460,7 @@ class IVVI(VisaInstrument):
             return 'POS'
         else:
             return 'Invalid polarity in memory'
-        
+
     def set_parameter_bounds(self, name, min_value, max_value):
         parameter = self.parameters[name]
         if not isinstance(parameter.vals, Numbers):
