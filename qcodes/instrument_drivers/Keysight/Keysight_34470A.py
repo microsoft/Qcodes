b9f8d7a17806bc68311f562b9ee7997fbb0fb0cc
Andrea Corna <a.corna@tudelft.nl>
2018-07-02 18:53:52 +0200

    Refactor Keysight 344xxA code and small fixes

--- qcodes/instrument_drivers/Keysight/Keysight_34470A.py
+++ qcodes/instrument_drivers/Keysight/Keysight_34470A.py
@@ -1,8 +1,8 @@
 from .private.Keysight_344xxA import _Keysight_344xxA
 
-class Keysight_34465A(_Keysight_344xxA):
+class Keysight_34470A(_Keysight_344xxA):
     """
-    This is the qcodes driver for the Keysight 34465A Multimeter
+    This is the qcodes driver for the Keysight 34470A Multimeter
     """
     def __init__(self, name, address, utility_freq=50, silent=False,
                  **kwargs):
