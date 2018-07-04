61c6723dcff3978bb369e653c04b6e1f07bee20b
Jens H. Nielsen <j.h.nielsen@nbi.ku.dk>
2018-07-04 13:05:52 +0200

    give function a more general name
    This does not only give out volt and curr

--- qcodes/instrument_drivers/tektronix/Keithley_2400.py
+++ qcodes/instrument_drivers/tektronix/Keithley_2400.py
@@ -34,14 +34,14 @@ class Keithley_2400(VisaInstrument):
                            label='Current Compliance')
 
         self.add_parameter('volt',
-                           get_cmd=self._get_volt_and_curr,
+                           get_cmd=self._get_read_output_protected,
                            get_parser=self._volt_parser,
                            set_cmd=':SOUR:VOLT:LEV {:.8f}',
                            label='Voltage',
                            unit='V')
 
         self.add_parameter('curr',
-                           get_cmd=self._get_volt_and_curr,
+                           get_cmd=self._get_read_output_protected,
                            get_parser=self._curr_parser,
                            set_cmd=':SOUR:CURR:LEV {:.8f}',
                            label='Current',
@@ -77,12 +77,12 @@ class Keithley_2400(VisaInstrument):
                            label='Current integration time')
 
         self.add_parameter('resistance',
-                           get_cmd=self._get_volt_and_curr,
+                           get_cmd=self._get_read_output_protected,
                            get_parser=self._resistance_parser,
                            label='Resistance',
                            unit='Ohm')
 
-    def _get_volt_and_curr(self) -> str:
+    def _get_read_output_protected(self) -> str:
         """
         This wrapper function around ":READ?" exists because calling
         ":READ?" on an instrument with output disabled is an error.
