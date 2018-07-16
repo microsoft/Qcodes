e4599c00d8c0085b80a998578c60d2dbca9fc86b
Pieter Eendebak <pieter.eendebak@tno.nl>
2018-07-16 16:08:43 +0200

    autopep8

--- qcodes/instrument_drivers/QuTech/M2j.py
+++ qcodes/instrument_drivers/QuTech/M2j.py
@@ -14,19 +14,19 @@ class M2j(Instrument):
     def __init__(self, name, spi_rack, module, **kwargs):
         """
         Qcodes driver for the M2j RF amplifier SPI-rack module.
-    
+
         Args:
             name (str): name of the instrument.
-    
+
             spi_rack (SPI_rack): instance of the SPI_rack class as defined in
                 the spirack package. This class manages communication with the
                 individual modules.
-    
+
             module (int): module number as set on the hardware.
-    
+
         The gain and RF_level can only be set to the device, but not read from the device.
-        
-        Parameters: 
+
+        Parameters:
             gain: Amplifier gain in dB, range 32 to 55 dB
             RF_level: Measured RF power after amplification (not calibrated)
         """
@@ -75,7 +75,7 @@ class M2j(Instrument):
         """ Measure the power and convert it to dBm. Calibrated using an R&S
          SMA100 source. Linear relation between set power and measured data.
          Measurement range -80 to -40 dBm.
-        """ 
+        """
         x = self.m2j.get_level()
         a = 0
         b = 1
