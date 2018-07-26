94365f55cf0762a3f90dfc0bf16561ea54bbb4c1
MIkhail Astafev <astafan8@gmail.com>
2018-07-26 20:11:34 +0200

    Make run_timestamp - formatted timestamp; run_timestamp_raw - seconds

--- qcodes/tests/dataset/test_dataset_loading.py
+++ qcodes/tests/dataset/test_dataset_loading.py
@@ -1,13 +1,14 @@
 import os
+import tempfile
 import time
+from math import floor
 
 import pytest
-import tempfile
 
 import qcodes as qc
+from qcodes.dataset.data_set import new_data_set, load_by_id, load_by_counter
 from qcodes.dataset.database import initialise_database
 from qcodes.dataset.experiment_container import new_experiment
-from qcodes.dataset.data_set import new_data_set, load_by_id, load_by_counter
 
 
 @pytest.fixture(scope="function")
@@ -74,6 +75,26 @@ def test_run_timestamp(empty_temp_db):
     ds = new_data_set("my_first_ds")
     t_after_data_set = time.time()
 
-    actual_run_timestamp = ds.run_timestamp
+    actual_run_timestamp_raw = ds.run_timestamp_raw
 
-    assert t_before_data_set < actual_run_timestamp < t_after_data_set
+    assert t_before_data_set < actual_run_timestamp_raw < t_after_data_set
+
+
+def test_run_timestamp_with_default_format(empty_temp_db):
+    _ = new_experiment(name="for_loading", sample_name="no_sample")
+
+    t_before_data_set = time.time()
+    ds = new_data_set("my_first_ds")
+    t_after_data_set = time.time()
+
+    # Note that here we also test the default format of `run_timestamp`
+    actual_run_timestamp_raw = time.mktime(
+        time.strptime(ds.run_timestamp, "%Y-%m-%d %H:%M:%S"))
+
+    # Note that because the default format precision is 1 second, we add this
+    # second to the right side of the comparison
+    t_before_data_set_secs = floor(t_before_data_set)
+    t_after_data_set_secs = floor(t_after_data_set)
+    assert t_before_data_set_secs \
+           <= actual_run_timestamp_raw \
+           <= t_after_data_set_secs + 1
