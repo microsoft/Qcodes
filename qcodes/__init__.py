eeb38d6b3e559e8e942f580a21cbe082571d974a
MIkhail Astafev <astafan8@gmail.com>
2018-07-26 20:32:12 +0200

    Make initialise_or_create_database_at available from qcodes

--- qcodes/__init__.py
+++ qcodes/__init__.py
@@ -78,7 +78,8 @@ from qcodes.dataset.experiment_container import new_experiment, load_experiment,
     load_last_experiment, experiments, load_or_create_experiment
 from qcodes.dataset.sqlite_settings import SQLiteSettings
 from qcodes.dataset.param_spec import ParamSpec
-from qcodes.dataset.database import initialise_database
+from qcodes.dataset.database import initialise_database, \
+    initialise_or_create_database_at
 
 try:
     get_ipython() # type: ignore # Check if we are in iPython
