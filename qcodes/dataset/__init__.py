c927448d7a28c86a46d4a7e38aa9e736d39e1835
WilliamHPNielsen <whpn@mailbox.org>
2018-09-05 11:21:24 +0200

    Improve the GUID component retrival menu
    Check for out-of-range ints, give better explanation,
    and save to home

--- qcodes/dataset/__init__.py
+++ qcodes/dataset/__init__.py
@@ -7,19 +7,27 @@ loc = cfg['GUID_components']['location']
 station = cfg['GUID_components']['work_station']
 
 if station == 0 or loc == 0:
-    print('Welcome to QCoDeS!\nTo use QCoDeS, please provide the system with '
-          'enough information to generate unique dataset identifiers.')
+    mssg = """
+           Welcome to QCoDeS!
+           This is the first-time-only configuration menu. When using the
+           QCoDeS dataset (source files in qcodes/dataset/*), a GUID is
+           generated for each run that goes into the database. This is helpful
+           when sharing data with other people or just yourself across
+           different machines. To enable the GUID generation, please provide
+           one or two input integers to identify your machine.
+           """
+    print(mssg)
 
 if loc == 0:
-    while loc == 0:
-        loc = int(input('Please enter an integer as your location code: '))
+    while loc < 1 or loc > 256:
+        loc = int(input('Please enter an integer (1-256) as your'
+                        ' location code: '))
     cfg['GUID_components']['location'] = loc
 
 if station == 0:
-    while station == 0:
-        station = int(input('Please enter an integer as your work'
+    while station < 1 or station > 16777216:
+        station = int(input('Please enter an integer (1-16777216) as your work'
                             ' station code: '))
     cfg['GUID_components']['work_station'] = station
 
-with open(cfg.default_file_name, "w") as fp:
-    json.dump(cfg.current_config, fp, indent=4)
+cfg.save_to_home()
