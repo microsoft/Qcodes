5abd9598f66ecb0968f46c95706d73dc2ecfb11c
schatoor <schatoor@gmail.com>
2018-09-06 06:52:41 -0700

    1) Get rid of the old driver 2) Make the existing drivers base on the new driver

--- qcodes/instrument_drivers/Keysight/N5222B.py
+++ qcodes/instrument_drivers/Keysight/N5222B.py
@@ -7,10 +7,3 @@ class N5222B(N52xxBase):
     min_power: float = -90
     max_power: float = 12
     port_count: int = 4
-
-    def __init__(self, name, address, **kwargs):
-        super().__init__(name, address, **kwargs)
-
-
-if __name__ == "__main__":
-    N5222B("test", 'GPIB0::16::INSTR')
--- qcodes/instrument_drivers/Keysight/N5245A.py
+++ qcodes/instrument_drivers/Keysight/N5245A.py
@@ -1,15 +1,10 @@
-from . import N52xx
+from qcodes.instrument_drivers.Keysight.N52xx import N52xxBase
 
-class N5245A(N52xx.PNAxBase):
-    def __init__(self, name, address, **kwargs):
-        super().__init__(name, address,
-                         min_freq=10e6, max_freq=50e9,
-                         min_power=-30, max_power=13,
-                         nports=4,
-                         **kwargs)
 
-        options = self.get_options()
-        if "419" in options:
-            self._set_power_limits(min_power=-90, max_power=13)
-        if "080" in options:
-            self._enable_fom()
+class N5245A(N52xxBase):
+    min_freq: float = 10e6
+    max_freq: float = 50e9
+    min_power: float = -30
+    max_power: float = 13
+    port_count: int = 4
+
