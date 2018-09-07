aefa304ae3427b8a2cd8fbfb5a0779895c566485
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2018-09-07 09:31:58 +0200

    loop dont use deprecated escape seq

--- qcodes/loops.py
+++ qcodes/loops.py
@@ -90,17 +90,18 @@ class Loop(Metadatable):
         progress_interval: should progress of the loop every x seconds. Default
             is None (no output)
 
-    After creating a Loop, you attach ``action``\s to it, making an ``ActiveLoop``
+    After creating a Loop, you attach one or more ``actions`` to it, making an
+    ``ActiveLoop``
 
     TODO:
         how? Maybe obvious but not specified! that you can ``.run()``,
         or you can ``.run()`` a ``Loop`` directly, in which
-        case it takes the default ``action``\s from the default ``Station``
+        case it takes the default ``actions`` from the default ``Station``
 
-    ``actions`` are a sequence of things to do at each ``Loop`` step: they can be
-    ``Parameter``\s to measure, ``Task``\s to do (any callable that does not yield
-    data), ``Wait`` times, or other ``ActiveLoop``\s or ``Loop``\s to nest inside
-    this one.
+    ``actions`` is a sequence of things to do at each ``Loop`` step: that can be
+    a ``Parameter`` to measure, a ``Task`` to do (any callable that does not
+    yield data), ``Wait`` times, or another ``ActiveLoop`` or ``Loop`` to nest
+    inside this one.
     """
     def __init__(self, sweep_values, delay=0, station=None,
                  progress_interval=None):
@@ -260,7 +261,7 @@ class Loop(Metadatable):
         """
         Attach actions to be performed after the loop completes.
 
-        These can only be *Task* and *Wait* actions, as they may not generate
+        These can only be ``Task`` and ``Wait`` actions, as they may not generate
         any data.
 
         returns a new Loop object - the original is untouched
@@ -274,7 +275,7 @@ class Loop(Metadatable):
             examples of this ? with default actions.
 
         Args:
-            \*actions: *Task* and *Wait* objects to execute in order
+            *actions: ``Task`` and ``Wait`` objects to execute in order
 
             overwrite: (default False) whether subsequent .then() calls (including
                 calls in an ActiveLoop after .then() has already been called on
@@ -336,12 +337,13 @@ def _attach_bg_task(loop, task, bg_final_task, min_delay):
 
 class ActiveLoop(Metadatable):
     """
-    Created by attaching actions to a *Loop*, this is the object that actually
-    runs a measurement loop. An *ActiveLoop* can no longer be nested, only run,
-    or used as an action inside another `Loop` which will run the whole thing.
+    Created by attaching ``actions`` to a ``Loop``, this is the object that
+    actually runs a measurement loop. An ``ActiveLoop`` can no longer be nested,
+    only run, or used as an action inside another ``Loop`` which will run the
+    whole thing.
 
-    The *ActiveLoop* determines what *DataArray*\s it will need to hold the data
-    it collects, and it creates a *DataSet* holding these *DataArray*\s
+    The ``ActiveLoop`` determines what ``DataArrays`` it will need to hold the
+    data it collects, and it creates a ``DataSet`` holding these ``DataArrays``
     """
 
     # Currently active loop, is set when calling loop.run(set_active=True)
@@ -383,14 +385,16 @@ class ActiveLoop(Metadatable):
         """
         Attach actions to be performed after the loop completes.
 
-        These can only be `Task` and `Wait` actions, as they may not generate
-        any data.
+        These can only be ``Task`` and ``Wait`` actions, as they may not
+        generate any data.
 
         returns a new ActiveLoop object - the original is untouched
 
-        \*actions: `Task` and `Wait` objects to execute in order
+
 
         Args:
+            *actions: ``Task`` and ``Wait`` objects to execute in order
+
             overwrite: (default False) whether subsequent .then() calls (including
                 calls in an ActiveLoop after .then() has already been called on
                 the Loop) will add to each other or overwrite the earlier ones.
