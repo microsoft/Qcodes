4e69819de953b3f2576ab0e908361ebd2fc31c2d
WilliamHPNielsen <whpn@mailbox.org>
2018-09-12 11:04:20 +0200

    Add two interactive functions to set GUID components

--- qcodes/dataset/guids.py
+++ qcodes/dataset/guids.py
@@ -72,3 +72,54 @@ def parse_guid(guid: str) -> Dict[str, int]:
     components['time'] = int(guid[16:], base=16)
 
     return components
+
+
+def set_guid_location_code() -> None:
+    """
+    Interactive function to set the location code.
+    """
+    cfg = Config()
+    old_loc = cfg['GUID_components']['location']
+    print(f'Updating GUID location code. Current location code is: {old_loc}')
+    if old_loc != 0:
+        print('That is a non-default location code. Perhaps you should not '
+              'change it? Re-enter that code to leave it unchanged.')
+    loc_str = input('Please enter the new location code (1-256): ')
+    try:
+        location = int(loc_str)
+    except ValueError:
+        print('The location code must be an integer. No update performed.')
+        return
+    if not(257 > location > 0):
+        print('The location code must be between 1 and 256 (both included). '
+              'No update performed')
+        return
+
+    cfg['GUID_components']['location'] = location
+    cfg.save_to_home()
+
+
+def set_guid_work_station_code() -> None:
+    """
+    Interactive function to set the work station code
+    """
+    cfg = Config()
+    old_ws = cfg['GUID_components']['work_station']
+    print('Updating GUID work station code. '
+          'Current work station code is: {old_ws}')
+    if old_ws != 0:
+        print('That is a non-default work station code. Perhaps you should not'
+              ' change it? Re-enter that code to leave it unchanged.')
+    ws_str = input('Please enter the new work station code (1-16777216): ')
+    try:
+        work_station = int(ws_str)
+    except ValueError:
+        print('The work station code must be an integer. No update performed.')
+        return
+    if not(16777216 > work_station > 0):
+        print('The work staion code must be between 1 and 256 (both included).'
+              ' No update performed')
+        return
+
+    cfg['GUID_components']['work_station'] = work_station
+    cfg.save_to_home()
