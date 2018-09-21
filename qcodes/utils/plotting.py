b952c02aa9eb4066a505c82f5b895bef258c4e62
Dominik Vogel <a-dovoge@microsoft.com>
2018-09-21 18:05:53 +0200

    fix types

--- qcodes/utils/plotting.py
+++ qcodes/utils/plotting.py
@@ -125,7 +125,7 @@ def apply_color_scale_limits(colorbar: matplotlib.pyplot.colorbar,
                                '`apply_color_scale_limits:\n\n`'
                                + apply_color_scale_limits.__doc__)
         else:
-            data_lim = tuple(sorted(data_lim))
+            data_lim = cast(Tuple[float, float], tuple(sorted(data_lim)))
     # if `None` is provided in the new limits don't change this limit
     vlim = [new or old for new, old in zip(new_lim, colorbar.get_clim())]
     # sort limits in case they were given in a wrong order
