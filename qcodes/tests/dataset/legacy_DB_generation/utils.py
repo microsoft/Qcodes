91688754a7022ccb6372ba1b2262328764fa2911
WilliamHPNielsen <whpn@mailbox.org>
2018-10-05 15:30:09 +0200

    Remove unused variable

--- qcodes/tests/dataset/legacy_DB_generation/utils.py
+++ qcodes/tests/dataset/legacy_DB_generation/utils.py
@@ -17,8 +17,6 @@ GIT_HASHES: Dict[int, str] = {0: '78d42620fc245a975b5a615ed5e33061baac7846',
                               1: '056d59627e22fa3ca7aad4c265e9897c343f79cf',
                               2: '5202255924542dad6841dfe3d941a7f80c43956c'}
 
-DB_NAMES: Dict[int, List[str]] = {0: ['']}
-
 gitrepopath = os.sep.join(os.path.realpath(__file__).split(os.sep)[:-5])
 repo = Repo(gitrepopath)
 
