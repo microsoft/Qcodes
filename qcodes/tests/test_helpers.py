5641b33d5edb662d81da7927a1d9b7c5263d4a21
Dominik Vogel <a-dovoge@microsoft.com>
2018-10-23 11:11:55 +0200

    fix import location for tests

--- qcodes/tests/test_helpers.py
+++ qcodes/tests/test_helpers.py
@@ -15,7 +15,7 @@ from qcodes.utils.helpers import (is_sequence, permissive_range, wait_secs,
                                   named_repr, make_sweep, is_sequence_of,
                                   compare_dictionaries, NumpyJSONEncoder,
                                   partial_with_docstring)
-from qcodes.utils.logger import LogCapture
+from qcodes.logger.logger import LogCapture
 from qcodes.utils.helpers import is_function, attribute_set_to
 
 
