f63ba4573793afaedba059ac081638b8bef71f2e
Stefan Droege <StefanD986@users.noreply.github.com>
2018-10-25 11:10:18 +0200

    adds more driver functions and enables (optionally) testing with the real instrument.

--- qcodes/instrument_drivers/Keysight/keysight_b220x.py
+++ qcodes/instrument_drivers/Keysight/keysight_b220x.py
@@ -1,4 +1,5 @@
-from qcodes import VisaInstrument, validators
+from qcodes import VisaInstrument
+from qcodes.utils.validators import MultiType, Ints, Enum, Lists
 
 
 class KeysightB220X(VisaInstrument):
@@ -9,13 +10,26 @@ class KeysightB220X(VisaInstrument):
     def __init__(self, name, address, **kwargs):
         super().__init__(name, address, terminator='\r', **kwargs)
 
+        self.add_function(name='clear_status',
+                          call_cmd='*CLS')
+
+        self.add_function(name='reset',
+                          call_cmd='*RST')
+
+        self.add_parameter(name='get_status',
+                           get_cmd='*ESR?',
+                           get_parser=lambda status_str: int(status_str))
+
+        self.add_parameter(name='get_error',
+                           get_cmd=':SYST:ERR?')
+
         self.add_function(name='connect',
                           call_cmd=self._connect,
-                          args=[validators.Ints(1, 14), validators.Ints(1, 48)])
+                          args=[Ints(1, 14), Ints(1, 48)])
 
         self.add_function(name='disconnect',
                           call_cmd=self._connect,
-                          args=[validators.Ints(1, 14), validators.Ints(1, 48)])
+                          args=[Ints(1, 14), Ints(1, 48)])
 
         self.add_function(name='disconnect_all',
                           call_cmd=':OPEN:CARD 0')
@@ -30,8 +44,8 @@ class KeysightB220X(VisaInstrument):
                            get_cmd=':CONN:SEQ? 0',
                            set_cmd=':CONN:SEQ 0,{}',
                            val_mapping={'none': 'NSEQ',
-                                       'bbm': 'BBM',
-                                       'mbb': 'MBBR'})
+                                        'bbm': 'BBM',
+                                        'mbb': 'MBBR'})
 
         self.add_function(name='bias_disable_all',
                           call_cmd=':BIAS:CHAN:DIS:CARD 0')
@@ -41,29 +55,82 @@ class KeysightB220X(VisaInstrument):
 
         self.add_function(name='bias_enable_channel',
                           call_cmd=':BIAS:CHAN:ENAB (@001{:02d})',
-                          args=[validators.Ints(1, 48), ])
+                          args=[Ints(1, 48), ])
 
         self.add_function(name='bias_disable_channel',
                           call_cmd=':BIAS:CHAN:DIS (@001{:02d})',
-                          args=[validators.Ints(1, 48), ])
+                          args=[Ints(1, 48), ])
 
         self.add_parameter(name='bias_input_port',
                            get_cmd=':BIAS:PORT? 0',
                            set_cmd=':BIAS:PORT 0,{}',
-                           vals=validators.MultiType(validators.Ints(1, 14),
-                                                     validators.Enum(-1))
+                           vals=MultiType(Ints(1, 14),
+                                                     Enum(-1)),
+                           get_parser=lambda response: int(response)
                            )
+
+        self.add_parameter(name='bias_mode',
+                           get_cmd=':BIAS? 0',
+                           set_cmd=':BIAS 0,{}',
+                           val_mapping={True: 1,
+                                        False: 0}
+                           )
+
+        self.add_function(name='gnd_disable_all',
+                          call_cmd=':AGND:CHAN:DIS:CARD 0')
+
+        self.add_function(name='gnd_disable_channel',
+                          call_cmd=':AGND:CHAN:DIS (@001{:02d})',
+                          args=[Ints(1, 48), ])
+
+        self.add_function(name='gnd_enable_all',
+                          call_cmd=':AGND:CHAN:ENAB:CARD 0')
+
+        self.add_function(name='gnd_enable_channel',
+                          call_cmd=':AGND:CHAN:ENAB (@001{:02d})',
+                          args=[Ints(1, 48), ])
+
+        self.add_parameter(name='gnd_input_port',
+                           get_cmd=':AGND:PORT? 0',
+                           set_cmd=':AGND:PORT 0,{}',
+                           vals=MultiType(Ints(1, 14),
+                                                     Enum(-1)),
+                           get_parser=lambda response: int(response)
+                           )
+
+        self.add_parameter(name='gnd_mode',
+                           get_cmd=':AGND? 0',
+                           set_cmd=':AGND 0,{}',
+                           val_mapping={True: 1,
+                                        False: 0}
+                           )
+
+        self.add_parameter(name='ground_enabled_unused_inputs',
+                           get_cmd=':AGND:UNUSED? 0',
+                           set_cmd=':AGND:UNUSED 0,{}',
+                           get_parser=lambda response: [int(x) for x in response.split(',') if x.strip().isdigit()],
+                           set_parser=lambda value: str(value).strip('[]()')
+                           # todo: add `vals=MultiType(Ints(1, 14), Lists(Ints(1, 14)))` as soon as #1337 is fixed
+                           )
+
+        self.add_parameter(name='couple_ports',
+                           get_cmd=':COUP:PORT? 0',
+                           set_cmd=':COUP:PORT 0,{}',
+                           set_parser=lambda value: str(value).strip('[]()'),
+                           get_parser=lambda response: [int(x) for x in response.split(',') if x.strip().isdigit()]
+                           # todo: add `vals=MultiType(Enum(1, 3, 5, 7, 9, 11, 13), Lists(Enum(1, 3, 5, 7, 9, 11, 13)))` as soon as #1337 is fixed
+                           )
+
+        self.add_function(name='couple_port_autodetect',
+                          call_cmd=':COUP:PORT:DET')
+
 
     def _connect(self, channel1, channel2):
         self.write(":CLOS (@{card:01d}{ch1:02d}{ch2:02d})".format(card=0,
-                                                                ch1=channel1,
-                                                                ch2=channel2))
+                                                                  ch1=channel1,
+                                                                  ch2=channel2))
 
     def _disconnect(self, channel1, channel2):
         self.write(":OPEN (@{card:01d}{ch1:02d}{ch2:02d})".format(card=0,
-                                                                ch1=channel1,
-                                                                ch2=channel2))
-
-
-
-
+                                                                  ch1=channel1,
+                                                                  ch2=channel2))
--- qcodes/tests/drivers/test_keysight_b220x.py
+++ qcodes/tests/drivers/test_keysight_b220x.py
@@ -1,46 +1,145 @@
 import pytest
-from qcodes.instrument_drivers.test import DriverTestCase
+from pyvisa.errors import VisaIOError
+from qcodes.instrument_drivers.Keysight.keysight_b220x import KeysightB220X
+
 
 @pytest.fixture
-def uut():
-    from qcodes.instrument_drivers.Keysight.keysight_b220x import KeysightB220X
-    import qcodes.instrument.sims as sims
-    path_to_yaml = sims.__file__.replace('__init__.py', 'keysight_b220x.yaml')
+def uut() -> KeysightB220X:
+    try:
+        uut = KeysightB220X('switch_matrix',
+                            address='GPIB::22::INSTR')
+    except (ValueError, VisaIOError):
+        # Either there is no VISA lib installed or there was no real instrument found at the
+        # specified address => use simulated instrument
+        import qcodes.instrument.sims as sims
+        path_to_yaml = sims.__file__.replace('__init__.py', 'keysight_b220x.yaml')
 
-    uut = KeysightB220X('switch_matrix',
-                        address='GPIB::1::INSTR',
-                        visalib=path_to_yaml+'@sim'
-                        )
+        uut = KeysightB220X('switch_matrix',
+                            address='GPIB::1::INSTR',
+                            visalib=path_to_yaml + '@sim'
+                            )
+
+    uut.get_status()
+    uut.clear_status()
+    uut.reset()
 
     yield uut
 
     uut.close()
 
 
-class TestSimulatedKeysightB220X:
-    def test_idn_command(self, uut):
-        assert "AGILENT" in uut.IDN()['vendor']
-        assert 0 == int(uut.ask('*ESR?'))
-
-    def test_connection_rule(self, uut):
-        uut.connection_rule('single')
-        assert 0 == int(uut.ask('*ESR?'))
-        assert 'single' == uut.connection_rule()
-        assert 0 == int(uut.ask('*ESR?'))
+def test_idn_command(uut):
+    assert "AGILENT" in uut.IDN()['vendor']
+    assert 0 == uut.get_status()
 
 
-    def test_bias_disable_all(self, uut):
-        uut.bias_disable_all()
-        assert 0 == int(uut.ask('*ESR?'))
+def test_connection_rule(uut):
+    uut.connection_rule('single')
+    assert 0 == uut.get_status()
+    assert 'single' == uut.connection_rule()
+    assert 0 == uut.get_status()
 
-    def test_bias_disable_channel(self, uut):
-        uut.bias_disable_channel(1)
-        assert 0 == int(uut.ask('*ESR?'))
 
-    def test_bias_enable_all(self, uut):
-        uut.bias_enable_all()
-        assert 0 == int(uut.ask('*ESR?'))
+def test_bias_disable_all(uut):
+    uut.bias_disable_all()
+    assert 0 == uut.get_status()
 
-    def test_bias_enable_channel(self, uut):
-        uut.bias_enable_channel(1)
-        assert 0 == int(uut.ask('*ESR?'))
+
+def test_bias_disable_channel(uut):
+    uut.bias_disable_channel(1)
+    assert 0 == uut.get_status()
+
+
+def test_bias_enable_all(uut):
+    uut.bias_enable_all()
+    assert 0 == uut.get_status()
+
+
+def test_bias_enable_channel(uut):
+    uut.bias_enable_channel(1)
+    assert 0 == uut.get_status()
+
+
+def test_bias_input_port(uut):
+    assert 10 == uut.bias_input_port()
+    uut.bias_input_port(9)
+    assert 9 == uut.bias_input_port()
+    assert 0 == uut.get_status()
+
+
+def test_bias_mode(uut):
+    uut.bias_mode(True)
+    assert uut.bias_mode()
+    assert 0 == uut.get_status()
+
+
+def test_gnd_disable_all(uut):
+    uut.gnd_disable_all()
+    assert 0 == uut.get_status()
+
+
+def test_gnd_disable_channel(uut):
+    uut.gnd_disable_channel(1)
+    assert 0 == uut.get_status()
+
+
+def test_gnd_enable_all(uut):
+    uut.gnd_enable_all()
+    assert 0 == uut.get_status()
+
+
+def test_gnd_enable_channel(uut):
+    uut.gnd_enable_channel(1)
+    assert 0 == uut.get_status()
+
+
+def test_gnd_input_port(uut):
+    assert 12 == uut.gnd_input_port()
+    uut.gnd_input_port(5)
+    assert 5 == uut.gnd_input_port()
+    assert 0 == uut.get_status()
+
+
+def test_gnd_mode(uut):
+    assert not uut.gnd_mode()
+    uut.gnd_mode(True)
+    assert uut.gnd_mode()
+    assert 0 == uut.get_status()
+
+
+def test_ground_enabled_unused_inputs(uut):
+    uut.ground_enabled_unused_inputs()
+    assert 0 == uut.get_status()
+    uut.ground_enabled_unused_inputs(1)
+    assert 0 == uut.get_status()
+    uut.ground_enabled_unused_inputs([1, 2, 3])
+    assert 0 == uut.get_status()
+
+
+def test_couple_ports(uut):
+    assert not uut.couple_ports()
+    assert 0 == uut.get_status()
+
+    uut.couple_ports(1)
+    assert 0 == uut.get_status()
+    assert [1] == uut.couple_ports()
+
+    uut.couple_ports([1, 3, 5])
+    assert 0 == uut.get_status()
+    assert [1,3,5] == uut.couple_ports()
+
+    # todo: Add as soon as #1337 is fixed
+    # with pytest.raises(ValueError):
+    #    uut.couple_ports(2)
+    # with pytest.raises(ValueError):
+    #    uut.couple_ports([2, 3])
+
+
+def test_couple_port_autodetect(uut):
+    uut.couple_port_autodetect()
+    assert 0 == uut.get_status()
+
+def test_get_error(uut):
+    uut.get_error()
+    assert 0 == uut.get_status()
+
