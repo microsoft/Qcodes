b3a6936ca2bdd7738c447dc0e9623a09a8742434
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2018-10-30 21:17:20 +0100

    correct logic in plotting

--- qcodes/utils/plotting.py
+++ qcodes/utils/plotting.py
@@ -35,13 +35,15 @@ def auto_range_iqr(data_array: np.ndarray,
     This function computes the inter-quartile-range (IQR), defined by Q3-Q1,
     i.e. the percentiles for 75% and 25% of the destribution. The region
     without outliers is defined by [Q1-1.5*IQR, Q3+1.5*IQR].
+
     Args:
         data_array: numpy array of arbitrary dimension containing the
             statistical data
         cutoff_percentile: percentile of data that may maximally be clipped
             on both sides of the distribution.
             If given a tuple (a,b) the percentile limits will be a and 100-b.
-    returns:
+
+    Returns:
         vmin, vmax: region limits [vmin, vmax]
     """
     if isinstance(cutoff_percentile, tuple):
