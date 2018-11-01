369bf17d679a7d0ea16c537e834916713a8cc139
Mikhail Astafev <astafan8@gmail.com>
2018-11-01 16:58:25 +0100

    Update qcodes/dataset/data_set.py
    Co-Authored-By: Dominik-Vogel <30660470+Dominik-Vogel@users.noreply.github.com>

--- qcodes/dataset/data_set.py
+++ qcodes/dataset/data_set.py
@@ -774,7 +774,7 @@ class DataSet(Sized):
         except AttributeError:
             keys = ','.join(subscribers.keys())
             raise RuntimeError(
-                f'subscribe_from_config: failed to subscribe "{name}" DataSet '
+                f'subscribe_from_config: failed to subscribe "{name}" to DataSet '
                 f'from list of subscribers in `qcodesrc.json` (subscriptions.'
                 f'subscribers). Chose one of: {keys}')
         # get callback from string
