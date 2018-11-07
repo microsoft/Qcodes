b29b61659797f67dfaf74e4a341c7fa6a5289024
Christopher <chgranad@microsoft.com>
2018-11-07 10:17:50 +0100

    New async utils.

---
+++ qcodes/utils/async_utils.py
@@ -0,0 +1,32 @@
+from typing import TypeVar, Awaitable
+import asyncio
+from threading import Thread
+from contextlib import contextmanager
+
+T = TypeVar('T')
+
+def sync(task : Awaitable[T]) -> T:
+    loop : asyncio.BaseEventLoop = asyncio.get_event_loop()
+    if loop.is_running():
+        # We can't run in the same event loop that's already
+        # running, so spawn a new thread.
+        raise RuntimeError(
+            r"Cannot run synchronously when an event loop is already running. "
+            r"Please make sure to use sync() only at the outermost level in your "
+                 r"call stack. "
+            r"If you are using IPython's %autoawait extension, please use await the "
+                 r"async version of this function/method, or disable %autoawait."
+        )
+    else:
+        return loop.run_until_complete(task)
+
+@contextmanager
+def cancelling(*tasks : asyncio.Future) -> None:
+    try:
+        yield
+    finally:
+        for task in tasks:
+            try:
+                task.cancel()
+            except:
+                pass
