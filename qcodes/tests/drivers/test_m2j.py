427dd21d170fa7a8a15071f05bcbb6dd138fb31d
qSaevar <saevar.steinn@gmail.com>
2018-11-11 15:16:58 +0100

    Addressed review comments

--- qcodes/tests/drivers/test_m2j.py
+++ qcodes/tests/drivers/test_m2j.py
@@ -1,22 +1,23 @@
 import sys
 import unittest
-from unittest.mock import patch, MagicMock, call
+from unittest.mock import MagicMock, call
 
 sys.modules['spirack'] = MagicMock(name='spirack')
 from spirack import M2j_module, SPI_rack
 from qcodes.instrument_drivers.QuTech.M2j import M2j
 
 
-class TestZIHDAWG8(unittest.TestCase):
+class TestM2j(unittest.TestCase):
 
     def test_gain(self):
         spi_rack = MagicMock()
         m2j = M2j('test', spi_rack, 42)
-
+        gain_to_low = 32
+        gain_to_high = 56
         with self.assertRaises(ValueError):
-            m2j.gain.set(32)
+            m2j.gain.set(gain_to_low)
         with self.assertRaises(ValueError):
-            m2j.gain.set(56)
+            m2j.gain.set(gain_to_high)
 
         for gain in range(33, 56):
             m2j.gain.set(gain)
@@ -26,6 +27,27 @@ class TestZIHDAWG8(unittest.TestCase):
                       1434, 1381, 1331, 1284, 1238]
         calls = [call.set_gain(ref_scale) for ref_scale in ref_scales]
         m2j.m2j.assert_has_calls(calls)
+        m2j.close()
+
+    def test_gain_floats(self):
+        spi_rack = MagicMock()
+        m2j = M2j('test_float', spi_rack, 42)
+        gain_to_low = 32.5
+        gain_to_high = 55.1
+        with self.assertRaises(ValueError):
+            m2j.gain.set(gain_to_low)
+        with self.assertRaises(ValueError):
+            m2j.gain.set(gain_to_high)
+
+        for gain in [g + 0.1 for g in range(33, 55)]:
+            m2j.gain.set(gain)
+
+        ref_scales = [4095, 3690, 3291, 3005, 2781, 2598, 2442, 2307, 2188,
+                      2081, 1984, 1896, 1815, 1739, 1669, 1603, 1542, 1483,
+                      1428, 1376, 1326, 1279]
+        calls = [call.set_gain(ref_scale) for ref_scale in ref_scales]
+        m2j.m2j.assert_has_calls(calls)
+        m2j.close()
 
     def test_rf_level(self):
         spi_rack = MagicMock()
@@ -33,3 +55,4 @@ class TestZIHDAWG8(unittest.TestCase):
 
         m2j.RF_level.get()
         m2j.m2j.assert_has_calls([call.get_level()])
+        m2j.close()
