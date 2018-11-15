f5683c5a71ecffcb8f3e701ba33f2d633f8e6b7d
Pieter Eendebak <pieter.eendebak@tno.nl>
2018-11-15 10:45:16 +0100

    update to S5i

--- qcodes/instrument_drivers/QuTech/S5i.py
+++ qcodes/instrument_drivers/QuTech/S5i.py
@@ -20,34 +20,90 @@ class S5i(Instrument):
             individual modules.
 
         module (int): module number as set on the hardware.
+
+    Parameters:
+        enabled: switched output on/off
+        frequency: Frequency on Hz, range 40 MHz - 4 GHz, stepsize 1 MHz
+        freq_fine: Frequency on Hz, range 40 MHz - 4 GHz, stepsize 10 kHz.
+        Difference between frequency and freq_fine unclear!?
+        power: power in dBm, range -20 to +15 dBm
+
     """
 
     def __init__(self, name, spi_rack, module, **kwargs):
         super().__init__(name, **kwargs)
 
-        self.s5i = S5i_module(spi_rack, module)
-
-        self.add_parameter('use_external_reference',
-                           label='Use external reference',
-                           get_cmd=self._use_external_reference,
-                           set_cmd=self.s5i.use_external_reference,
+        self.s5i = S5i_module(spi_rack, module, frequency=41e6,
+                              enable_output=1, output_level=0)
+        # Put low freq as it is unlikely it will be resonant with something.
+        self.add_parameter('enabled',
+                           label='RF_on/off',
+                           set_cmd=self.s5i.enable_output_soft,
                            vals=Bool())
-
-        self.add_parameter('frequency_stepsize',
-                           label='Frequency stepsize',
-                           get_cmd=self._get_stepsize,
-                           set_cmd=self.s5i.set_stepsize,
-                           units='Hz',
-                           vals=Numbers())
 
         self.add_parameter('frequency',
                            label='Frequency',
                            get_cmd=self._get_rf_frequency,
                            set_cmd=self.s5i.set_frequency,
-                           units='Hz',
+                           unit='Hz',
                            vals=Numbers())
 
-        self.add_function('optimize_for_frequency', call_cmd=self._optimize)
+        self.add_parameter('freq_fine',
+                           label='Frequency fine',
+                           get_cmd=self._get_rf_frequency,
+                           set_cmd=self.s5i.set_frequency_optimally,
+                           unit='Hz',
+                           vals=Numbers())
+
+        self.add_parameter('power',
+                           label='Power',
+                           unit='dBm',
+                           set_cmd=self._set_power,
+                           vals=Numbers())
+
+    def _set_power(self, dbm):
+        self.s5i.set_output_power(self.convert_dbm_to_ref_scale(dbm))
+
+    @staticmethod
+    def convert_ref_scale_to_dbm(ref_scale):
+        # Made using a simple fit to measured data. This is temporarily and
+        # should be done in the module itselves.
+        a = -30.1271
+        b = 58.8666
+        c = -7.29323
+        d = -145.572
+        x1 = 1.10351
+        x2 = 0.52127
+
+        x = ref_scale
+        return a + b * x + c * (x - x1)**2 + d * (x - x2)**3
+
+    @staticmethod
+    def convert_dbm_to_ref_scale(dBm):
+        if dBm < -20:
+            print(
+                "notice, lower limit for the power is -20dBm. The limit will be the set value.")
+        if dBm > 15:
+            print(
+                "notice, upper limit for the power is 15dBm. The limit will be the set value.")
+        a = 0.563451
+        b = 0.0182626
+        c = -0.000556757
+        d = -8.44786e-05
+        x1 = 1.29053
+        x2 = -0.230088
+        e = 2.82792e-07
+        x3 = -2.647
+        x = dBm
+
+        ref_scale = a + b * x + c * \
+            (x - x1)**2 + d * (x - x2)**3 + e * (x - x3)**5
+        if ref_scale < 0:
+            ref_scale = 0
+        if ref_scale > 1:
+            ref_scale = 1
+
+        return ref_scale
 
     def _use_external_reference(self):
         return self.s5i.use_external
@@ -57,6 +113,3 @@ class S5i(Instrument):
 
     def _get_rf_frequency(self):
         return self.s5i.rf_frequency
-
-    def _optimize(self):
-        self.s5i.set_frequency_optimally(self.s5i.rf_frequency)
