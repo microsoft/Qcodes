5aa6a609437eba8f470b4869d8b3d1944fa527bc
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2018-11-19 10:40:48 +0100

    Add rabbitmq interface
    (cherry picked from commit a8dbce4e279fde8f04ecff316aa7cf96e90ec7aa)

---
+++ qcodes/dataset/rabbitmq_storage_interface.py
@@ -0,0 +1,35 @@
+import pickle
+from typing import Dict
+
+import pika
+
+from .data_storage_interface import (
+    DataStorageInterface, VALUES)
+
+
+class RabbitMQStorageInterface(DataStorageInterface):
+
+    def __init__(self, guid: str, ds):
+        super().__init__(guid)
+        self._connection = pika.BlockingConnection(
+            pika.ConnectionParameters('localhost'))
+        self._channel = self._connection.channel()
+        self._channel.exchange_declare(exchange='mydata',
+                                       exchange_type='fanout')
+        # the configuration of channes should perhaps be read from
+        # a config file
+        self._channel.queue_declare(queue='tordensky', durable=True)
+        self._channel.queue_bind(exchange='mydata', queue='tordensky')
+        self._channel.queue_declare(queue='localstorage', durable=True)
+        self._channel.queue_bind(exchange='mydata', queue='localstorage')
+
+    def store_results(self, results: Dict[str, VALUES]):
+        results_dump = pickle.dumps(results)
+        self._channel.publish(exchange='mydata',
+                              routing_key='',
+                              body=results_dump,
+                              properties=pika.BasicProperties(
+                                  # todo this should include the chunk id
+                                  headers={'guid': self.guid},
+                                  delivery_mode=2))
+
