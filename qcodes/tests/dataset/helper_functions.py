442c4bfc5743035e4249c898697aea46975bb88a
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2018-12-10 14:23:49 +0100

    Test content of data arrays

--- qcodes/tests/dataset/helper_functions.py
+++ qcodes/tests/dataset/helper_functions.py
@@ -1,22 +1,25 @@
 from typing import Sequence, Tuple, Dict
 
 import numpy as np
-
+from numpy.testing import assert_allclose
 
 def verify_data_dict(data: Dict[str, Dict[str, np.ndarray]],
                      parameter_names: Sequence[str],
                      expected_names: Dict[str, Sequence[str]],
-                     expected_shapes: Dict[str, Sequence[Tuple[int, ...]]]) -> None:
+                     expected_shapes: Dict[str, Sequence[Tuple[int, ...]]],
+                     expected_values: Dict[str, Sequence[np.ndarray]]) -> None:
     """
     Simple helper function to verify a dict of data
 
     Args:
-        data: The data to verify
-        parameter_names: names of the parameters requested
-        expected_names: names of the parameters expected to be loaded
-        expected_shapes: shapes of the parameters loaded
-
-    Returns:
+        data: The dict data to verify the shape and content of.
+        parameter_names: names of the parameters loaded as top level
+            keys in the dict.
+        expected_names: names of the parameters expected as keys in the second
+            level.
+        expected_shapes: expected shapes of the parameters loaded in the values
+            of the dict.
+        expected_values: expected content of the data arrays.
 
     """
     # check that all the expected parameters in the dict are
@@ -26,11 +29,14 @@ def verify_data_dict(data: Dict[str, Dict[str, np.ndarray]],
         innerdata = data[param]
         verify_data_dict_for_single_param(innerdata,
                                           expected_names[param],
-                                          expected_shapes[param])
+                                          expected_shapes[param],
+                                          expected_values[param])
 
 
 def verify_data_dict_for_single_param(datadict: Dict[str, np.ndarray],
                                       names: Sequence[str],
-                                      shapes: Sequence[Tuple[int, ...]]):
-    for name, shape in zip(names, shapes):
+                                      shapes: Sequence[Tuple[int, ...]],
+                                      values):
+    for name, shape, value in zip(names, shapes, values):
         assert datadict[name].shape == shape
+        np.testing.assert_allclose(datadict[name], value)
