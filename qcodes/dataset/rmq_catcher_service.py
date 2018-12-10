953e552f887f03917f0d1ac737f5818131d09749
WilliamHPNielsen <whpn@mailbox.org>
2018-12-10 13:54:56 +0100

    Refactor catcher service to use reader and writer

--- qcodes/dataset/rmq_catcher_service.py
+++ qcodes/dataset/rmq_catcher_service.py
@@ -2,13 +2,15 @@
 # writes them to disk
 
 import argparse
-from typing import List, Dict
+from typing import List, Dict, Optional
 import time
 import datetime
 
 from qcodes.dataset.rmq_queue_consumer import QueueConsumer, RMQConsumer
-from qcodes.dataset.data_storage_interface import DataStorageInterface
-from qcodes.dataset.sqlite_storage_interface import SqliteStorageInterface
+from qcodes.dataset.data_storage_interface import (DataReaderInterface,
+                                                   DataWriterInterface)
+from qcodes.dataset.sqlite_storage_interface import (SqliteReaderInterface,
+                                                     SqliteWriterInterface)
 
 
 class Catcher:
@@ -18,24 +20,35 @@ class Catcher:
     """
 
     def __init__(self, consumer: type,
-                 local_reader_writer: type,
-                 consumer_kwargs: Dict = {}):
+                 reader: type = SqliteReaderInterface,
+                 writer: type = SqliteWriterInterface,
+                 consumer_kwargs: Optional[Dict] = None,
+                 reader_kwargs: Optional[Dict] = None,
+                 writer_kwargs: Optional[Dict] = None):
+
+        consumer_kwargs = consumer_kwargs or {}
+        reader_kwargs = reader_kwargs or {}
+        writer_kwargs = writer_kwargs or {}
 
         if not issubclass(consumer, QueueConsumer):
             raise ValueError('Received an invalid consumer, '
                              f'f{consumer}, which is not a '
                              'subclass of QueueConsumer')
 
-        # NB: as soon as possible, we should split the DataStorageInterface
-        # into a reader and a writer and use those here
-        if not issubclass(local_reader_writer, DataStorageInterface):
-            raise ValueError('Received an invalid local_reader_writer, '
-                             f'f{local_reader_writer}, which is not a '
-                             'subclass of DataStorageInterface')
+        if not issubclass(reader, DataReaderInterface):
+            raise ValueError('Received an invalid local storage reader, '
+                             f'f{reader}, which is not a '
+                             'subclass of DataReaderInterface')
+
+        if not issubclass(writer, DataWriterInterface):
+            raise ValueError('Received an invalid local storage writer, '
+                             f'f{writer}, which is not a '
+                             'subclass of DataWriterInterface')
 
         self.consumer = consumer(callback=self.message_callback,
                                  **consumer_kwargs)
-        self.rw_factory = local_reader_writer
+        self.reader_factory = reader
+        self.writer_factory = writer
 
         self.active_guids: List[str] = []
 
@@ -51,7 +64,8 @@ class Catcher:
 
 def current_time() -> str:
     ts_raw = time.time()
-    ts = datetime.utcfromtimestamp(ts_raw).strftime('%Y-%m-%d %H:%M:%S')
+    time_fmt = '%Y-%m-%d %H:%M:%S'
+    ts = datetime.datetime.utcfromtimestamp(ts_raw).strftime(time_fmt)
     return ts
 
 
@@ -61,8 +75,7 @@ if __name__ == "__main__":
                                                   'service'))
     args = parser.parse_args()
 
-    catcher = Catcher(consumer=RMQConsumer,
-                      local_reader_writer=SqliteStorageInterface)
+    catcher = Catcher(consumer=RMQConsumer)
 
     print(f'Started RabbitMQ Local Storage Consumer at {current_time()}')
     print('Press Ctrl-C to stop')
