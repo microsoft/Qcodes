68aee5929267a035caa7e60f06bd7f01e78ce8bb
WilliamHPNielsen <whpn@mailbox.org>
2018-12-12 15:37:11 +0100

    Squash: Update MetaData class

--- qcodes/dataset/data_storage_interface.py
+++ qcodes/dataset/data_storage_interface.py
@@ -36,15 +36,15 @@ class MetaData:
     Metadata class holds information that comes together with the actual
     data but is not the actual data.
     """
-    run_description: RunDescriber
-    name: str
-    exp_name: str
-    sample_name: str
-    run_started: Optional[float] = None
-    run_completed: Optional[float] = None
-    snapshot: Optional[dict] = None
-    tags: Dict[str, Any] = field(default_factory=dict)
-    tier: int = 1
+    run_description: _Optional[str] = NOT_GIVEN
+    exp_name: _Optional[str] = NOT_GIVEN
+    sample_name: _Optional[str] = NOT_GIVEN
+    name: _Optional[str] = NOT_GIVEN
+    run_started: _Optional[Optional[float]] = NOT_GIVEN
+    run_completed: _Optional[Optional[float]] = NOT_GIVEN
+    snapshot: _Optional[Optional[dict]] = NOT_GIVEN
+    tags: _Optional[Dict[str, Any]] = NOT_GIVEN
+    tier: _Optional[int] = NOT_GIVEN
 
 
 class DataReaderInterface(ABC):
