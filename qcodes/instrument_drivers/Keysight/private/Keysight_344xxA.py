6104cd7fc51444439e35201e17302184a7079478
Mikhail Astafev <astafan8@gmail.com>
2019-01-04 19:41:46 +0100

    CLarify docstrings for error and flush_error_queue methods

--- qcodes/instrument_drivers/Keysight/private/Keysight_344xxA.py
+++ qcodes/instrument_drivers/Keysight/private/Keysight_344xxA.py
@@ -593,7 +593,17 @@ class _Keysight_344xxA(VisaInstrument):
 
     def error(self) -> Tuple[int, str]:
         """
-        Return the first error message in the queue.
+        Return the first error message in the queue. It also clears it from
+        the error queue.
+
+        Up to 20 errors can be stored in the instrument's error queue.
+        Error retrieval is first-in-first-out (FIFO).
+
+        If more than 20 errors have occurred, the most recent error stored
+        in the queue is replaced with -350,"Queue overflow". No additional
+        errors are stored until you remove errors from the queue. If no
+        errors have occurred when you read the error queue, the instrument
+        responds with +0,"No error".
 
         Returns:
             The error code and the error message.
@@ -606,7 +616,7 @@ class _Keysight_344xxA(VisaInstrument):
 
     def flush_error_queue(self, verbose: bool=True) -> None:
         """
-        Clear the instrument error queue.
+        Clear the instrument error queue, and prints it.
 
         Args:
             verbose: If true, the error messages are printed.
