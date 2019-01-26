a18eab6ff820843ee8eea36ada915b24d89f67e8
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2019-01-26 18:24:15 +0100

    correct v3 hash

--- qcodes/tests/dataset/legacy_DB_generation/utils.py
+++ qcodes/tests/dataset/legacy_DB_generation/utils.py
@@ -24,10 +24,7 @@ from git import Repo
 GIT_HASHES: Dict[int, str] = {0: '78d42620fc245a975b5a615ed5e33061baac7846',
                               1: '056d59627e22fa3ca7aad4c265e9897c343f79cf',
                               2: '5202255924542dad6841dfe3d941a7f80c43956c',
-                              # NOTE: commit hash associated with version 3
-                              # will need to be updated once we move to
-                              # version 4
-                              3: 'a5d8edae05898df0cbfb5d9048565df91efea692'}
+                              3: '01cb405d47b72b1a5bab227dcf4b69866ecebb0f'}
 
 gitrepopath = os.sep.join(os.path.realpath(__file__).split(os.sep)[:-5])
 repo = Repo(gitrepopath)
