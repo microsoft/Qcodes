b48fb9ca69a441a8954b281d73cbc03ce929753e
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2019-02-15 14:40:27 +0100

    Correct rst syntax

--- qcodes/monitor/monitor.py
+++ qcodes/monitor/monitor.py
@@ -10,15 +10,13 @@ Monitor a set of parameters in a background thread
 stream output over websocket
 
 To start monitor, run this file, or if qcodes is installed as a module:
-```
-% python -m qcodes.monitor.monitor
-```
+
+``% python -m qcodes.monitor.monitor``
 
 Add parameters to monitor in your measurement by creating a new monitor with a list
 of parameters to monitor:
-```
-monitor = qcodes.Monitor(param1, param2, param3, ...)
-```
+
+``monitor = qcodes.Monitor(param1, param2, param3, ...)``
 """
 import sys
 import logging
