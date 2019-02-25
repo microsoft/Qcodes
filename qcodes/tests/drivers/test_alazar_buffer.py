d55ad1454c64c78198d34e7fd34ae58aad5225fd
Mikhail Astafev <astafan8@gmail.com>
2019-02-25 13:19:44 +0100

    Add a primitive test for alazar dma buffer

---
+++ qcodes/tests/drivers/test_alazar_buffer.py
@@ -0,0 +1,31 @@
+import ctypes
+
+import pytest
+
+from qcodes.instrument_drivers.AlazarTech.ATS import Buffer
+from qcodes.instrument_drivers.AlazarTech.ATS import os as ats_os
+
+
+def test_buffer_initiates_only_on_windows(monkeypatch):
+    with monkeypatch.context() as m:
+        m.setattr(ats_os, 'name', 'nt')
+
+        b = Buffer(ctypes.c_uint8, 25)
+    
+    with monkeypatch.context() as m:
+        m.setattr(ats_os, 'name', 'other_os')
+
+        with pytest.raises(Exception, match="Unsupported OS"):
+            b = Buffer(ctypes.c_uint8, 25)
+
+
+def test_buffer_is_allocated_when_initiated():
+    b = Buffer(ctypes.c_uint8, 25)
+    assert b._allocated is True
+
+
+@pytest.mark.parametrize('ctype', (ctypes.c_uint8, ctypes.c_uint16, 
+                                   ctypes.c_uint32, ctypes.c_int32, 
+                                   ctypes.c_float))
+def test_supported_ctypes_for_sample(ctype):
+    b = Buffer(ctype, 8)
