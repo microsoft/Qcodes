52320457e8af838a0ba7b731944fb5b570404603
Simon Zihlmann <simon.zihlmann@unibas.ch>
2019-03-10 16:03:42 +0100

    style: remove trailing white space

--- qcodes/dataset/sqlite_base.py
+++ qcodes/dataset/sqlite_base.py
@@ -256,7 +256,7 @@ def _convert_numeric(value: bytes) -> Union[float, int, str]:
     # outcome is 'nan'
     if np.isnan(numeric):
         return numeric
-    
+
     # Then we check if the outcome is 'inf', includes +inf and -inf
     if np.isinf(numeric):
         return numeric
