c65c7c863493df938246877a79b07f9d4af121e9
Maximilian Götsch <maximilian.goetsch@student.uibk.ac.at>
2019-04-09 10:03:26 +0200

    feat: Add driver for the Mightex SSE-1304-U CCD spectrometer

---
+++ qcodes/instrument_drivers/mightex/SSE1304U.py
@@ -0,0 +1,175 @@
+from pathlib import Path
+from typing import Union
+import ctypes as ct
+import numpy as np
+import numpy.ctypeslib as npct
+
+import qcodes as qc
+import qcodes.utils.validators as vals
+
+from .dllwrapper import MightexDLLWrapper, FrameRecord  
+
+class WavelengthAxis(qc.Parameter):
+    ''' Holds the wavelengths corresponding to CCD pixels. '''
+    
+    def get_raw(self) -> np.ndarray:
+        return self.root_instrument.calib(np.arange(0, 3648))
+    
+
+class IntensitySpectrum(qc.ParameterWithSetpoints):
+    ''' Holds the measured intensity. '''
+    
+    def get_raw(self) -> np.ndarray:
+        ''' Get an averaged frame with dark data subtracted. '''
+        avg = self.root_instrument.averaging()
+        frames = []
+        
+        for i in range(avg):
+            frame = self.root_instrument.get_single_frame()
+            frames.append(frame - self.root_instrument.drkdata)
+            
+        return sum(frames) / avg
+        
+    
+class MightexSSE_1304_U(qc.Instrument):
+    ''' Driver for the Mightex SSE-1304-U spectrometer. '''
+    
+    def __init__(self, name: str,
+                 dll_path: Union[str, Path]=r'C:\Windows\System32',
+                 dev_id: int=1, **kwargs):
+        super().__init__(name, **kwargs)
+        
+        self._dll = MightexDLLWrapper(dll_path)
+        self._dev_id = dev_id
+        
+        self.add_parameter('exposure_time',
+                           unit='ms',
+                           set_cmd=self.set_exp_t,
+                           get_cmd=self.get_exp_t,
+                           get_parser=float,
+                           vals=qc.validators.Numbers(0.1, 6500))
+        
+        self.add_parameter('device_on',
+                           set_cmd=self.set_onoff,
+                           get_cmd=self.is_on,
+                           vals=qc.validators.Bool())
+        
+        self.add_parameter('serial',
+                           get_cmd=self.get_serialno)
+        
+        self.add_parameter('auto_drk_on',
+                           set_cmd=self.set_auto_drk,
+                           get_cmd=self.is_auto_drk,
+                           vals=qc.validators.Bool())
+        
+        self.add_parameter('wl_axis',
+                           parameter_class=WavelengthAxis,
+                           vals=vals.Arrays(shape=(3648,)),
+                           label='Wavelength',
+                           unit='nm')
+        
+        self.add_parameter('spectrum',
+                           parameter_class=IntensitySpectrum,
+                           setpoints=(self.wl_axis,),
+                           vals=vals.Arrays(shape=(3648,)),
+                           label='Intensity')
+        
+        self.add_parameter('averaging',
+                           get_cmd=self.get_averaging,
+                           set_cmd=self.set_averaging,
+                           vals=vals.Ints(1))
+        
+        self.add_parameter('darkframe',
+                           get_cmd=self.rec_darkframe,
+                           set_cmd=self.set_darkframe,
+                           vals=vals.Arrays(shape=(3648,)))
+        
+        self.init_device()
+    
+    def init_device(self):
+        ''' Prepares the spectrometer for recording. '''
+        self.free_device()  # there might be leftovers from previous runs
+        
+        n_connected = self._dll.init(None)
+        if n_connected < self._dev_id:
+            raise RuntimeError(f'No Mightex with id {self._dev_id} connected')
+            
+        self.device_on(True)
+        self.exposure_time(0.1)
+        self.averaging(1)
+        self.drkdata = np.zeros((3648,))
+        
+    def free_device(self):
+        ''' Releases all resources for the spectrometer. '''
+        self._dll.uninit()
+    
+    def __del__(self):
+        self.free_device()
+        
+    def set_exp_t(self, t: float) -> None:
+        ''' Sets the exposure time to `t` milliseconds. '''
+        # The spectrometer can only handle multiples of 100 microseconds
+        self._exp_t = int(round(t * 10) * 100)
+        self._dll.set_exposuretime(self._dev_id, self._exp_t)
+        
+    def get_exp_t(self) -> float:
+        return self._exp_t / 1e3  # in milliseconds
+    
+    def set_onoff(self, on: bool) -> None:
+        ''' Turns the device on and off. '''
+        self._is_on = on
+        self._dll.set_activestatus(self._dev_id, 1 if on else 0)
+    
+    def is_on(self) -> bool:
+        return self._is_on
+    
+    def get_serialno(self) -> str:
+        ''' Retrieves the device's serial number. '''
+        mod_no = ct.create_string_buffer(b'\000' * 32)
+        ser_no = ct.create_string_buffer(b'\000' * 32)
+        self._dll.get_serialno(self._dev_id, mod_no, ser_no)
+        return mod_no.value.decode() + ':' + ser_no.value.decode()
+    
+    def set_auto_drk(self, on: bool) -> None:
+        ''' Sets auto dark compensation for the spectrometer. '''
+        self._auto_drk_on = on
+        self._dll.set_auto_dark(self._dev_id, 1, 1 if on else 0)
+    
+    def is_auto_drk(self) -> bool:
+        return self._auto_drk_on
+    
+    def get_single_frame(self) -> np.ndarray:
+        ''' Retrieves a raw single frame from the spectrometer. '''
+        buffer = ct.POINTER(FrameRecord)()
+        
+        self._dll.start_framegrab(self._dev_id)
+        self._dll.get_framedata(self._dev_id, 1, 1, ct.byref(buffer))
+        
+        return npct.as_array(buffer.contents.CCDData, shape=(3648,)).copy()
+    
+    @property
+    def calib(self):
+        ''' Returns a function to calibrate pixels to wavelength. '''
+        # These values are taken from the default calibration file
+        # shipped with the device.
+        return np.poly1d([
+                8.86531947319252E-10,
+                -6.85899136487025E-6,
+                -0.105986182449581,
+                1019.5211382018
+                ])
+    
+    def get_averaging(self) -> int:
+        return self._avg
+    
+    def set_averaging(self, avg: int):
+        ''' Sets the number of frames that should be averaged
+        for each measurement. '''
+        self._avg = avg if avg > 0 else 1
+    
+    def rec_darkframe(self) -> np.ndarray:
+        self.drkdata = self.get_single_frame()
+        return self.drkdata
+    
+    def set_darkframe(self, drk: np.ndarray):
+        self.drkdata = drk
---
+++ qcodes/instrument_drivers/mightex/dllwrapper.py
@@ -0,0 +1,54 @@
+import ctypes as ct
+import numpy.ctypeslib as npct
+
+''' This module communicates with the SSE SDK dll provided by Mightex. '''
+
+
+class FrameRecord(ct.Structure):
+    ''' This struct is used by the DLL to communicate recorded data. '''
+    _fields_ = [
+            ('CCDData', ct.POINTER(ct.c_double)),
+            ('CalibData', ct.POINTER(ct.c_double)),
+            ('AbsIntData', ct.POINTER(ct.c_double))]
+    
+    
+class MightexDLLWrapper(object):
+    ''' Provides the functions from the Mightex SDK dll. For documentation
+    on the API see the manual provided with the SDK. '''
+    
+    def __init__(self, dllpath=r'C:\windows\system32'):
+        self._dll = npct.load_library('MT_Spectrometer_SDK.dll', dllpath)
+        
+        ## map the dll functions to class methods
+        
+        self.init = self._dll.MTSSE_InitDevice
+        self.init.argtypes = [ct.c_void_p]
+        
+        self.uninit = self._dll.MTSSE_UnInitDevice
+        self.uninit.argtypes = []
+        
+        self.start_framegrab = self._dll.MTSSE_StartFrameGrab
+        self.start_framegrab.argtypes = [ct.c_int]
+        
+        self.stop_framegrab = self._dll.MTSSE_StopFrameGrab
+        self.stop_framegrab.argtypes = []
+        
+        self.get_framedata = self._dll.MTSSE_GetDeviceSpectrometerFrameData
+        self.get_framedata.argtypes = [ct.c_int, ct.c_int, ct.c_int,
+                                       ct.POINTER(ct.POINTER(FrameRecord))]
+        
+        self.get_serialno = self._dll.MTSSE_GetDeviceModuleNoSerialNo
+        self.get_serialno.argtypes = [ct.c_int, ct.c_char_p,
+                                      ct.c_char_p]
+        
+        self.set_exposuretime = self._dll.MTSSE_SetDeviceExposureTime
+        self.set_exposuretime.argtypes = [ct.c_int, ct.c_int]
+        
+        self.set_avg_framenum = self._dll.MTSSE_SetDeviceAverageFrameNum
+        self.set_avg_framenum.argtypes = [ct.c_int, ct.c_int]
+        
+        self.set_activestatus = self._dll.MTSSE_SetDeviceActiveStatus
+        self.set_activestatus.argtypes = [ct.c_int, ct.c_int]
+        
+        self.set_auto_dark = self._dll.MTSSE_SetDeviceSpectrometerAutoDarkStatus
+        self.set_auto_dark.argtypes = [ct.c_int, ct.c_int, ct.c_int]
