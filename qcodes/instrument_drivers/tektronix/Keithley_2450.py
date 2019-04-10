641afdc212531feaf9aaa31968e823c09f16f85b
sohailc <schatoor@gmail.com>
2019-04-10 16:18:50 -0700

    Complete re-write of driver

--- qcodes/instrument_drivers/tektronix/Keithley_2450.py
+++ qcodes/instrument_drivers/tektronix/Keithley_2450.py
@@ -1,97 +1,280 @@
-import logging
-from typing import Union
+import re
+from typing import Dict, Union, Any
 import visa
 
-from qcodes import VisaInstrument
-from qcodes.instrument_drivers.tektronix.Keithley_2400 import Keithley_2400
+from qcodes import VisaInstrument, InstrumentChannel
+from qcodes.instrument.parameter import Parameter, _BaseParameter
+import qcodes.utils.validators as vals
 
 
-log = logging.getLogger(__name__)
-
-
-class _Keithley_2450(VisaInstrument):
+class JustInTimeParameter(_BaseParameter):
     """
-    A 2450 driver with the full set of features. Note that the command set
-    between the 2400 and 2450 are different (when not running in compatibility mode)
-    and its not just a question of adjusting ranges or other minor modifications.
-    Please consult appendix E of the 2450 manual for the (substantial) differences
-    """
+    Part of the set and get command strings are resolved right before the
+    `ask_raw` and `write_raw` are called.
 
-    def __init__(self, name, address, **kwargs):
-        super().__init__(name, address, **kwargs)
-        raise NotImplementedError("This driver has not been developed yet")
+    For instance, if the get_cmd is ":SENS:{function}:RANG?", then at
+    call time we look up what the latest value of the
+    "instrument.function" parameter is. If this is `CURR`, we send
+    the string ":SENS:CURR:RANG?" to the instrument.
 
-
-class Keithley_2450:
-    """
-    The driver for the Keithley 2450 source meter. We can select the command
-    set of this instrument to be compatible with the 2400, but this will reduce
-    the feature set. We will accept this for now.
-
-    Note that before doing anything else, we need to set the language of the
-    instrument to SCPI or SCPI2400 instead of the Tektronix propriety TSP
-    language, even before initializing the parent Keithley_2400 class. Otherwise
-    any SCPI command in the init of that class will likely make this driver break.
+    Everything between the curly brackets needs to be a Parameter attached
+    to the parent instrument.
     """
 
-    def __new__(
-            cls,
+    def __init__(
+            self,
             name: str,
-            address: str,
-            compatibility_mode: Union[None, bool] = None,
-            **kwargs
-    ) -> VisaInstrument:
+            instrument: Union[VisaInstrument, InstrumentChannel],
+            get_cmd: str,
+            set_cmd: str,
+            unit: str = None,
+            **kwargs: Dict[str, Any]
+    ) -> None:
+
+        super().__init__(name, instrument, **kwargs)
+        self.unit = unit
+        self._get_cmd = get_cmd
+        self._set_cmd = set_cmd
+
+    def _resolve_kwargs(self, command_string: str) -> Dict[str, Any]:
         """
         Args:
-            name
-            address
-            compatibility_mode: If None, the current compatibility mode setting will be
-                unchanged.
+            command_string: E.g. ":SENS:{function}:RANG?"
 
         Returns:
-            VisaInstrument
+            dict: E.g. {"function": "current"} if self.instrument.current.get() = "current"
+        """
+        def get_param_value(parameter_name):
+            param = self.instrument.parameters[parameter_name]
+            value = param.get_latest()
+            if value is None:
+                value = param.get()
+
+            return value
+
+        parameter_names = re.findall(r"{(\w+)}", command_string)
+
+        return {
+            param_name: get_param_value(param_name)
+            for param_name in parameter_names
+        }
+
+    def get_raw(self) -> Any:
+        kwargs = self._resolve_kwargs(self._get_cmd)
+        get_cmd = self._get_cmd.format(**kwargs)
+
+        return self.instrument.ask_raw(get_cmd)
+
+    def set_raw(self, value: Any) -> None:
+        kwargs = self._resolve_kwargs(self._set_cmd)
+        set_cmd = self._set_cmd.format(**kwargs)
+
+        self.instrument.write_raw(set_cmd.format(value))
+
+
+class Sense2450(InstrumentChannel):
+    """Sense sub-module for the Keithley 2450 source meter"""
+    range_limits = {
+        "CURR": vals.Numbers(10E-9, 1),
+        "RES": vals.Numbers(20, 200E6),
+        "VOLT": vals.Numbers(0.02, 200)
+    }
+
+    def __init__(self, parent: 'Keithley2450', name: str) -> None:
+        super().__init__(parent, name)
+
+        self.add_parameter(
+            "function",
+            set_cmd=":SENS:FUNC \"{}\"",
+            get_cmd=":SENS:FUNC?",
+            vals=vals.Enum(
+                "CURR",
+                "VOLT",
+                "RES"
+            ),
+            get_parser=self._function_get_parser
+        )
+
+        self.add_parameter(
+            "four_wire_measurement",
+            set_cmd=":SENSe:{function}:RSENse {{}}",
+            get_cmd=":SENSe:{function}:RSENse?",
+            val_mapping={
+                True: "1",
+                False: "0"
+            },
+            parameter_class=JustInTimeParameter
+        )
+
+        self.add_parameter(
+            "range",
+            set_cmd=":SENSe:{function}:RANGe {{}}",
+            get_cmd=":SENSe:{function}:RANGe?",
+            set_parser=self._range_validator,
+            get_parser=float,
+            parameter_class=JustInTimeParameter
+        )
+
+    @staticmethod
+    def _function_get_parser(get_value: str) -> str:
+        """
+        Args:
+            get_value: ""CURR:DC"" (Note the quotation marks in the string)
+
+        Returns:
+            str: "CURR"
         """
 
+        return_value = get_value.strip("\"")
+        if return_value.endswith(":DC"):
+            return_value = return_value.split(":")[0]
+
+        return return_value
+
+    def _range_validator(self, range_value: float) -> float:
+        """Validate that the range, given the function, is correct"""
+        validator = Sense2450.range_limits[self.function()]
+        validator.validate(range_value)
+        return range_value
+
+
+class Source2450(InstrumentChannel):
+    """Source sub-module for the Keithley 2450 source meter"""
+    range_limits = {
+        "CURR": vals.Numbers(-1, 1),
+        "VOLT": vals.Numbers(-200, 200)
+    }
+
+    def __init__(self, parent: 'Keithley2450', name: str):
+        super().__init__(parent, name)
+
+        self.add_parameter(
+            "function",
+            set_cmd="SOUR:FUNC {}",
+            get_cmd="SOUR:FUNC?",
+            vals=vals.Enum(
+                "CURR",
+                "VOLT"
+            )
+        )
+
+        self.add_parameter(
+            "range",
+            set_cmd=":SOUR:{function}:RANGe {{}}",
+            get_cmd=":SOUR:{function}:RANGe?",
+            set_parser=self._range_validator,
+            get_parser=float,
+            parameter_class=JustInTimeParameter
+        )
+
+        for source_name, unit in [("current", "A"), ("voltage", "V")]:
+            self.add_parameter(
+                f"{source_name}_setpoint",
+                set_cmd=f"SOUR:{source_name} {{}}",
+                get_cmd=f"SOUR:{source_name}?",
+                unit=unit,
+                get_parser=float
+            )
+
+    def _range_validator(self, range_value: float) -> float:
+        """Validate that the range, given the function, is correct"""
+        validator = Source2450.range_limits[self.function()]
+        validator.validate(range_value)
+        return range_value
+
+
+class Keithley2450(VisaInstrument):
+    """
+    The QCoDeS driver for the Keithley 2450 source meter
+    """
+    @staticmethod
+    def set_correct_protocol(address: str) -> None:
+        """
+        The correct communication protocol is SCPI, make sure this is set
+
+        Args:
+            address: Visa resource address
+        """
+        Keithley2450._check_and_adjust_protocol(address)
+
+    @staticmethod
+    def _check_and_adjust_protocol(address, raise_on_fail=False) -> None:
+        """
+        Args:
+            address: Visa resource address
+            raise_on_fail: If True and the language mode is anything other then "SCPI", raise a runtime error.
+                If 'raise_on_fail' is False and the language mode is anything other then "SCPI"
+                adjust the language to "SCPI"
+        """
         resource_manager = visa.ResourceManager()
         raw_instrument = resource_manager.open_resource(address)
-        current_language = raw_instrument.query("*LANG?").strip()
+        language = raw_instrument.query("*LANG?")
 
-        if current_language == "TSP":
-            log.warning("The instrument is in TSP mode which is not supported.")
-            if compatibility_mode is None:
-                log.warning("Switching 'compatibility mode' to 'True'")
-                compatibility_mode = True
+        if language != "SCPI":
+            if raise_on_fail:
+                raise RuntimeError(
+                    f"The instrument is in {language} mode which is not supported."
+                    f"Please run `Keithley2450.set_correct_protocol(address)` and try to "
+                    f"initialize the driver again"
+                )
+            else:
+                raw_instrument.write("*LANG SCPI")
+                print("PLease power cycle the instrument to make the change take effect")
 
-        if compatibility_mode is None:
-            target_language = current_language
-        elif compatibility_mode:
-            target_language = "SCPI2400"
-        else:
-            target_language = "SCPI"
+        raw_instrument.close()
 
-        if current_language != target_language:
+    def __init__(self, name: str, address: str, **kwargs) -> None:
 
-            raw_instrument.write(f"*LANG {target_language}")
+        # Before doing anything else, make sure the instrument has the correct
+        # protocol mode (SCPI), else raise a runtime error
+        Keithley2450._check_and_adjust_protocol(address, raise_on_fail=True)
 
-            raise RuntimeError(
-                f"A language change from {current_language} to {target_language} "
-                f"is needed. This has been adjusted, but a power cycle is "
-                f"needed to make this take effect. Please reboot the instrument "
-                f"and try again"
-            )
+        super().__init__(name, address, terminator='\n', **kwargs)
 
-        if current_language == "SCPI2400":
-            driver_instance = Keithley_2400(name, address, **kwargs)
+        self.add_parameter(
+            "terminals",
+            set_cmd="ROUTe:TERMinals {}",
+            get_cmd="ROUTe:TERMinals?",
+            vals=vals.Enum("REAR", "FRONT")
+        )
 
-            driver_instance.log.warning(
-                "This driver is running in compatibility mode with the Keithley 2400. "
-                "This means that a number of newer features introduced in the 2450 will "
-                "not be available. Please consult Appendix D of the 2450 manual to see if "
-                "this is acceptable to you"
-            )
+        self.add_parameter(
+            "output",
+            set_cmd=":OUTP {}",
+            get_cmd=":OUTP?",
+            val_mapping={
+                True: "1",
+                False: "0"
+            }
+        )
 
-        else:
-            driver_instance = _Keithley_2450(name, address, **kwargs)
+        self.add_submodule(
+            "sense",
+            Sense2450(self, "sense")
+        )
 
+        self.add_submodule(
+            "source",
+            Source2450(self, "source")
+        )
 
-        return driver_instance
+        self.connect_message()
+
+    @property
+    def measure(self) -> Parameter:
+
+        units = {
+            "CURR": "A",
+            "VOLT": "V",
+            "RES": "Ohm"
+        }
+        sense_function = self.sense.function()
+        unit = units[sense_function]
+
+        return Parameter(
+            "measure",
+            instrument=self,
+            get_cmd=":MEASure?",
+            unit=unit,
+            get_parser=float
+        )
