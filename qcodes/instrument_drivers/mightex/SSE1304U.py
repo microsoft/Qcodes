4567ce0aff175406483ddfa0c11750cb237474d4
Maximilian Götsch <maximilian.goetsch@student.uibk.ac.at>
2019-04-17 12:37:33 +0200

    use double quotes for docstrings
    Mightex Spectrometer parameters now check for the right type of the root instrument

--- qcodes/instrument_drivers/mightex/SSE1304U.py
+++ qcodes/instrument_drivers/mightex/SSE1304U.py
@@ -1,3 +1,5 @@
+""" This module provides a driver for the Mightex SSE-1304-U spectrometer. """
+
 from pathlib import Path
 from typing import Union
 import ctypes as ct
@@ -10,29 +12,47 @@ import qcodes.utils.validators as vals
 from .dllwrapper import MightexDLLWrapper, FrameRecord  
 
 class WavelengthAxis(qc.Parameter):
-    ''' Holds the wavelengths corresponding to CCD pixels. '''
+    """ Holds the wavelengths corresponding to CCD pixels. """
     
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        if not isinstance(self.root_instrument, MightexSSE_1304_U):
+            raise ValueError('WavelengthAxis added to the wrong instrument '
+                             'type. This parameter is only compatible with'
+                             'MightexSSE_1304_U.')
+        self.spectrometer: MightexSSE_1304_U = self.root_instrument
+        
     def get_raw(self) -> np.ndarray:
-        return self.root_instrument.calib(np.arange(0, 3648))
+        return self.spectrometer.calib(np.arange(0, 3648))
     
 
 class IntensitySpectrum(qc.ParameterWithSetpoints):
-    ''' Holds the measured intensity. '''
+    """ Holds the measured intensity. """
     
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        if not isinstance(self.root_instrument, MightexSSE_1304_U):
+            raise ValueError('WavelengthAxis added to the wrong instrument '
+                             'type. This parameter is only compatible with'
+                             'MightexSSE_1304_U.')
+        self.spectrometer: MightexSSE_1304_U = self.root_instrument
+        
     def get_raw(self) -> np.ndarray:
-        ''' Get an averaged frame with dark data subtracted. '''
-        avg = self.root_instrument.averaging()
+        """ Get an averaged frame with dark data subtracted. """
+        avg = self.spectrometer.averaging()
         frames = []
         
         for i in range(avg):
-            frame = self.root_instrument.get_single_frame()
-            frames.append(frame - self.root_instrument.drkdata)
+            frame = self.spectrometer.get_single_frame()
+            frames.append(frame - self.spectrometer.drkdata)
             
         return sum(frames) / avg
         
     
 class MightexSSE_1304_U(qc.Instrument):
-    ''' Driver for the Mightex SSE-1304-U spectrometer. '''
+    """ Driver for the Mightex SSE-1304-U spectrometer. """
     
     def __init__(self, name: str,
                  dll_path: Union[str, Path]=r'C:\Windows\System32',
@@ -87,7 +107,7 @@ class MightexSSE_1304_U(qc.Instrument):
         self.init_device()
     
     def init_device(self):
-        ''' Prepares the spectrometer for recording. '''
+        """ Prepares the spectrometer for recording. """
         self.free_device()  # there might be leftovers from previous runs
         
         n_connected = self._dll.init(None)
@@ -100,14 +120,14 @@ class MightexSSE_1304_U(qc.Instrument):
         self.drkdata = np.zeros((3648,))
         
     def free_device(self):
-        ''' Releases all resources for the spectrometer. '''
+        """ Releases all resources for the spectrometer. """
         self._dll.uninit()
     
     def __del__(self):
         self.free_device()
         
     def set_exp_t(self, t: float) -> None:
-        ''' Sets the exposure time to `t` milliseconds. '''
+        """ Sets the exposure time to `t` milliseconds. """
         # The spectrometer can only handle multiples of 100 microseconds
         self._exp_t = int(round(t * 10) * 100)
         self._dll.set_exposuretime(self._dev_id, self._exp_t)
@@ -116,7 +136,7 @@ class MightexSSE_1304_U(qc.Instrument):
         return self._exp_t / 1e3  # in milliseconds
     
     def set_onoff(self, on: bool) -> None:
-        ''' Turns the device on and off. '''
+        """ Turns the device on and off. """
         self._is_on = on
         self._dll.set_activestatus(self._dev_id, 1 if on else 0)
     
@@ -124,14 +144,14 @@ class MightexSSE_1304_U(qc.Instrument):
         return self._is_on
     
     def get_serialno(self) -> str:
-        ''' Retrieves the device's serial number. '''
+        """ Retrieves the device's serial number. """
         mod_no = ct.create_string_buffer(b'\000' * 32)
         ser_no = ct.create_string_buffer(b'\000' * 32)
         self._dll.get_serialno(self._dev_id, mod_no, ser_no)
         return mod_no.value.decode() + ':' + ser_no.value.decode()
     
     def set_auto_drk(self, on: bool) -> None:
-        ''' Sets auto dark compensation for the spectrometer. '''
+        """ Sets auto dark compensation for the spectrometer. """
         self._auto_drk_on = on
         self._dll.set_auto_dark(self._dev_id, 1, 1 if on else 0)
     
@@ -139,7 +159,7 @@ class MightexSSE_1304_U(qc.Instrument):
         return self._auto_drk_on
     
     def get_single_frame(self) -> np.ndarray:
-        ''' Retrieves a raw single frame from the spectrometer. '''
+        """ Retrieves a raw single frame from the spectrometer. """
         buffer = ct.POINTER(FrameRecord)()
         
         self._dll.start_framegrab(self._dev_id)
@@ -149,7 +169,7 @@ class MightexSSE_1304_U(qc.Instrument):
     
     @property
     def calib(self):
-        ''' Returns a function to calibrate pixels to wavelength. '''
+        """ Returns a function to calibrate pixels to wavelength. """
         # These values are taken from the default calibration file
         # shipped with the device.
         return np.poly1d([
@@ -163,8 +183,8 @@ class MightexSSE_1304_U(qc.Instrument):
         return self._avg
     
     def set_averaging(self, avg: int):
-        ''' Sets the number of frames that should be averaged
-        for each measurement. '''
+        """ Sets the number of frames that should be averaged
+        for each measurement. """
         self._avg = avg if avg > 0 else 1
     
     def rec_darkframe(self) -> np.ndarray:
--- qcodes/instrument_drivers/mightex/dllwrapper.py
+++ qcodes/instrument_drivers/mightex/dllwrapper.py
@@ -1,11 +1,11 @@
+""" This module communicates with the SSE SDK dll provided by Mightex. """
+
 import ctypes as ct
 import numpy.ctypeslib as npct
 
-''' This module communicates with the SSE SDK dll provided by Mightex. '''
-
 
 class FrameRecord(ct.Structure):
-    ''' This struct is used by the DLL to communicate recorded data. '''
+    """ This struct is used by the DLL to communicate recorded data. """
     _fields_ = [
             ('CCDData', ct.POINTER(ct.c_double)),
             ('CalibData', ct.POINTER(ct.c_double)),
@@ -13,13 +13,13 @@ class FrameRecord(ct.Structure):
     
     
 class MightexDLLWrapper(object):
-    ''' Provides the functions from the Mightex SDK dll. For documentation
-    on the API see the manual provided with the SDK. '''
+    """ Provides the functions from the Mightex SDK dll. For documentation
+    on the API see the manual provided with the SDK. """
     
     def __init__(self, dllpath=r'C:\windows\system32'):
         self._dll = npct.load_library('MT_Spectrometer_SDK.dll', dllpath)
         
-        ## map the dll functions to class methods
+        # map the dll functions to class methods
         
         self.init = self._dll.MTSSE_InitDevice
         self.init.argtypes = [ct.c_void_p]
