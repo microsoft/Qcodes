d81f77b8bcfb99763b262a2a267aea5565f1791c
WilliamHPNielsen <whpn@mailbox.org>
2019-04-26 10:52:41 +0200

    Remove add_parameter from test file

--- qcodes/tests/dataset/test_dataset_basic.py
+++ qcodes/tests/dataset/test_dataset_basic.py
@@ -11,10 +11,11 @@ from hypothesis import given, settings
 import hypothesis.strategies as hst
 
 import qcodes as qc
-from qcodes import ParamSpec, new_data_set, new_experiment, experiments
+from qcodes import new_data_set, new_experiment, experiments
 from qcodes import load_by_id, load_by_counter
 from qcodes.dataset.descriptions import RunDescriber
 from qcodes.dataset.dependencies import InterDependencies_
+from qcodes.dataset.param_spec import ParamSpecBase
 from qcodes.tests.common import error_caused_by
 from qcodes.dataset.sqlite_base import _unicode_categories, get_non_dependencies
 from qcodes.dataset.database import get_DB_location
@@ -29,7 +30,7 @@ from qcodes.tests.dataset.dataset_fixtures import scalar_dataset, \
     standalone_parameters_dataset, array_in_scalar_dataset_unrolled, \
     varlen_array_in_scalar_dataset
 # pylint: disable=unused-import
-from qcodes.tests.dataset.test_descriptions import some_paramspecs
+from qcodes.tests.dataset.test_dependencies import some_interdeps
 
 pytest.register_assert_rewrite('qcodes.tests.dataset.helper_functions')
 from qcodes.tests.dataset.helper_functions import verify_data_dict
@@ -106,9 +107,10 @@ def test_dataset_states():
     with pytest.raises(RuntimeError, match=match):
         ds.add_results([{'x': 1}])
 
-    parameter = ParamSpec(name='single', paramtype='numeric',
-                          label='', unit='N/A')
-    ds.add_parameter(parameter)
+    parameter = ParamSpecBase(name='single', paramtype='numeric',
+                              label='', unit='N/A')
+    idps = InterDependencies_(standalones=(parameter,))
+    ds.set_interdependencies(idps)
 
     ds.mark_started()
 
@@ -117,11 +119,11 @@ def test_dataset_states():
     assert ds.started is True
     assert ds.completed is False
 
-    match = ('Can not add parameters to a DataSet that has '
+    match = ('Can not set interdependencies on a DataSet that has '
              'been started.')
 
     with pytest.raises(RuntimeError, match=match):
-        ds.add_parameter(parameter)
+        ds.set_interdependencies(idps)
 
     ds.add_result({parameter.name: 1})
     ds.add_results([{parameter.name: 1}])
@@ -133,11 +135,11 @@ def test_dataset_states():
     assert ds.started is True
     assert ds.completed is True
 
-    match = ('Can not add parameters to a DataSet that has '
+    match = ('Can not set interdependencies on a DataSet that has '
              'been started.')
 
     with pytest.raises(RuntimeError, match=match):
-        ds.add_parameter(parameter)
+        ds.set_interdependencies(idps)
 
     match = ('This DataSet is complete, no further '
              'results can be added to it.')
@@ -251,8 +253,7 @@ def test_add_experiments(experiment_name,
                                                           exp.exp_id,
                                                           loaded_dataset.counter)
 
-
-def test_add_paramspec(dataset):
+def test_set_interdependencies(dataset):
     exps = experiments()
     assert len(exps) == 1
     exp = exps[0]
@@ -260,13 +261,14 @@ def test_add_paramspec(dataset):
     assert exp.sample_name == "test-sample"
     assert exp.last_counter == 1
 
-    parameter_a = ParamSpec("a_param", "NUMERIC")
-    parameter_b = ParamSpec("b_param", "NUMERIC", key="value", number=1)
-    parameter_c = ParamSpec("c_param", "array", inferred_from=[parameter_a,
-                                                               parameter_b])
-    dataset.add_parameter(parameter_a)
-    dataset.add_parameter(parameter_b)
-    dataset.add_parameter(parameter_c)
+    parameter_a = ParamSpecBase("a_param", "NUMERIC")
+    parameter_b = ParamSpecBase("b_param", "NUMERIC")
+    parameter_c = ParamSpecBase("c_param", "array")
+
+    idps = InterDependencies_(
+        inferences={parameter_c: (parameter_a, parameter_b)})
+
+    dataset.set_interdependencies(idps)
 
     # write the parameters to disk
     dataset.mark_started()
@@ -284,52 +286,7 @@ def test_add_paramspec(dataset):
         ps = paramspecs[expected_param_name]
         assert ps.name == expected_param_name
 
-    assert set(paramspecs['c_param'].inferred_from_) == {'a_param', 'b_param'}
-
     assert paramspecs == dataset.paramspecs
-
-
-def test_add_paramspec_one_by_one(dataset):
-    exps = experiments()
-    assert len(exps) == 1
-    exp = exps[0]
-    assert exp.name == "test-experiment"
-    assert exp.sample_name == "test-sample"
-    assert exp.last_counter == 1
-
-    parameters = [ParamSpec("a", "NUMERIC"),
-                  ParamSpec("b", "NUMERIC", key="value", number=1),
-                  ParamSpec("c", "array")]
-    for parameter in parameters:
-        dataset.add_parameter(parameter)
-
-    # test that we can not re-add any parameter already added once
-    for param in parameters:
-        with pytest.raises(ValueError, match=f'Duplicate parameter name: '
-                                             f'{param.name}'):
-            dataset.add_parameter(param)
-
-    dataset.mark_started()
-    shadow_ds = make_shadow_dataset(dataset)
-
-    paramspecs = shadow_ds.paramspecs
-
-    expected_keys = ['a', 'b', 'c']
-    keys = sorted(list(paramspecs.keys()))
-    assert keys == expected_keys
-    for expected_param_name in expected_keys:
-        ps = paramspecs[expected_param_name]
-        assert ps.name == expected_param_name
-
-    assert paramspecs == dataset.paramspecs
-
-    # Test that is not possible to add any parameter to the dataset
-    with pytest.raises(RuntimeError, match='Can not add parameters to a '
-                                           'DataSet that has been started.'):
-        dataset.add_parameter(parameters[0])
-
-    assert len(dataset.paramspecs.keys()) == 3
-    assert len(shadow_ds.paramspecs.keys()) == 3
 
 
 @pytest.mark.usefixtures("experiment")
@@ -341,10 +298,13 @@ def test_add_data_1d():
     assert exp.sample_name == "test-sample"
     assert exp.last_counter == 0
 
-    psx = ParamSpec("x", "numeric")
-    psy = ParamSpec("y", "numeric", depends_on=['x'])
+    psx = ParamSpecBase("x", "numeric")
+    psy = ParamSpecBase("y", "numeric")
 
-    mydataset = new_data_set("test-dataset", specs=[psx, psy])
+    idps = InterDependencies_(dependencies={psy: (psx,)})
+
+    mydataset = new_data_set("test-dataset")
+    mydataset.set_interdependencies(idps)
     mydataset.mark_started()
 
     expected_x = []
@@ -385,8 +345,11 @@ def test_add_data_array():
     assert exp.sample_name == "test-sample"
     assert exp.last_counter == 0
 
-    mydataset = new_data_set("test", specs=[ParamSpec("x", "numeric"),
-                                            ParamSpec("y", "array")])
+    idps = InterDependencies_(
+        standalones=(ParamSpecBase("x", "numeric"),
+                     ParamSpecBase("y", "array")))
+    mydataset = new_data_set("test")
+    mydataset.set_interdependencies(idps)
     mydataset.mark_started()
 
     expected_x = []
@@ -415,12 +378,12 @@ def test_adding_too_many_results():
     insert_many_values function of the sqlite_base module
     """
     dataset = new_data_set("test_adding_too_many_results")
-    xparam = ParamSpec("x", "numeric", label="x parameter",
-                       unit='V')
-    yparam = ParamSpec("y", 'numeric', label='y parameter',
-                       unit='Hz', depends_on=[xparam])
-    dataset.add_parameter(xparam)
-    dataset.add_parameter(yparam)
+    xparam = ParamSpecBase("x", "numeric", label="x parameter",
+                           unit='V')
+    yparam = ParamSpecBase("y", 'numeric', label='y parameter',
+                           unit='Hz')
+    idps = InterDependencies_(dependencies={yparam: (xparam,)})
+    dataset.set_interdependencies(idps)
     dataset.mark_started()
     n_max = qc.SQLiteSettings.limits['MAX_VARIABLE_NUMBER']
 
@@ -471,9 +434,7 @@ def test_load_by_counter_for_nonexisting_experiment(nonexisting_exp_id):
 @pytest.mark.usefixtures("empty_temp_db")
 def test_dataset_with_no_experiment_raises():
     with pytest.raises(ValueError):
-        new_data_set("test-dataset",
-                     specs=[ParamSpec("x", "numeric"),
-                            ParamSpec("y", "numeric")])
+        new_data_set("test-dataset")
 
 
 def test_guid(dataset):
@@ -486,8 +447,9 @@ def test_numpy_ints(dataset):
     """
      Test that we can insert numpy integers in the data set
     """
-    xparam = ParamSpec('x', 'numeric')
-    dataset.add_parameter(xparam)
+    xparam = ParamSpecBase('x', 'numeric')
+    idps = InterDependencies_(standalones=(xparam,))
+    dataset.set_interdependencies(idps)
     dataset.mark_started()
 
     numpy_ints = [
@@ -505,8 +467,9 @@ def test_numpy_floats(dataset):
     """
     Test that we can insert numpy floats in the data set
     """
-    float_param = ParamSpec('y', 'numeric')
-    dataset.add_parameter(float_param)
+    float_param = ParamSpecBase('y', 'numeric')
+    idps = InterDependencies_(standalones=(float_param,))
+    dataset.set_interdependencies(idps)
     dataset.mark_started()
 
     numpy_floats = [np.float, np.float16, np.float32, np.float64]
@@ -517,8 +480,9 @@ def test_numpy_floats(dataset):
 
 
 def test_numpy_nan(dataset):
-    parameter_m = ParamSpec("m", "numeric")
-    dataset.add_parameter(parameter_m)
+    parameter_m = ParamSpecBase("m", "numeric")
+    idps = InterDependencies_(standalones=(parameter_m,))
+    dataset.set_interdependencies(idps)
     dataset.mark_started()
 
     data_dict = [{"m": value} for value in [0.0, np.nan, 1.0]]
@@ -531,8 +495,9 @@ def test_numpy_inf(dataset):
     """
     Test that we can insert and retrieve numpy inf in the data set
     """
-    parameter_m = ParamSpec("m", "numeric")
-    dataset.add_parameter(parameter_m)
+    parameter_m = ParamSpecBase("m", "numeric")
+    idps = InterDependencies_(standalones=(parameter_m,))
+    dataset.set_interdependencies(idps)
     dataset.mark_started()
 
     data_dict = [{"m": value} for value in [-np.inf, np.inf]]
@@ -547,15 +512,13 @@ def test_missing_keys(dataset):
     example handy when having an interleaved 1D and 2D sweep.
     """
 
-    x = ParamSpec("x", paramtype='numeric')
-    y = ParamSpec("y", paramtype='numeric')
-    a = ParamSpec("a", paramtype='numeric', depends_on=[x])
-    b = ParamSpec("b", paramtype='numeric', depends_on=[x, y])
+    x = ParamSpecBase("x", paramtype='numeric')
+    y = ParamSpecBase("y", paramtype='numeric')
+    a = ParamSpecBase("a", paramtype='numeric')
+    b = ParamSpecBase("b", paramtype='numeric')
 
-    dataset.add_parameter(x)
-    dataset.add_parameter(y)
-    dataset.add_parameter(a)
-    dataset.add_parameter(b)
+    idps = InterDependencies_(dependencies={a: (x,), b: (x, y)})
+    dataset.set_interdependencies(idps)
     dataset.mark_started()
 
     def fa(xv):
@@ -589,9 +552,8 @@ def test_missing_keys(dataset):
     assert dataset.get_setpoints("b")['y'] == expected_setpoints[1]
 
 
-def test_get_description(experiment, some_paramspecs):
+def test_get_description(experiment, some_interdeps):
 
-    paramspecs = some_paramspecs[2]
 
     ds = DataSet()
 
@@ -600,22 +562,9 @@ def test_get_description(experiment, some_paramspecs):
     desc = ds.description
     assert desc == RunDescriber(InterDependencies_())
 
-    ds.add_parameter(paramspecs['ps1'])
-    desc = ds.description
-    expected_desc = RunDescriber(
-                        InterDependencies_(
-                            standalones=(
-                                paramspecs['ps1'].base_version(),)))
-    assert desc == expected_desc
+    ds.set_interdependencies(some_interdeps[1])
 
-    ds.add_parameter(paramspecs['ps2'])
-    desc = ds.description
-    expected_desc = RunDescriber(
-                        InterDependencies_(
-                            dependencies=(
-                                {paramspecs['ps2'].base_version():
-                                 (paramspecs['ps1'].base_version(),)})))
-    assert desc == expected_desc
+    assert ds._interdeps == some_interdeps[1]
 
     # the run description gets written as the dataset is marked as started,
     # so now no description should be stored in the database
@@ -666,11 +615,10 @@ def test_metadata(experiment, request):
     assert error_caused_by(e, bad_tag_msg)
 
 
-def test_the_same_dataset_as(some_paramspecs, experiment):
-    paramspecs = some_paramspecs[2]
+def test_the_same_dataset_as(some_interdeps, experiment):
+
     ds = DataSet()
-    ds.add_parameter(paramspecs['ps1'])
-    ds.add_parameter(paramspecs['ps2'])
+    ds.set_interdependencies(some_interdeps[1])
     ds.mark_started()
     ds.add_result({'ps1': 1, 'ps2': 2})
 
@@ -682,7 +630,7 @@ def test_the_same_dataset_as(some_paramspecs, experiment):
 
 
 class TestGetData:
-    x = ParamSpec("x", paramtype='numeric')
+    x = ParamSpecBase("x", paramtype='numeric')
     n_vals = 5
     xvals = list(range(n_vals))
     # this is the format of how data is returned by DataSet.get_data
@@ -695,7 +643,8 @@ class TestGetData:
         This fixture creates a DataSet with values that is to be used by all
         the tests in this class
         """
-        dataset.add_parameter(self.x)
+        idps = InterDependencies_(standalones=(self.x,))
+        dataset.set_interdependencies(idps)
         dataset.mark_started()
         for xv in self.xvals:
             dataset.add_result({self.x.name: xv})
