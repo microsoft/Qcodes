bd9f21724404615e4de2cddd71ff490d7530bb24
Mikhail Astafev <astafan8@gmail.com>
2019-05-17 20:04:47 +0200

    Use ConnectionPlus from sqlite.connection in sqlite.database
    as opposed to from sqlite_base.

--- qcodes/dataset/sqlite/database.py
+++ qcodes/dataset/sqlite/database.py
@@ -8,10 +8,10 @@ from typing import Union
 import numpy as np
 from numpy import ndarray
 
+from qcodes.dataset.sqlite.connection import ConnectionPlus
 from qcodes.dataset.sqlite.db_upgrades import _latest_available_version, \
     get_user_version, perform_db_upgrade
 from qcodes.dataset.sqlite.initial_schema import init_db
-from qcodes.dataset.sqlite_base import ConnectionPlus
 import qcodes.config
 from qcodes.utils.types import complex_types, complex_type_union
 
