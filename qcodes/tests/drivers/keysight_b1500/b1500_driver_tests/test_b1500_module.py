c5c4ec6ab354f7e12812c69e46c10969035a6af7
Mikhail Astafev <astafan8@gmail.com>
2019-06-05 16:08:32 +0200

    Move from_model_name method to KeysightB1500 from B1500Module
    this allows to remove cycle dependency between modules and the
    mainframe classes and their methods.

--- qcodes/tests/drivers/keysight_b1500/b1500_driver_tests/test_b1500_module.py
+++ qcodes/tests/drivers/keysight_b1500/b1500_driver_tests/test_b1500_module.py
@@ -3,28 +3,28 @@ from unittest.mock import MagicMock
 import pytest
 
 from qcodes.instrument_drivers.Keysight.keysightb1500.KeysightB1500 import \
-    B1500Module, B1517A, B1520A, B1530A
+    KeysightB1500, B1517A, B1520A, B1530A
 
 
 def test_make_module_from_model_name():
     mainframe = MagicMock()
 
     with pytest.raises(NotImplementedError):
-        B1500Module.from_model_name(model='unsupported_module', slot_nr=0,
-                                    parent=mainframe, name='dummy')
-
-    smu = B1500Module.from_model_name(model='B1517A', slot_nr=1,
+        KeysightB1500.from_model_name(model='unsupported_module', slot_nr=0,
                                       parent=mainframe, name='dummy')
+
+    smu = KeysightB1500.from_model_name(model='B1517A', slot_nr=1,
+                                        parent=mainframe, name='dummy')
 
     assert isinstance(smu, B1517A)
 
-    cmu = B1500Module.from_model_name(model='B1520A', slot_nr=2,
-                                      parent=mainframe)
+    cmu = KeysightB1500.from_model_name(model='B1520A', slot_nr=2,
+                                        parent=mainframe)
 
     assert isinstance(cmu, B1520A)
 
-    aux = B1500Module.from_model_name(model='B1530A', slot_nr=3,
-                                      parent=mainframe)
+    aux = KeysightB1500.from_model_name(model='B1530A', slot_nr=3,
+                                        parent=mainframe)
 
     assert isinstance(aux, B1530A)
 
