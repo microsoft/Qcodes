26b39dab0831357f26265f7d8eed412eb192b3b2
Mikhail Astafev <astafan8@gmail.com>
2019-06-05 14:06:27 +0200

    Get rid of classes in B1500 tests + some reformatting

--- qcodes/tests/drivers/keysight_b1500/b1500_driver_tests/test_b1500.py
+++ qcodes/tests/drivers/keysight_b1500/b1500_driver_tests/test_b1500.py
@@ -7,8 +7,7 @@ from qcodes.instrument_drivers.Keysight.keysightb1500 import KeysightB1500
 from qcodes.instrument_drivers.Keysight.keysightb1500.KeysightB1500 import \
     parse_module_query_response
 from qcodes.instrument_drivers.Keysight.keysightb1500.constants import \
-    SlotNr, \
-    ChNr
+    SlotNr, ChNr
 
 
 @pytest.fixture
@@ -37,44 +36,48 @@ def b1500():
     instance.close()
 
 
-class TestB1500:
-    def test_init(self, b1500):
-        assert hasattr(b1500, 'smu1')
-        assert hasattr(b1500, 'smu2')
-        assert hasattr(b1500, 'cmu1')
+def test_init(b1500):
+    assert hasattr(b1500, 'smu1')
+    assert hasattr(b1500, 'smu2')
+    assert hasattr(b1500, 'cmu1')
 
-    def test_submodule_access_by_class(self, b1500):
-        assert b1500.smu1 in b1500.by_class['SMU']
-        assert b1500.smu2 in b1500.by_class['SMU']
-        assert b1500.cmu1 in b1500.by_class['CMU']
 
-    def test_submodule_access_by_slot(self, b1500):
-        assert b1500.smu1 is b1500.by_slot[SlotNr.SLOT01]
-        assert b1500.smu2 is b1500.by_slot[SlotNr.SLOT02]
-        assert b1500.cmu1 is b1500.by_slot[3]
+def test_submodule_access_by_class(b1500):
+    assert b1500.smu1 in b1500.by_class['SMU']
+    assert b1500.smu2 in b1500.by_class['SMU']
+    assert b1500.cmu1 in b1500.by_class['CMU']
 
-    def test_submodule_access_by_channel(self, b1500):
-        assert b1500.smu1 is b1500.by_channel[ChNr.SLOT_01_CH1]
-        assert b1500.smu2 is b1500.by_channel[ChNr.SLOT_02_CH1]
-        assert b1500.cmu1 is b1500.by_channel[ChNr.SLOT_03_CH1]
-        assert b1500.aux1 is b1500.by_channel[ChNr.SLOT_06_CH1]
-        assert b1500.aux1 is b1500.by_channel[ChNr.SLOT_06_CH2]
 
-    def test_enable_multiple_channels(self, b1500):
-        mock_write = MagicMock()
-        b1500.write = mock_write
+def test_submodule_access_by_slot(b1500):
+    assert b1500.smu1 is b1500.by_slot[SlotNr.SLOT01]
+    assert b1500.smu2 is b1500.by_slot[SlotNr.SLOT02]
+    assert b1500.cmu1 is b1500.by_slot[3]
 
-        b1500.enable_channels({1, 2, 3})
 
-        mock_write.assert_called_once_with("CN 1,2,3")
+def test_submodule_access_by_channel(b1500):
+    assert b1500.smu1 is b1500.by_channel[ChNr.SLOT_01_CH1]
+    assert b1500.smu2 is b1500.by_channel[ChNr.SLOT_02_CH1]
+    assert b1500.cmu1 is b1500.by_channel[ChNr.SLOT_03_CH1]
+    assert b1500.aux1 is b1500.by_channel[ChNr.SLOT_06_CH1]
+    assert b1500.aux1 is b1500.by_channel[ChNr.SLOT_06_CH2]
 
-    def test_disable_multiple_channels(self, b1500):
-        mock_write = MagicMock()
-        b1500.write = mock_write
 
-        b1500.disable_channels({1, 2, 3})
+def test_enable_multiple_channels(b1500):
+    mock_write = MagicMock()
+    b1500.write = mock_write
 
-        mock_write.assert_called_once_with("CL 1,2,3")
+    b1500.enable_channels({1, 2, 3})
+
+    mock_write.assert_called_once_with("CN 1,2,3")
+
+
+def test_disable_multiple_channels(b1500):
+    mock_write = MagicMock()
+    b1500.write = mock_write
+
+    b1500.disable_channels({1, 2, 3})
+
+    mock_write.assert_called_once_with("CL 1,2,3")
 
 
 def test_parse_module_query_response():
--- qcodes/tests/drivers/keysight_b1500/b1500_driver_tests/test_b1520a_cmu.py
+++ qcodes/tests/drivers/keysight_b1500/b1500_driver_tests/test_b1520a_cmu.py
@@ -6,40 +6,54 @@ from qcodes.instrument_drivers.Keysight.keysightb1500.KeysightB1500 import \
     B1520A
 
 
-class TestB1520A:
-    def setup_method(self, method):
-        """ setup any state specific to the execution of the given class (which
-        usually contains tests).
-        """
-        self.mainframe = MagicMock()
-        self.slot_nr = 3
-        self.cmu = B1520A(parent=self.mainframe, name='B1520A',
-                          slot_nr=self.slot_nr)
+@pytest.fixture
+def mainframe():
+    yield MagicMock()
 
-    def test_force_dc_voltage(self):
-        self.cmu.voltage_dc(10)
 
-        self.mainframe.write.assert_called_once_with('DCV 3,10')
+@pytest.fixture
+def cmu(mainframe):
+    slot_nr = 3
+    cmu = B1520A(parent=mainframe, name='B1520A', slot_nr=slot_nr)
+    yield cmu
 
-    def test_force_ac_voltage(self):
-        self.cmu.voltage_ac(0.1)
 
-        self.mainframe.write.assert_called_once_with('ACV 3,0.1')
+def test_force_dc_voltage(cmu):
+    mainframe = cmu.parent
 
-    def test_set_ac_frequency(self):
-        self.cmu.frequency(100e3)
+    cmu.voltage_dc(10)
 
-        self.mainframe.write.assert_called_once_with('FC 3,100000.0')
+    mainframe.write.assert_called_once_with('DCV 3,10')
 
-    def test_get_capacitance(self):
-        self.mainframe.ask.return_value = "NCC-1.45713E-06,NCY-3.05845E-03"
 
-        result = self.cmu.capacitance()
+def test_force_ac_voltage(cmu):
+    mainframe = cmu.parent
 
-        assert pytest.approx((-1.45713E-06, -3.05845E-03)) == result
+    cmu.voltage_ac(0.1)
 
-    def test_raise_error_on_unsupported_result_format(self):
-        self.mainframe.ask.return_value = "NCR-1.1234E-03,NCX-4.5677E-03"
+    mainframe.write.assert_called_once_with('ACV 3,0.1')
 
-        with pytest.raises(ValueError):
-            self.cmu.capacitance()
+
+def test_set_ac_frequency(cmu):
+    mainframe = cmu.parent
+
+    cmu.frequency(100e3)
+
+    mainframe.write.assert_called_once_with('FC 3,100000.0')
+
+
+def test_get_capacitance(cmu):
+    mainframe = cmu.parent
+
+    mainframe.ask.return_value = "NCC-1.45713E-06,NCY-3.05845E-03"
+
+    assert pytest.approx((-1.45713E-06, -3.05845E-03)) == cmu.capacitance()
+
+
+def test_raise_error_on_unsupported_result_format(cmu):
+    mainframe = cmu.parent
+
+    mainframe.ask.return_value = "NCR-1.1234E-03,NCX-4.5677E-03"
+
+    with pytest.raises(ValueError):
+        cmu.capacitance()
