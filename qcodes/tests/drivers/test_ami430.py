da5b8c1923db39cc568baabdcc7bcdd7a95e14e6
Mikhail Astafev <astafan8@gmail.com>
2019-06-18 00:27:49 +0200

    Test changing ramp_rate_units/field_units parameters of AMI430

--- qcodes/tests/drivers/test_ami430.py
+++ qcodes/tests/drivers/test_ami430.py
@@ -815,3 +815,83 @@ def test_numeric_field_limit(magnet_axes_instances, field_limit, request):
     with pytest.raises(ValueError,
                        match='_set_fields aborted; field would exceed limit'):
         ami.cartesian(target_outside_limit)
+
+
+def test_ramp_rate_units_and_field_units_at_init(ami430):
+    """
+    Test values of ramp_rate_units and field_units parameters at init,
+    and the units of other parameters which depend on the
+    values of ramp_rate_units and field_units parameters.
+    """
+    initial_ramp_rate_units = ami430.ramp_rate_units()
+    initial_field_units = ami430.field_units()
+
+    assert initial_ramp_rate_units == 'seconds'
+    assert initial_field_units == 'tesla'
+
+    assert ami430.coil_constant.unit == "T/A"
+    assert ami430.field_limit.unit == "T"
+    assert ami430.field.unit == "T"
+    assert ami430.setpoint.unit == "T"
+    assert ami430.ramp_rate.unit == "T/s"
+    assert ami430.current_ramp_limit.unit == "A/s"
+    assert ami430.field_ramp_limit.unit == "T/s"
+
+
+@pytest.mark.parametrize(('new_value', 'unit_string', 'scale'),
+                         (('seconds', 's', 1), ('minutes', 'min', 1/60)),
+                         ids=('seconds', 'minutes'))
+def test_change_ramp_rate_units_parameter(ami430, new_value, unit_string,
+                                          scale):
+    """
+    Test that changing value of ramp_rate_units parameter is reflected in
+    settings of other magnet parameters.
+    """
+    coil_constant_unit = ami430.coil_constant.unit
+    field_limit_unit = ami430.field_limit.unit
+    field_unit = ami430.field.unit
+    setpoint_unit = ami430.setpoint.unit
+
+    ami430.ramp_rate_units(new_value)
+
+    ramp_rate_units__actual = ami430.ramp_rate_units()
+    assert ramp_rate_units__actual == new_value
+
+    assert ami430.coil_constant.unit == coil_constant_unit
+    assert ami430.field_limit.unit == field_limit_unit
+    assert ami430.field.unit == field_unit
+    assert ami430.setpoint.unit == setpoint_unit
+
+    assert ami430.ramp_rate.unit.endswith("/" + unit_string)
+    assert ami430.current_ramp_limit.unit.endswith("/" + unit_string)
+    assert ami430.field_ramp_limit.unit.endswith("/" + unit_string)
+
+    assert ami430.current_ramp_limit.scale == scale
+
+
+@pytest.mark.parametrize(('new_value', 'unit_string'),
+                         (('tesla', 'T'), ('kilogauss', 'kG')),
+                         ids=('tesla', 'kilogauss'))
+def test_change_field_units_parameter(ami430, new_value, unit_string):
+    """
+    Test that changing value of field_units parameter is reflected in
+    settings of other magnet parameters.
+    """
+    current_ramp_limit_unit = ami430.current_ramp_limit.unit
+    current_ramp_limit_scale = ami430.current_ramp_limit.scale
+
+    ami430.field_units(new_value)
+
+    field_units__actual = ami430.field_units()
+    assert field_units__actual == new_value
+
+    assert ami430.current_ramp_limit.unit == current_ramp_limit_unit
+    assert ami430.current_ramp_limit.scale == current_ramp_limit_scale
+
+    assert ami430.field_limit.unit == unit_string
+    assert ami430.field.unit == unit_string
+    assert ami430.setpoint.unit == unit_string
+
+    assert ami430.coil_constant.unit.startswith(unit_string + "/")
+    assert ami430.ramp_rate.unit.startswith(unit_string + "/")
+    assert ami430.field_ramp_limit.unit.startswith(unit_string + "/")
