658431917a093d042fd217c2f0ae1fa448087709
Harshit Lakhotia <lakhotiaharshit@gmail.com>
2019-07-18 13:25:03 +0200

    Define class for sampling measurement

--- qcodes/instrument_drivers/Keysight/keysightb1500/KeysightB1500_SamplingMeasurement.py
+++ qcodes/instrument_drivers/Keysight/keysightb1500/KeysightB1500_SamplingMeasurement.py
@@ -21,32 +21,9 @@ spa.smu1.voltage(1e-6)
 
 
 
-_sampling_index = 'X'
-_current_measurement_value = 'I'
-_voltage_measurement_value = 'V'
-_normal_status = 'N'
-_compliance_issue = 'C'
-_values_separator = ','
-_channel_list = {'A': 'CH1',
-                 'B': 'CH2',
-                 'C': 'CH3',
-                 'D': 'CH4',
-                 'E': 'CH5',
-                 'F': 'CH6',
-                 'G': 'CH7',
-                 'H': 'CH8',
-                 'I': 'CH9',
-                 'J': 'CH10',
-                 'Z': 'XDATA'
-                 }
-
-_error_list = {'C': 'Reached_compliance_limit',
-               'N': 'Normal',
-               'T': 'Another_channel_reached_compliance_limit',
-               'V': 'Measured_data_over_measurement_range'
-               }
 
 class SamplingMeasurement(ParameterWithSetpoints):
+
 
     def __init__(self, name, instrument):
         super.__init__(name)
@@ -62,6 +39,32 @@ class SamplingMeasurement(ParameterWithSetpoints):
         return data
 
     def parse_fmt_1_0_response(raw_data):
+        _sampling_index = 'X'
+        _current_measurement_value = 'I'
+        _voltage_measurement_value = 'V'
+        _normal_status = 'N'
+        _compliance_issue = 'C'
+        _values_separator = ','
+        _channel_list = {'A': 'CH1',
+                         'B': 'CH2',
+                         'C': 'CH3',
+                         'D': 'CH4',
+                         'E': 'CH5',
+                         'F': 'CH6',
+                         'G': 'CH7',
+                         'H': 'CH8',
+                         'I': 'CH9',
+                         'J': 'CH10',
+                         'Z': 'XDATA'
+                         }
+
+        _error_list = {'C': 'Reached_compliance_limit',
+                       'N': 'Normal',
+                       'T': 'Another_channel_reached_compliance_limit',
+                       'V': 'Measured_data_over_measurement_range'
+                       }
+
+
         data_val = []
         data_status = []
         data_channel = []
@@ -83,43 +86,6 @@ class SamplingMeasurement(ParameterWithSetpoints):
 
         data = FMTResponse(data_val, data_status, data_channel, data_datatype)
         return data
-
-
-def parse_fmt_1_1_response(resp):
-    indices_val = []
-    indices_status = []
-    indices_channel = []
-    indices_datatype = []
-
-    data_val = []
-    data_status = []
-    data_channel = []
-    data_datatype = []
-
-    FMTResponse = namedtuple('FMTResponse', 'value status channel type')
-
-    for str_value in raw_data.split(_values_separator):
-        status = str_value[0]
-        channel_id = _channel_list[str_value[1]]
-
-
-        datatype = str_value[2]
-        value = float(str_value[3:])
-        if datatype == _sampling_index:
-            indices_val.append(value)
-            indices_status.append(status)
-            indices_channel.append(channel_id)
-            indices_datatype.append(datatype)
-        else:
-            data_val.append(value)
-            data_status.append(status)
-            data_channel.append(channel_id)
-            data_datatype.append(datatype)
-
-    data = FMTResponse(data_val, data_status, data_channel, data_datatype)
-    indices = FMTResponse(indices_val, indices_status, indices_channel, indices_datatype)
-    return indices, data
-
 
 
 
