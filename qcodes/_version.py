0451bb566ffcd64c8f0b2ad5f5976909ac651780
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2019-07-18 10:44:57 +0200

    remove not needed quotes

--- qcodes/_version.py
+++ qcodes/_version.py
@@ -41,7 +41,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "'v'"
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "qcodes-"
     cfg.versionfile_source = "qcodes/_version.py"
     cfg.verbose = False
