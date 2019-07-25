d5668f61fbee8f67e59755f771305d50661bacd3
Harshit Lakhotia <lakhotiaharshit@gmail.com>
2019-07-25 15:25:24 +0200

    Refactor: Beautify according to PEP8 recommendations

--- qcodes/instrument_drivers/Keysight/keysightb1500/KeysightB1500_SamplingMeasurement.py
+++ qcodes/instrument_drivers/Keysight/keysightb1500/KeysightB1500_SamplingMeasurement.py
@@ -1,12 +1,16 @@
+import warnings
+from collections import namedtuple
+
+import numpy
+
+from qcodes import ParameterWithSetpoints
 from qcodes.instrument_drivers.Keysight.keysightb1500 import KeysightB1500, \
     MessageBuilder, constants
-from collections import namedtuple
-from qcodes import ParameterWithSetpoints
-import numpy
-import warnings
+
 
 class MeasurementNotTaken(Exception):
     pass
+
 
 class SamplingMeasurement(ParameterWithSetpoints):
     """
@@ -14,7 +18,7 @@ class SamplingMeasurement(ParameterWithSetpoints):
     parameter analyzer B1500A.
     """
 
-    _timeout_response_factor  = 10.00
+    _timeout_response_factor = 10.00
     # This factor is a bit higher than the ratio between
     # the measured measurement-time and the calculated measurement
     # (from the user input). Check :get_raw: method to find its usage.
@@ -27,45 +31,48 @@ class SamplingMeasurement(ParameterWithSetpoints):
 
     def get_raw(self):
         """
-        sets up the measurement by calling :_set_up:
+        Sets up the measurement by calling :_set_up:
 
         Automatically sets up the visa time out.
 
         The visa time-out should be longer than the time it takes to finish
-        the sampling measurement. The reason is that while measurement is running
-        the data keeps on appending in the buffer of SPA. Only when the measurement
-        is finished the data is returned to the VISA handle.
+        the sampling measurement. The reason is that while measurement is
+        running the data keeps on appending in the buffer of SPA. Only when
+        the measurement is finished the data is returned to the VISA handle.
         Hence during this time the VISA is idle and waiting for the response.
         If the timeout is lower than the total run time of the measurement,
         VISA will give error.
 
-        We set the Visa timeout to be the measurement_time times the _timeout_response_factor  .
-        Strictly speaking the timeout should be just higher the measurement time.
+        We set the Visa timeout to be the measurement_time times the
+        `_timeout_response_factor`. Strictly speaking the timeout should be
+        just higher the measurement time.
 
-        :return: numpy array with sampling measurement
+        Return:
+            numpy array with sampling measurement
         """
         try:
             measurement_time = self.instrument._total_measurement_time()
             # set time out to this value
             time_out = measurement_time * self._timeout_response_factor
 
-            #default timeout
+            # default timeout
             default_timeout = self.root_instrument.timeout()
 
-            #if time out to be set is lower than the default value
+            # if time out to be set is lower than the default value
             # then keep default
             if time_out < default_timeout:
-                 time_out = default_timeout
+                time_out = default_timeout
 
             with self.root_instrument.timeout.set_to(time_out):
                 self._set_up()
-                raw_data = self.root_instrument.ask(MessageBuilder().xe().message)
+                raw_data = self.root_instrument.ask(
+                    MessageBuilder().xe().message)
                 self.data = self.parse_fmt_1_0_response(raw_data)
             return numpy.array(self.data.value)
         except TypeError:
             raise Exception('set timing parameters first')
 
-    def parse_fmt_1_0_response(self,raw_data_val):
+    def parse_fmt_1_0_response(self, raw_data_val):
         """
         parse the raw data from SPA into a named tuple
         with names
@@ -110,11 +117,12 @@ class SamplingMeasurement(ParameterWithSetpoints):
         number of data values which were not measured under "N" (normal)
         status.
 
-        For the list of all the status values and their meaning refer to :class:`constants.ComplianceStatus`.
+        For the list of all the status values and their meaning refer to
+        :class:`constants.ComplianceStatus`.
 
         """
 
-        if hasattr(self,'data'):
+        if hasattr(self, 'data'):
             data = self.data
             total_count = len(data.status)
             normal_count = data.status.count('N')
@@ -122,11 +130,15 @@ class SamplingMeasurement(ParameterWithSetpoints):
             if total_count == normal_count:
                 print('All measurements are normal')
             else:
-                indices = [i for i, x in enumerate(data.status) if x == "C" or x == "T"]
-                warnings.warn(f'{str(exception_count)} measurements were out of compliance at {str(indices)}')
+                indices = [i for i, x in enumerate(data.status)
+                           if x == "C" or x == "T"]
+                warnings.warn(f'{str(exception_count)} measurements were '
+                              f'out of compliance at {str(indices)}')
 
             compliance_error_list = constants.ComplianceErrorList
-            compliance_list = [compliance_error_list[key].value for key in data.status]
+            compliance_list = [compliance_error_list[key].value
+                               for key in data.status]
             return compliance_list
         else:
-            raise MeasurementNotTaken('First run sampling_measurement method to generate the data')
+            raise MeasurementNotTaken('First run sampling_measurement'
+                                      ' method to generate the data')
