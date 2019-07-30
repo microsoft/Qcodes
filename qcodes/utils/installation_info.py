3d6678e1ef7b564e3a0ef591f490dc2a8bf77b82
WilliamHPNielsen <whpn@mailbox.org>
2019-07-30 13:36:47 +0200

    Remove unused type ignore

--- qcodes/utils/installation_info.py
+++ qcodes/utils/installation_info.py
@@ -72,7 +72,7 @@ def get_qcodes_version() -> str:
     """
     Get the version of the currently installed QCoDeS
     """
-    return qcodes.version.__version__  # type: ignore
+    return qcodes.version.__version__
 
 
 def get_qcodes_requirements() -> List[str]:
