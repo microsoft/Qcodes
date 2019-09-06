f7695ea94d191bcded4ca177f3b77a662eb470c8
Harshit Lakhotia <lakhotiaharshit@gmail.com>
2019-09-06 17:10:57 +0200

    update snapshot_exclude when mode is changed

--- qcodes/instrument_drivers/yokogawa/GS200.py
+++ qcodes/instrument_drivers/yokogawa/GS200.py
@@ -489,9 +489,18 @@ class GS200(VisaInstrument):
         if mode == "VOLT":
             self.range = self.voltage_range
             self.output_level = self.voltage
+            self.voltage_range.snaphot_exclude = False
+            self.voltage.snaphot_exclude = False
+            self.current_range.snaphot_exclude = True
+            self.current.snaphot_exclude = True
+
         else:
             self.range = self.current_range
             self.output_level = self.current
+            self.voltage_range.snaphot_exclude = True
+            self.voltage.snaphot_exclude = True
+            self.current_range.snaphot_exclude = False
+            self.current.snaphot_exclude = False
 
         self.write("SOUR:FUNC {}".format(mode))
         self._cached_mode = mode
