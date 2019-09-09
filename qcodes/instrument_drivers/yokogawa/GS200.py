384977a58e08c498a39a0b7123f74916029e7026
Mikhail Astafev <astafan8@gmail.com>
2019-09-09 11:35:02 +0200

    and one more time

--- qcodes/instrument_drivers/yokogawa/GS200.py
+++ qcodes/instrument_drivers/yokogawa/GS200.py
@@ -228,7 +228,7 @@ class GS200(VisaInstrument):
                            unit='V',
                            set_cmd=partial(self._get_set_output, "VOLT"),
                            get_cmd=partial(self._get_set_output, "VOLT")
-                          )
+                           )
 
         self.add_parameter('current',
                            label='Current',
