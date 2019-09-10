e4bc27025b1343010763ea0da57262fda84e5088
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2019-09-10 10:31:16 +0200

    Only replace signit handler if the default one is installed

--- qcodes/utils/delaykeyboardinterrupt.py
+++ qcodes/utils/delaykeyboardinterrupt.py
@@ -10,15 +10,18 @@ class DelayedKeyboardInterrupt:
     Inspired by https://stackoverflow.com/questions/842557/how-to-prevent-a-block-of-code-from-being-interrupted-by-keyboardinterrupt-in-py
     """
     signal_received = None
+    old_handler = None
 
     def __enter__(self) -> None:
-        self.old_handler = signal.signal(signal.SIGINT, self.handler)
+        if signal.getsignal(signal.SIGINT) is signal.default_int_handler:
+            self.old_handler = signal.signal(signal.SIGINT, self.handler)
 
     def handler(self, sig, frame):
         self.signal_received = (sig, frame)
         logging.debug('SIGINT received. Delaying KeyboardInterrupt.')
 
     def __exit__(self, exception_type, value, traceback):
-        signal.signal(signal.SIGINT, self.old_handler)
+        if self.old_handler is not None:
+            signal.signal(signal.SIGINT, self.old_handler)
         if self.signal_received is not None:
             self.old_handler(*self.signal_received)
