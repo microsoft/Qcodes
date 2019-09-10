b04c6b598c40746b9b33788ba8ff0d34c341745d
Dominik Vogel <a-dovoge@microsoft.com>
2019-09-10 18:00:43 +0200

    Include generator into test, fix generator passing

--- qcodes/tests/mockers/test_simulated_ats_api.py
+++ qcodes/tests/mockers/test_simulated_ats_api.py
@@ -6,16 +6,20 @@ from qcodes.instrument_drivers.AlazarTech.ATS9360 import (
     AlazarTech_ATS9360 as ATS9360)
 
 
+def ones_generator(data):
+    data[:] = np.ones(data.shape)
+
 
 @pytest.fixture(scope='function')
 def simulated_alazar():
     driver = ATS9360(
         'Alazar',
-        api=SimulatedAlazarATSAPI(dll_path='simulated'))
+        api=SimulatedAlazarATSAPI(
+            dll_path='simulated',
+            buffer_generator=ones_generator)
+    )
     with driver.syncing():
         driver.sample_rate(1_000_000_000)
-        driver.decimation(1)
-        driver.trigger_delay(0)
     try:
         yield driver
     finally:
@@ -41,15 +45,12 @@ def alazar_ctrl():
 
 def test_simulated_alazar(simulated_alazar, alazar_ctrl):
     alazar = simulated_alazar
-    alazar.aux_io_mode('AUX_IN_TRIGGER_ENABLE')
-    alazar.aux_io_param('TRIG_SLOPE_POSITIVE')
-    alazar.sync_settings_to_card()
-
-    alazar.acquire(
+    buffers_per_acquisition = 10
+    data = alazar.acquire(
         mode='NPT',
         samples_per_record=int(1e4*128),
         records_per_buffer=1,
-        buffers_per_acquisition=1,
+        buffers_per_acquisition=buffers_per_acquisition,
         channel_selection='A',
         enable_record_headers=None,
         alloc_buffers=None,
@@ -59,5 +60,8 @@ def test_simulated_alazar(simulated_alazar, alazar_ctrl):
         allocated_buffers=4,
         buffer_timeout=None,
         acquisition_controller=alazar_ctrl)
+    assert len(data) == buffers_per_acquisition
+    for d in data:
+        assert np.allclose(d, np.ones(d.shape))
 
 
