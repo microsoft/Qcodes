d707e52c614fd48a653555ef3066a97f101db356
Jens Hedegaard Nielsen <Jens.Nielsen@microsoft.com>
2019-09-19 17:12:03 +0200

    Fix rst formatting in readme (#1729)
    * Fix rst formatting in readme
    
    * mark content type for readme as rst

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(name='qcodes',
                   'Copenhagen / Delft / Sydney / Microsoft quantum computing '
                   'consortium',
       long_description=readme(),
+      long_description_content_type='text/x-rst',
       url='https://github.com/QCoDeS/Qcodes',
       classifiers=[
           'Development Status :: 3 - Alpha',
