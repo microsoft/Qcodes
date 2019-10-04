0319cbee0ac01dfce2aaf401c2b83eba833b1097
AUC Hardal <auchardal@gmail.com>
2019-10-04 15:05:34 +0200

    Fix number of channels

--- qcodes/instrument_drivers/AimTTi/AimTTi_PL601P_channels.py
+++ qcodes/instrument_drivers/AimTTi/AimTTi_PL601P_channels.py
@@ -3,12 +3,12 @@ from qcodes import InstrumentChannel, ChannelList
 
 
 class AimTTiChannel(InstrumentChannel):
-    def __init__(self, name, channel, **kwargs):
-        super().__init__(name, channel, **kwargs)
+    def __init__(self, parent, name, channel, **kwargs):
+        super().__init__(parent, name, **kwargs)
 
         self.add_parameter('volt',
                            get_cmd=f'V{channel}?',
-                           get_parser=float,
+                           #get_parser=float,
                            set_cmd=f'V{channel} {{}}',
                            label='Voltage',
                            unit='V')
@@ -20,6 +20,12 @@ class AimTTiChannel(InstrumentChannel):
                            label='Current',
                            unit='A')
 
+    def _get_voltage_value(self):
+        ch = 1
+        _voltage = self.ask(f'V{ch}?')
+        print(_voltage)
+        #_voltage_split = _voltage.split()
+        #return _voltage_split[1]
 class AimTTi(VisaInstrument):
     """
     This is the QCoDeS driver for the Aim TTi PL-P series power supply.
@@ -30,9 +36,10 @@ class AimTTi(VisaInstrument):
         channels = ChannelList(self, "Channels", AimTTiChannel,
                                snapshotable=False)
 
-        for channel_number in range(1, 4):
-            channel = AimTTiChannel(self, f"{channel_number}")
-            channels.append(channel)
+        numChannels = 1
+        channel = AimTTiChannel(self, f'ch{numChannels}', numChannels)
+        channels.append(channel)
+        self.add_submodule(f'ch{numChannels}', channel)
 
         channels.lock()
         self.add_submodule('channels', channels)
