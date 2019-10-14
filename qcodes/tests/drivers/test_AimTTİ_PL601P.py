e027cc0c7d9f8ea63bab3539cd8c7e2d3b780095
AUC Hardal <auchardal@gmail.com>
2019-10-14 11:26:40 +0200

    Add fixture for the channels

--- qcodes/tests/drivers/test_AimTTİ_PL601P.py
+++ qcodes/tests/drivers/test_AimTTİ_PL601P.py
@@ -15,8 +15,23 @@ def driver():
     yield driver
     driver.close()
 
+
+@pytest.fixture(scope='function')
+def channels(driver):
+    ch_names = {'ch1'}
+    assert ch_names == set(list(driver.submodules.keys()))
+
+    yield tuple(getattr(driver, ch_name)
+                for ch_name in ch_names)
+
+
 def test_idn(driver):
     assert {'firmware': '3.05-4.06',
             'model': 'PL601-P',
             'serial': '514710',
             'vendor': 'THURLBY THANDAR'} == driver.IDN()
+
+
+def test_channel_parameters(driver):
+    driver.ch1.volt(1.0)
+    assert 1.0 == driver.ch1.volt()
