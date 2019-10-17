848f15033e59d5c708cc0af4ea6a9cbe18c04f1e
Mikhail Astafev <astafan8@gmail.com>
2019-10-17 14:45:54 +0200

    Use default delay for test subscriber from config
    otherwise, it would sometimes fail because the CI infra is slow,
    which forces to restart the CI job completely and waste time.

--- qcodes/tests/dataset/test_subscribing.py
+++ qcodes/tests/dataset/test_subscribing.py
@@ -174,7 +174,7 @@ def test_subscription_from_config(dataset, basic_subscriber):
             expected_state[x+1] = [(x, y)]
 
             @retry_until_does_not_throw(
-                exception_class_to_expect=AssertionError, delay=0, tries=10)
+                exception_class_to_expect=AssertionError, tries=10)
             def assert_expected_state():
                 assert dataset.subscribers[sub_id].state == expected_state
                 assert dataset.subscribers[sub_id_c].state == expected_state
