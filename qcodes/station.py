0447df203b82395551c2829a888fbfc59f2e0a66
Dominik Vogel <a-dovoge@microsoft.com>
2019-10-23 15:54:20 +0200

    Fix mypy issues

--- qcodes/station.py
+++ qcodes/station.py
@@ -586,9 +586,9 @@ def update_config_schema(
 
     """
 
-    def instrument_names_from_module(module: ModuleType) -> Iterator[str]:
+    def instrument_names_from_module(module: ModuleType) -> List[str]:
         submodules = list(pkgutil.walk_packages(
-            module.__path__,
+            module.__path__,  # type: ignore  # mypy issue #1422
             module.__name__ + '.'))
         res = set()
         for s in submodules:
