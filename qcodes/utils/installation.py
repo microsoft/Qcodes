ae4a6388a0a8627fba75e5ad6801f8da14357e0f
Dominik Vogel <a-dovoge@microsoft.com>
2019-10-23 14:31:46 +0200

    Add docstring and minor fixes to installation script

--- qcodes/utils/installation.py
+++ qcodes/utils/installation.py
@@ -1,15 +1,40 @@
+"""This module contains helper scripts to make certain installation tasks
+easier."""
+
 import os
 import json
-from qcodes.station import SCHEMA_PATH
+from qcodes.station import SCHEMA_PATH, update_config_schema
 
 
 def register_station_schema_with_vscode():
-    config_path = os.path.expandvars(os.path.join('%APPDATA%', 'Code', 'User', 'settings.json'))
+    """This function registeres the qcodes station schema with vscode.
+
+    Run this function to add the user generated station schema to the list of
+    associated schemas for the Red Hat YAML schema extension for vscode.
+    (https://marketplace.visualstudio.com/items?itemName=redhat.vscode-yaml)
+
+    This function will effectively add an entry to `yaml.schemas` in the user
+    config file of vscode, which is located under
+    `%APPDATA/Code/User/settings.json`.
+
+    You can alternatively access this
+    setting via File->Preferences->Settings and search for `yaml.schemas`.
+
+    To enable autocompletinon of QCoDeS instrument from additional packages run
+    `qcodes.station.update_config_schema`.
+
+    For more information consult `qcodes/docs/examples/Station.ipynb`.
+    """
+    if not os.path.exists(SCHEMA_PATH):
+        update_config_schema()
+    config_path = os.path.expandvars(
+        os.path.join('%APPDATA%', 'Code', 'User', 'settings.json'))
     with open(config_path, 'r+') as f:
         data = json.load(f)
-        # "file:///Users/a-dovoge/Qcodes/qcodes/dist/schemas/station-template.schema.json": "*.station.yaml"
-    _, schema_path = os.path.splitdrive(SCHEMA_PATH)
-    data.setdefault('yaml.schemas', {})[r'file:\\' + schema_path] = '*.station.yaml'
-    os.remove(config_path)
-    with open(config_path, 'w') as f:
+    data.setdefault(
+        'yaml.schemas', {}
+    )[r'file:\\' + os.path.splitdrive(SCHEMA_PATH)[1]] = '*.station.yaml'
+    config_path_new = 'new_' + config_path
+    with open(config_path_new, 'w') as f:
         json.dump(data, f, indent=4)
+    os.replace(config_path, config_path_new)
