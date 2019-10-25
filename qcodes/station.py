524d72f1a8e9c2f4a0f9c6e192d025aa16271474
Dominik Vogel <a-dovoge@microsoft.com>
2019-10-25 11:27:13 +0200

    Fix Docstring

--- qcodes/station.py
+++ qcodes/station.py
@@ -581,7 +581,8 @@ def update_config_schema(
 
     Args:
         additional_instrument_modules: python modules that contain
-            :class:`qcodes.InstrumentBase` definitions (and subclasses thereof) to be included as
+            :class:`qcodes.instrument.InstrumentBase` definitions
+            (and subclasses thereof) to be included as
             values for instrument definition in th station definition
             yaml files.
 
