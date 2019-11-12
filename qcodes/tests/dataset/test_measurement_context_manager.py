94e3fbfa6da4309ff2830e98f63d2e28928c1707
lakhotiaharshit <lakhotiaharshit@gmail.com>
2019-11-12 09:20:45 +0100

    Add tests using pytest to check logging message contains extra info

--- qcodes/tests/dataset/test_measurement_context_manager.py
+++ qcodes/tests/dataset/test_measurement_context_manager.py
@@ -2,7 +2,7 @@ import re
 import os
 from time import sleep
 import json
-import unittest
+import logging
 
 import pytest
 from hypothesis import given, settings
@@ -218,35 +218,32 @@ def SpectrumAnalyzer():
     SA.close()
 
 
-class MeasurementLoggingTest(unittest.TestCase):
+@pytest.fixture
+def meas_with_registered_param(DAC, DMM):
+    meas = Measurement()
+    meas.register_parameter(DAC.ch1)
+    meas.register_parameter(DMM.v1, setpoints=[DAC.ch1])
+    yield meas
 
-    def test_logging_message_contain_extra_log_info(self):
-        with self.assertLogs(level='INFO') as log:
-            meas = Measurement()
-            dac = DummyInstrument('dummy_dac', gates=['ch1', 'ch2'])
-            dmm = DummyInstrument('dummy_dmm', gates=['v1', 'v2'])
-            meas.register_parameter(dac.ch1)
-            meas.register_parameter(dmm.v1, setpoints=[dac.ch1])
 
-            with meas.run() as _:
-                pass
-            self.assertEqual(len(log.output), 5)
-            self.assertEqual(len(log.records), 5)
-            self.assertIn('Registered dummy_dac_ch1 in the Measurement.',
-                          log.output[0])
-            self.assertIn('Registered dummy_dmm_v1 in the Measurement.',
-                          log.output[1])
-            self.assertIn('Set the run_timestamp of run_id',
-                          log.output[2])
-            self.assertIn('Starting measurement with guid',
-                          log.output[3])
-            self.assertIn('Finished measurement with guid', log.output[4])
+def test_measurement_context_manager_logs(caplog, meas_with_registered_param):
+    caplog.set_level(logging.INFO)
+    with meas_with_registered_param.run() as _:
+        pass
 
-            meas._extra_log_info = 'some extra info'
-            with meas.run() as _:
-                pass
-            self.assertIn('some extra info', log.output[6])
-            self.assertIn('some extra info', log.output[7])
+    assert "Set the run_timestamp of run_id" in caplog.text
+    assert "Starting measurement with guid" in caplog.text
+    assert "Finished measurement with guid" in caplog.text
+
+
+def test_measurement_context_manager_log_includes_extra_info(caplog,
+                                                             meas_with_registered_param):
+    caplog.set_level(logging.INFO)
+    meas_with_registered_param._extra_log_info = "some extra info"
+    with meas_with_registered_param.run() as _:
+        pass
+
+    assert "some extra info" in caplog.text
 
 
 def test_register_parameter_numbers(DAC, DMM):
