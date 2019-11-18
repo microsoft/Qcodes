dac94215cadabceed244e858f64d3fcd71839f48
AUC Hardal <auchardal@gmail.com>
2019-11-18 15:08:45 +0100

    Make parameters fixture

--- test_doNd.py
+++ test_doNd.py
@@ -13,33 +13,39 @@ import pytest
 config.user.mainfolder = "output"  # set ouput folder for doNd's
 new_experiment("doNd-tests", sample_name="no sample")
 
-_param = Parameter('simple_parameter',
+@pytest.fixture()
+def _parameters():
+
+    _param = Parameter('simple_parameter',
                    set_cmd=None,
                    get_cmd=lambda: 1)
 
-_paramComplex = Parameter('simple_complex_parameter',
+    _paramComplex = Parameter('simple_complex_parameter',
                    set_cmd=None,
                    get_cmd=lambda: 1 + 1j,
                    vals=validators.ComplexNumbers())
 
-_param_set = Parameter('simple_setter_parameter',
+    _param_set = Parameter('simple_setter_parameter',
                        set_cmd=None,
                        get_cmd=None)
 
+    return _param, _paramComplex, _param_set
 
-def _param_func():
+
+def _param_func(_p):
     """
     A private utility function.
     """
     _new_param = Parameter('modified_parameter',
                            set_cmd= None,
-                           get_cmd= lambda: _param.get()*2)
+                           get_cmd= lambda: _p.get()*2)
     assert _new_param.get() == 2
     return _new_param
 
 
-def test_do0d():
+def test_do0d(_parameters):
 
+    _param, _paramComplex, _ = _parameters
     do0d(_param)
     do0d(_param, write_period=1)
     do0d(_param, write_period=1, do_plot=False)
@@ -48,10 +54,10 @@ def test_do0d():
     do0d(_paramComplex, write_period=1)
     do0d(_paramComplex, write_period=1, do_plot=False)
 
-    do0d(_param_func(), write_period=1, do_plot=False)
+    do0d(_param_func(_param), write_period=1, do_plot=False)
 
     do0d(_param, _paramComplex, write_period=1, do_plot=False)
-    do0d(_param_func(), _paramComplex, write_period=1, do_plot=False)
+    do0d(_param_func(_param), _paramComplex, write_period=1, do_plot=False)
 
     _data = do0d(_param)
     assert type(_data[0]) == int
@@ -59,16 +65,18 @@ def test_do0d():
     _dataComplex = do0d(_paramComplex)
     assert type(_dataComplex[0]) == int
 
-    _dataFunc = do0d(_param_func())
+    _dataFunc = do0d(_param_func(_param))
     assert type(_dataFunc[0]) == int
 
 
-def test_do1d():
+def test_do1d(_paramters):
 
     _start = 0
     _stop = 1
     _num_points = 1
     _delay_list = [0, 0.1, 1]
+
+    _param, _paramComplex, _param_set = _parameters
 
     for _delay in _delay_list:
         do1d(_param_set, _start, _stop, _num_points, _delay, _param)
@@ -80,7 +88,7 @@ def test_do1d():
     assert type(_data[0]) == int
 
 
-def test_do2d():
+def test_do2d(_parameters):
 
     _start_p1 = 0
     _stop_p1 = 1
@@ -91,6 +99,8 @@ def test_do2d():
     _stop_p2 = 1.1
     _num_points_p2 = 2
     _delay_p2 = 0.01
+
+    _param, _paramComplex, _param_set = _parameters
 
     do2d(_param_set, _start_p1, _stop_p1, _num_points_p1, _delay_p1,
          _param_set, _start_p2, _stop_p2, _num_points_p2, _delay_p2,
