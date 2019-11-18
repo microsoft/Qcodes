05449ae26cc7cf936080b503b003c687edfef634
Mikhail Astafev <astafan8@gmail.com>
2019-11-18 23:12:30 +0100

    Cover Parameter.snapshot method with tests for various flags

--- qcodes/tests/test_parameter.py
+++ qcodes/tests/test_parameter.py
@@ -1770,6 +1770,247 @@ def test_deprecated_param_warns():
     assert a.get_count == 3
 
 
+NOT_PASSED = 'NOT_PASSED'
+
+
+@pytest.fixture(params=(True, False, NOT_PASSED))
+def snapshot_get(request):
+    return request.param
+
+
+@pytest.fixture(params=(True, False, NOT_PASSED))
+def snapshot_value(request):
+    return request.param
+
+
+@pytest.fixture(params=(None, False, NOT_PASSED))
+def get_cmd(request):
+    return request.param
+
+
+def create_parameter(snapshot_get, snapshot_value, get_cmd, offset=NOT_PASSED):
+    kwargs = dict(set_cmd=None,
+                  label='Parameter',
+                  unit='a.u.',
+                  docstring='some docs')
+
+    if offset != NOT_PASSED:
+        kwargs.update(offset=offset)
+
+    if snapshot_get != NOT_PASSED:
+        kwargs.update(snapshot_get=snapshot_get)
+
+    if snapshot_value != NOT_PASSED:
+        kwargs.update(snapshot_value=snapshot_value)
+
+    if get_cmd != NOT_PASSED:
+        kwargs.update(get_cmd=get_cmd)
+
+    p = Parameter('p', **kwargs)
+
+    if get_cmd is not False:
+        def wrap_in_call_counter(get_func):
+            call_count = 0
+
+            def wrapped_func(*args, **kwargs):
+                nonlocal call_count
+                call_count += 1
+                return get_func(*args, **kwargs)
+
+            wrapped_func.call_count = lambda: call_count
+
+            return wrapped_func
+
+        p.get = wrap_in_call_counter(p.get)
+        assert p.get.call_count() == 0  # pre-condition
+    else:
+        assert not hasattr(p, 'get')  # pre-condition
+
+    return p
+
+
+@pytest.fixture
+def parameterized_parameter(snapshot_get, snapshot_value, get_cmd):
+    p = create_parameter(snapshot_get, snapshot_value, get_cmd)
+    return p
+
+
+@pytest.mark.parametrize('cache_is_valid', (True, False))
+@pytest.mark.parametrize('update', (True, False, NOT_PASSED))
+def test_snapshot_contains_parameter_attributes(
+        parameterized_parameter, cache_is_valid, update):
+    p = parameterized_parameter
+
+    if cache_is_valid:
+        p.set(42)
+
+    if update != NOT_PASSED:
+        s = p.snapshot(update=update)
+    else:
+        s = p.snapshot()
+
+    assert isinstance(s, dict)
+
+    # Not metadata key in the snapshot because we didn't pass any metadata
+    # for the parameter
+    # TODO: test for parameter with metadata
+    assert 'metadata' not in s
+
+    assert s['__class__'] == 'qcodes.instrument.parameter.Parameter'
+    assert s['full_name'] == 'p'
+
+    # The following is because the parameter does not belong to an instrument
+    # TODO: test for a parameter that is attached to instrument
+    assert 'instrument' not in s
+    assert 'instrument_name' not in s
+
+    # These attributes have value of ``None`` hence not included in the snapshot
+    # TODO: test snapshot when some of these are not None
+    none_attrs = ('step', 'scale', 'offset', 'val_mapping', 'vals')
+    for attr in none_attrs:
+        assert getattr(p, attr) is None  # pre-condition
+        assert attr not in s
+
+    # TODO: test snapshot when some of these are None
+    not_none_attrs = {'name': p.name, 'label': p.label, 'unit': p.unit,
+                      'inter_delay': p.inter_delay, 'post_delay': p.post_delay}
+    for attr, value in not_none_attrs.items():
+        assert s[attr] == value
+
+
+@pytest.mark.parametrize('update', (True, False, NOT_PASSED))
+@pytest.mark.parametrize('cache_is_valid', (True, False))
+def test_snapshot_timestamp_depends_only_on_cache_validity(
+        parameterized_parameter, update, cache_is_valid):
+    p = parameterized_parameter
+
+    if cache_is_valid:
+        t0 = datetime.now()
+        p.set(42)
+
+    if update != NOT_PASSED:
+        s = p.snapshot(update=update)
+    else:
+        s = p.snapshot()
+
+    if cache_is_valid:
+        ts = datetime.strptime(s['ts'], '%Y-%m-%d %H:%M:%S')
+        t0_up_to_seconds = t0.replace(microsecond=0)
+        assert ts >= t0_up_to_seconds
+    else:
+        assert s['ts'] is None
+
+
+@pytest.mark.parametrize('cache_is_valid', (True, False))
+@pytest.mark.parametrize('update', (True, False, NOT_PASSED))
+def test_snapshot_when_snapshot_value_is_false(
+        snapshot_get, get_cmd, cache_is_valid, update):
+
+    p = create_parameter(
+        snapshot_get=snapshot_get, snapshot_value=False, get_cmd=get_cmd)
+
+    if cache_is_valid:
+        p.set(42)
+
+    if update != NOT_PASSED:
+        s = p.snapshot(update=update)
+    else:
+        s = p.snapshot()
+
+    assert 'value' not in s
+    assert 'raw_value' not in s
+
+    if get_cmd is not False:
+        assert p.get.call_count() == 0
+
+
+def test_snapshot_value_is_true_by_default(snapshot_get, get_cmd):
+    p = create_parameter(
+        snapshot_value=NOT_PASSED,
+        snapshot_get=snapshot_get, get_cmd=get_cmd)
+    assert p._snapshot_value is True
+
+
+def test_snapshot_get_is_true_by_default(snapshot_value, get_cmd):
+    p = create_parameter(
+        snapshot_get=NOT_PASSED,
+        snapshot_value=snapshot_value, get_cmd=get_cmd)
+    assert p._snapshot_get is True
+
+
+@pytest.mark.parametrize('update', (True, False, NOT_PASSED))
+@pytest.mark.parametrize('cache_is_valid', (True, False))
+def test_snapshot_when_snapshot_get_is_false(get_cmd, update, cache_is_valid):
+    p = create_parameter(
+        snapshot_get=False,
+        snapshot_value=True, get_cmd=get_cmd, offset=4)
+
+    if cache_is_valid:
+        p.set(42)
+
+    if update != NOT_PASSED:
+        s = p.snapshot(update=update)
+    else:
+        s = p.snapshot()
+
+    if cache_is_valid:
+        assert s['value'] == 42
+        assert s['raw_value'] == 46
+    else:
+        assert s['value'] is None
+        assert s['raw_value'] is None
+
+    if get_cmd is not False:
+        assert p.get.call_count() == 0
+
+
+@pytest.mark.parametrize('update', (True, False, NOT_PASSED))
+@pytest.mark.parametrize('cache_is_valid', (True, False))
+def test_snapshot_of_non_gettable_parameter_mirrors_cache(
+        update, cache_is_valid):
+    p = create_parameter(
+        snapshot_get=True, snapshot_value=True, get_cmd=False, offset=4)
+
+    if cache_is_valid:
+        p.set(42)
+
+    if update != NOT_PASSED:
+        s = p.snapshot(update=update)
+    else:
+        s = p.snapshot()
+
+    if cache_is_valid:
+        assert s['value'] == 42
+        assert s['raw_value'] == 46
+    else:
+        assert s['value'] is None
+        assert s['raw_value'] is None
+
+
+@pytest.mark.parametrize('update', (True, False, NOT_PASSED))
+@pytest.mark.parametrize('cache_is_valid', (True, False))
+def test_snapshot_of_gettable_parameter_depends_on_update(update, cache_is_valid):
+    p = create_parameter(
+        snapshot_get=True, snapshot_value=True, get_cmd=lambda: 69, offset=4)
+
+    if cache_is_valid:
+        p.set(42)
+
+    if update != NOT_PASSED:
+        s = p.snapshot(update=update)
+    else:
+        s = p.snapshot()
+
+    if update is not True and cache_is_valid:
+        assert s['value'] == 42
+        assert s['raw_value'] == 46
+        assert p.get.call_count() == 0
+    else:
+        assert s['value'] == 65
+        assert s['raw_value'] == 69
+        assert p.get.call_count() == 1
+
+
 def test_delegate_parameter_init():
     """
     Test that the lable and unit get used from source parameter if not
