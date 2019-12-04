4699e895e271c3f19223d72b3b482da87c5288a2
Liang Gao (KFORCE INC) <v-liagao@microsoft.com>
2019-12-04 13:18:07 -0800

    fix the lakeshore325 sensor status bug

---
+++ qcodes/tests/drivers/test_lakeshore_325.py
@@ -0,0 +1,27 @@
+import numpy as np
+from hypothesis import given
+import hypothesis.strategies as st
+from qcodes.instrument_drivers.Lakeshore import Model_325
+
+COMPONENTS = [0, 1, 16, 32, 64, 128]
+
+
+@given(
+    st.lists(st.sampled_from(COMPONENTS[1:]), 1, 5, unique=True).map(sorted)
+)
+def test_decode_sensor_status(component):
+    """
+    The sensor status is one of the status codes, or a sum thereof. Multiple
+    status are possible as they are not necessarily mutually exclusive.
+    The static method '_get_sum_terms' in the Model_325_Sensor class would
+    decode the status into status code(s).
+    """
+    status = sum(component)
+    status_codes = Model_325.Model_325_Sensor._get_sum_terms(COMPONENTS,
+                                                             status)
+    assert np.all(status_codes == component[::-1])
+
+
+def test_decode_sensor_status_0():
+    status_codes = Model_325.Model_325_Sensor._get_sum_terms(COMPONENTS, 0)
+    assert np.all(status_codes == [0])
