b95a9e9b530b68b5e40d8d81fc21084c5ba002c1
AUC Hardal <auchardal@gmail.com>
2020-01-16 13:42:30 +0100

    Add tests for matrix rows and columns

--- qcodes/tests/drivers/test_keithley_3706A.py
+++ qcodes/tests/drivers/test_keithley_3706A.py
@@ -44,3 +44,25 @@ def test_get_interlock_state(driver):
         dict_list.append(dictionary)
     assert tuple(dict_list) == driver.get_interlock_state()
 
+
+def test_get_number_of_rows(driver):
+    assert [6, 6, 6] == driver._get_number_of_rows()
+
+
+def test_get_number_of_columns(driver):
+    assert [16, 16, 16] == driver._get_number_of_columns()
+
+
+def test_get_rows(driver):
+    rows_in_a_slot = ['1', '2', '3', '4', '5', '6']
+    assert rows_in_a_slot == driver._get_rows()[0]
+    assert rows_in_a_slot == driver._get_rows()[1]
+    assert rows_in_a_slot == driver._get_rows()[2]
+
+
+def test_get_columns(driver):
+    columns_in_a_slot = ['01', '02', '03', '04', '05', '06', '07', '08', '09',
+                         '10', '11', '12', '13', '14', '15', '16']
+    assert columns_in_a_slot == driver._get_columns()[0]
+    assert columns_in_a_slot == driver._get_columns()[1]
+    assert columns_in_a_slot == driver._get_columns()[2]
