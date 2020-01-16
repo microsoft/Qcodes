80521bd23967781d3131627759967350e3036ec8
AUC Hardal <auchardal@gmail.com>
2020-01-16 12:12:31 +0100

    Add test for slot ids

--- qcodes/tests/drivers/test_keithley_3706A.py
+++ qcodes/tests/drivers/test_keithley_3706A.py
@@ -10,7 +10,7 @@ visalib = sims.__file__.replace('__init__.py', 'Keithley_3706A.yaml@sim')
 @pytest.fixture(scope='function')
 def driver():
     driver = Keithley_3706A('Keithley_3706A',
-                            address='GPIB::13::INSTR',
+                            address='GPIB::11::INSTR',
                             visalib=visalib)
     yield driver
     driver.close()
@@ -26,4 +26,8 @@ def test_idn(driver):
 def test_switch_card_idn(driver):
     assert {'slot_no': "1", 'model': "3730", 'mtype': "6x16 High Density Matrix",
             'firmware': "01.40h", 'serial': "4447332"} == driver.get_switch_cards()[0]
+
+
+def test_installed_card_id(driver):
+    assert ['1', '2', '3'] == driver._get_slot_id()
 
