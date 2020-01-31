62a997ee096146c104d2fbe260d0d6cd6412f217
AUC Hardal <auchardal@gmail.com>
2020-01-31 15:09:19 +0100

    f-string conversion III

--- qcodes/instrument_drivers/rohde_schwarz/private/HMC804x.py
+++ qcodes/instrument_drivers/rohde_schwarz/private/HMC804x.py
@@ -9,59 +9,48 @@ class RohdeSchwarzHMC804xChannel(InstrumentChannel):
                  channel: Union[str, int]) -> None:
         super().__init__(parent, name)
 
-        select_cmd = ":INSTrument:NSELect {};".format(channel)
+        select_cmd = f":INSTrument:NSELect {channel};"
+        voltage_amp = ":SOURce:VOLTage:LEVel:IMMediate:AMPLitude"
+        current_amp = ":SOURce:CURRent:LEVel:IMMediate:AMPLitude"
 
         self.add_parameter("set_voltage",
                            label='Target voltage output',
-                           set_cmd="{} "
-                                   ":SOURce:VOLTage:LEVel:IMMediate:AMPLitude "
-                                   "{}".format(select_cmd, '{}'),
-                           get_cmd="{} "
-                                   ":SOURce:VOLTage:LEVel:IMMediate:AMPLitude?".
-                                   format(select_cmd),
+                           set_cmd=f"{select_cmd} {voltage_amp} {{}}",
+                           get_cmd=f"{select_cmd} {voltage_amp}?",
                            get_parser=float,
                            unit='V',
                            vals=vals.Numbers(0, 32.050)
                           )
         self.add_parameter("set_current",
                            label='Target current output',
-                           set_cmd="{} "
-                                   ":SOURce:CURRent:LEVel:IMMediate:AMPLitude "
-                                   "{}".format(select_cmd, '{}'),
-                           get_cmd="{} "
-                                   ":SOURce:CURRent:LEVel:IMMediate:AMPLitude?".
-                                   format(select_cmd),
+                           set_cmd=f"{select_cmd} {current_amp} {{}}",
+                           get_cmd=f"{select_cmd} {current_amp}?",
                            get_parser=float,
                            unit='A',
                            vals=vals.Numbers(0.5e-3, self._parent.max_current)
                            )
         self.add_parameter('state',
                            label='Output enabled',
-                           set_cmd='{} :OUTPut:CHANnel:STATe {}'.
-                                   format(select_cmd, '{}'),
-                           get_cmd='{} :OUTPut:CHANnel:STATe?'.
-                                   format(select_cmd),
+                           set_cmd=f'{select_cmd} :OUTPut:CHANnel:STATe {{}}',
+                           get_cmd=f'{select_cmd} :OUTPut:CHANnel:STATe?',
                            val_mapping={'ON': 1, 'OFF': 0},
                            vals=vals.Enum('ON', 'OFF')
                            )
         self.add_parameter("voltage",
                            label='Measured voltage',
-                           get_cmd="{} :MEASure:SCALar:VOLTage:DC?".format(
-                               select_cmd),
+                           get_cmd=f"{select_cmd} :MEASure:SCALar:VOLTage:DC?",
                            get_parser=float,
                            unit='V',
                           )
         self.add_parameter("current",
                            label='Measured current',
-                           get_cmd="{} :MEASure:SCALar:CURRent:DC?".format(
-                               select_cmd),
+                           get_cmd=f"{select_cmd} :MEASure:SCALar:CURRent:DC?",
                            get_parser=float,
                            unit='A',
                            )
         self.add_parameter("power",
                            label='Measured power',
-                           get_cmd="{} :MEASure:SCALar:POWer?".format(
-                               select_cmd),
+                           get_cmd=f"{select_cmd} :MEASure:SCALar:POWer?",
                            get_parser=float,
                            unit='W',
                            )
@@ -97,7 +86,7 @@ class _RohdeSchwarzHMC804x(VisaInstrument):
         channels = ChannelList(self, "SupplyChannel",
                                RohdeSchwarzHMC804xChannel, snapshotable=False)
         for ch_num in range(1, num_channels+1):
-            ch_name = "ch{}".format(ch_num)
+            ch_name = f"ch{ch_num}"
             channel = RohdeSchwarzHMC804xChannel(self, ch_name, ch_num)
             channels.append(channel)
             self.add_submodule(ch_name, channel)
