f5d1f4c656c0677fdafc84762eb90deb4e7144d0
AUC Hardal <auchardal@gmail.com>
2020-02-07 15:06:06 +0100

    Extend convenience connect methods with capability of disconnect

--- qcodes/instrument_drivers/tektronix/Keithley_3706A.py
+++ qcodes/instrument_drivers/tektronix/Keithley_3706A.py
@@ -503,13 +503,15 @@ class Keithley_3706A(VisaInstrument):
             analog_backplane_relays.append(''.join(element))
         return analog_backplane_relays
 
-    def connect_row_to_columns(self, slot_id: int, row_id: int,
-                               columns: List[int]) -> List[str]:
+    def connect_or_disconnect_row_to_columns(self, action: str,
+                                             slot_id: int, row_id: int,
+                                             columns: List[int]) -> List[str]:
         """
         A convenient function that connects given columns to a row of a
         slot and opens the formed channels.
 
         Args:
+            action: Identifies the action either as "connect" or "disconnect".
             slot_id: The specifier for the slot from which the row and columns
                 will be selected.
             row_id: The specifier for the row to which the provided columns
@@ -517,6 +519,9 @@ class Keithley_3706A(VisaInstrument):
             columns: The specifiers of the columns will be connected to the
                 provided row.
         """
+        if action not in ["connect", "disconnect"]:
+            raise ValueError("The action should be identified as either "
+                             "connect or disconnect.")
         slots = self._get_slot_ids()
         slot = str(slot_id)
         if slot not in slots:
@@ -529,20 +534,25 @@ class Keithley_3706A(VisaInstrument):
                 columns_list.append('0' + str(i))
             else:
                 columns_list.append(str(i))
-        channels_to_connect = []
+        channels_to_connect_or_disconnect = []
         for element in itertools.product(slot, row, columns_list):
-            channels_to_connect.append(''.join(element))
-        for channel in channels_to_connect:
-            self.open_channel(channel)
-        return channels_to_connect
+            channels_to_connect_or_disconnect.append(''.join(element))
+        for channel in channels_to_connect_or_disconnect:
+            if action == "connect":
+                self.open_channel(channel)
+            else:
+                self.close_channel(channel)
+        return channels_to_connect_or_disconnect
 
-    def connect_column_to_rows(self, slot_id: int, column_id,
-                               rows: List[int]) -> List[str]:
+    def connect_or_disconnect_column_to_rows(self, action: str,
+                                             slot_id: int, column_id: int,
+                                             rows: List[int]) -> List[str]:
         """
         A convenient function that connects given rows to a column of a
         slot and opens the formed channels.
 
         Args:
+            action: Identifies the action either as "connect" or "disconnect".
             slot_id: The specifier for the slot from which the row and columns
                 will be selected.
             column_id: The specifier for the column to which the provided rows
@@ -550,6 +560,9 @@ class Keithley_3706A(VisaInstrument):
             rows: The specifiers of the rows will be connected to the
                 provided column.
         """
+        if action not in ["connect", "disconnect"]:
+            raise ValueError("The action should be identified as either "
+                             "connect or disconnect.")
         slots = self._get_slot_ids()
         slot = str(slot_id)
         if slot not in slots:
@@ -561,12 +574,15 @@ class Keithley_3706A(VisaInstrument):
         else:
             column.append(str(column_id))
         rows_list = [str(x) for x in rows]
-        channels_to_connect = []
+        channels_to_connect_or_disconnect = []
         for element in itertools.product(slot, rows_list, column):
-            channels_to_connect.append(''.join(element))
-        for channel in channels_to_connect:
-            self.open_channel(channel)
-        return channels_to_connect
+            channels_to_connect_or_disconnect.append(''.join(element))
+        for channel in channels_to_connect_or_disconnect:
+            if action == "connect":
+                self.open_channel(channel)
+            else:
+                self.close_channel(channel)
+        return channels_to_connect_or_disconnect
 
     def get_idn(self) -> Dict[str, Optional[str]]:
         """
