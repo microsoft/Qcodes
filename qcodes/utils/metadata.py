220c846a7d8aa675d16d10640453fe52fdf76169
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-03-17 10:58:26 +0100

    pep8

--- qcodes/utils/metadata.py
+++ qcodes/utils/metadata.py
@@ -58,8 +58,9 @@ class Metadatable:
 
         return snap
 
-    def snapshot_base(self, update: bool = False,
-                      params_to_skip_update: Optional[Sequence[str]] = None) -> Dict:
+    def snapshot_base(
+            self, update: bool = False,
+            params_to_skip_update: Optional[Sequence[str]] = None) -> Dict:
         """
         Override this with the primary information for a subclass.
         """
@@ -69,9 +70,9 @@ class Metadatable:
 class ParameterDiff(NamedTuple):
     # Cannot be generic in Python < 3.7:
     # https://stackoverflow.com/questions/50530959/generic-namedtuple-in-python-3-6
-    left_only : ParameterDict[Any]
-    right_only : ParameterDict[Any]
-    changed : ParameterDict[Tuple[Any, Any]]
+    left_only: ParameterDict[Any]
+    right_only: ParameterDict[Any]
+    changed: ParameterDict[Tuple[Any, Any]]
 
 ## FUNCTIONS ##
 
