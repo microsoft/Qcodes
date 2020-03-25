c95d651bd59c97e219e2cc6e79ecc6e1b6b90cb3
lakhotiaharshit <lakhotiaharshit@gmail.com>
2020-03-25 11:56:08 +0100

    Copy import of data to one level down

---
+++ qcodes/data/__init__.py
@@ -0,0 +1,6 @@
+from qcodes.data.location import FormatLocation
+from qcodes.data.data_array import DataArray
+from qcodes.data.format import Formatter
+from qcodes.data.gnuplot_format import GNUPlotFormat
+from qcodes.data.hdf5_format import HDF5Format
+from qcodes.data.io import DiskIO
