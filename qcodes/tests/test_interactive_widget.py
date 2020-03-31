a4699e95878fd04407551191949f5c78b1eb32a5
Bas Nijholt <basnijholt@gmail.com>
2020-03-31 15:40:07 +0200

    improve tests

--- qcodes/tests/test_interactive_widget.py
+++ qcodes/tests/test_interactive_widget.py
@@ -1,4 +1,7 @@
+import time
+
 import pytest
+from ipywidgets import Textarea, Button
 
 # we only need `experiment` here, but pytest does not discover the dependencies
 # by itself so we also need to import all the fixtures this one is dependent
@@ -30,7 +33,34 @@ def test_full_widget():
 
 
 def test_expandable_dict(
-    standalone_parameters_dataset, tab
+    tab, standalone_parameters_dataset
 ):  # pylint: disable=redefined-outer-name
+    ds = standalone_parameters_dataset
     dct = {"a": {"b": "c", "d": {"e": "f"}}}
-    interactive_widget.expandable_dict(dct, dataset, tab)
+    box = interactive_widget.expandable_dict(dct, tab, ds)
+    button, = box.children
+    button.click()
+    text_area, *buttons = box.children
+    snapshot_button = next(b for b in buttons if "snapshot" in b.description.lower())
+    snapshot_button.click()  # should make a snapshot in the tab
+    time.sleep(1)  # after click
+    assert "snapshot" in tab.get_title(1)
+
+
+def test_editable_metadata(standalone_parameters_dataset):
+    ds = standalone_parameters_dataset
+    box = interactive_widget.editable_metadata(ds)
+    button = box.children[0]
+    button.click()
+    assert len(box.children) == 2
+    text_area, save_button = box.children
+    assert isinstance(text_area, Textarea)
+    assert isinstance(button, Button)
+    test_test = 'test value'
+    text_area.value = test_test
+    save_button.click()
+    time.sleep(1)  # after click
+    # Test if metadata is saved.
+    assert ds.metadata[interactive_widget._META_DATA_KEY] == test_test
+
+    assert box.children[0].description == test_test
