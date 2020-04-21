d185efd79bd7fbbc7c5c9e45888a1956310a381a
Pieter Eendebak <pieter.eendebak@gmail.com>
2020-04-21 16:42:42 +0200

    autopep

--- qcodes/tests/test_slack.py
+++ qcodes/tests/test_slack.py
@@ -4,51 +4,49 @@ from unittest.mock import patch, MagicMock
 
 class TestSlack(TestCase):
 
-
     def test_Slack(self):
 
-        slack_config = {'bot_name': 'test_bot', 'token': '123123', 'names': ['dummyuser']}    
+        slack_config = {'bot_name': 'test_bot', 'token': '123123', 'names': ['dummyuser']}
 
         mock_slacker_module = MagicMock(name='slacker_module')
         slacker_class_mock = MagicMock(name='SlackerMock')
         slacker_class_mock.users.get_user_id = MagicMock(return_value='bot_id')
         response = MagicMock()
-        response.body = {'members': [{'name':'dummyuser', 'id': '123'}] }
+        response.body = {'members': [{'name': 'dummyuser', 'id': '123'}]}
         slacker_class_mock.users.list = MagicMock(name='mock_users_list', return_value=response)
-        mock_slacker_module.Slacker = slacker_class_mock        
+        mock_slacker_module.Slacker = slacker_class_mock
         mock_slacker_module.Slacker = MagicMock(return_value=slacker_class_mock)
-        
+
         with patch.dict('sys.modules', slacker=mock_slacker_module):
-        
+
             from qcodes.utils.slack import Slack
-        
+
             slack = Slack(config=slack_config, auto_start=False)
-            
-
-            
-config = {'bot_name': 'test_bot', 'token': '123123', 'names': ['dummyuser']}    
 
 
-#%%
+config = {'bot_name': 'test_bot', 'token': '123123', 'names': ['dummyuser']}
+
+
+# %%
 mock_slacker_module = MagicMock(name='slacker_module')
 slacker_class_mock = MagicMock(name='SlackerMock')
 slacker_class_mock.users.get_user_id = MagicMock(return_value='bot_id')
 response = MagicMock()
-response.body = {'members': [{'name':'dummyuser', 'id': '123'}] }
+response.body = {'members': [{'name': 'dummyuser', 'id': '123'}]}
 slacker_class_mock.users.list = MagicMock(name='mock_users_list', return_value=response)
 mock_slacker_module.Slacker = slacker_class_mock
 
-r=slacker_class_mock.users.get_user_id()
+r = slacker_class_mock.users.get_user_id()
 print(r)
-r=slacker_class_mock.users.list()
+r = slacker_class_mock.users.list()
 print(r)
 
 mock_slacker_module.Slacker = MagicMock(return_value=slacker_class_mock)
 
-#%%
+# %%
 with patch.dict('sys.modules', slacker=mock_slacker_module):
 
-#with patch.object(slacker, 'Slacker', return_value=slacker_class_mock ) as slacker_mock:
+    # with patch.object(slacker, 'Slacker', return_value=slacker_class_mock ) as slacker_mock:
     #from slacker import Slacker
 
     from slacker import Slacker
@@ -58,10 +56,9 @@ with patch.dict('sys.modules', slacker=mock_slacker_module):
     slack.users.get_user_id('s')
     slack.users.list()
     #slack.get_users = get_users
-    
+
     print(slack)
-    
+
     slack = Slack(config=config, auto_start=False)
-    users = {'dummyuser':{'id': '123'}}
-    r=slack.get_im_ids(users)
-            
+    users = {'dummyuser': {'id': '123'}}
+    r = slack.get_im_ids(users)
