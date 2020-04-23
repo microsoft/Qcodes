f2161d619d3f76013fb426c8f22510cebc379a5e
John Watson <john.watson@microsoft.com>
2020-04-23 20:49:15 +0200

    Set MM to Spot_C before _get_capacitance call

--- qcodes/instrument_drivers/Keysight/keysightb1500/KeysightB1520A.py
+++ qcodes/instrument_drivers/Keysight/keysightb1500/KeysightB1520A.py
@@ -171,6 +171,8 @@ class B1520A(B1500Module):
         self.write(msg.message)
 
     def _get_capacitance(self) -> Tuple[float, float]:
+        self._set_measurement_mode(constants.MM.Mode.SPOT_C)
+        
         msg = MessageBuilder().tc(
             chnum=self.channels[0], mode=constants.RangingMode.AUTO
         )
