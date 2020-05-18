de918af5fdc77dcda04d38891c907db5b8d9cf75
Harshit Lakhotia <lakhotiaharshit@gmail.com>
2020-05-18 11:43:05 +0200

    Remove addition extra line

--- qcodes/instrument_drivers/Keysight/keysightb1500/KeysightB1500_base.py
+++ qcodes/instrument_drivers/Keysight/keysightb1500/KeysightB1500_base.py
@@ -105,7 +105,6 @@ class KeysightB1500(VisaInstrument):
                     Note that the integration time will not be updated
                     if a non-integer value is written to the B1500."""))
 
-
         # Instrument is initialized with this setting having value of
         # `False`, hence let's set the parameter to this value since it is
         # not possible to request this value from the instrument.
