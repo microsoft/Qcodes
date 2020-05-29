6f911b3038dfbc51733431be3c6e693768a04806
Mikhail Astafev <astafan8@gmail.com>
2020-05-29 16:34:15 +0200

    Use pyvisa-sim to test error handling of B1500

--- qcodes/tests/drivers/keysight_b1500/b1500_driver_tests/test_b1500.py
+++ qcodes/tests/drivers/keysight_b1500/b1500_driver_tests/test_b1500.py
@@ -199,10 +199,5 @@ def test_self_calibration_failed(b1500):
 
 
 def test_error_message(b1500):
-    mock_ask = MagicMock()
-    b1500.ask = mock_ask
-    mock_ask.return_value = '0,"No Error."'
-
     response = b1500.error_message()
     assert '0,"No Error."' == response
-    mock_ask.assert_called_once_with(f'ERRX?')
