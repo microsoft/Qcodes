4475fa53b9d22c5dfa0b7e1d09ce8f66cafa1d9e
AUC Hardal <auchardal@gmail.com>
2020-06-16 14:04:25 +0200

    Fix conftest

--- qcodes/tests/conftest.py
+++ qcodes/tests/conftest.py
@@ -5,6 +5,18 @@ import qcodes as qc
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "win32: tests that only run under windows")
+    config.addinivalue_line(
+        "markers", "env(name): mark test to run only on named environment"
+    )
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "-E",
+        action="store",
+        metavar="NAME",
+        help="only run tests marked with 'env(NAME)' to run in environment NAME.",
+    )
 
 
 def pytest_runtest_setup(item):
@@ -12,6 +24,15 @@ def pytest_runtest_setup(item):
     supported_platforms = ALL.intersection(mark.name for mark in item.iter_markers())
     if supported_platforms and sys.platform not in supported_platforms:
         pytest.skip(f"cannot run on platform {sys.platform}")
+
+    env_names = [mark.args[0] for mark in item.iter_markers(name="env")]
+    if env_names:
+        env_from_option = item.config.getoption("-E")
+        if env_from_option not in env_names:
+            pytest.skip(
+                f"Not running in {env_names!r} environment(s). "
+                f"Use '-E' command line option to run this test."
+            )
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -43,29 +64,3 @@ def disable_config_subscriber():
         yield
     finally:
         qc.config.subscription.default_subscribers = original_state
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "-E",
-        action="store",
-        metavar="NAME",
-        help="only run tests marked with 'env(NAME)' to run in environment NAME.",
-    )
-
-
-def pytest_configure(config):
-    config.addinivalue_line(
-        "markers", "env(name): mark test to run only on named environment"
-    )
-
-
-def pytest_runtest_setup(item):
-    env_names = [mark.args[0] for mark in item.iter_markers(name="env")]
-    if env_names:
-        env_from_option = item.config.getoption("-E")
-        if env_from_option not in env_names:
-            pytest.skip(
-                f"Not running in {env_names!r} environment(s). "
-                f"Use '-E' command line option to run this test."
-            )
