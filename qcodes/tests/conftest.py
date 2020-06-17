b09f8740ea7ef732bf79117fd718fa4828afd4d5
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2020-06-17 09:53:11 +0200

    add missing newline

--- qcodes/tests/conftest.py
+++ qcodes/tests/conftest.py
@@ -34,7 +34,7 @@ def disable_telemetry():
 def disable_config_subscriber():
     """
     We do not want the tests to send generate subscription events unless specifically
-    enabled in the test. So disable any default subscriber defined
+    enabled in the test. So disable any default subscriber defined.
     """
 
     original_state = qc.config.subscription.default_subscribers
