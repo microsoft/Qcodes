257391eef59b669195d56ccfd81b5a8246020aaf
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-07-03 10:34:19 +0200

    fix imports

--- qcodes/dataset/data_set_cache.py
+++ qcodes/dataset/data_set_cache.py
@@ -1,10 +1,11 @@
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Optional, Dict
 
 import numpy as np
 
 from qcodes.dataset.sqlite.queries import completed, get_dataset_num_rows
 
 if TYPE_CHECKING:
+    import pandas as pd
     from .data_set import DataSet, ParameterData
 
 
@@ -32,7 +33,7 @@ class DataSetCache:
             self._last_read_row = num_rows
         self._dataset._completed = completed(self._dataset.conn, self._dataset.run_id)
 
-    def _merge_data_dicts_into_data(self, new_data_dicts: ParameterData) -> None:
+    def _merge_data_dicts_into_data(self, new_data_dicts: 'ParameterData') -> None:
         if self._data is None:
             raise RuntimeError
         for (old_outer_name, old_outer_data), (new_outer_name, new_outer_data) in zip(self._data.items(),
@@ -42,9 +43,9 @@ class DataSetCache:
                 merged_inner_dict[old_name] = np.append(old_value, new_value)
             self._data[old_outer_name] = merged_inner_dict
 
-    def data(self) -> Optional[ParameterData]:
+    def data(self) -> Optional['ParameterData']:
         self.load_data_from_db()
         return self._data
 
-    def as_pandas_dataframe(self):
-        pass
+    def as_pandas_dataframe(self) -> Dict[str, "pd.DataFrame"]:
+        raise NotImplemented
