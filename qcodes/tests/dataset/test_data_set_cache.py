7cafb5857855d61e99b4fdc64ba0849e7d63c0b7
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-07-10 14:09:28 +0200

    add test for multiparameter

--- qcodes/tests/dataset/test_data_set_cache.py
+++ qcodes/tests/dataset/test_data_set_cache.py
@@ -104,6 +104,37 @@ def test_cache_1d_array_in_1d(experiment, DAC, channel_array_instrument, n_point
                                                 data)
 
 
+@pytest.mark.parametrize("bg_writing", [True, False])
+@pytest.mark.parametrize("storage_type", ['numeric', 'array'])
+@settings(deadline=None, max_examples=10)
+@given(n_points=hst.integers(min_value=1, max_value=21))
+def test_cache_multiparam_in_1d(experiment, DAC, channel_array_instrument, n_points, bg_writing, storage_type):
+    param = channel_array_instrument.A.dummy_2d_multi_parameter
+    meas = Measurement()
+    meas.register_parameter(DAC.ch1, paramtype=storage_type)
+    meas.register_parameter(param, setpoints=(DAC.ch1,), paramtype=storage_type)
+    with meas.run(write_in_background=bg_writing) as datasaver:
+        dataset = datasaver.dataset
+        for i, v1 in enumerate(np.linspace(-1, 1, n_points)):
+            datasaver.add_result((DAC.ch1, v1),
+                                 (param, param.get()))
+            datasaver.flush_data_to_database(block=True)
+            data = dataset.cache.data()
+            n_rows_written = i+1
+            for j, subparam in enumerate(param.names):
+                if storage_type == 'numeric':
+                    expected_shape = n_rows_written * np.prod(param.shapes[j])
+                else:
+                    expected_shape = (n_rows_written,) + param.shapes[j]
+                assert data[subparam][subparam].shape == expected_shape
+                # TODO why does this not pass
+                # assert data[subparam][DAC.ch1.full_name].shape == expected_shape
+                for setpoint_name in param.setpoint_full_names[j]:
+                    assert data[subparam][setpoint_name].shape == expected_shape
+            _assert_parameter_data_is_identical(dataset.get_parameter_data(),
+                                                data)
+
+
 def _assert_parameter_data_is_identical(expected: Dict[str, Dict[str, np.ndarray]],
                                         actual: Dict[str, Dict[str, np.ndarray]]):
     assert expected.keys() == actual.keys()
