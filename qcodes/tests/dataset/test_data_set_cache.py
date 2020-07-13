cd023647bed8c0af601b5a850dd616d4b03a6263
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-07-13 10:54:27 +0200

    also test against text param

--- qcodes/tests/dataset/test_data_set_cache.py
+++ qcodes/tests/dataset/test_data_set_cache.py
@@ -147,19 +147,27 @@ def test_cache_multiparam_in_1d(experiment, DAC, channel_array_instrument, n_poi
 
 @pytest.mark.parametrize("bg_writing", [True, False])
 @pytest.mark.parametrize("storage_type", ['array', None])
+@pytest.mark.parametrize("outer_param_type", ['numeric', 'text'])
 @settings(deadline=None, max_examples=10)
 @given(n_points=hst.integers(min_value=1, max_value=21))
-def test_cache_complexmultiparam_in_1d(experiment, DAC, channel_array_instrument, n_points, bg_writing, storage_type):
+def test_cache_complex_array_param_in_1d(experiment, DAC, channel_array_instrument, n_points, bg_writing, storage_type, outer_param_type):
     param = channel_array_instrument.A.dummy_complex_array_parameter
     meas = Measurement()
-    meas.register_parameter(DAC.ch1, paramtype=storage_type)
-    meas.register_parameter(param, setpoints=(DAC.ch1,), paramtype=storage_type)
+    if outer_param_type == 'numeric':
+        outer_param = DAC.ch1
+        outer_setpoints = np.linspace(-1, 1, n_points)
+        outer_storage_type = storage_type
+    else:
+        outer_param = channel_array_instrument.A.dummy_text
+        outer_setpoints = ['A', 'B', 'C', 'D']
+        outer_storage_type = 'text'
+    meas.register_parameter(outer_param, paramtype=outer_storage_type)
+    meas.register_parameter(param, setpoints=(outer_param,), paramtype=storage_type)
     array_used = _array_param_used_in_tree(meas)
-
     with meas.run(write_in_background=bg_writing) as datasaver:
         dataset = datasaver.dataset
-        for i, v1 in enumerate(np.linspace(-1, 1, n_points)):
-            datasaver.add_result((DAC.ch1, v1),
+        for i, v1 in enumerate(outer_setpoints):
+            datasaver.add_result((outer_param, v1),
                                  (param, param.get()))
             datasaver.flush_data_to_database(block=True)
             data = dataset.cache.data()
@@ -172,7 +180,7 @@ def test_cache_complexmultiparam_in_1d(experiment, DAC, channel_array_instrument
             assert data[param.full_name][param.full_name].shape == expected_shape
             if storage_type != 'array':
                 # TODO the expansion both for cache and get_parameter_data is buggy iff all types are array
-                assert data[param.full_name][DAC.ch1.full_name].shape == expected_shape
+                assert data[param.full_name][outer_param.full_name].shape == expected_shape
             for setpoint_name in param.setpoint_full_names:
                 assert data[param.full_name][setpoint_name].shape == expected_shape
             _assert_parameter_data_is_identical(dataset.get_parameter_data(),
