f929e6e80ab90216b08e7668fa8e9d73bf71c032
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-07-13 10:35:30 +0200

    add complext test case

--- qcodes/tests/dataset/test_data_set_cache.py
+++ qcodes/tests/dataset/test_data_set_cache.py
@@ -145,6 +145,40 @@ def test_cache_multiparam_in_1d(experiment, DAC, channel_array_instrument, n_poi
                                                 data)
 
 
+@pytest.mark.parametrize("bg_writing", [True, False])
+@pytest.mark.parametrize("storage_type", ['array', None])
+@settings(deadline=None, max_examples=10)
+@given(n_points=hst.integers(min_value=1, max_value=21))
+def test_cache_complexmultiparam_in_1d(experiment, DAC, channel_array_instrument, n_points, bg_writing, storage_type):
+    param = channel_array_instrument.A.dummy_complex_array_parameter
+    meas = Measurement()
+    meas.register_parameter(DAC.ch1, paramtype=storage_type)
+    meas.register_parameter(param, setpoints=(DAC.ch1,), paramtype=storage_type)
+    array_used = _array_param_used_in_tree(meas)
+
+    with meas.run(write_in_background=bg_writing) as datasaver:
+        dataset = datasaver.dataset
+        for i, v1 in enumerate(np.linspace(-1, 1, n_points)):
+            datasaver.add_result((DAC.ch1, v1),
+                                 (param, param.get()))
+            datasaver.flush_data_to_database(block=True)
+            data = dataset.cache.data()
+            n_rows_written = i+1
+
+            if array_used:
+                expected_shape = (n_rows_written,) + param.shape
+            else:
+                expected_shape = n_rows_written * np.prod(param.shape)
+            assert data[param.full_name][param.full_name].shape == expected_shape
+            if storage_type != 'array':
+                # TODO the expansion both for cache and get_parameter_data is buggy iff all types are array
+                assert data[param.full_name][DAC.ch1.full_name].shape == expected_shape
+            for setpoint_name in param.setpoint_full_names:
+                assert data[param.full_name][setpoint_name].shape == expected_shape
+            _assert_parameter_data_is_identical(dataset.get_parameter_data(),
+                                                data)
+
+
 def _assert_parameter_data_is_identical(expected: Dict[str, Dict[str, np.ndarray]],
                                         actual: Dict[str, Dict[str, np.ndarray]]):
     assert expected.keys() == actual.keys()
