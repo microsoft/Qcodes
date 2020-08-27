0d6decab2f88a8f15441075af4b4b6e0b57e83e8
Bas Nijholt <basnijholt@gmail.com>
2020-08-27 16:35:12 +0200

    update the tests to work with latest updates

--- qcodes/tests/test_interactive_widget.py
+++ qcodes/tests/test_interactive_widget.py
@@ -7,12 +7,12 @@ from ipywidgets import Textarea, Button
 # by itself so we also need to import all the fixtures this one is dependent
 # on
 # pylint: disable=unused-import
-from qcodes.tests.dataset.temporary_databases import (
+from qcodes.tests.dataset.conftest import (
     dataset,
     experiment,
     empty_temp_db,
+    standalone_parameters_dataset,
 )
-from qcodes.tests.dataset.dataset_fixtures import standalone_parameters_dataset
 from qcodes import interactive_widget
 
 
@@ -32,32 +32,42 @@ def test_full_widget():
     interactive_widget.experiments_widget()
 
 
-def test_expandable_dict(
+def test_button_to_text(
+    tab, standalone_parameters_dataset
+):  # pylint: disable=redefined-outer-name
+    box = interactive_widget.button_to_text("title", "body")
+    (button,) = box.children
+    button.click()
+    time.sleep(1)  # after click
+    text_area, back_button = box.children
+    assert "body" in text_area.value
+    back_button.click()
+    time.sleep(1)  # after click
+    assert len(box.children) == 1
+
+
+def test_snapshot_button(
     tab, standalone_parameters_dataset
 ):  # pylint: disable=redefined-outer-name
     ds = standalone_parameters_dataset
-    dct = {"a": {"b": "c", "d": {"e": "f"}}}
-    box = interactive_widget.expandable_dict(dct, tab, ds)
-    button, = box.children
-    button.click()
-    text_area, *buttons = box.children
-    snapshot_button = next(b for b in buttons if "snapshot" in b.description.lower())
-    snapshot_button.click()  # should make a snapshot in the tab
+    snapshot_button = interactive_widget._get_snapshot_button(ds, tab)
+    snapshot_button.click()
     time.sleep(1)  # after click
     # maybe use https://github.com/jupyter-widgets/ipywidgets/issues/2417
     assert "snapshot" in tab.get_title(1)
 
 
-def test_editable_metadata(standalone_parameters_dataset):
+def test_editable_metadata(standalone_parameters_dataset):  # pylint: disable=redefined-outer-name
     ds = standalone_parameters_dataset
     box = interactive_widget.editable_metadata(ds)
     button = box.children[0]
     button.click()
     assert len(box.children) == 2
-    text_area, save_button = box.children
+    text_area, save_box = box.children
+    save_button = save_box.children[0]
     assert isinstance(text_area, Textarea)
     assert isinstance(button, Button)
-    test_test = 'test value'
+    test_test = "test value"
     text_area.value = test_test
     save_button.click()
     time.sleep(1)  # after click
