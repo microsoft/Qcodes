feec9291cfa4b92d4da126902edb2c6e5d5c7dda
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-09-25 17:22:38 +0200

    update detect shape to work on more that one parameter at a time

--- qcodes/dataset/descriptions/detect_shapes.py
+++ qcodes/dataset/descriptions/detect_shapes.py
@@ -1,4 +1,4 @@
-from typing import Union, Sequence, Dict, Any, Tuple
+from typing import Union, Sequence, Dict, Any, Tuple, Optional, Sized
 from numbers import Integral
 from collections import abc
 
@@ -12,15 +12,15 @@ from qcodes.utils.validators import Arrays
 
 
 def get_shape_of_measurement(
-        meas_param: _BaseParameter,
-        *steps: Union[int, Sequence[Any], np.ndarray]
+        parameters: Sequence[_BaseParameter],
+        steps: Optional[Union[Sequence[int], Sequence[Sized]]] = None
 ) -> Dict[str, Tuple[int, ...]]:
     """
     Construct the shape of a measurement of a dependent parameter from the
     parameter and the axes it is to be sweept over.
 
     Args:
-        meas_param: The dependent parameter to construct the shape for
+        parameters: The dependent parameters to construct the shape for
         *steps: Zero or more dimensions that the parameter is to be sweept over.
            These can be given either as the length of the dimension or as
            a sequence of numbers to sweep over.
@@ -29,34 +29,38 @@ def get_shape_of_measurement(
         A dictionary from the parameter name to a tuple of integers describing
         the shape.
     """
-    shapes: Dict[str, Tuple[int, ...]]
+    shapes: Dict[str, Tuple[int, ...]] = {}
 
-    if isinstance(meas_param, MultiParameter):
-        shapes = _get_shapes_of_multi_parameter(param=meas_param)
-    elif _param_is_array_like(meas_param):
-        meas_shape = _get_shape_of_arrayparam(meas_param)
-        shapes = {meas_param.full_name: meas_shape}
-    else:
-        shapes = {meas_param.full_name: ()}
+    for param in parameters:
 
-    for step in steps:
-        for name in shapes.keys():
-            shapes[name] = shapes[name] + (_get_shape_of_step(step),)
+        if isinstance(param, MultiParameter):
+            shapes.update(_get_shapes_of_multi_parameter(param=param))
+        elif _param_is_array_like(param):
+            shapes[param.full_name] = _get_shape_of_arrayparam(param)
+        else:
+            shapes[param.full_name] = ()
+
+        for step in steps:
+            for name in shapes.keys():
+                shapes[name] = shapes[name] + (_get_shape_of_step(step),)
 
     return shapes
 
 
 def _get_shape_of_step(
-        step: Union[int, np.integer, Sequence[Any], np.ndarray]
+        step: Union[int, np.integer, Sized, np.ndarray]
 ) -> int:
     if isinstance(step, Integral):
         return int(step)
-    elif isinstance(step, abc.Sequence):
-        return len(step)
     elif isinstance(step, np.ndarray):
+        # we explicitly check for numpy arrays
+        # even if the numpy array is sized
+        # to verify that they are one dimensional
         if not len(step.shape) == 1:
-            raise TypeError("A step must b|e a one dimensional sweep")
+            raise TypeError("A step must be a one dimensional sweep")
         return int(step.shape[0])
+    elif isinstance(step, abc.Sized):
+        return len(step)
     else:
         raise TypeError(f"get_shape_of_step takes "
                         f"either an integer or a sequence "
