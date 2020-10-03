e82f2d33127edcc99500edb0e0bdc0c39465c052
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-10-03 12:58:56 +0200

    move list tests

--- qcodes/tests/test_validators.py
+++ qcodes/tests/test_validators.py
@@ -5,7 +5,7 @@ import numpy as np
 import pytest
 from hypothesis import given
 from qcodes.utils.validators import (Callable,
-                                     ComplexNumbers, Dict, Ints, Lists)
+                                     ComplexNumbers, Dict)
 
 
 class AClass:
@@ -16,32 +16,6 @@ class AClass:
 
 def a_func():
     pass
-
-
-class TestLists(TestCase):
-
-    def test_type(self):
-        l = Lists()
-        v1 = ['a', 'b', 5]
-        l.validate(v1)
-
-        v2 = 234
-        with pytest.raises(TypeError):
-            l.validate(v2)
-
-    def test_elt_vals(self):
-        l = Lists(Ints(max_value=10))
-        v1 = [0, 1, 5]
-        l.validate(v1)
-
-        v2 = [0, 1, 11]
-        with pytest.raises(ValueError):
-            l.validate(v2)
-
-    def test_valid_values(self):
-        val = Lists(Ints(max_value=10))
-        for vval in val.valid_values:
-            val.validate(vval)
 
 
 class TestCallable(TestCase):
