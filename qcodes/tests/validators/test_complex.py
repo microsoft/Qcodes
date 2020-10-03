1d2d4dd4dffd6c7f62f4894bd9c8215e7304054c
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-10-03 13:03:03 +0200

    move dict and complex to their own files

--- qcodes/tests/validators/test_complex.py
+++ qcodes/tests/validators/test_complex.py
@@ -4,33 +4,7 @@ import hypothesis.strategies as hst
 import numpy as np
 import pytest
 from hypothesis import given
-from qcodes.utils.validators import (ComplexNumbers, Dict)
-
-
-class AClass:
-
-    def method_a(self):
-        raise RuntimeError('function should not get called')
-
-
-def a_func():
-    pass
-
-
-class TestDict(TestCase):
-
-    def test_dict(self):
-        d = Dict()
-        test_dict = {}
-        d.validate(test_dict)
-        test_int = 5
-        with pytest.raises(TypeError):
-            d.validate(test_int)
-
-    def test_valid_values(self):
-        val = Dict()
-        for vval in val.valid_values:
-            val.validate(vval)
+from qcodes.utils.validators import ComplexNumbers
 
 
 @given(complex_val=hst.complex_numbers())
---
+++ qcodes/tests/validators/test_dict.py
@@ -0,0 +1,18 @@
+import pytest
+
+from qcodes.utils.validators import Dict
+
+
+def test_dict():
+    d = Dict()
+    test_dict = {}
+    d.validate(test_dict)
+    test_int = 5
+    with pytest.raises(TypeError):
+        d.validate(test_int)
+
+
+def test_valid_values():
+    val = Dict()
+    for vval in val.valid_values:
+        val.validate(vval)
