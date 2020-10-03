b3147ef2c398d1944052a2aad013725b4bbb4b90
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-10-03 14:06:18 +0200

    move attributes set to tests

--- qcodes/tests/test_helpers.py
+++ qcodes/tests/test_helpers.py
@@ -23,43 +23,6 @@ class NoDelDict(dict):
         raise KeyError('get your hands off me!')
 
 
-class TestAttributeSetToContextManager(TestCase):
-    """
-    Test attribute_set_to context manager
-    """
-
-    class X:
-        y = 0
-
-    def test_attribute_set_to_value(self):
-        """Test setting attribute to a value"""
-        x = self.X()
-        x.y = 1
-
-        assert 1 == x.y
-
-        with attribute_set_to(x, 'y', 2):
-            assert 2 == x.y
-
-        assert 1 == x.y
-
-    def test_attribute_set_to_object(self):
-        """Test setting attribute to an object"""
-        x = self.X()
-        original_object = self.X()
-        x.y = original_object
-
-        assert original_object == x.y
-        assert original_object is x.y
-
-        new_object = self.X()
-        with attribute_set_to(x, 'y', new_object):
-            assert new_object == x.y
-            assert new_object is x.y
-
-        assert original_object == x.y
-        assert original_object is x.y
-
 
 class TestPartialWithDocstring(TestCase):
     """Test the sane partial function"""
