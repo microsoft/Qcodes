6f5297bbde35e8bc882736e1d4bc6fe3dd5122e4
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-10-03 13:39:41 +0200

    move waitsec tests

--- qcodes/tests/test_helpers.py
+++ qcodes/tests/test_helpers.py
@@ -1,46 +1,15 @@
 import json
-import time
 from collections import OrderedDict
-from datetime import datetime
 from unittest import TestCase
 
 import pytest
 import numpy as np
 
-from qcodes.utils.helpers import (wait_secs,
-                                  strip_attrs, full_class,
+from qcodes.utils.helpers import (strip_attrs, full_class,
                                   named_repr, is_sequence_of,
                                   compare_dictionaries, NumpyJSONEncoder,
                                   partial_with_docstring)
-from qcodes.logger.logger import LogCapture
 from qcodes.utils.helpers import attribute_set_to
-
-
-class TestWaitSecs(TestCase):
-    def test_bad_calls(self):
-        bad_args = [None, datetime.now()]
-        for arg in bad_args:
-            with pytest.raises(TypeError):
-                wait_secs(arg)
-
-    def test_good_calls(self):
-        for secs in [0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1]:
-            finish_clock = time.perf_counter() + secs
-            secs_out = wait_secs(finish_clock)
-            assert secs_out > secs - 1e-4
-            # add a tiny offset as this test may fail if
-            # otherwise if the two calls to perf_counter are close
-            # enough to return the same result as a + b - a cannot
-            # in general be assumed to be <= b in floating point
-            # math (here a is perf_counter() and b is the wait time
-            assert secs_out <= secs+1e-14
-
-    def test_warning(self):
-        with LogCapture() as logs:
-            secs_out = wait_secs(time.perf_counter() - 1)
-        assert secs_out == 0
-
-        assert logs.value.count('negative delay') == 1, logs.value
 
 
 class A:
