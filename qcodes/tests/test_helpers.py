c1d3b7dbb32332a141e9f3b50c1512ffbbce3d62
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-10-03 13:42:18 +0200

    move strip atters tests

--- qcodes/tests/test_helpers.py
+++ qcodes/tests/test_helpers.py
@@ -27,46 +27,6 @@ class NoDelDict(dict):
         raise KeyError('get your hands off me!')
 
 
-class TestStripAttrs(TestCase):
-    def test_normal(self):
-        a = A()
-        a.x = 15
-        a.z = 25
-
-        strip_attrs(a)
-
-        assert a.x == 5
-        assert not hasattr(a, 'z')
-        assert a.y == 6
-
-    def test_pathological(self):
-        # just make sure this never errors, since it's meant to be used
-        # during deletion
-        a = A()
-        a.__dict__ = BadKeysDict()
-
-        a.fruit = 'mango'
-        with pytest.raises(RuntimeError):
-            a.__dict__.keys()
-
-        strip_attrs(a)
-        # no error, but the attribute is still there
-        assert a.fruit == 'mango'
-
-        a = A()
-        a.__dict__ = NoDelDict()
-        s = 'can\'t touch this!'
-        a.x = s
-
-        assert a.x == s
-        # not sure how this doesn't raise, but it doesn't.
-        # with self.assertRaises(KeyError):
-        #     del a.x
-
-        strip_attrs(a)
-        assert a.x == s
-
-
 class TestClassStrings(TestCase):
     # use a standard library object so we don't need to worry about where
     # this test is run. A little annoying to find one we can mutate though!
