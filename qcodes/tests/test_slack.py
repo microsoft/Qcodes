57ec3681cd0ab904680c212e7b80887c41c3bbe0
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-10-04 11:44:33 +0200

    break some lines

--- qcodes/tests/test_slack.py
+++ qcodes/tests/test_slack.py
@@ -1,10 +1,16 @@
 def test_slack(mocker):
 
-    slack_config = {'bot_name': 'bot', 'token': '123', 'names': ['dummyuser']}
+    slack_config = {
+        'bot_name': 'bot',
+        'token': '123',
+        'names': ['dummyuser']
+    }
 
     mock_slacker_module = mocker.MagicMock(name='slacker_module')
     slacker_class_mock = mocker.MagicMock(name='SlackerMock')
-    slacker_class_mock.users.get_user_id = mocker.MagicMock(return_value='bot_id')
+    slacker_class_mock.users.get_user_id = mocker.MagicMock(
+        return_value='bot_id'
+    )
     response = mocker.MagicMock()
     response.body = {'members': [{'name': 'dummyuser', 'id': '123'}]}
     slacker_class_mock.users.list = mocker.MagicMock(
@@ -12,7 +18,9 @@ def test_slack(mocker):
         return_value=response
     )
     mock_slacker_module.Slacker = slacker_class_mock
-    mock_slacker_module.Slacker = mocker.MagicMock(return_value=slacker_class_mock)
+    mock_slacker_module.Slacker = mocker.MagicMock(
+        return_value=slacker_class_mock
+    )
 
     mocker.patch.dict('sys.modules', slacker=mock_slacker_module)
     from qcodes.utils.slack import Slack
