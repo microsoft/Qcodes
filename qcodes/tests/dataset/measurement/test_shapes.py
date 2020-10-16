774fd9bb8eb13e83920088abdcf2374a04754a5a
Jens Hedegaard Nielsen <jenielse@microsoft.com>
2020-10-16 16:52:24 +0200

    add 2d test

--- qcodes/tests/dataset/measurement/test_shapes.py
+++ qcodes/tests/dataset/measurement/test_shapes.py
@@ -36,6 +36,7 @@ def test_datasaver_1d(experiment, DAC, DMM, caplog,
     if n_points == n_points_expected:
         assert len(caplog.record_tuples) == 0
     else:
+        assert len(caplog.record_tuples) == 2
         exp_module = "qcodes.dataset.sqlite.queries"
         exp_level = logging.WARNING
         exp_msg = ("Tried to set data shape for {} in "
@@ -54,3 +55,75 @@ def test_datasaver_1d(experiment, DAC, DMM, caplog,
                                                           DMM.v1.full_name,
                                                           n_points,
                                                           n_points_expected))
+
+
+@given(n_points_1=hst.integers(min_value=1, max_value=50),
+       n_points_2=hst.integers(min_value=1, max_value=50))
+@example(n_points_1=5, n_points_2=10)
+def test_datasaver_2d(experiment, DAC, DMM, caplog,
+                      n_points_1, n_points_2):
+    meas = Measurement()
+    meas.register_parameter(DAC.ch1)
+    meas.register_parameter(DAC.ch2)
+    meas.register_parameter(DMM.v1, setpoints=(DAC.ch1,
+                                               DAC.ch2))
+
+    n_points_expected_1 = 5
+    n_points_expected_2 = 10
+
+    meas.set_shapes({DMM.v1.full_name: (n_points_expected_1,
+                                        n_points_expected_2,)})
+
+    with meas.run() as datasaver:
+
+        for set_v_1 in np.linspace(0, 1, n_points_1):
+            for set_v_2 in np.linspace(0, 1, n_points_2):
+                datasaver.add_result((DAC.ch1, set_v_1),
+                                     (DAC.ch2, set_v_2),
+                                     (DMM.v1, DMM.v1()))
+
+    ds = datasaver.dataset
+    caplog.clear()
+    data = ds.get_parameter_data()
+
+    if n_points_1*n_points_2 == n_points_expected_1*n_points_expected_2:
+        assert len(caplog.record_tuples) == 0
+        for dataarray in data[DMM.v1.full_name].values():
+            assert dataarray.shape == (n_points_expected_1, n_points_expected_2)
+    else:
+        assert len(caplog.record_tuples) == 3
+        exp_module = "qcodes.dataset.sqlite.queries"
+        exp_level = logging.WARNING
+        exp_msg = ("Tried to set data shape for {} in "
+                   "dataset {} "
+                   "from metadata when loading "
+                   "but found inconsistent lengths {} and {}")
+        assert caplog.record_tuples[0] == (
+            exp_module,
+            exp_level,
+            exp_msg.format(
+                DMM.v1.full_name,
+                DMM.v1.full_name,
+                n_points_1*n_points_2,
+                n_points_expected_1*n_points_expected_2
+            )
+        )
+        assert caplog.record_tuples[1] == (
+            exp_module,
+            exp_level,
+            exp_msg.format(
+                DAC.ch1.full_name,
+                DMM.v1.full_name,
+                n_points_1*n_points_2,
+                n_points_expected_1*n_points_expected_2)
+        )
+        assert caplog.record_tuples[2] == (
+            exp_module,
+            exp_level,
+            exp_msg.format(
+                DAC.ch2.full_name,
+                DMM.v1.full_name,
+                n_points_1*n_points_2,
+                n_points_expected_1*n_points_expected_2
+            )
+        )
