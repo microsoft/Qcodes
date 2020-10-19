7d692ceb77f3ae5f28b7f46d7b11adfe36251664
Trevor Morgan <63689909+trevormorgan@users.noreply.github.com>
2020-10-18 20:48:13 -0700

    imnport pyvisa as visa change

--- qcodes/tests/test_visa.py
+++ qcodes/tests/test_visa.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from unittest.mock import patch
-import visa
+import pyvisa as visa
 from qcodes.instrument.visa import VisaInstrument
 from qcodes.utils.validators import Numbers
 import warnings
