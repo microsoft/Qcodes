4a971c069e20e2fd1b2a5419861eddcb548d3369
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2020-11-23 14:18:45 +0100

    explicit generics in qdev and qdevil drivers

--- qcodes/instrument_drivers/QDevil/QDevil_QDAC.py
+++ qcodes/instrument_drivers/QDevil/QDevil_QDAC.py
@@ -140,7 +140,7 @@ class QDacChannel(InstrumentChannel):
     def snapshot_base(
             self,
             update: Optional[bool] = False,
-            params_to_skip_update: Optional[Sequence[str]] = None) -> Dict:
+            params_to_skip_update: Optional[Sequence[str]] = None) -> Dict[Any, Any]:
         update_currents = self._parent._update_currents and update
         if update and not self._parent._get_status_performed:
             self._parent._update_cache(update_currents=update_currents)
@@ -423,7 +423,7 @@ class QDac(VisaInstrument):
 
     def snapshot_base(self,
                       update: Optional[bool] = False,
-                      params_to_skip_update: Optional[Sequence[str]] = None) -> Dict:
+                      params_to_skip_update: Optional[Sequence[str]] = None) -> Dict[Any, Any]:
         update_currents = self._update_currents and update is True
         if update:
             self._update_cache(update_currents=update_currents)
