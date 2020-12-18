d5744f527f2e93436cdb6745da35ab153dbab25a
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2020-12-18 10:52:31 +0100

    correct logic

--- qcodes/utils/helpers.py
+++ qcodes/utils/helpers.py
@@ -226,13 +226,12 @@ def named_repr(obj: Any) -> str:
 
 K = TypeVar('K', bound=Hashable)
 L = TypeVar('L', bound=Hashable)
-M = TypeVar('M', bound=Hashable)
 
 
 def deep_update(
         dest: MutableMapping[K, Any],
         update: Mapping[L, Any]
-) -> MutableMapping[M, Any]:
+) -> MutableMapping[Union[K, L], Any]:
     """
     Recursively update one JSON structure with another.
 
@@ -240,13 +239,14 @@ def deep_update(
     If the original value is a dictionary and the new value is not, or vice versa,
     we also replace the value completely.
     """
+    dest_int = cast(MutableMapping[Union[K, L], Any], dest)
     for k, v_update in update.items():
-        v_dest = dest.get(k)
+        v_dest = dest_int.get(k)
         if isinstance(v_update, abc.Mapping) and isinstance(v_dest, abc.MutableMapping):
             deep_update(v_dest, v_update)
         else:
-            dest[k] = deepcopy(v_update)
-    return dest
+            dest_int[k] = deepcopy(v_update)
+    return dest_int
 
 
 # could use numpy.arange here, but
