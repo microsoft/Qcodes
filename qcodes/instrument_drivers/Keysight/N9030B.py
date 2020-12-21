3a663f9162bdc983d7da44ad01ed9519263c4026
Neil Dick <neildick@microsoft.com>
2020-12-22 10:35:49 +1100

    feat: N9030B Add missing type annotations
    Add the missing type annotations for the main class.

--- qcodes/instrument_drivers/Keysight/N9030B.py
+++ qcodes/instrument_drivers/Keysight/N9030B.py
@@ -6,7 +6,7 @@ class N9030B(VisaInstrument):
     Driver for Keysight N9030B PXA signal analyzer.
     """
 
-    def __init__(self, name, address, **kwargs):
+    def __init__(self, name: str, address: str, **kwargs: Any) -> None:
         super().__init__(name, address, terminator='\n', **kwargs)
 
         self.connect_message()
