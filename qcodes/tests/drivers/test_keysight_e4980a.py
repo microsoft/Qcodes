fb813739d546f546a8b51d7ff0040dfc159305f5
Akshita07 <aksingh0728@gmail.com>
2021-01-11 14:53:22 +0100

    Rename according to convention

--- qcodes/tests/drivers/test_keysight_e4980a.py
+++ qcodes/tests/drivers/test_keysight_e4980a.py
@@ -3,16 +3,16 @@ import qcodes.instrument_drivers.Keysight.keysight_e4980a as E4980A
 
 import qcodes.instrument.sims as sims
 
-visalib = sims.__file__.replace('__init__.py', 'Keysight_E4980A.yaml@sim')
+VISALIB = sims.__file__.replace('__init__.py', 'Keysight_E4980A.yaml@sim')
 
 
 @pytest.fixture
 def driver():
-    driver = E4980A.KeysightE4980A('E4980A',
-                                   address="GPIB::1::INSTR",
-                                   visalib=visalib)
-    yield driver
-    driver.close()
+    instr = E4980A.KeysightE4980A('E4980A',
+                                  address="GPIB::1::INSTR",
+                                  visalib=VISALIB)
+    yield instr
+    instr.close()
 
 
 def test_idn(driver):
