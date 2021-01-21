c4d146f1e32ea6b8352616f2289be7b7ff47262d
Akshita07 <aksingh0728@gmail.com>
2021-01-21 11:12:35 +0100

    raise error from e

--- qcodes/instrument_drivers/Keysight/N9030B.py
+++ qcodes/instrument_drivers/Keysight/N9030B.py
@@ -260,8 +260,9 @@ class SpectrumAnalyzerMode(InstrumentChannel):
                                     f"{self.root_instrument.measurement()}"
                                     f"{trace_num}?")
                 data = np.array(data_str.rstrip().split(",")).astype("float64")
-        except TimeoutError:
-            raise TimeoutError("Couldn't receive any data. Command timed out.")
+        except TimeoutError as e:
+            raise TimeoutError("Couldn't receive any data. Command timed "
+                               "out.") from e
 
         trace_data = data[1::2]
         return trace_data
@@ -445,8 +446,9 @@ class PhaseNoiseMode(InstrumentChannel):
             data_str = self.ask(f":READ:{self.root_instrument.measurement()}"
                                 f"{trace_num}?")
             data = np.array(data_str.rstrip().split(",")).astype("float64")
-        except TimeoutError:
-            raise TimeoutError("Couldn't receive any data. Command timed out.")
+        except TimeoutError as e:
+            raise TimeoutError("Couldn't receive any data. Command timed "
+                               "out.") from e
 
         trace_data = data[1::2]
         return trace_data
