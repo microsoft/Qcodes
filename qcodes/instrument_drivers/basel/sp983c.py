6f58c0c8477068aff4ce86ff7986aebf6e6d0a6f
Akshita07 <aksingh0728@gmail.com>
2021-01-27 10:21:29 +0100

    Update doc string

--- qcodes/instrument_drivers/basel/sp983c.py
+++ qcodes/instrument_drivers/basel/sp983c.py
@@ -13,12 +13,20 @@ class SP983C(Instrument):
     This driver supports both the SP 983 and SP 983C models. These differ only
     in their handling of input offset voltage. It is the responsibility of the
     user to capture the input offset, (from the voltage supply) and compensate
-    that as needed depending on the model.
+    that as needed for SP 983. For SP 983C model, 'input_offset_voltage'
+    argument can be used to set up offset.
 
     Note that, as this is a purely virtual driver, there is no support
     for the the remote control interface (SP 983a). It is the responsibility of
     the user to ensure that values set here are in accordance with the values
     set on the instrument.
+
+    Args:
+        name
+        input_offset_voltage: (Optional) A source input offset voltage
+            parameter. The range for input is -10 to 10 Volts and it is
+            user's responsibility to ensure this. This source parameter is
+            used to set offset voltage parameter of the preamp.
     """
 
     def __init__(self, name: str,
