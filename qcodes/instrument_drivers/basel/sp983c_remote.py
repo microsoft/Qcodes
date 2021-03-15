1786145943da405820a3a78a37ae81e67f648fd7
Akshita07 <aksingh0728@gmail.com>
2021-03-15 10:13:58 +0100

    Make get, set methods private, update fcut allowed values and add a get parser for it

--- qcodes/instrument_drivers/basel/sp983c_remote.py
+++ qcodes/instrument_drivers/basel/sp983c_remote.py
@@ -13,17 +13,19 @@ class SP983A(VisaInstrument):
             "gain",
             label="Gain",
             unit="V/A",
-            set_cmd=self.set_gain,
-            get_cmd=self.get_gain,
+            set_cmd=self._set_gain,
+            get_cmd=self._get_gain,
             vals=vals.Enum(1e5, 1e6, 1e7, 1e8, 1e9),
         )
         self.add_parameter(
             "fcut",
             unit="Hz",
             label="Filter Cut-Off Frequency",
-            set_cmd=self.set_filter,
-            get_cmd=self.get_filter,
-            vals=vals.Enum(30, 100, 300, 1000, 3000, 30_000, 100_000, "FULL"),
+            get_cmd=self._get_filter,
+            get_parser=self._parse_filter_value,
+            set_cmd=self._set_filter,
+            vals={30: '30', 100: '100', 300: '300', 1000: '1000', 3000: '3000',
+                  10e3: '10k', 30e3: '30k', 100e3: '100k', 'Full': 'FULL'},
         )
         self.add_parameter(
             "overload_status",
@@ -32,29 +34,32 @@ class SP983A(VisaInstrument):
             get_cmd="GET O"
         )
 
-    def set_gain(self, value: float) -> None:
+    def _set_gain(self, value: float) -> None:
         r = self.ask(f"SET G 1E{int(np.log10(value))}")
         if r != "OK":
             raise ValueError(f"Expected OK return but got: {r}")
 
-    def get_gain(self) -> float:
+    def _get_gain(self) -> float:
         s = self.ask("GET G")
         r = s.split("Gain: ")[1]
         return float(r)
 
-    def set_filter(self, value: Union[str, float]) -> None:
+    def _set_filter(self, value: str) -> None:
         r = self.ask(f"SET F {value}")
         if r != "OK":
             raise ValueError(f"Expected OK return but got: {r}")
 
-    def get_filter(self) -> Union[str, float]:
+    def _get_filter(self) -> str:
         s = self.ask("GET F")
-        r = s.split("Filter: ")[1]
-        if r == "Full":
-            return r.upper()
-        elif r[-3::] == "kHz":
-            return float(r[0:-3]) * 1e3
-        elif r[-2::] == "Hz":
-            return float(r[0:-2])
+        return s.split("Filter: ")[1]
+
+    @staticmethod
+    def _parse_filter_value(val: str) -> Union[str, float]:
+        if val.startswith("F"):
+            return "Full"
+        elif val[-3::] == "kHz":
+            return float(val[0:-3]) * 1e3
+        elif val[-2::] == "Hz":
+            return float(val[0:-2])
         else:
-            raise ValueError(f"Could not interpret result. Got: {s}")
+            raise ValueError(f"Could not interpret result. Got: {val}")
