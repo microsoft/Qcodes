a17d56fd1d9b1fcb86ebfacd268b2e5722334873
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-04-08 00:56:14 -0700

    add device meta to __init__

--- qcodes/instrument_drivers/meta/__init__.py
+++ qcodes/instrument_drivers/meta/__init__.py
@@ -2,3 +2,9 @@ from qcodes.instrument_drivers.meta.instrument_meta import (
     MetaParameter,
     InstrumentMeta
 )
+
+from qcodes.instrument_drivers.meta.chip_meta import (
+    ChipMeta,
+    DeviceMeta,
+    DeviceMetaParameter
+)
