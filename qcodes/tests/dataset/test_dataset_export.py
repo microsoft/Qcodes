4d6c1079c07781197ed9f4fc2b5c3879f1fbd2bb
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2021-04-19 13:58:52 +0200

    check that metadata round trip

--- qcodes/tests/dataset/test_dataset_export.py
+++ qcodes/tests/dataset/test_dataset_export.py
@@ -4,6 +4,7 @@ import os
 import pytest
 import xarray as xr
 
+import qcodes
 from qcodes import new_data_set
 from qcodes.dataset.descriptions.dependencies import InterDependencies_
 from qcodes.dataset.descriptions.param_spec import ParamSpecBase
@@ -232,7 +233,6 @@ def test_export_to_xarray_ds_dict_extra_metadata(mock_dataset):
 
 def test_export_to_xarray_extra_metadate_can_be_stored(mock_dataset, tmp_path):
 
-    import json
     nt_metadata = {
         "foo": {
             "bar": {
@@ -243,6 +243,20 @@ def test_export_to_xarray_extra_metadate_can_be_stored(mock_dataset, tmp_path):
     }
     mock_dataset.add_metadata("foo_metadata", json.dumps(nt_metadata))
     mock_dataset.export(export_type="netcdf", path=str(tmp_path))
+    data_as_xarray = mock_dataset.to_xarray_dataset()
+
+    loaded_data = xr.load_dataset(
+        tmp_path/f"{qcodes.config.dataset.export_prefix}{mock_dataset.run_id}.nc"
+    )
+
+    # check that the metadata in the qcodes dataset is roundtripped to the loaded
+    # dataset
+    for key in mock_dataset.metadata.keys():
+        assert mock_dataset.metadata[key] == loaded_data.attrs[key]
+    # check that the added metadata roundtrip correctly
+    assert loaded_data.attrs["foo_metadata"] == json.dumps(nt_metadata)
+    # check that all attrs roundtrip correctly within the xarray ds
+    assert loaded_data.attrs == data_as_xarray.attrs
 
 
 def _assert_xarray_metadata_is_as_expected(xarray_ds, qc_dataset):
