f5d58cf529dc9fe11a321936efa3003179d5417b
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2021-05-12 18:45:22 +0200

    some notes

--- qcodes/tests/dataset/measurement/test_dataset_in_memory.py
+++ qcodes/tests/dataset/measurement/test_dataset_in_memory.py
@@ -1,11 +1,25 @@
+import os
+
 import numpy as np
 
 from qcodes.dataset.data_set_in_memory import DataSetInMem
 
 
-def test_dataset_in_memory_smoke_test(meas_with_registered_param, DMM, DAC):
+def test_dataset_in_memory_smoke_test(meas_with_registered_param, DMM, DAC, tmp_path):
     with meas_with_registered_param.run(dataset_class=DataSetInMem) as datasaver:
         for set_v in np.linspace(0, 25, 10):
             DAC.ch1.set(set_v)
             get_v = DMM.v1()
             datasaver.add_result((DAC.ch1, set_v), (DMM.v1, get_v))
+
+    dataset = datasaver.dataset
+    os.chdir(tmp_path)
+    dataset.export(export_type="netcdf", path=".")
+    loaded_ds = DataSetInMem.load_from_netcdf("qcodes_1.nc")
+    1 + 1
+
+
+# todo missing from runs table
+# snapshot, completed timestamp, parameters (do we care), verify other metadata
+# When should metadata be added. In the old dataset it used to be added as
+# soon as you call add_metadata
