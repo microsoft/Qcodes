ce53ca2f035eacb68d9019c21a23d008745fddf0
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2021-06-14 14:07:38 +0200

    try removing test to check for sideeffect

--- qcodes/tests/dataset/measurement/test_dataset_in_memory.py
+++ qcodes/tests/dataset/measurement/test_dataset_in_memory.py
@@ -5,18 +5,18 @@ import numpy as np
 from qcodes.dataset.data_set_in_memory import DataSetInMem
 
 
-def test_dataset_in_memory_smoke_test(meas_with_registered_param, DMM, DAC, tmp_path):
-    with meas_with_registered_param.run(dataset_class=DataSetInMem) as datasaver:
-        for set_v in np.linspace(0, 25, 10):
-            DAC.ch1.set(set_v)
-            get_v = DMM.v1()
-            datasaver.add_result((DAC.ch1, set_v), (DMM.v1, get_v))
-
-    dataset = datasaver.dataset
-    os.chdir(tmp_path)
-    dataset.export(export_type="netcdf", path=".")
-    loaded_ds = DataSetInMem.load_from_netcdf("qcodes_1.nc")
-    assert dataset.the_same_dataset_as(loaded_ds)
+# def test_dataset_in_memory_smoke_test(meas_with_registered_param, DMM, DAC, tmp_path):
+#     with meas_with_registered_param.run(dataset_class=DataSetInMem) as datasaver:
+#         for set_v in np.linspace(0, 25, 10):
+#             DAC.ch1.set(set_v)
+#             get_v = DMM.v1()
+#             datasaver.add_result((DAC.ch1, set_v), (DMM.v1, get_v))
+#
+#     dataset = datasaver.dataset
+#     os.chdir(tmp_path)
+#     dataset.export(export_type="netcdf", path=".")
+#     loaded_ds = DataSetInMem.load_from_netcdf("qcodes_1.nc")
+#     assert dataset.the_same_dataset_as(loaded_ds)
 
 
 # todo missing from runs table
