7025dd31c199c31385564879417954120b7f64ac
Sohail Chatoor <sochatoo@microsoft.com>
2021-06-16 12:51:13 -0700

    instead of monkey patching create an explicit class

--- qcodes/instrument/abstract.py
+++ qcodes/instrument/abstract.py
@@ -20,7 +20,7 @@ class AbstractParameterException(Exception):
     """
 
 
-def abstract_instrument(cls: Type[InstrumentBase]) -> Type[InstrumentBase]:
+def abstract_instrument(klass: Type[InstrumentBase]) -> Type[InstrumentBase]:
     """
     A class decorator to create an abstract instrument. Abstract
     instruments are allowed to have abstract parameters, but
@@ -29,74 +29,68 @@ def abstract_instrument(cls: Type[InstrumentBase]) -> Type[InstrumentBase]:
     dynamically.
 
     Args:
-        cls: The class to be decorated
+        klass: The class to be decorated
 
     Returns:
         The decorated class
     """
 
-    def __init_subclass__(sub_cls: Type[InstrumentBase]) -> None:
+    class Abstract(klass):
+        def __init_subclass__(cls: Type[InstrumentBase]) -> None:
 
-        __init__ = sub_cls.__init__
+            __init__ = cls.__init__
 
-        def __init_new__(self, *args: Any, **kwargs: Any) -> None:
-            """
-            Subclasses of an abstract instrument should check
-            after initialization whether there still are
-            abstract parameters. If there are, we should raise
-            an exception.
-            """
-            __init__(self, *args, **kwargs)
-            # after the usual initialization...
+            def __init_new__(self: InstrumentBase, *args: Any, **kwargs: Any) -> None:
+                """
+                Subclasses of an abstract instrument should check
+                after initialization whether there still are
+                abstract parameters. If there are, we should raise
+                an exception.
+                """
+                __init__(self, *args, **kwargs)
+                # after the usual initialization...
 
-            abstract_parameters = [
-                parameter.name for parameter in self.parameters.values()
-                if isinstance(parameter, AbstractParameter)
-            ]
+                abstract_parameters = [
+                    parameter.name for parameter in self.parameters.values()
+                    if isinstance(parameter, AbstractParameter)
+                ]
 
-            if any(abstract_parameters):
-                cls_name = sub_cls.__name__
+                if any(abstract_parameters):
+                    cls_name = cls.__name__
 
-                raise AbstractParameterException(
-                    f"Class '{cls_name}' has un-implemented Abstract Parameter(s): " +
-                    ", ".join([f"'{name}'" for name in abstract_parameters])
-                )
+                    raise AbstractParameterException(
+                        f"Class '{cls_name}' has un-implemented Abstract Parameter(s): " +
+                        ", ".join([f"'{name}'" for name in abstract_parameters])
+                    )
 
-        # See https://github.com/python/mypy/issues/2427
-        sub_cls.__init__ = __init_new__  # type: ignore
+            # See https://github.com/python/mypy/issues/2427
+            cls.__init__ = __init_new__  # type: ignore
 
-    original_add_parameter = cls.add_parameter
+        def add_parameter(
+                self, name: str, parameter_class: Type[_BaseParameter] = Parameter,
+                **kwargs: Any
+        ) -> None:
 
-    @wraps(original_add_parameter)
-    def add_parameter(
-            self, name: str, parameter_class: Type[_BaseParameter] = Parameter,
-            **kwargs: Any
-    ) -> None:
+            existing_parameter = self.parameters.get(name, None)
 
-        existing_parameter = self.parameters.get(name, None)
+            if isinstance(existing_parameter, AbstractParameter):
+                # For abstract parameters, we define special behavior.
+                existing_unit = getattr(existing_parameter, "unit", None)
+                new_unit = kwargs.get("unit", None)
 
-        if isinstance(existing_parameter, AbstractParameter):
-            # For abstract parameters, we define special behavior.
-            existing_unit = getattr(existing_parameter, "unit", None)
-            new_unit = kwargs.get("unit", None)
+                if existing_unit and existing_unit != new_unit:
+                    raise AbstractParameterException(
+                        f"The unit of the parameter '{name}' is '{new_unit}', "
+                        f"which is inconsistent with the unit '{existing_unit}' "
+                        f"specified in the baseclass {klass.__name__!r} "
+                    )
 
-            if existing_unit and existing_unit != new_unit:
-                raise AbstractParameterException(
-                    f"The unit of the parameter '{name}' is '{new_unit}', "
-                    f"which is inconsistent with the unit '{existing_unit}' "
-                    f"specified in the baseclass {cls.__name__!r} "
-                )
+                # Remove the original abstract parameter to make room for the implementation
+                # in the subclass.
+                del self.parameters[name]
 
-            # Remove the original abstract parameter to make room for the implementation
-            # in the subclass.
-            del self.parameters[name]
+            super().add_parameter(
+                name, parameter_class=parameter_class, **kwargs
+            )
 
-        original_add_parameter(
-            self, name, parameter_class, **kwargs
-        )
-
-    # See https://github.com/python/mypy/issues/2427
-    cls.__init_subclass__ = classmethod(__init_subclass__)  # type: ignore
-    cls.add_parameter = add_parameter  # type: ignore
-
-    return cls
+    return Abstract
