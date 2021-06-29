74711f90394f82f2772fc181500feb1862e7e100
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2021-06-29 11:57:29 +0200

    use correct dataset class in test

--- qcodes/tests/dataset/measurement/test_dataset_in_memory.py
+++ qcodes/tests/dataset/measurement/test_dataset_in_memory.py
@@ -22,7 +22,7 @@ def test_dataset_in_memory_smoke_test(meas_with_registered_param, DMM, DAC, tmp_
 def test_dataset_in_memory_does_not_create_runs_table(
     meas_with_registered_param, DMM, DAC, tmp_path
 ):
-    with meas_with_registered_param.run() as datasaver:
+    with meas_with_registered_param.run(dataset_class=DataSetInMem) as datasaver:
         for set_v in np.linspace(0, 25, 10):
             DAC.ch1.set(set_v)
             get_v = DMM.v1()
