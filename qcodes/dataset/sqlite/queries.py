e3967cc9284d15fc5e11e55f2197024fed5ea654
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2021-07-07 11:10:16 +0200

    extract to function

--- qcodes/dataset/sqlite/queries.py
+++ qcodes/dataset/sqlite/queries.py
@@ -313,26 +313,42 @@ def get_parameter_data_for_one_paramtree(
             dtype: Optional[type] = np.float64
         else:
             dtype = None
-        try:
-            with warnings.catch_warnings():
-                warnings.filterwarnings(
-                    "ignore",
-                    category=VisibleDeprecationWarning,
-                    message="Creating an ndarray from ragged nested sequences"
-                )
-                # numpy warns here and coming versions
-                # will eventually raise
-                # for ragged arrays if you don't explicitly set
-                # dtype=object
-                # It is time consuming to detect ragged arrays here
-                # and it is expected to be a relatively rare situation
-                # so fallback to object if the regular dtype fail
-                param_data[paramspec.name] = np.array(column_data, dtype=dtype)
-        except:
-            # Not clear which error to catch here. This will only be clarified
-            # once numpy actually starts to raise here.
-            param_data[paramspec.name] = np.array(column_data, dtype=object)
+        param_data[paramspec.name] = list_of_data_to_maybe_ragged_nd_array(
+            column_data, dtype
+        )
     return param_data, n_rows
+
+
+def list_of_data_to_maybe_ragged_nd_array(
+    column_data: Sequence[Any],
+    dtype,
+) -> np.ndarray:
+    """
+    Convert a (nested) Sequence of data to numpy arrays. Handle that
+    the elements of the sequence may not have the same length in which
+    case the returned array will be of dtype object.
+
+    """
+    try:
+        with warnings.catch_warnings():
+            warnings.filterwarnings(
+                "ignore",
+                category=VisibleDeprecationWarning,
+                message="Creating an ndarray from ragged nested sequences",
+            )
+            # numpy warns here and coming versions
+            # will eventually raise
+            # for ragged arrays if you don't explicitly set
+            # dtype=object
+            # It is time consuming to detect ragged arrays here
+            # and it is expected to be a relatively rare situation
+            # so fallback to object if the regular dtype fail
+            data = np.array(column_data, dtype=dtype)
+    except:
+        # Not clear which error to catch here. This will only be clarified
+        # once numpy actually starts to raise here.
+        data = np.array(column_data, dtype=object)
+    return data
 
 
 def _expand_data_to_arrays(data: List[List[Any]], paramspecs: Sequence[ParamSpecBase]) -> None:
