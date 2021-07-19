c9e60152a5a315d19ab0c044f24ac2be5258e2e7
FarBo <farzadb58@gmail.com>
2021-07-19 15:40:10 +0200

    created the module to host active experiment setting

---
+++ qcodes/dataset/experiment_setting.py
@@ -0,0 +1,38 @@
+"""Module for setttings indirectly related to experiments."""
+
+from typing import Optional
+
+# The active exp_id in the kernel. The default is None, means the last exp_id
+# should be active, if available.
+active_experiment: Optional[int] = None
+
+
+def _set_active_experiment_id(exp_id: int) -> None:
+    """
+    Sets the active_experiment to the exp_id of a created/ loaded experiment.
+
+    Args:
+        exp_id: The exp_id of an experiment that is created/ loaded.
+    """
+    global active_experiment
+    active_experiment = exp_id
+
+
+def get_active_experiment_id() -> Optional[int]:
+    """
+    Gets the active exp_id.
+
+    Returns:
+        Active exp_id. If the return is None, then the active exp_id will be
+        the last exp_id in experiments list, if available.
+    """
+    global active_experiment
+    return active_experiment
+
+
+def reset_active_experiment_id() -> None:
+    """
+    Resets the active_experiment to the default.
+    """
+    global active_experiment
+    active_experiment = None
