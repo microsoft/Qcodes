c28451617d80d955cbc7c29763b372307118ccdb
FarBo <farzadb58@gmail.com>
2021-07-21 09:37:36 +0200

    add a function to handle active experiment return+docstring improvements

--- qcodes/dataset/experiment_settings.py
+++ qcodes/dataset/experiment_settings.py
@@ -2,17 +2,18 @@
 
 from typing import Optional
 
-# The active exp_id in the kernel. The default is None, means the last exp_id
-# should be active, if available.
+# The active experiment's exp_id. The default is None.
 active_experiment: Optional[int] = None
 
 
-def _set_active_experiment_id(exp_id: int) -> None:
+def _set_active_experiment_id(exp_id: Optional[int]) -> None:
     """
-    Sets the active_experiment to the exp_id of a created/ loaded experiment.
+    Sets the global active_experiment to the exp_id of a created/ loaded
+    experiment. When a database is loaded, sets to the maximum exp_id in
+    the database. If it is a new database, sets to None.
 
     Args:
-        exp_id: The exp_id of an experiment that is created/ loaded.
+        exp_id: The exp_id of an experiment.
     """
     global active_experiment
     active_experiment = exp_id
@@ -20,11 +21,13 @@ def _set_active_experiment_id(exp_id: int) -> None:
 
 def get_active_experiment_id() -> Optional[int]:
     """
-    Gets the active exp_id.
+    Gets the updated global active_experiment.
 
     Returns:
-        Active exp_id. If the return is None, then the active exp_id will be
-        the last exp_id in experiments list, if available.
+        Returns the latest created/ loaded experiment's exp_id in the kernel.
+        If no experiment is started in the kernel and only a database is
+        initialized, the return will be the maximum exp_id in the database
+        or None, if it is a new database.
     """
     global active_experiment
     return active_experiment
@@ -32,7 +35,21 @@ def get_active_experiment_id() -> Optional[int]:
 
 def reset_active_experiment_id() -> None:
     """
-    Resets the active_experiment to the default.
+    Resets the active_experiment to None.
     """
     global active_experiment
     active_experiment = None
+
+
+def _handle_active_experiment_id_return() -> Optional[int]:
+    """
+    Checks if get_active_experiment_id is an existing exp_id and return it,
+    and if it is None, raise error that no experiment is initialized.
+    """
+    active_experiment = get_active_experiment_id()
+    if active_experiment is not None:
+        return active_experiment
+    else:
+        raise ValueError("No experiments found."
+                         " You can create one with:"
+                         " load_or_create_experiment(name, sample_name)")
