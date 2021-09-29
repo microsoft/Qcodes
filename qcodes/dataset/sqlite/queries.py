52dba66b7f44c3bdf26965106825115cd6dd81af
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2021-09-29 11:02:12 +0200

    move queries to the right module

--- qcodes/dataset/sqlite/queries.py
+++ qcodes/dataset/sqlite/queries.py
@@ -1987,3 +1987,58 @@ def get_experiment_attributes_by_exp_id(
     }
 
     return exp_attrs
+
+
+def _populate_results_table(
+    source_conn: ConnectionPlus,
+    target_conn: ConnectionPlus,
+    source_table_name: str,
+    target_table_name: str,
+) -> None:
+    """
+    Copy over all the entries of the results table
+    """
+    get_data_query = f"""
+                     SELECT *
+                     FROM "{source_table_name}"
+                     """
+
+    source_cursor = source_conn.cursor()
+    target_cursor = target_conn.cursor()
+
+    for row in source_cursor.execute(get_data_query):
+        column_names = ",".join(row.keys()[1:])  # the first key is "id"
+        values = tuple(val for val in row[1:])
+        value_placeholders = sql_placeholder_string(len(values))
+        insert_data_query = f"""
+                             INSERT INTO "{target_table_name}"
+                             ({column_names})
+                             values {value_placeholders}
+                             """
+        target_cursor.execute(insert_data_query, values)
+
+
+def _rewrite_timestamps(
+    target_conn: ConnectionPlus,
+    target_run_id: int,
+    correct_run_timestamp: Optional[float],
+    correct_completed_timestamp: Optional[float],
+) -> None:
+    """
+    Update the timestamp to match the original one
+    """
+    query = """
+            UPDATE runs
+            SET run_timestamp = ?
+            WHERE run_id = ?
+            """
+    cursor = target_conn.cursor()
+    cursor.execute(query, (correct_run_timestamp, target_run_id))
+
+    query = """
+            UPDATE runs
+            SET completed_timestamp = ?
+            WHERE run_id = ?
+            """
+    cursor = target_conn.cursor()
+    cursor.execute(query, (correct_completed_timestamp, target_run_id))
