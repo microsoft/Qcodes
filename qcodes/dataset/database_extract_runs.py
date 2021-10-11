4294ea665ece7d8fd4aec163a1b01f5947ebf5ab
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2021-10-11 14:30:39 +0200

    move reused code to avoid cirular dependency

--- qcodes/dataset/database_extract_runs.py
+++ qcodes/dataset/database_extract_runs.py
@@ -1,14 +1,11 @@
 import os
-from typing import Optional
 from warnings import warn
 
 import numpy as np
 
 from qcodes.dataset.data_set import DataSet
-from qcodes.dataset.data_set_protocol import DataSetProtocol
-from qcodes.dataset.descriptions.versioning.converters import new_to_old
+from qcodes.dataset.dataset_helpers import _add_run_to_runs_table
 from qcodes.dataset.experiment_container import _create_exp_if_needed
-from qcodes.dataset.linked_datasets.links import links_to_str
 from qcodes.dataset.sqlite.connection import ConnectionPlus, atomic
 from qcodes.dataset.sqlite.database import (
     connect,
@@ -16,14 +13,10 @@ from qcodes.dataset.sqlite.database import (
 )
 from qcodes.dataset.sqlite.queries import (
     _populate_results_table,
-    _rewrite_timestamps,
-    add_data_to_dynamic_columns,
-    create_run,
     get_exp_ids_from_run_ids,
     get_experiment_attributes_by_exp_id,
     get_runid_from_guid,
     is_run_id_in_database,
-    mark_run_complete,
 )
 
 
@@ -152,46 +145,3 @@ def _extract_single_dataset_into_db(dataset: DataSet,
     _populate_results_table(
         source_conn, target_conn, dataset.table_name, target_table_name
     )
-
-
-def _add_run_to_runs_table(
-    dataset: DataSetProtocol,
-    target_conn: ConnectionPlus,
-    target_exp_id: int,
-    create_run_table: bool = True,
-) -> Optional[str]:
-    if dataset._parameters is not None:
-        param_names = dataset._parameters.split(",")
-    else:
-        param_names = []
-    parspecs_dict = {
-        p.name: p for p in new_to_old(dataset.description.interdeps).paramspecs
-    }
-    parspecs = [parspecs_dict[p] for p in param_names]
-    metadata = dataset.metadata
-    snapshot_raw = dataset._snapshot_raw
-    captured_run_id = dataset.captured_run_id
-    captured_counter = dataset.captured_counter
-    parent_dataset_links = links_to_str(dataset.parent_dataset_links)
-    _, target_run_id, target_table_name = create_run(
-        target_conn,
-        target_exp_id,
-        name=dataset.name,
-        guid=dataset.guid,
-        parameters=parspecs,
-        metadata=metadata,
-        captured_run_id=captured_run_id,
-        captured_counter=captured_counter,
-        parent_dataset_links=parent_dataset_links,
-        create_run_table=create_run_table,
-    )
-    mark_run_complete(target_conn, target_run_id)
-    _rewrite_timestamps(target_conn,
-                        target_run_id,
-                        dataset.run_timestamp_raw,
-                        dataset.completed_timestamp_raw)
-    if snapshot_raw is not None:
-        add_data_to_dynamic_columns(
-            target_conn, target_run_id, {"snapshot": snapshot_raw}
-        )
-    return target_table_name
