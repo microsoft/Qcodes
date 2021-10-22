b52ebde9d90778f878eba6d95f887430549358d1
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2021-10-22 15:12:06 +0200

    test error handling with empty interdeps

--- qcodes/tests/dataset/test_dataset_in_memory_bacis.py
+++ qcodes/tests/dataset/test_dataset_in_memory_bacis.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 
+import pytest
+
 import qcodes as qc
-from qcodes import load_by_guid, load_or_create_experiment, new_data_set
+from qcodes import load_by_guid, load_or_create_experiment
 from qcodes.dataset.data_set_in_memory import DataSetInMem
+from qcodes.dataset.descriptions.dependencies import InterDependencies_
 from qcodes.dataset.sqlite.database import connect
 
 
@@ -49,3 +52,9 @@ def test_write_metadata_to_explicit_db(empty_temp_db):
     loaded_ds = load_by_guid(ds.guid, conn=connect(extra_db_location))
 
     ds.the_same_dataset_as(loaded_ds)
+
+
+def test_no_interdeps_raises_in_prepare(experiment):
+    ds = DataSetInMem.create_new_run(name="foo")
+    with pytest.raises(RuntimeError, match="No parameters supplied"):
+        ds.prepare(interdeps=InterDependencies_(), snapshot={})
