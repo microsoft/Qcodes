42098d8aa263775bbdf1c98db26caec83ca93f74
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2021-11-02 15:11:17 +0100

    update location of db fixtures

--- qcodes/tests/dataset/test_dataset_in_memory.py
+++ qcodes/tests/dataset/test_dataset_in_memory.py
@@ -217,7 +217,7 @@ def test_dataset_in_memory_does_not_create_runs_table(
 
 def test_load_from_netcdf_and_write_metadata_to_db(empty_temp_db):
     netcdf_file_path = (
-        Path(__file__).parent / "fixtures" / "db_files" / "version8" / "qcodes_2.nc"
+        Path(__file__).parent / "fixtures" / "db_files" / "netcdf" / "qcodes_2.nc"
     )
 
     if not os.path.exists(str(netcdf_file_path)):
@@ -250,7 +250,7 @@ def test_load_from_netcdf_and_write_metadata_to_db(empty_temp_db):
 
 def test_load_from_netcdf_no_db_file(non_created_db):
     netcdf_file_path = (
-        Path(__file__).parent / "fixtures" / "db_files" / "version8" / "qcodes_2.nc"
+        Path(__file__).parent / "fixtures" / "db_files" / "netcdf" / "qcodes_2.nc"
     )
 
     if not os.path.exists(str(netcdf_file_path)):
