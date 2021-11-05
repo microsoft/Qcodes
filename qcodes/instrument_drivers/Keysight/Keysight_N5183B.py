c57cd68a506a97c8be71c5837dc28e85b51da919
yoshi74ls181 <yoshi74ls181@gmail.com>
2021-11-05 16:50:19 +0900

    Fix power and frequency ranges of N51x1 and N51x3

--- qcodes/instrument_drivers/Keysight/Keysight_N5183B.py
+++ qcodes/instrument_drivers/Keysight/Keysight_N5183B.py
@@ -1,3 +1,6 @@
-from qcodes.instrument_drivers.Keysight.N51x1 import N51x1 as N5183B
-# N5183B has the same interface as N51x1 devices
-# This file is to allow for improved device labeling.
+from qcodes.instrument_drivers.Keysight.N51x1 import N51x1
+
+
+class N5183B(N51x1):
+    def __init__(self, name: str, address: str, **kwargs):
+        super().__init__(name, address, min_power=-20, max_power=19, **kwargs)
