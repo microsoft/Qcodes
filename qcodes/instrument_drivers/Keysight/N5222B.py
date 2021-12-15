eca2d76b43c3b313b40040d31aacd094b9260c0d
Cassandra Chua <cchua@microsoft.com>
2021-12-15 16:28:16 +1100

    Modify to include all options with attenuator

--- qcodes/instrument_drivers/Keysight/N5222B.py
+++ qcodes/instrument_drivers/Keysight/N5222B.py
@@ -3,16 +3,15 @@ from typing import Any
 from . import N52xx
 
 class N5222B(N52xx.PNABase):
-    """
-    Driver for Keysight PNA N5222B
-    """
     def __init__(self, name: str, address: str, **kwargs: Any):
+        """Driver for Keysight PNA N5222B."""
         super().__init__(name, address,
                          min_freq=10e6, max_freq=26.5e9,
                          min_power=-30, max_power=13,
                          nports=4,
                          **kwargs)
 
-        options = self.get_options()
-        if "419" in options:
-            self._set_power_limits(min_power=-90, max_power=13)
+        attenuators_options = {'217', '219', '220', '417', '419', '420'}
+        options = set(self.get_options())
+        if attenuators_options.intersection(options):
+            self._set_power_limits(min_power=-95, max_power=13)
