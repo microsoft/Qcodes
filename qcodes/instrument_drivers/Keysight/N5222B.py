3a10649fa07c666d72dead31392244ad75f33bc0
Cassandra Chua <cchua@microsoft.com>
2021-12-15 16:19:09 +1100

    Add docstring and power limit option

--- qcodes/instrument_drivers/Keysight/N5222B.py
+++ qcodes/instrument_drivers/Keysight/N5222B.py
@@ -3,9 +3,16 @@ from typing import Any
 from . import N52xx
 
 class N5222B(N52xx.PNABase):
+    """
+    Driver for Keysight PNA N5222B
+    """
     def __init__(self, name: str, address: str, **kwargs: Any):
         super().__init__(name, address,
                          min_freq=10e6, max_freq=26.5e9,
-                         min_power=-90, max_power=13,
+                         min_power=-30, max_power=13,
                          nports=4,
                          **kwargs)
+
+        options = self.get_options()
+        if "419" in options:
+            self._set_power_limits(min_power=-90, max_power=13)
