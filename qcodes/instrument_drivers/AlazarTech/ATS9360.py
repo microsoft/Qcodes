b617b231a6042bbdecf0ddbb4e9973386cdc705f
Jens H. Nielsen <jenshnielsen@gmail.com>
2021-12-20 09:15:41 +0100

    Alazar replace distutils with packaging

--- qcodes/instrument_drivers/AlazarTech/ATS9360.py
+++ qcodes/instrument_drivers/AlazarTech/ATS9360.py
@@ -1,7 +1,7 @@
-from distutils.version import LooseVersion
 from typing import Any
 
 import numpy as np
+from packaging import version
 
 from qcodes.utils import validators
 
@@ -326,8 +326,9 @@ class AlazarTech_ATS9360(AlazarTech_ATS):
     def _get_trigger_holdoff(self) -> bool:
         fwversion = self.get_idn()['firmware']
 
-        if not isinstance(fwversion, str) or LooseVersion(fwversion) < \
-                LooseVersion(self._trigger_holdoff_min_fw_version):
+        if not isinstance(fwversion, str) or version.parse(fwversion) < version.parse(
+            self._trigger_holdoff_min_fw_version
+        ):
             return False
 
         # we want to check if the 26h bit (zero indexed) is high or not
@@ -344,13 +345,16 @@ class AlazarTech_ATS9360(AlazarTech_ATS):
         return bool(bin(output)[-27])
 
     def _set_trigger_holdoff(self, value: bool) -> None:
-        fwversion = self.get_idn()['firmware']
-        if not isinstance(fwversion, str) or  LooseVersion(fwversion) < \
-                LooseVersion(self._trigger_holdoff_min_fw_version):
-            raise RuntimeError(f"Alazar 9360 requires at least firmware "
-                               f"version {self._trigger_holdoff_min_fw_version}"
-                               f" for trigger holdoff support. "
-                               f"You have version {fwversion}")
+        fwversion = self.get_idn()["firmware"]
+        if not isinstance(fwversion, str) or version.parse(fwversion) < version.parse(
+            self._trigger_holdoff_min_fw_version
+        ):
+            raise RuntimeError(
+                f"Alazar 9360 requires at least firmware "
+                f"version {self._trigger_holdoff_min_fw_version}"
+                f" for trigger holdoff support. "
+                f"You have version {fwversion}"
+            )
         current_value = self._read_register(58)
 
         if value is True:
