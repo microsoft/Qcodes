037b365002f5ac64450076a81fa77da6033dbaae
Mikhail Astafev <astafan8@gmail.com>
2022-01-11 15:58:23 +0100

    Update test_keithley_3706A.py

--- qcodes/tests/drivers/test_keithley_3706A.py
+++ qcodes/tests/drivers/test_keithley_3706A.py
@@ -81,9 +81,9 @@ def test_slot_names(driver):
 
 def test_get_interlock_state(driver):
     dict_list = (
-        {'slot_no': 1, 'state': 'Interlock is disengaged'},
-        {'slot_no': 2, 'state': 'Interlock is disengaged'},
-        {'slot_no': 3, 'state': 'Interlock is engaged'},
+        {'slot_no': '1', 'state': 'Interlock is disengaged'},
+        {'slot_no': '2', 'state': 'Interlock is disengaged'},
+        {'slot_no': '3', 'state': 'Interlock is engaged'},
     )
     assert dict_list == driver.get_interlock_state()
 
