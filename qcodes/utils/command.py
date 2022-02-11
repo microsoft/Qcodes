dcb0893494bc55a1888d0fee0502a9ed98833ee9
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-02-11 17:01:45 +0100

    try typing command

--- qcodes/utils/command.py
+++ qcodes/utils/command.py
@@ -1,8 +1,14 @@
+from typing import Any, Callable, Optional, TypeVar, Union
+
 from qcodes.utils.helpers import is_function
 
 
 class NoCommandError(Exception):
     pass
+
+
+Output = TypeVar("Output")
+ParsedOutput = TypeVar("ParsedOutput")
 
 
 class Command:
@@ -45,26 +51,35 @@ class Command:
         NoCommandError: If no cmd is found no_cmd_function is missing.
     """
 
-    def __init__(self, arg_count, cmd=None, exec_str=None, input_parser=None,
-                 output_parser=None, no_cmd_function=None):
+    def __init__(
+        self,
+        arg_count: int,
+        cmd: Optional[Union[str, Callable[..., Output]]] = None,
+        exec_str: Optional[Callable[[str], Output]] = None,
+        input_parser: Optional[Callable] = None,
+        output_parser: Optional[Callable[[Output], ParsedOutput]] = None,
+        no_cmd_function: Optional[Callable] = None,
+    ):
 
         self.arg_count = arg_count
 
-        if (no_cmd_function is not None and
-                not is_function(no_cmd_function, arg_count)):
-            raise TypeError('no_cmd_function must be none or a function '
-                            'taking the same args as the command, not '
-                            '{}'.format(no_cmd_function))
+        if no_cmd_function is not None and not is_function(no_cmd_function, arg_count):
+            raise TypeError(
+                f"no_cmd_function must be none or a function "
+                f"taking the same args as the command, not "
+                f"{no_cmd_function}"
+            )
 
         if input_parser is None:
-            parse_input = False
+            parse_input: Union[bool, str] = False
         elif is_function(input_parser, arg_count):
             parse_input = True if arg_count == 1 else 'multi'
             self.input_parser = input_parser
         else:
             raise TypeError(
-                'input_parser must be a function with one arg_count args,'
-                ' not {}'.format(repr(input_parser)))
+                f"input_parser must be a function with one arg_count args,"
+                f" not {input_parser!r}"
+            )
 
         if output_parser is None:
             parse_output = False
@@ -73,11 +88,15 @@ class Command:
             self.output_parser = output_parser
         else:
             raise TypeError(
-                'output_parser must be a function with one arg,'
-                ' not {}'.format(repr(output_parser)))
+                f"output_parser must be a function with one arg,"
+                f" not {output_parser!r}"
+            )
 
         if isinstance(cmd, str):
             self.cmd_str = cmd
+            if exec_str is None:
+                raise TypeError("exec_str cannot be None if " "cmd is a str.")
+
             self.exec_str = exec_str
 
             if is_function(exec_str, 1):
@@ -97,6 +116,7 @@ class Command:
                 )
 
         elif is_function(cmd, arg_count):
+            assert cmd is not None
             self._cmd = cmd
             self.exec_function = {  # (parse_input, parse_output)
                 (False, False): cmd,
@@ -124,55 +144,55 @@ class Command:
     # overhead from branching or extra function calls and stack frames.
     # TODO(giulioungaretti) wihtout benchmarks this is "just like your opinion man"
 
-    def call_by_str(self, *args):
+    def call_by_str(self, *args: Any) -> Output:
         """Execute a formatted string."""
         return self.exec_str(self.cmd_str.format(*args))
 
-    def call_by_str_parsed_out(self, *args):
+    def call_by_str_parsed_out(self, *args: Any) -> ParsedOutput:
         """Execute a formatted string with output parsing."""
         return self.output_parser(self.exec_str(self.cmd_str.format(*args)))
 
-    def call_by_str_parsed_in(self, arg):
+    def call_by_str_parsed_in(self, arg: Any) -> Output:
         """Execute a formatted string with 1-arg input parsing."""
         return self.exec_str(self.cmd_str.format(self.input_parser(arg)))
 
-    def call_by_str_parsed_in_out(self, arg):
+    def call_by_str_parsed_in_out(self, arg: Any) -> ParsedOutput:
         """Execute a formatted string with 1-arg input and output parsing."""
         return self.output_parser(self.exec_str(
             self.cmd_str.format(self.input_parser(arg))))
 
-    def call_by_str_parsed_in2(self, *args):
+    def call_by_str_parsed_in2(self, *args: Any) -> Output:
         """Execute a formatted string with multi-arg input parsing."""
         return self.exec_str(self.cmd_str.format(*self.input_parser(*args)))
 
-    def call_by_str_parsed_in2_out(self, *args):
+    def call_by_str_parsed_in2_out(self, *args: Any) -> ParsedOutput:
         """Execute a formatted string with multi-arg input & output parsing."""
         return self.output_parser(self.exec_str(
             self.cmd_str.format(*self.input_parser(*args))))
 
     # And the same for parsing + command as a function
 
-    def call_cmd_parsed_out(self, *args):
+    def call_cmd_parsed_out(self, *args: Any) -> ParsedOutput:
         """Execute a function with output parsing."""
         return self.output_parser(self._cmd(*args))
 
-    def call_cmd_parsed_in(self, arg):
+    def call_cmd_parsed_in(self, arg: Any) -> Output:
         """Execute a function with 1-arg input parsing."""
         return self._cmd(self.input_parser(arg))
 
-    def call_cmd_parsed_in_out(self, arg):
+    def call_cmd_parsed_in_out(self, arg: Any) -> ParsedOutput:
         """Execute a function with 1-arg input and output parsing."""
         return self.output_parser(self._cmd(self.input_parser(arg)))
 
-    def call_cmd_parsed_in2(self, *args):
+    def call_cmd_parsed_in2(self, *args: Any) -> Output:
         """Execute a function with multi-arg input parsing."""
         return self._cmd(*self.input_parser(*args))
 
-    def call_cmd_parsed_in2_out(self, *args):
+    def call_cmd_parsed_in2_out(self, *args: Any) -> ParsedOutput:
         """Execute a function with multi-arg input & output parsing."""
         return self.output_parser(self._cmd(*self.input_parser(*args)))
 
-    def __call__(self, *args):
+    def __call__(self, *args: Any) -> Union[Output, ParsedOutput]:
         """Invoke the command."""
         if len(args) != self.arg_count:
             raise TypeError(
