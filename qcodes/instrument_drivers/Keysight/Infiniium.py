18623b0cedc53e1389324a3247924b683182f178
Sebastian Pauka <spauka@microsoft.com>
2022-02-14 14:01:24 +1100

    Infiniium fix float typo

--- qcodes/instrument_drivers/Keysight/Infiniium.py
+++ qcodes/instrument_drivers/Keysight/Infiniium.py
@@ -798,10 +798,10 @@ class Infiniium(VisaInstrument):
         # Sample Rate
         try:
             # Set BW to auto in order to query this
-            bw_set = np.float(self.ask(":ACQ:BAND?"))
+            bw_set = float(self.ask(":ACQ:BAND?"))
             if np.isclose(bw_set, self.max_bw):
                 # Auto returns max bandwidth
-                bw_set = "AUTO"
+                bw_set = "AUTO"  # type: ignore
             self.write(":ACQ:BAND AUTO")
             self.min_srat, self.max_srat = 10.0, 99.0e9  # Set large limits
             srat = self.ask(":ACQ:SRAT:TESTLIMITS?")
