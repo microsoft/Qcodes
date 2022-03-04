c88ac7b960bb13a3f1010f1fca5184a0472bf7be
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-03-04 15:17:26 +0100

    Add shim for backwards compatibility

---
+++ qcodes/utils/dataset/doNd.py
@@ -0,0 +1,21 @@
+from qcodes.dataset.doNd import (
+    AbstractSweep,
+    ArraySweep,
+    LinSweep,
+    LogSweep,
+    do0d,
+    do1d,
+    do2d,
+    dond,
+)
+
+__all__ = [
+    "do0d",
+    "do1d",
+    "do2d",
+    "dond",
+    "AbstractSweep",
+    "ArraySweep",
+    "LinSweep",
+    "LogSweep",
+]
