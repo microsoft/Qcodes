345a47fd636fb3cda2df70143a93e490aac30a50
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-03-07 11:08:19 +0100

    temp disable old location

--- qcodes/utils/dataset/doNd.py
+++ qcodes/utils/dataset/doNd.py
@@ -1,23 +1,23 @@
-from qcodes.dataset.doNd import (
-    AbstractSweep,
-    ArraySweep,
-    LinSweep,
-    LogSweep,
-    do0d,
-    do1d,
-    do2d,
-    dond,
-)
-from qcodes.dataset.plotting import plot_and_save_image as plot
-
-__all__ = [
-    "do0d",
-    "do1d",
-    "do2d",
-    "dond",
-    "AbstractSweep",
-    "ArraySweep",
-    "LinSweep",
-    "LogSweep",
-    "plot",
-]
+# from qcodes.dataset.doNd import (
+#     AbstractSweep,
+#     ArraySweep,
+#     LinSweep,
+#     LogSweep,
+#     do0d,
+#     do1d,
+#     do2d,
+#     dond,
+# )
+# from qcodes.dataset.plotting import plot_and_save_image as plot
+#
+# __all__ = [
+#     "do0d",
+#     "do1d",
+#     "do2d",
+#     "dond",
+#     "AbstractSweep",
+#     "ArraySweep",
+#     "LinSweep",
+#     "LogSweep",
+#     "plot",
+# ]
