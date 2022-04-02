575ede2e2e3b0fcc9738a42c690d429f5a909ba0
Serwan <serwan.asaad@gmail.com>
2022-04-02 20:56:23 +0200

    creating tests for measurement loop

---
+++ qcodes/tests/dataset/measurement_loop/__init__.py
@@ -0,0 +1,8 @@
+
+import pytest
+
+
+from qcodes.dataset.measurement import Measurement, Sweep
+
+
+
---
+++ qcodes/tests/dataset/measurement_loop/test_measurement_loop_basics.py
@@ -0,0 +1,13 @@
+import math
+import pytest
+
+def test_sqrt():
+   num = 25
+   assert math.sqrt(num) == 5
+
+def testsquare():
+   num = 7
+   assert 7*7 == 40
+
+def testequality():
+   assert 10 == 11
