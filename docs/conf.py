d56eb429b0e88d8e7fa1290880c5fe6c65cf5d12
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-04-17 10:08:16 +0200

    update matplotlib api

--- docs/conf.py
+++ docs/conf.py
@@ -389,7 +389,7 @@ texinfo_show_urls = 'footnote'
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
-    "matplotlib": ("https://matplotlib.org/", None),
+    "matplotlib": ("https://matplotlib.org/stable", None),
     "python": ("https://docs.python.org/3.7/", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
     "py": ("https://pylib.readthedocs.io/en/stable/", None),
