9c7822758a55d11d070f22062f50ffc9efcbf7ea
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-04-17 10:33:14 +0200

    Add missing methods to public api

--- qcodes/instrument/__init__.py
+++ qcodes/instrument/__init__.py
@@ -1,6 +1,7 @@
 from .base import Instrument, find_or_create_instrument
 from .channel import ChannelList, ChannelTuple, InstrumentChannel, InstrumentModule
 from .function import Function
+from .group_parameter import Group, GroupParameter
 from .ip import IPInstrument
 from .parameter import (
     ArrayParameter,
@@ -13,6 +14,7 @@ from .parameter import (
     ScaledParameter,
     combine,
 )
+from .specialized_parameters import ElapsedTimeParameter
 from .sweep_values import SweepFixedValues, SweepValues
 from .visa import VisaInstrument
 
@@ -24,10 +26,13 @@ __all__ = [
     "InstrumentChannel",
     "InstrumentModule",
     "Function",
+    "Group",
+    "GroupParameter",
     "IPInstrument",
     "ArrayParameter",
     "CombinedParameter",
     "DelegateParameter",
+    "ElapsedTimeParameter",
     "ManualParameter",
     "MultiParameter",
     "Parameter",
