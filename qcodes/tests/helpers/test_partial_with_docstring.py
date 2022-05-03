3d588e612f95e74459636a2b6465c0c93f3a7ee4
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-05-03 15:18:32 +0200

    Test that partial_with_docstring returns values correctly and can handle non kwargs

--- qcodes/tests/helpers/test_partial_with_docstring.py
+++ qcodes/tests/helpers/test_partial_with_docstring.py
@@ -8,3 +8,25 @@ def test_partial_with_docstring():
     docstring = "some docstring"
     g = partial_with_docstring(f, docstring)
     assert g.__doc__ == docstring
+
+
+def test_partial_with_docstring_returns_value():
+    def f(a: int, b: int):
+        return a + b
+
+    docstring = "some docstring"
+    g = partial_with_docstring(f, docstring, a=1)
+
+    assert g.__doc__ == docstring
+    assert g(2) == 3
+
+
+def test_partial_with_docstring_returns_value_2():
+    def f(a: int, b: int):
+        return a + b
+
+    docstring = "some docstring"
+    g = partial_with_docstring(f, docstring, a=1)
+
+    assert g.__doc__ == docstring
+    assert g(b=2) == 3
