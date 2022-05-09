a992554b8db5ce0793f9d7d7a68d1be7b492b7b1
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-05-09 13:48:37 +0200

    simplify test conditions

--- qcodes/tests/parameter/test_parameter_cache.py
+++ qcodes/tests/parameter/test_parameter_cache.py
@@ -366,22 +366,14 @@ def test_marking_invalid_via_instrument(dummy_instrument):
 
         for instrument_module in dummy_instrument.instrument_modules.values():
             for param in instrument_module.parameters.values():
-                # Multi Array and ParamWithSetpoints are not updated in snapshot
-                # so not marked valid by snapshot.update(None)
-                # here we therefor exclude them
-                if param.short_name not in [
-                    "dummy_multi_parameter",
-                    "dummy_scalar_multi_parameter",
-                    "dummy_2d_multi_parameter",
-                    "dummy_2d_multi_parameter_2",
-                    "dummy_array_parameter",
-                    "dummy_sp_axis",
-                    "dummy_sp_axis_2",
-                    "dummy_complex_array_parameter",
-                    "dummy_parameter_with_setpoints",
-                    "dummy_parameter_with_setpoints_2d",
-                    "dummy_parameter_with_setpoints_complex",
-                ]:
+                # parameters not snapshotted will not have a cache
+                # updated when calling snapshot(update=None) os
+                # exclude them
+                if (
+                    param._snapshot_get is True
+                    and param.snapshot_value is True
+                    and param.snapshot_exclude is False
+                ):
                     assert param.cache.valid is valid, param.full_name
 
     dummy_instrument.snapshot(update=None)
