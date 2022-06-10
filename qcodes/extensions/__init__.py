65c8d6630b890b3c36ee9fb64444bde2f9306676
Jens Hedegaard Nielsen <jenshnielsen@gmail.com>
2022-06-10 13:08:44 +0200

    Update qcodes/extensions/__init__.py
    Co-authored-by: Mikhail Astafev <astafan8@gmail.com>

--- qcodes/extensions/__init__.py
+++ qcodes/extensions/__init__.py
@@ -1,6 +1,6 @@
 """
 The extensions module contains smaller modules that extend the functionality of QCoDeS.
-These modules may import from all of QCoDeS but do not them self get imported into QCoDeS.
+These modules may import from all of QCoDeS but do not themselves get imported into QCoDeS.
 """
 from .installation import register_station_schema_with_vscode
 from .slack import Slack, SlackTimeoutWarning
