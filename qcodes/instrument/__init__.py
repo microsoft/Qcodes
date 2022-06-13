a61199afb3490a03fd246510e7b50b75a73a3079
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-06-13 20:36:39 +0200

    cleanup instrument module

--- qcodes/instrument/__init__.py
+++ qcodes/instrument/__init__.py
@@ -1,49 +1,34 @@
+# left here for backwards compatibility
+# but not part of the api officially
+from qcodes.parameters import (
+    ArrayParameter,
+    CombinedParameter,
+    DelegateParameter,
+    ManualParameter,
+    MultiParameter,
+    Parameter,
+    ParameterWithSetpoints,
+    ScaledParameter,
+    SweepFixedValues,
+    SweepValues,
+    combine,
+)
+
 from .base import Instrument, InstrumentBase, find_or_create_instrument
 from .channel import ChannelList, ChannelTuple, InstrumentChannel, InstrumentModule
 from .function import Function
-
-# todo which part of parameter should be exported here for backwards compatibility
-# from .group_parameter import Group, GroupParameter
 from .ip import IPInstrument
-
-# from .parameter import (
-#     ArrayParameter,
-#     CombinedParameter,
-#     DelegateParameter,
-#     ManualParameter,
-#     MultiParameter,
-#     Parameter,
-#     ParameterWithSetpoints,
-#     ScaledParameter,
-#     combine,
-# )
-# from .specialized_parameters import ElapsedTimeParameter
-# from qcodes.parameters.sweep_values import SweepFixedValues, SweepValues
 from .visa import VisaInstrument
 
 __all__ = [
-    # "ArrayParameter",
     "ChannelList",
     "ChannelTuple",
-    # "CombinedParameter",
-    # "DelegateParameter",
-    # "ElapsedTimeParameter",
     "Function",
-    # "Group",
-    # "GroupParameter",
     "IPInstrument",
     "Instrument",
     "InstrumentBase",
     "InstrumentChannel",
     "InstrumentModule",
-    # "ManualParameter",
-    # "MultiParameter",
-    # "Parameter",
-    # "ParameterWithSetpoints",
-    # "ScaledParameter",
-    # "SweepFixedValues",
-    # "SweepValues",
     "VisaInstrument",
-    # "combine",
     "find_or_create_instrument",
 ]
