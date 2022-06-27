be6906895922329a37aa06b3bda0691a3f8ea133
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-06-27 10:28:09 +0200

    Add installation info to public api

--- qcodes/utils/__init__.py
+++ qcodes/utils/__init__.py
@@ -1,12 +1,20 @@
 from . import validators
 from .delaykeyboardinterrupt import DelayedKeyboardInterrupt
 from .deprecate import QCoDeSDeprecationWarning, deprecate, issue_deprecation_warning
+from .installation_info import (
+    convert_legacy_version_to_supported_version,
+    get_all_installed_package_versions,
+    is_qcodes_installed_editably,
+)
 from .json_utils import NumpyJSONEncoder
 
 __all__ = [
     "DelayedKeyboardInterrupt",
     "NumpyJSONEncoder",
-    "deprecate",
     "QCoDeSDeprecationWarning",
+    "convert_legacy_version_to_supported_version",
+    "deprecate",
+    "get_all_installed_package_versions",
+    "is_qcodes_installed_editably",
     "issue_deprecation_warning",
 ]
