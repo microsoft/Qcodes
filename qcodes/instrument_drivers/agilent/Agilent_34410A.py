6497ed016b6f82777ae51d39a45b073313bf3167
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-07-07 10:18:09 +0200

    Cleanup naming of Agilent 344 drivers

---
+++ qcodes/instrument_drivers/agilent/Agilent_34410A.py
@@ -0,0 +1,5 @@
+from ._Agilent_344xxA import _Agilent344xxA
+
+
+class Agilent34410A(_Agilent344xxA):
+    pass
---
+++ qcodes/instrument_drivers/agilent/__init__.py
@@ -0,0 +1,5 @@
+from .Agilent_34401A import Agilent34401A
+from .Agilent_34410A import Agilent34410A
+from .Agilent_34411A import Agilent34411A
+
+__all__ = ["Agilent34401A", "Agilent34410A", "Agilent34411A"]
