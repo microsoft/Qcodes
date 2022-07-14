26b9365bc49cd2a7fb41212c544b2216c5b2118f
Mikhail Astafev <astafan8@gmail.com>
2022-07-14 10:47:42 +0200

    format

--- qcodes/tests/parameter/test_parameter_scale_offset.py
+++ qcodes/tests/parameter/test_parameter_scale_offset.py
@@ -33,9 +33,11 @@ def test_scale_raw_value():
 #        independently
 
 # define shorthands for strategies
-TestFloats = hst.floats(min_value=-1e40, max_value=1e40).filter(lambda x: abs(x) >= 1e-20)
-SharedSize = hst.shared(hst.integers(min_value=1, max_value=100), key='shared_size')
-ValuesScalar = hst.shared(hst.booleans(), key='values_scalar')
+TestFloats = hst.floats(min_value=-1e40, max_value=1e40).filter(
+    lambda x: abs(x) >= 1e-20
+)
+SharedSize = hst.shared(hst.integers(min_value=1, max_value=100), key="shared_size")
+ValuesScalar = hst.shared(hst.booleans(), key="values_scalar")
 
 
 # the following test stra
@@ -94,9 +96,11 @@ def test_scale_and_offset_raw_value_iterable(values, offsets, scales):
         # not too different from ``values*scales``
         tolerance = 1e7
         if (
-            abs(values * scales) >= abs(offsets) and abs(values * scales) < tolerance * abs(offsets)
+            abs(values * scales) >= abs(offsets)
+            and abs(values * scales) < tolerance * abs(offsets)
         ) or (
-            abs(values * scales) < abs(offsets) and abs(offsets) < tolerance * abs(values * scales)
+            abs(values * scales) < abs(offsets)
+            and abs(offsets) < tolerance * abs(values * scales)
         ):
             # testing conversion back and forth
             p(values)
