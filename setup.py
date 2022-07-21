5415649763961f5dca7a8f0bcdcfef4f7c3b1806
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-07-21 10:39:45 +0200

    Error with old setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
+import setuptools
 from setuptools import setup
 from versioningit import get_cmdclasses
+
+if int(setuptools.__version__.split(".")[0]) < 61:
+    raise RuntimeError(
+        "At least setuptools 61 is required to install qcodes from source"
+    )
 
 if __name__ == "__main__":
     setup(
