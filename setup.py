02621a4605761b5690622888f937b959387d574d
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-07-21 11:37:25 +0200

    raise an error if installing from source using old pip

--- setup.py
+++ setup.py
@@ -7,6 +7,15 @@ if int(setuptools.__version__.split(".")[0]) < 61:
         "At least setuptools 61 is required to install qcodes from source"
     )
 
+try:
+    import pip
+
+    if int(pip.__version__.split(".")[0]) < 19:
+        raise RuntimeError("At least pip 19 is required to install qcodes from source")
+except ImportError:
+    # we are not being executed from pip so pip version is not important
+    pass
+
 if __name__ == "__main__":
     setup(
         cmdclass=get_cmdclasses(),
