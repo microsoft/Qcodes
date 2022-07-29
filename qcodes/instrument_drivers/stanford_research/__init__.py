1c8cf7ac86522d4120fa57160bc8ace5b922af59
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-07-29 16:45:42 +0200

    add sr560 driver to public api

--- qcodes/instrument_drivers/stanford_research/__init__.py
+++ qcodes/instrument_drivers/stanford_research/__init__.py
@@ -1,5 +1,6 @@
 from .SG384 import SG384
 from .SR86x import SR86xBuffer
+from .SR560 import SR560
 from .SR830 import SR830
 from .SR860 import SR860
 from .SR865 import SR865
@@ -7,6 +8,7 @@ from .SR865A import SR865A
 
 __all__ = [
     "SG384",
+    "SR560",
     "SR830",
     "SR860",
     "SR865",
