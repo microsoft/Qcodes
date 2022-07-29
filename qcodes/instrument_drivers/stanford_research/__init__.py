df0189f0eafa6ddd430bdca27bfdb998aeb17f58
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-07-29 16:27:21 +0200

    Import Stanford drivers at top level

---
+++ qcodes/instrument_drivers/stanford_research/__init__.py
@@ -0,0 +1,15 @@
+from .SG384 import SG384
+from .SR86x import SR86xBuffer
+from .SR830 import SR830
+from .SR860 import SR860
+from .SR865 import SR865
+from .SR865A import SR865A
+
+__all__ = [
+    "SG384",
+    "SR830",
+    "SR860",
+    "SR865",
+    "SR865A",
+    "SR86xBuffer",
+]
