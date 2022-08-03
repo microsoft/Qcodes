25887ed91e62b6d95edd9b5bbc2e36887bcc83a4
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-08-03 08:03:13 +0200

    Add Renamed Lakeshore 336

--- qcodes/instrument_drivers/Lakeshore/__init__.py
+++ qcodes/instrument_drivers/Lakeshore/__init__.py
@@ -5,6 +5,12 @@ from .Lakeshore_model_325 import (
     LakeshoreModel325Sensor,
     LakeshoreModel325Status,
 )
+from .Lakeshore_model_336 import (
+    LakeshoreModel336,
+    LakeshoreModel336Channel,
+    LakeshoreModel336CurrentSource,
+    LakeshoreModel336VoltageSource,
+)
 
 __all__ = [
     "LakeshoreModel325",
@@ -12,4 +18,8 @@ __all__ = [
     "LakeshoreModel325Heater",
     "LakeshoreModel325Sensor",
     "LakeshoreModel325Status",
+    "LakeshoreModel336",
+    "LakeshoreModel336Channel",
+    "LakeshoreModel336CurrentSource",
+    "LakeshoreModel336VoltageSource",
 ]
