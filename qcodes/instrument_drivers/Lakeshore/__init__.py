b5e746ab76b373132c36382f7fa7d01e4ef2199b
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-08-03 07:56:42 +0200

    Add Lakeshore model 325 with better naming

---
+++ qcodes/instrument_drivers/Lakeshore/__init__.py
@@ -0,0 +1,15 @@
+from .Lakeshore_model_325 import (
+    LakeshoreModel325,
+    LakeshoreModel325Curve,
+    LakeshoreModel325Heater,
+    LakeshoreModel325Sensor,
+    LakeshoreModel325Status,
+)
+
+__all__ = [
+    "LakeshoreModel325",
+    "LakeshoreModel325Curve",
+    "LakeshoreModel325Heater",
+    "LakeshoreModel325Sensor",
+    "LakeshoreModel325Status",
+]
