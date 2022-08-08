e47dfff6e0f3db65efb26881ef2c098d05e6c5d5
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-08-08 10:17:28 +0200

    let old driver match existing idn

--- qcodes/instrument_drivers/basel/sp983c.py
+++ qcodes/instrument_drivers/basel/sp983c.py
@@ -3,4 +3,20 @@ Legacy module kept around for backwards compatibility reasons.
 Will eventually be deprecated and removed
 
 """
-from .BaselSP983c import BaselSP983c as SP983C
+from typing import Dict, Optional
+
+from .BaselSP983c import BaselSP983c
+
+
+class SP983C(BaselSP983c):
+    def get_idn(self) -> Dict[str, Optional[str]]:
+        vendor = "Physics Basel"
+        model = "SP 983(c)"
+        serial = None
+        firmware = None
+        return {
+            "vendor": vendor,
+            "model": model,
+            "serial": serial,
+            "firmware": firmware,
+        }
