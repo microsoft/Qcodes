b46038a5d974b28b0396a6abbc8339de46b6f653
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-08-21 14:58:46 +0200

    Add backwards compat for DG1062

---
+++ qcodes/instrument_drivers/rigol/DG1062.py
@@ -0,0 +1,10 @@
+"""
+Old alias for RigolDG1062 for backwards compatibility.
+Will eventually be deprecated and removed.
+"""
+
+from .RigolDG1062 import RigolDG1062, RigolDG1062Burst, RigolDG1062Channel
+
+
+class DG1062(RigolDG1062):
+    pass
