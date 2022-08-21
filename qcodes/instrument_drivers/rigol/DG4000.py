fcfc8ee766fa3fea37fbb7c73b1f09e8d219cfb3
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-08-21 14:26:46 +0200

    Add old alias for DG4000

---
+++ qcodes/instrument_drivers/rigol/DG4000.py
@@ -0,0 +1,17 @@
+"""
+Old alias for RigolDG4000 for backwards compatibility.
+Will eventually be deprecated and removed.
+"""
+
+from .RigolDG4000 import (
+    RigolDG4000,
+    clean_string,
+    is_number,
+    parse_multiple_outputs,
+    parse_single_output,
+    parse_string_output,
+)
+
+
+class Rigol_DG4000(RigolDG4000):
+    pass
