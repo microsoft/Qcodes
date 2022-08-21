384d0e145fcdbaa3b355164881613df812ffdc71
Serwan <serwan.asaad@gmail.com>
2022-08-21 17:13:34 +0200

    improve database for tests

--- qcodes/tests/dataset/measurement_loop/test_measurement_loop_basics.py
+++ qcodes/tests/dataset/measurement_loop/test_measurement_loop_basics.py
@@ -13,57 +13,33 @@ from qcodes.dataset.measurement_loop import MeasurementLoop, Sweep
 from qcodes.utils.dataset.doNd import LinSweep
 
 
-# @pytest.fixture
-# def create_dummy_database():
-#     @contextlib.contextmanager
-#     def func_context_manager():
-#         with tempfile.TemporaryDirectory() as temporary_folder:
-#             temporary_folder = tempfile.TemporaryDirectory()
-#             print(f"Created temporary folder for database: {temporary_folder}")
-
-#             assert Path(temporary_folder.name).exists()
-#             db_path = Path(temporary_folder.name) / "test_database.db"
-#             initialise_or_create_database_at(str(db_path))
-
-#             try:
-#                 exp = load_or_create_experiment("test_experiment")
-#                 yield exp
-#             finally:
-#                 exp.conn.close()
-
-#     return func_context_manager
-
-
 @pytest.mark.usefixtures("empty_temp_db", "experiment")
-def test_original_dond(create_dummy_database):
-    with create_dummy_database():
-        from qcodes.utils.dataset.doNd import LinSweep, dond
+def test_original_dond():
+    from qcodes.utils.dataset.doNd import LinSweep, dond
 
-        p1_get = ManualParameter("p1_get", initial_value=1)
-        p2_get = ManualParameter("p2_get", initial_value=1)
-        p1_set = ManualParameter("p1_set", initial_value=1)
-        dond(
-            p1_set, 0, 1, 101,
-            p1_get, p2_get
-        )
-
-
-def test_create_measurement(create_dummy_database):
-    with create_dummy_database():
-        MeasurementLoop("test")
+    p1_get = ManualParameter("p1_get", initial_value=1)
+    p2_get = ManualParameter("p2_get", initial_value=1)
+    p1_set = ManualParameter("p1_set", initial_value=1)
+    dond(
+        p1_set, 0, 1, 101,
+        p1_get, p2_get
+    )
 
 
-def test_basic_1D_measurement(create_dummy_database):
-    with create_dummy_database():
-        # Initialize parameters
-        p1_get = ManualParameter("p1_get")
-        p1_set = ManualParameter("p1_set")
+def test_create_measurement():
+    MeasurementLoop("test")
 
-        with MeasurementLoop("test") as msmt:
-            for val in Sweep(p1_set, 0, 1, 11):
-                assert p1_set() == val
-                p1_get(val + 1)
-                msmt.measure(p1_get)
+
+def test_basic_1D_measurement():
+    # Initialize parameters
+    p1_get = ManualParameter("p1_get")
+    p1_set = ManualParameter("p1_set")
+
+    with MeasurementLoop("test") as msmt:
+        for val in Sweep(p1_set, 0, 1, 11):
+            assert p1_set() == val
+            p1_get(val + 1)
+            msmt.measure(p1_get)
 
     data = msmt.dataset
     assert data.name == "test"
@@ -76,20 +52,19 @@ def test_basic_1D_measurement(create_dummy_database):
     assert np.allclose(data_arrays["p1_set"], np.linspace(0, 1, 11))
 
 
-def test_basic_2D_measurement(create_dummy_database):
-    with create_dummy_database():
-        # Initialize parameters
-        p1_get = ManualParameter("p1_get")
-        p1_set = ManualParameter("p1_set")
-        p2_set = ManualParameter("p2_set")
+def test_basic_2D_measurement():
+    # Initialize parameters
+    p1_get = ManualParameter("p1_get")
+    p1_set = ManualParameter("p1_set")
+    p2_set = ManualParameter("p2_set")
 
-        with MeasurementLoop("test") as msmt:
-            for val in Sweep(p1_set, 0, 1, 11):
-                assert p1_set() == val
-                for val2 in Sweep(p2_set, 0, 1, 11):
-                    assert p2_set() == val2
-                    p1_get(val + 1)
-                    msmt.measure(p1_get)
+    with MeasurementLoop("test") as msmt:
+        for val in Sweep(p1_set, 0, 1, 11):
+            assert p1_set() == val
+            for val2 in Sweep(p2_set, 0, 1, 11):
+                assert p2_set() == val2
+                p1_get(val + 1)
+                msmt.measure(p1_get)
 
     data = msmt.dataset
     assert data.name == "test"
@@ -109,19 +84,18 @@ def test_basic_2D_measurement(create_dummy_database):
     )
 
 
-def test_1D_measurement_duplicate_get(create_dummy_database):
-    with create_dummy_database():
-        # Initialize parameters
-        p1_get = ManualParameter("p1_get")
-        p1_set = ManualParameter("p1_set")
+def test_1D_measurement_duplicate_get():
+    # Initialize parameters
+    p1_get = ManualParameter("p1_get")
+    p1_set = ManualParameter("p1_set")
 
-        with MeasurementLoop("test") as msmt:
-            for val in Sweep(p1_set, 0, 1, 11):
-                assert p1_set() == val
-                p1_get(val + 1)
-                msmt.measure(p1_get)
-                p1_get(val + 0.5)
-                msmt.measure(p1_get)
+    with MeasurementLoop("test") as msmt:
+        for val in Sweep(p1_set, 0, 1, 11):
+            assert p1_set() == val
+            p1_get(val + 1)
+            msmt.measure(p1_get)
+            p1_get(val + 0.5)
+            msmt.measure(p1_get)
 
     data = msmt.dataset
     assert data.name == "test"
@@ -137,21 +111,20 @@ def test_1D_measurement_duplicate_get(create_dummy_database):
         assert np.allclose(data_arrays["p1_set"], np.linspace(0, 1, 11))
 
 
-def test_1D_measurement_duplicate_getset(create_dummy_database):
-    with create_dummy_database():
-        # Initialize parameters
-        p1_get = ManualParameter("p1_get")
-        p1_set = ManualParameter("p1_set")
+def test_1D_measurement_duplicate_getset():
+    # Initialize parameters
+    p1_get = ManualParameter("p1_get")
+    p1_set = ManualParameter("p1_set")
 
-        with MeasurementLoop("test") as msmt:
-            for val in Sweep(p1_set, 0, 1, 11):
-                assert p1_set() == val
-                p1_get(val + 1)
-                msmt.measure(p1_get)
-            for val in Sweep(p1_set, 0, 1, 11):
-                assert p1_set() == val
-                p1_get(val + 0.5)
-                msmt.measure(p1_get)
+    with MeasurementLoop("test") as msmt:
+        for val in Sweep(p1_set, 0, 1, 11):
+            assert p1_set() == val
+            p1_get(val + 1)
+            msmt.measure(p1_get)
+        for val in Sweep(p1_set, 0, 1, 11):
+            assert p1_set() == val
+            p1_get(val + 0.5)
+            msmt.measure(p1_get)
 
     data = msmt.dataset
     assert data.name == "test"
@@ -171,36 +144,34 @@ def test_1D_measurement_duplicate_getset(create_dummy_database):
         assert np.allclose(data_arrays[set_key], np.linspace(0, 1, 11))
 
 
-def test_2D_measurement_initialization(create_dummy_database):
-    with create_dummy_database():
-        # Initialize parameters
-        p1_get = ManualParameter("p1_get")
-        p1_set = ManualParameter("p1_set")
-        p2_set = ManualParameter("p2_set")
+def test_2D_measurement_initialization():
+    # Initialize parameters
+    p1_get = ManualParameter("p1_get")
+    p1_set = ManualParameter("p1_set")
+    p2_set = ManualParameter("p2_set")
 
-        with MeasurementLoop("test") as msmt:
-            outer_sweep = Sweep(p1_set, 0, 1, 11)
-            for k, val in enumerate(outer_sweep):
-                assert p1_set() == val
+    with MeasurementLoop("test") as msmt:
+        outer_sweep = Sweep(p1_set, 0, 1, 11)
+        for k, val in enumerate(outer_sweep):
+            assert p1_set() == val
 
-                for val2 in Sweep(p2_set, 0, 1, 11):
-                    assert p2_set() == val2
-                    p1_get(val + 1)
-                    msmt.measure(p1_get)
+            for val2 in Sweep(p2_set, 0, 1, 11):
+                assert p2_set() == val2
+                p1_get(val + 1)
+                msmt.measure(p1_get)
 
 
-def test_initialize_empty_dataset(create_dummy_database):
+def test_initialize_empty_dataset():
     from qcodes import Measurement
 
-    with create_dummy_database():
-        msmt = Measurement()
-      #   msmt.register_parameter(p1_set)
-      #   msmt.register_parameter(p1_get, setpoints=(p1_set,))
-        with msmt.run(allow_empty_dataset=True) as datasaver:
-           pass
+    msmt = Measurement()
+    #   msmt.register_parameter(p1_set)
+    #   msmt.register_parameter(p1_get, setpoints=(p1_set,))
+    with msmt.run(allow_empty_dataset=True) as datasaver:
+        pass
 
 
-def test_nested_measurement(create_dummy_database):
+def test_nested_measurement():
     def nested_measurement():
         # Initialize parameters
         p1_get = ManualParameter("p1_get")
@@ -212,15 +183,13 @@ def test_nested_measurement(create_dummy_database):
                 p1_get(val + 1)
                 msmt.measure(p1_get)
 
+    # Initialize parameters
+    p2_set = ManualParameter("p2_set")
 
-    with create_dummy_database():
-        # Initialize parameters
-        p2_set = ManualParameter("p2_set")
-
-        with MeasurementLoop("test") as msmt:
-            for val2 in Sweep(p2_set, 0, 1, 11):
-                assert p2_set() == val2
-                nested_measurement()
+    with MeasurementLoop("test") as msmt:
+        for val2 in Sweep(p2_set, 0, 1, 11):
+            assert p2_set() == val2
+            nested_measurement()
 
     data = msmt.dataset
     assert data.name == "test"
@@ -240,11 +209,10 @@ def test_nested_measurement(create_dummy_database):
     )
 
 
-def test_measurement_no_parameter(create_dummy_database):
-    with create_dummy_database():
-        with MeasurementLoop("test") as msmt:
-            for val in Sweep(np.linspace(0, 1, 11), 'p1_set', label='p1 label', unit='V'):
-                msmt.measure(val+1, name='p1_get')
+def test_measurement_no_parameter():
+    with MeasurementLoop("test") as msmt:
+        for val in Sweep(np.linspace(0, 1, 11), 'p1_set', label='p1 label', unit='V'):
+            msmt.measure(val+1, name='p1_get')
 
     data = msmt.dataset
     assert data.name == "test"
@@ -255,3 +223,11 @@ def test_measurement_no_parameter(create_dummy_database):
 
     assert np.allclose(data_arrays["p1_get"], np.linspace(1, 2, 11))
     assert np.allclose(data_arrays["p1_set"], np.linspace(0, 1, 11))
+
+
+# def test_measurement_percentage_complete():
+#     with MeasurementLoop("test") as msmt:
+#         for val in Sweep(np.linspace(0, 1, 11), 'p1_set'):
+#             print(msmt.percentage_complete())
+#             msmt.measure(val+1, name='p1_get')
+#             print(msmt.percentage_complete())
