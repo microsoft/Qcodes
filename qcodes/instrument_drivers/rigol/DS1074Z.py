00267a44d5dffd1c8d617ab204a407c08c5f0dbb
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-08-21 14:32:29 +0200

    add alias for DS1074Z

---
+++ qcodes/instrument_drivers/rigol/DS1074Z.py
@@ -0,0 +1,9 @@
+"""
+Old alias for RigolDS1074Z for backwards compatibility.
+Will eventually be deprecated and removed.
+"""
+from .RigolDS1074Z import RigolDS1074Z, RigolDS1074ZChannel
+
+
+class DS1074Z(RigolDS1074Z):
+    pass
