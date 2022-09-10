65ff041e6d4ff1f4455481f61034cc0fa11d72de
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-09-10 10:50:03 +0200

    Exclude QDev from old api docs

--- docs/make.bat
+++ docs/make.bat
@@ -47,6 +47,7 @@ sphinx-apidoc  -o  _auto  -d 10 ..\qcodes ^
     ..\qcodes\instrument_drivers\Keithley ^
     ..\qcodes\instrument_drivers\Lakeshore ^
     ..\qcodes\instrument_drivers\QuantumDesign\DynaCoolPPMS\private\* ^
+    ..\qcodes\instrument_drivers\QDev\* ^
     ..\qcodes\instrument_drivers\rigol\* ^
     ..\qcodes\instrument_drivers\stahl\* ^
     ..\qcodes\instrument_drivers\stanford_research\* ^
