6654bd9662cdae4a545e6cf28be03454907ea68f
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-09-12 10:49:37 +0200

    Add keysight 344xx driver to public api

--- qcodes/instrument_drivers/Keysight/Keysight_34465A_submodules.py
+++ qcodes/instrument_drivers/Keysight/Keysight_34465A_submodules.py
@@ -3,10 +3,16 @@ from typing import Any
 from .private.Keysight_344xxA_submodules import _Keysight_344xxA
 
 
-class Keysight_34465A(_Keysight_344xxA):
+class Keysight34465A(_Keysight_344xxA):
     """
     This is the qcodes driver for the Keysight 34465A Multimeter
     """
     def __init__(self, name: str, address: str, silent: bool = False,
                  **kwargs: Any):
         super().__init__(name, address, silent, **kwargs)
+
+
+class Keysight_34465A(Keysight34465A):
+    """
+    Alias for backwards compatibility.
+    """
--- qcodes/instrument_drivers/Keysight/__init__.py
+++ qcodes/instrument_drivers/Keysight/__init__.py
@@ -1,1 +1,15 @@
-# Intentionally left blank
+from .Keysight_34410A_submodules import Keysight34410A
+from .Keysight_34411A_submodules import Keysight34411A
+from .Keysight_34460A_submodules import Keysight34460A
+from .Keysight_34461A_submodules import Keysight34461A
+from .Keysight_34465A_submodules import Keysight34465A
+from .Keysight_34470A_submodules import Keysight34470A
+
+__all__ = [
+    "Keysight34410A",
+    "Keysight34411A",
+    "Keysight34460A",
+    "Keysight34461A",
+    "Keysight34465A",
+    "Keysight34470A",
+]
