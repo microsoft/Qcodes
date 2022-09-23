2005af5a9983076e6b3467079d50edf4eaf647c5
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-09-23 09:42:58 +0200

    Add N5245A to public api

--- qcodes/instrument_drivers/Keysight/N5245A.py
+++ qcodes/instrument_drivers/Keysight/N5245A.py
@@ -1,19 +1,9 @@
 from typing import Any
 
-from . import N52xx
+from .Keysight_N5245A import KeysightN5245A
 
 
-class N5245A(N52xx.PNAxBase):
-    def __init__(self, name: str, address: str, **kwargs: Any):
-        super().__init__(name, address,
-                         min_freq=10e6, max_freq=50e9,
-                         min_power=-30, max_power=13,
-                         nports=4,
-                         **kwargs)
-
-        attenuators_options = {'219', '419'}
-        options = set(self.get_options())
-        if attenuators_options.intersection(options):
-            self._set_power_limits(min_power=-90, max_power=13)
-        if "080" in options:
-            self._enable_fom()
+class N5245A(KeysightN5245A):
+    """
+    Alias for backwards compatibility
+    """
