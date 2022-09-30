ba22bb0381f2ae13a3809d2fade9e6744552b4b9
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-09-30 14:35:31 +0200

    Remove deprecated import of loop at the top level of qcodes

--- qcodes/__init__.py
+++ qcodes/__init__.py
@@ -80,42 +80,11 @@ if config.core.import_legacy_api:
     from qcodes.utils import QCoDeSDeprecationWarning
 
     warnings.warn(
-        "import_legacy_api config option is deprecated and will be removed "
-        "in a future release please update your imports to import these "
-        "modules directly.",
+        "import_legacy_api config option has no effect "
+        "and will be removed in the future. "
+        "Please avoid setting this in your `qcodesrc.json` config file.",
         QCoDeSDeprecationWarning,
     )
-
-    plotlib = config.gui.plotlib
-    if plotlib in {"QT", "all"}:
-        try:
-            from qcodes.plots.pyqtgraph import QtPlot
-        except Exception:
-            print(
-                "pyqtgraph plotting not supported, "
-                'try "from qcodes.plots.pyqtgraph import QtPlot" '
-                "to see the full error"
-            )
-
-    if plotlib in {"matplotlib", "all"}:
-        try:
-            from qcodes.plots.qcmatplotlib import MatPlot
-        except Exception:
-            print(
-                "matplotlib plotting not supported, "
-                'try "from qcodes.plots.qcmatplotlib import MatPlot" '
-                "to see the full error"
-            )
-    from qcodes.actions import BreakIf, Task, Wait
-    from qcodes.data.data_array import DataArray
-    from qcodes.data.data_set import DataSet, load_data, new_data
-    from qcodes.data.format import Formatter
-    from qcodes.data.gnuplot_format import GNUPlotFormat
-    from qcodes.data.hdf5_format import HDF5Format
-    from qcodes.data.io import DiskIO
-    from qcodes.data.location import FormatLocation
-    from qcodes.loops import Loop, active_data_set, active_loop
-    from qcodes.measure import Measure
 
 
 try:
