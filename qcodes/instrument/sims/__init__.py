395590ea51da2a5754ba237bdaa8fa41da2bc243
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-11-04 13:24:16 +0100

    Add function to return traversable obj for sims yaml

--- qcodes/instrument/sims/__init__.py
+++ qcodes/instrument/sims/__init__.py
@@ -1,1 +1,13 @@
-# :)
+import sys
+from importlib.abc import Traversable
+
+if sys.version_info >= (3, 9):
+    from importlib.resources import as_file, files
+else:
+    from importlib_resources import as_file, files
+
+
+def get_sim_file_path(filename: str) -> Traversable:
+
+    file = files("qcodes.instrument.sims") / filename
+    return file
