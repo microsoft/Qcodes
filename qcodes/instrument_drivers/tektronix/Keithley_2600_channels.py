10317266507b91cead899893de47782458f2088a
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-11-18 15:31:05 +0100

    avid using non reexported StrEnum with import

--- qcodes/instrument_drivers/tektronix/Keithley_2600_channels.py
+++ qcodes/instrument_drivers/tektronix/Keithley_2600_channels.py
@@ -3,19 +3,27 @@ Alias left for backwards compatibility.
 Keithly drivers have moved to the Keithley module
 """
 import logging
+import sys
+from enum import Enum
 
 from qcodes.instrument_drivers.Keithley._Keithley_2600 import (
     Keithley2600,
     Keithley2600Channel,
     LuaSweepParameter,
     MeasurementStatus,
-    StrEnum,
     TimeAxis,
     TimeTrace,
     _MeasurementCurrentParameter,
     _MeasurementVoltageParameter,
     _ParameterWithStatus,
 )
+
+if sys.version_info >= (3, 11):
+    from enum import StrEnum
+else:
+
+    class StrEnum(str, Enum):
+        pass
 
 log = logging.getLogger(__name__)
 
