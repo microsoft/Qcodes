95dd538cda2e3513d9b1a76bfc0b51aa8e1fa45f
Jens H. Nielsen <jenshnielsen@gmail.com>
2023-01-08 15:56:10 +0100

    wip add test

--- qcodes/tests/dataset/test_dataset_export.py
+++ qcodes/tests/dataset/test_dataset_export.py
@@ -1,12 +1,13 @@
 import json
 import logging
 import os
+from pathlib import Path
 
 import pytest
 import xarray as xr
 
 import qcodes
-from qcodes.dataset import load_from_netcdf, new_data_set
+from qcodes.dataset import get_data_export_path, load_from_netcdf, new_data_set
 from qcodes.dataset.descriptions.dependencies import InterDependencies_
 from qcodes.dataset.descriptions.param_spec import ParamSpecBase
 from qcodes.dataset.descriptions.versioning import serialization as serial
@@ -238,6 +239,21 @@ def test_export_netcdf(tmp_path_factory, mock_dataset, caplog):
         assert ds.attrs[attr] == val
 
     assert mock_dataset.export_info.export_paths["nc"] == file_path
+
+
+def test_export_netcdf_default_dir(tmp_path_factory, mock_dataset):
+    mock_dataset.export(export_type="netcdf", prefix="qcodes_")
+
+    assert qcodes.config.dataset.export_path == "{db_location}"
+    export_path = Path(mock_dataset.export_info.export_paths["nc"])
+    exported_dir = export_path.parent
+    export_dir_stem = exported_dir.stem
+    database_path = Path(qcodes.config.core.db_location)
+    database_file_name = database_path.name
+    database_dir = Path(qcodes.config.core.db_location).parent
+    assert exported_dir.parent == database_dir
+    assert export_dir_stem == database_file_name.replace(".", "_")
+    assert exported_dir == get_data_export_path()
 
 
 def test_export_netcdf_csv(tmp_path_factory, mock_dataset):
