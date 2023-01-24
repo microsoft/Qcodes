3482d83e4a3e315fff3f3b072412c2e36f6ff1b6
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2023-01-24 09:47:17 +0100

    Add Minicircuits RC SP4T to public api

--- qcodes/instrument_drivers/Minicircuits/__init__.py
+++ qcodes/instrument_drivers/Minicircuits/__init__.py
@@ -1,3 +1,4 @@
+from ._minicircuits_rc_sp4t import MiniCircuitsRCSP4T, MiniCircuitsRCSP4TChannel
 from .RUDAT_13G_90 import MiniCircuitsRudat13G90Usb
 from .USB_SPDT import MiniCircuitsSwitchChannelUsb, MiniCircuitsUsbSPDT
 
