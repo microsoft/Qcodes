ff385d54f106618adbf3b3342e2219209453a78c
Jonathan Becker <jonbecker@microsoft.com>
2023-03-01 15:21:17 +0100

    typing

--- qcodes/instrument_drivers/Lakeshore/Lakeshore_model_335.py
+++ qcodes/instrument_drivers/Lakeshore/Lakeshore_model_335.py
@@ -17,13 +17,35 @@ from .Lakeshore_model_336 import (
 _channel_name_to_command_map: Dict[str, str] = {'A': 'A',
                                                 'B': 'B'}
 
+class LakeshoreModel335Channel(LakeshoreModel336Channel):
+    """
+    An InstrumentChannel representing a single sensor on a Lakeshore Model 335.
+
+    """
+    def __init__(self, parent: "LakeshoreModel335", name: str, channel: str):
+        super().__init__(parent, name, channel)
+
+
+class LakeshoreModel335CurrentSource(LakeshoreModel336CurrentSource):
+    """
+    InstrumentChannel for current sources on Lakeshore Model 336.
+
+    Class for control outputs 1 and 2 of Lakeshore Model 336 that are variable DC current
+    sources referenced to chassis ground.
+    """
+    def __init__(
+        self, parent: "LakeshoreModel335", output_name: str, output_index: int
+    ):
+        super().__init__(parent, output_name, output_index, has_pid=True)
+
+
 class LakeshoreModel335(LakeshoreBase):
     """
     Lakeshore Model 335 Temperature Controller Driver
     """
     channel_name_command: Dict[str, str] = _channel_name_to_command_map
 
-    CHANNEL_CLASS = LakeshoreModel336Channel
+    CHANNEL_CLASS = LakeshoreModel335Channel
 
     input_channel_parameter_values_to_channel_name_on_instrument = \
         _channel_name_to_command_map
@@ -36,5 +58,5 @@ class LakeshoreModel335(LakeshoreBase):
             self.visa_handle.data_bits = 7
             self.visa_handle.parity = visa.constants.Parity(1)
 
-        self.output_1 = LakeshoreModel336CurrentSource(self, 'output_1', 1)
-        self.output_2 = LakeshoreModel336CurrentSource(self, 'output_2', 2)
+        self.output_1 = LakeshoreModel335CurrentSource(self, 'output_1', 1)
+        self.output_2 = LakeshoreModel335CurrentSource(self, 'output_2', 2)
