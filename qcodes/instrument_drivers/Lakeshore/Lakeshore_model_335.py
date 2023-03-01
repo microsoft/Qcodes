7a0eedc7c1d5adbd63ac4ddcb948b22b08c69010
Jonathan Becker <jonbecker@microsoft.com>
2023-03-01 14:51:24 +0100

    don't use legacy format

--- qcodes/instrument_drivers/Lakeshore/Lakeshore_model_335.py
+++ qcodes/instrument_drivers/Lakeshore/Lakeshore_model_335.py
@@ -5,10 +5,7 @@ import qcodes.utils.validators as vals
 from qcodes.instrument.group_parameter import GroupParameter, Group
 
 from .lakeshore_base import LakeshoreBase, BaseOutput, BaseSensorChannel
-from .Lakeshore_model_336 import LakeshoreModel336Channel as Model_336_Channel
-from .Lakeshore_model_336 import (
-    LakeshoreModel336CurrentSource as Output_336_CurrentSource,
-)
+from .Lakeshore_model_336 import LakeshoreModel336Channel,LakeshoreModel336CurrentSource
 
 # There are 4 sensors channels (a.k.a. measurement inputs) in Model 336.
 # Unlike other Lakeshore models, Model 336 refers to the channels using
@@ -22,7 +19,7 @@ class LakeshoreModel335(LakeshoreBase):
     """
     channel_name_command: Dict[str, str] = _channel_name_to_command_map
 
-    CHANNEL_CLASS = Model_336_Channel
+    CHANNEL_CLASS = LakeshoreModel336Channel
 
     input_channel_parameter_values_to_channel_name_on_instrument = \
         _channel_name_to_command_map
@@ -35,5 +32,5 @@ class LakeshoreModel335(LakeshoreBase):
             self.visa_handle.data_bits = 7
             self.visa_handle.parity = visa.constants.Parity(1)
 
-        self.output_1 = Output_336_CurrentSource(self, 'output_1', 1)
-        self.output_2 = Output_336_CurrentSource(self, 'output_2', 2)
+        self.output_1 = LakeshoreModel336CurrentSource(self, 'output_1', 1)
+        self.output_2 = LakeshoreModel336CurrentSource(self, 'output_2', 2)
