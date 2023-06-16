b6cb8cd09f3519373fae384cbfdad267c1b118a0
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2023-06-16 15:02:03 +0200

    write metadata to db use set database if not overwritten

--- qcodes/dataset/data_set_in_memory.py
+++ qcodes/dataset/data_set_in_memory.py
@@ -138,6 +138,9 @@ class DataSetInMem(BaseDataSet):
     def write_metadata_to_db(self, path_to_db: str | Path | None = None) -> None:
         from .experiment_container import load_or_create_experiment
 
+        if path_to_db is None:
+            path_to_db = self.path_to_db
+
         if self._dataset_is_in_runs_table(path_to_db=path_to_db):
             return
         if isinstance(path_to_db, Path):
