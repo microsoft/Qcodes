8a5edc506478d40fdee527f02e707dbc1c32eaf9
Jens Hedegaard Nielsen <Jens.Nielsen@microsoft.com>
2023-09-04 10:14:49 +0200

    Update qcodes/tests/test_visa.py
    Co-authored-by: Mikhail Astafev <astafan8@gmail.com>

--- qcodes/tests/test_visa.py
+++ qcodes/tests/test_visa.py
@@ -129,7 +129,7 @@ def test_visa_gc_closes_connection(caplog) -> None:
         use_magnet()
         gc.collect()
     # at this stage the instrument created in use_magnet has gone out of scope
-    # and we have triggered an explicit gc so the finalize method has been triggered/
+    # and we have triggered an explicit gc so the weakref.finalize function has been triggered
     # We test this
     # and the instrument should no longer be in the instrument registry
     assert list(Instrument._all_instruments.keys()) == []
