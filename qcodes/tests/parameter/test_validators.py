1e95be1c33ce49dae00cf6cd258440c937a84485
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2023-10-19 14:39:47 +0200

    Add failing test.

--- qcodes/tests/parameter/test_validators.py
+++ qcodes/tests/parameter/test_validators.py
@@ -3,7 +3,7 @@ import pytest
 from hypothesis import given
 
 from qcodes.parameters import Parameter
-from qcodes.validators import Ints
+from qcodes.validators import Ints, Numbers
 
 
 def test_add_ints_validator_to_parameter():
@@ -187,3 +187,10 @@ def test_replace_vals():
     p.remove_validator()
     p.vals = None
     assert len(p.validators) == 0
+
+
+def test_validators_step_int() -> None:
+    # this parameter should allow step to be set as a float since the parameter is in it self a float
+    param = Parameter("a", get_cmd=False, set_cmd=False, vals=Numbers(0, 10))
+
+    param.step = 0.1
