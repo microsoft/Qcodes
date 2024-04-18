999d26a95334bbee8c62e33e5e290d2bdfc40243
Trevor Morgan <trmorgan@microsoft.com>
2024-04-18 01:52:11 -0700

    formatting

--- src/qcodes/instrument_drivers/cryomagnetics/Cryomagnetics4G.py
+++ src/qcodes/instrument_drivers/cryomagnetics/Cryomagnetics4G.py
@@ -9,10 +9,10 @@ from qcodes.validators import Enum, Numbers
 @dataclass
 class CryomagneticsOperatingState:
     ramping: bool = False
-    holding : bool = False
+    holding: bool = False
     standby: bool = False
     quench_condition_present: bool = False
-    power_module_failure:  bool = False
+    power_module_failure: bool = False
 
     def can_start_ramping(self) -> bool:
         required_checks = [
@@ -51,19 +51,20 @@ class CryomagneticsModel4G(VisaInstrument):
         self._initialize_max_current_limits()
 
         # Adding parameters
-        self.add_parameter(name='units',
-                           set_cmd='UNITS {}',
-                           get_cmd='UNITS?',
-                           get_parser=str,
-                            vals=Enum('A', 'kG','T'),
-                           docstring="Field Units"
-                           )
+        self.add_parameter(
+            name="units",
+            set_cmd="UNITS {}",
+            get_cmd="UNITS?",
+            get_parser=str,
+            vals=Enum("A", "kG", "T"),
+            docstring="Field Units",
+        )
 
         self.add_parameter(
             "ramping_state_check_interval",
             initial_value=0.05,
             unit="s",
-            vals= Numbers(0, 10),
+            vals=Numbers(0, 10),
             set_cmd=None,
         )
 
@@ -77,13 +78,14 @@ class CryomagneticsModel4G(VisaInstrument):
             docstring="Magnetic Field in Tesla",
         )
 
-        self.add_parameter(name='rate',
-                           unit="T/min",
-                           get_cmd=self ._get_rate,
-                           set_cmd=self._set_rate,
-                           get_parser=float,
-                           docstring="Rate for magnetic field T/min"
-                           )
+        self.add_parameter(
+            name="rate",
+            unit="T/min",
+            get_cmd=self._get_rate,
+            set_cmd=self._set_rate,
+            get_parser=float,
+            docstring="Rate for magnetic field T/min",
+        )
 
         self.add_parameter(
             name="Vmag",
@@ -94,13 +96,14 @@ class CryomagneticsModel4G(VisaInstrument):
             docstring="Magnet sense voltage",
         )
 
-        self.add_parameter(name='Vout',
-                           unit="V",
-                           get_cmd ='VOUT?',
-                           get_parser=float,
-                           vals=Numbers(-12.8, 12.8),
-                           docstring="Magnet output voltage"
-                           )
+        self.add_parameter(
+            name="Vout",
+            unit="V",
+            get_cmd="VOUT?",
+            get_parser=float,
+            vals=Numbers(-12.8, 12.8),
+            docstring="Magnet output voltage",
+        )
 
         self.add_parameter(
             name="Iout",
@@ -111,15 +114,15 @@ class CryomagneticsModel4G(VisaInstrument):
         )
 
         # Add function to reset quench
-        self.add_function('QReset', call_cmd='QRESET')
+        self.add_function("QReset", call_cmd="QRESET")
         # Add function to set to remote  mode
-        self.add_function('remote', call_cmd='REMOTE')
+        self.add_function("remote", call_cmd="REMOTE")
         # Non-blocking ramping field to 0 function
-        self.add_function('off', call_cmd='SWEEP ZERO')
+        self.add_function("off", call_cmd="SWEEP ZERO")
         # Set to remote mode
         self.remote()
         #  Set units to tesla by default
-        self.units('T')
+        self.units("T")
         self.connect_message()
 
     def magnet_operating_state(self) -> CryomagneticsOperatingState:
@@ -175,7 +178,6 @@ class CryomagneticsModel4G(VisaInstrument):
 
         return operating_state
 
-
     def set_field(self, field_setpoint: float, block: bool = True) -> None:
         """
         Sets the magnetic field strength in Tesla using ULIM, LLIM, and SWEEP commands.
@@ -201,7 +203,7 @@ class CryomagneticsModel4G(VisaInstrument):
 
         # Check if we can start ramping
         try:
-             state = self.magnet_operating_state()
+            state = self.magnet_operating_state()
         except Cryomagnetics4GException as e:
             self.log.error(f"Cannot set field: {e}")  # Log the specific error
             return
@@ -224,7 +226,9 @@ class CryomagneticsModel4G(VisaInstrument):
                 return
 
             # Otherwise, wait until no longer ramping
-            self.log.debug(f"Starting blocking ramp of {self.name} to {field_setpoint} T")
+            self.log.debug(
+                f"Starting blocking ramp of {self.name} to {field_setpoint} T"
+            )
             exit_state = self.wait_while_ramping(field_setpoint)
             self.log.debug("Finished blocking ramp")
             # If we are now holding, it was successful
@@ -233,11 +237,12 @@ class CryomagneticsModel4G(VisaInstrument):
                 msg = "_set_field({}) failed with state: {}"
                 raise Cryomagnetics4GException(msg.format(field_setpoint, exit_state))
 
-
-    def  wait_while_ramping(self, value: float, threshold: float = 1e-5) -> CryomagneticsOperatingState:
+    def wait_while_ramping(
+        self, value: float, threshold: float = 1e-5
+    ) -> CryomagneticsOperatingState:
         """Waits while the magnet is ramping, checking the status byte instead of field value."""
         while True:
-            status_byte = int( self.ask("*STB?"))
+            status_byte = int(self.ask("*STB?"))
             if not bool(status_byte & 1):  # Check if ramping bit is clear
                 break
             self._sleep(self.ramping_state_check_interval())
@@ -245,12 +250,11 @@ class CryomagneticsModel4G(VisaInstrument):
         self._sleep(1.0)
         return self.magnet_operating_state()
 
-
     def _sleep(self, t: float) -> None:
         """
         Sleep for a number of seconds t. If we are or using
         the PyVISA 'sim' backend, omit this
-         """
+        """
 
         simmode = getattr(self, "visabackend", False) == "sim"
 
@@ -259,14 +263,15 @@ class CryomagneticsModel4G(VisaInstrument):
         else:
             time.sleep(t)
 
-
     def _get_field(self) -> float:
 
         current_value = self.ask("IMAG?")
         # Define a regular expression to match the floating point number and the unit
-        match = re.match(r"^([-+]?[0-9]*\.?[0-9]+)\s*([a-zA-Z]+)$", current_value.strip())
+        match = re.match(
+            r"^([-+]?[0-9]*\.?[0-9]+)\s*([a-zA-Z]+)$", current_value.strip()
+        )
 
-        if  not match:
+        if not match:
             raise ValueError(f"Invalid format for measurement: '{current_value}'")
 
         raw_value, unit = match.groups()
@@ -275,13 +280,13 @@ class CryomagneticsModel4G(VisaInstrument):
         try:
             numeric_value = float(raw_value)
         except ValueError:
-             raise ValueError(f"Unable to convert '{raw_value}' to float")
+            raise ValueError(f"Unable to convert '{raw_value}' to float")
 
         # Validate the unit part
         if unit != "kG":
             raise ValueError(f"Unexpected unit '{unit}'. Expected 'kG'")
         if self.units() == "A":
-            raise  ValueError(
+            raise ValueError(
                 "Current units are set to Amperes (A). Cannot retrieve magnetic field in these units."
             )
 
@@ -296,7 +301,7 @@ class CryomagneticsModel4G(VisaInstrument):
         Get the current ramp rate in Tesla per minute.
         """
         # Get the rate from the instrument in Amps per second
-        rate_amps_per_sec  = float(self.ask("RATE?"))
+        rate_amps_per_sec = float(self.ask("RATE?"))
         # Convert to Tesla per minute
         rate_tesla_per_min = rate_amps_per_sec * 60 / self.coil_constant
         return rate_tesla_per_min
@@ -314,14 +319,15 @@ class CryomagneticsModel4G(VisaInstrument):
         # (Implement a  more efficient lookup method here if needed)
         for range_index, (upper_limit, max_rate) in self.max_current_limits.items():
             if current_in_amps <= upper_limit:
-                actual_rate = min(rate_amps_per_sec,  max_rate)  # Ensure rate doesn't exceed maximum
+                actual_rate = min(
+                    rate_amps_per_sec, max_rate
+                )  # Ensure rate doesn't exceed maximum
                 self.write(f"RATE {range_index} {actual_rate}")
                 return
 
         raise ValueError("Current field is outside of defined rate ranges")
 
-
-    def _initialize_max_current_limits(self) ->  None:
+    def _initialize_max_current_limits(self) -> None:
         """
         Initialize the instrument with the provided current limits and rates.
         """
