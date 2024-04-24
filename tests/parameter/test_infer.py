9deb55c89094ff972e2644b01d7d2a9f4f3efcb6
Samantha Ho <samanthaho@microsoft.com>
2024-04-24 08:11:46 -0700

    Augmenting tests for code coverage

--- tests/parameter/test_infer.py
+++ tests/parameter/test_infer.py
@@ -2,14 +2,14 @@ from __future__ import annotations
 
 from typing import Any
 
-import numpy as np
 import pytest
 
-from qcodes.instrument import Instrument, InstrumentModule
+from qcodes.instrument import Instrument, InstrumentBase, InstrumentModule
 from qcodes.parameters import DelegateParameter, ManualParameter, Parameter
 from qcodes.parameters.infer import (
     InferAttrs,
     InferError,
+    _merge_user_and_class_attrs,
     get_parameter_chain,
     get_root_parameter,
     infer_channel,
@@ -36,17 +36,18 @@ class DummyInstrument(Instrument):
         self.module = DummyModule(name="module", parent=self)
 
 
-class DummyDelegateInstrument(Instrument):
+class DummyDelegateInstrument(InstrumentBase):
     def __init__(self, name: str):
         super().__init__(name=name)
         self.inst_delegate = DelegateParameter(
             name="inst_delegate", source=None, instrument=self, bind_to_instrument=True
         )
         self.module = DummyDelegateModule(name="dummy_delegate_module", parent=self)
+        self.inst_base_parameter = ManualParameter("inst_base_parameter")
 
 
 class DummyDelegateModule(InstrumentModule):
-    def __init__(self, name: str, parent: Instrument):
+    def __init__(self, name: str, parent: InstrumentBase):
         super().__init__(name=name, parent=parent)
         self.chan_delegate = DelegateParameter(
             name="chan_delegate", source=None, instrument=self, bind_to_instrument=True
@@ -108,8 +109,9 @@ def test_get_root_parameter_no_source(good_inst_delegates):
 
     good_inst_del_1.source = None
 
-    with pytest.raises(InferError):
+    with pytest.raises(InferError) as exc_info:
         get_root_parameter(good_inst_del_2)
+    assert "is not attached to a source" in str(exc_info.value)
 
 
 def test_get_root_parameter_no_user_attr(good_inst_delegates):
@@ -121,8 +123,9 @@ def test_get_root_parameter_no_user_attr(good_inst_delegates):
 def test_get_root_parameter_none_user_attr(good_inst_delegates):
     _, _, good_inst_del_3 = good_inst_delegates
     good_inst_del_3.linked_parameter = None
-    with pytest.raises(InferError):
+    with pytest.raises(InferError) as exc_info:
         get_root_parameter(good_inst_del_3, "linked_parameter")
+    assert "is not attached to a source on attribute" in str(exc_info.value)
 
 
 def test_infer_instrument_valid(instrument_fixture, good_inst_delegates):
@@ -137,8 +140,16 @@ def test_infer_instrument_no_instrument(instrument_fixture):
     no_inst_delegate = DelegateParameter(
         "no_inst_delegate", source=inst.bad_inst_parameter
     )
-    with pytest.raises(InferError):
+    with pytest.raises(InferError) as exc_info:
         infer_instrument(no_inst_delegate)
+    assert "has no instrument" in str(exc_info.value)
+
+
+def test_infer_instrument_root_instrument_base():
+    delegate_inst = DummyDelegateInstrument("dummy_delegate_instrument")
+
+    with pytest.raises(InferError):
+        infer_instrument(delegate_inst.inst_base_parameter)
 
 
 def test_infer_channel_valid(instrument_fixture):
@@ -154,39 +165,45 @@ def test_infer_channel_no_channel(instrument_fixture):
     no_chan_delegate = DelegateParameter(
         "no_chan_delegate", source=inst.module.bad_chan_parameter
     )
-    with pytest.raises(InferError):
-        infer_instrument(no_chan_delegate)
+    with pytest.raises(InferError) as exc_info:
+        infer_channel(no_chan_delegate)
+    assert "has no instrument" in str(exc_info.value)
+
+    inst_but_not_chan_delegate = DelegateParameter(
+        "inst_but_not_chan_delegate", source=inst.good_inst_parameter
+    )
+    with pytest.raises(InferError) as exc_info:
+        infer_channel(inst_but_not_chan_delegate)
+    assert "Could not determine a root instrument channel" in str(exc_info.value)
 
 
 def test_get_parameter_chain(instrument_fixture, good_inst_delegates):
     inst = instrument_fixture
     good_inst_del_1, good_inst_del_2, good_inst_del_3 = good_inst_delegates
     parameter_chain = get_parameter_chain(good_inst_del_3, "linked_parameter")
-    assert np.all(
-        [
-            param in parameter_chain
-            for param in (
-                inst.good_inst_parameter,
-                good_inst_del_1,
-                good_inst_del_2,
-                good_inst_del_3,
-            )
-        ]
-    )
+    assert set(
+        (
+            inst.good_inst_parameter,
+            good_inst_del_1,
+            good_inst_del_2,
+            good_inst_del_3,
+        )
+    ) == set(parameter_chain)
 
     # This is a broken chain. get_root_parameter would throw an InferError, but
     # get_parameter_chain should run successfully
     good_inst_del_1.source = None
     parameter_chain = get_parameter_chain(good_inst_del_3, "linked_parameter")
-    assert np.all(
-        [
-            param in parameter_chain
-            for param in (
-                good_inst_del_1,
-                good_inst_del_2,
-                good_inst_del_3,
-            )
-        ]
+    assert set((good_inst_del_1, good_inst_del_2, good_inst_del_3)) == set(
+        parameter_chain
+    )
+
+    # Make the linked_parameter at the end of the chain
+    good_inst_del_3.linked_parameter = None
+    good_inst_del_1.source = good_inst_del_3
+    parameter_chain = get_parameter_chain(good_inst_del_2, "linked_parameter")
+    assert set((good_inst_del_1, good_inst_del_2, good_inst_del_3)) == set(
+        parameter_chain
     )
 
 
@@ -201,3 +218,9 @@ def test_parameters_on_delegate_instruments(instrument_fixture, good_inst_delega
     assert infer_channel(delegate_inst.module.chan_delegate) is inst.module
     assert infer_instrument(delegate_inst.module.chan_delegate) is inst
     assert infer_instrument(delegate_inst.inst_delegate) is inst
+
+
+def test_merge_user_and_class_attrs():
+    InferAttrs.add_attr("attr1")
+    attr_set = _merge_user_and_class_attrs("attr2")
+    assert set(("attr1", "attr2")) == attr_set
