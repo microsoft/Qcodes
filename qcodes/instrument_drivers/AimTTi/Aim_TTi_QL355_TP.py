8fc48519ff4ca8ed3b4b36bad346673fde7c10f3
Mikhail Astafev <astafan8@gmail.com>
2024-04-29 14:19:19 +0200

    add types

--- qcodes/instrument_drivers/AimTTi/Aim_TTi_QL355_TP.py
+++ qcodes/instrument_drivers/AimTTi/Aim_TTi_QL355_TP.py
@@ -1,6 +1,8 @@
-from typing import Any
+from typing import Any, Callable
 
-from ._AimTTi_PL_P import AimTTi
+from qcodes.parameters import Parameter
+
+from ._AimTTi_PL_P import AimTTi, AimTTiChannel
 
 
 class AimTTiQL355TP(AimTTi):
@@ -18,25 +20,27 @@ class AimTTiQL355TP(AimTTi):
         super().__init__(name, address, **kwargs)
 
         for channel in [self.ch1, self.ch2]:
-            channel.add_parameter(
+            channel.over_voltage_protection = Parameter(
                 "over_voltage_protection",
                 get_cmd=self._get_value_reader(channel, "OVP"),
                 get_parser=float,
                 set_cmd=f"OVP{channel.channel} {{}}",
                 label="Over voltage protection",
                 unit="V",
+                instrument=channel,
             )
 
-            channel.add_parameter(
+            channel.over_current_protection = Parameter(
                 "over_current_protection",
                 get_cmd=self._get_value_reader(channel, "OCP"),
                 get_parser=float,
                 set_cmd=f"OCP{channel.channel} {{}}",
                 label="Over current protection",
                 unit="A",
+                instrument=channel,
             )
 
-    def _get_value_reader(self, channel, command):
+    def _get_value_reader(self, channel: "AimTTiChannel", command: str) -> Callable[[], float]:
         def _value_reader() -> float:
             channel_id = channel.channel
             _value = channel.ask_raw(f"{command}{channel_id}?")
@@ -44,6 +48,6 @@ class AimTTiQL355TP(AimTTi):
             return float(_value_split[1])
         return _value_reader
 
-    def trip_reset(self):
+    def trip_reset(self) -> None:
         """Clear all trip conditions on the device"""
         self.write("TRIPRST")
