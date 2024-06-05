3c0a1b302e8a2efed3f192975eeb77634d1b6e48
Jens H. Nielsen <jenshnielsen@gmail.com>
2024-06-05 11:59:31 +0200

    Run automatic refactor on qd driver

--- src/qcodes/instrument_drivers/QuantumDesign/DynaCoolPPMS/DynaCool.py
+++ src/qcodes/instrument_drivers/QuantumDesign/DynaCoolPPMS/DynaCool.py
@@ -21,6 +21,8 @@ from qcodes.instrument import VisaInstrument, VisaInstrumentKWArgs
 if TYPE_CHECKING:
     from typing_extensions import Unpack
 
+    from qcodes.parameters import Parameter
+
 
 class DynaCool(VisaInstrument):
     """
@@ -59,147 +61,186 @@ class DynaCool(VisaInstrument):
     ) -> None:
         super().__init__(name=name, address=address, **kwargs)
 
-        self.add_parameter('temperature',
-                           label='Temperature',
-                           unit='K',
-                           get_parser=partial(DynaCool._pick_one, 1, float),
-                           get_cmd='TEMP?')
+        self.temperature: Parameter = self.add_parameter(
+            "temperature",
+            label="Temperature",
+            unit="K",
+            get_parser=partial(DynaCool._pick_one, 1, float),
+            get_cmd="TEMP?",
+        )
+        """Parameter temperature"""
 
         # Note: from the Lyngby Materials Lab, we have been told that the
         # manual is wrong about the minimal temperature. The manual says
         # 1.8 K, but it is in fact 1.6 K
-        self.add_parameter('temperature_setpoint',
-                           label='Temperature setpoint',
-                           unit='K',
-                           vals=vals.Numbers(1.6, 400),
-                           set_cmd=partial(self._temp_setter,
-                                           'temperature_setpoint'),
-                           get_cmd=partial(self._temp_getter,
-                                           'temperature_setpoint'))
+        self.temperature_setpoint: Parameter = self.add_parameter(
+            "temperature_setpoint",
+            label="Temperature setpoint",
+            unit="K",
+            vals=vals.Numbers(1.6, 400),
+            set_cmd=partial(self._temp_setter, "temperature_setpoint"),
+            get_cmd=partial(self._temp_getter, "temperature_setpoint"),
+        )
+        """Parameter temperature_setpoint"""
 
-        self.add_parameter('temperature_rate',
-                           label='Temperature settle rate',
-                           unit='K/s',
-                           vals=vals.Numbers(0.0002, 0.3),
-                           set_parser=lambda x: x*60,  # conversion to K/min
-                           get_parser=lambda x: x/60,  # conversion to K/s
-                           set_cmd=partial(self._temp_setter,
-                                           'temperature_rate'),
-                           get_cmd=partial(self._temp_getter,
-                                           'temperature_rate'))
+        self.temperature_rate: Parameter = self.add_parameter(
+            "temperature_rate",
+            label="Temperature settle rate",
+            unit="K/s",
+            vals=vals.Numbers(0.0002, 0.3),
+            set_parser=lambda x: x * 60,  # conversion to K/min
+            get_parser=lambda x: x / 60,  # conversion to K/s
+            set_cmd=partial(self._temp_setter, "temperature_rate"),
+            get_cmd=partial(self._temp_getter, "temperature_rate"),
+        )
+        """Parameter temperature_rate"""
 
-        self.add_parameter('temperature_settling',
-                           label='Temperature settling mode',
-                           val_mapping={'fast settle': 0,
-                                        'no overshoot': 1},
-                           set_cmd=partial(self._temp_setter,
-                                           'temperature_settling'),
-                           get_cmd=partial(self._temp_getter,
-                                           'temperature_settling'))
+        self.temperature_settling: Parameter = self.add_parameter(
+            "temperature_settling",
+            label="Temperature settling mode",
+            val_mapping={"fast settle": 0, "no overshoot": 1},
+            set_cmd=partial(self._temp_setter, "temperature_settling"),
+            get_cmd=partial(self._temp_getter, "temperature_settling"),
+        )
+        """Parameter temperature_settling"""
 
-        self.add_parameter('temperature_state',
-                           label='Temperature tracking state',
-                           val_mapping={"tracking": 2,
-                                        'stable': 1,
-                                        'near': 5,
-                                        'chasing': 6,
-                                        'pot operation': 7,
-                                        'standby': 10,
-                                        'diagnostic': 13,
-                                        'impedance control error': 14,
-                                        'failure': 15},
-                           get_parser=partial(DynaCool._pick_one, 2, int),
-                           get_cmd='TEMP?')
+        self.temperature_state: Parameter = self.add_parameter(
+            "temperature_state",
+            label="Temperature tracking state",
+            val_mapping={
+                "tracking": 2,
+                "stable": 1,
+                "near": 5,
+                "chasing": 6,
+                "pot operation": 7,
+                "standby": 10,
+                "diagnostic": 13,
+                "impedance control error": 14,
+                "failure": 15,
+            },
+            get_parser=partial(DynaCool._pick_one, 2, int),
+            get_cmd="TEMP?",
+        )
+        """Parameter temperature_state"""
 
-        self.add_parameter('field_measured',
-                           label='Field',
-                           unit='T',
-                           get_cmd=self._measured_field_getter)
+        self.field_measured: Parameter = self.add_parameter(
+            "field_measured",
+            label="Field",
+            unit="T",
+            get_cmd=self._measured_field_getter,
+        )
+        """Parameter field_measured"""
 
-        self.add_parameter('field_target',
-                           label='Field target',
-                           unit='T',
-                           get_cmd=None,
-                           set_cmd=None,
-                           vals=vals.Numbers(-14, 14))
+        self.field_target: Parameter = self.add_parameter(
+            "field_target",
+            label="Field target",
+            unit="T",
+            get_cmd=None,
+            set_cmd=None,
+            vals=vals.Numbers(-14, 14),
+        )
+        """Parameter field_target"""
 
-        self.add_parameter('field_ramp',
-                           label='Field [ramp]',
-                           unit='T',
-                           get_cmd=None,
-                           set_cmd=self._field_ramp_setter,
-                           vals=vals.Numbers(-14, 14))
+        self.field_ramp: Parameter = self.add_parameter(
+            "field_ramp",
+            label="Field [ramp]",
+            unit="T",
+            get_cmd=None,
+            set_cmd=self._field_ramp_setter,
+            vals=vals.Numbers(-14, 14),
+        )
+        """Parameter field_ramp"""
 
-        self.add_parameter('field_rate',
-                           label='Field rate',
-                           unit='T/s',
-                           get_parser=lambda x: x*1e-4,  # Oe to T
-                           set_parser=lambda x: x*1e4,  # T to Oe
-                           set_cmd=None,
-                           get_cmd=None,
-                           initial_value=0,
-                           vals=vals.Numbers(0, 1))
+        self.field_rate: Parameter = self.add_parameter(
+            "field_rate",
+            label="Field rate",
+            unit="T/s",
+            get_parser=lambda x: x * 1e-4,  # Oe to T
+            set_parser=lambda x: x * 1e4,  # T to Oe
+            set_cmd=None,
+            get_cmd=None,
+            initial_value=0,
+            vals=vals.Numbers(0, 1),
+        )
+        """Parameter field_rate"""
 
-        self.add_parameter('field_approach',
-                           label='Field ramp approach',
-                           val_mapping={'linear': 0,
-                                        'no overshoot': 1,
-                                        'oscillate': 2},
-                           set_cmd=None,
-                           get_cmd=None,
-                           initial_value='linear')
+        self.field_approach: Parameter = self.add_parameter(
+            "field_approach",
+            label="Field ramp approach",
+            val_mapping={"linear": 0, "no overshoot": 1, "oscillate": 2},
+            set_cmd=None,
+            get_cmd=None,
+            initial_value="linear",
+        )
+        """Parameter field_approach"""
 
-        self.add_parameter('magnet_state',
-                           label='Magnet state',
-                           val_mapping={'unknown': 0,
-                                        'stable': 1,
-                                        'switch warming': 2,
-                                        'switch cool': 3,
-                                        'holding': 4,
-                                        'iterate': 5,
-                                        'ramping': 6,
-                                        'ramping ': 7,  # map must have inverse
-                                        'resetting': 8,
-                                        'current error': 9,
-                                        'switch error': 10,
-                                        'quenching': 11,
-                                        'charging error': 12,
-                                        'power supply error': 14,
-                                        'failure': 15},
-                           get_parser=partial(DynaCool._pick_one, 2, int),
-                           get_cmd='FELD?')
+        self.magnet_state: Parameter = self.add_parameter(
+            "magnet_state",
+            label="Magnet state",
+            val_mapping={
+                "unknown": 0,
+                "stable": 1,
+                "switch warming": 2,
+                "switch cool": 3,
+                "holding": 4,
+                "iterate": 5,
+                "ramping": 6,
+                "ramping ": 7,  # map must have inverse
+                "resetting": 8,
+                "current error": 9,
+                "switch error": 10,
+                "quenching": 11,
+                "charging error": 12,
+                "power supply error": 14,
+                "failure": 15,
+            },
+            get_parser=partial(DynaCool._pick_one, 2, int),
+            get_cmd="FELD?",
+        )
+        """Parameter magnet_state"""
 
-        self.add_parameter('chamber_temperature',
-                           label='Chamber Temperature',
-                           unit='K',
-                           get_parser=partial(DynaCool._pick_one, 1, float),
-                           get_cmd='CHAT?')
+        self.chamber_temperature: Parameter = self.add_parameter(
+            "chamber_temperature",
+            label="Chamber Temperature",
+            unit="K",
+            get_parser=partial(DynaCool._pick_one, 1, float),
+            get_cmd="CHAT?",
+        )
+        """Parameter chamber_temperature"""
 
-        self.add_parameter('chamber_state',
-                           label='Chamber vacuum state',
-                           val_mapping={'purged and sealed': 1,
-                                        'vented and sealed': 2,
-                                        'sealed': 3,
-                                        'performing purge/seal': 4,
-                                        'performing vent/seal': 5,
-                                        'pre-high vacuum': 6,
-                                        'high vacuum': 7,
-                                        'pumping continuously': 8,
-                                        'flooding continuously': 9},
-                           get_parser=partial(DynaCool._pick_one, 1, int),
-                           get_cmd='CHAM?')
+        self.chamber_state: Parameter = self.add_parameter(
+            "chamber_state",
+            label="Chamber vacuum state",
+            val_mapping={
+                "purged and sealed": 1,
+                "vented and sealed": 2,
+                "sealed": 3,
+                "performing purge/seal": 4,
+                "performing vent/seal": 5,
+                "pre-high vacuum": 6,
+                "high vacuum": 7,
+                "pumping continuously": 8,
+                "flooding continuously": 9,
+            },
+            get_parser=partial(DynaCool._pick_one, 1, int),
+            get_cmd="CHAM?",
+        )
+        """Parameter chamber_state"""
 
-        self.add_parameter('field_tolerance',
-                           label="Field Tolerance",
-                           unit='T',
-                           get_cmd=None,
-                           set_cmd=None,
-                           vals=vals.Numbers(0, 1e-2),
-                           set_parser=float,
-                           docstring="The tolerance below which fields are "
-                                     "considered identical in a "
-                                     "blocking ramp.",
-                           initial_value=5e-4)
+        self.field_tolerance: Parameter = self.add_parameter(
+            "field_tolerance",
+            label="Field Tolerance",
+            unit="T",
+            get_cmd=None,
+            set_cmd=None,
+            vals=vals.Numbers(0, 1e-2),
+            set_parser=float,
+            docstring="The tolerance below which fields are "
+            "considered identical in a "
+            "blocking ramp.",
+            initial_value=5e-4,
+        )
+        """The tolerance below which fields are considered identical in a blocking ramp."""
 
         # The error code of the latest command
         self._error_code = 0
