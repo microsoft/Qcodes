d0db8fa0285d4e1c995713b89a36ba48183305dc
Jens Hedegaard Nielsen <jenshnielsen@gmail.com>
2024-06-27 11:34:19 +0000

    Merge pull request #6174 from jenshnielsen/error_on_existing_attribute
    Error on setting parameter if this already exists as an attribute

--- src/qcodes/parameters/parameter_base.py
+++ src/qcodes/parameters/parameter_base.py
@@ -316,13 +316,28 @@ class ParameterBase(MetadatableWithName):
         self._abstract = abstract
 
         if instrument is not None and bind_to_instrument:
-            existing_parameter = instrument.parameters.get(name, None)
+            found_as_delegate = instrument.parameters.get(name, False)
+            # we allow properties since a pattern that has been seen in the wild
+            # is properties that are used to wrap parameters of the same name
+            # to define an interface for the instrument
+            is_property = isinstance(
+                getattr(instrument.__class__, name, None), property
+            )
+            found_as_attr = not is_property and hasattr(instrument, name)
 
-            if existing_parameter:
-                if not existing_parameter.abstract:
+            if found_as_delegate or found_as_attr:
+                existing_parameter = instrument.parameters.get(name, None)
+
+                if existing_parameter is not None and not existing_parameter.abstract:
                     raise KeyError(
                         f"Duplicate parameter name {name} on instrument {instrument}"
                     )
+                if existing_parameter is None:
+                    existing_attribute = getattr(instrument, name, None)
+                    if existing_attribute is not None:
+                        raise KeyError(
+                            f"Parameter {name} overrides an attribute of the same name on instrument {instrument}"
+                        )
 
             instrument.parameters[name] = self
 
