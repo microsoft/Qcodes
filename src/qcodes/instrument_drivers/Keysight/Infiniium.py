eeb965cd5e3f76edf26d47fc4581bea36c031a81
konosubakonoakua <ailike_meow@qq.com>
2024-08-13 16:43:35 +0800

    fix lint issues

--- src/qcodes/instrument_drivers/Keysight/Infiniium.py
+++ src/qcodes/instrument_drivers/Keysight/Infiniium.py
@@ -1,7 +1,7 @@
 import re
+from pathlib import Path
 from typing import TYPE_CHECKING, Any, ClassVar, Literal, Optional, Union
 
-from pathlib import Path
 
 import numpy as np
 from pyvisa import VisaIOError
@@ -1257,37 +1257,42 @@ class KeysightInfiniium(VisaInstrument):
             if timeout is not None:
                 self.visa_handle.timeout = old_timeout
 
-    def screenshot(self,
-                   path: Union[str, Path] = "./screenshot",
-                   with_time: bool = False,
-                   time_fmt: str = "%Y-%m-%d_%H-%M-%S",
-                   divider: str = "_") -> Optional[np.ndarray]:
+    def screenshot(
+        self,
+        path: Union[str, Path] = "./screenshot",
+        with_time: bool = False,
+        time_fmt: str = "%Y-%m-%d_%H-%M-%S",
+        divider: str = "_"
+    ) -> Optional[np.ndarray]:
         """ save screen to {path} with {image_type}: bmp, jpg, gif, tif, png
 
             return np.array if sucessfully saved, else return None
         """
-        from PIL.Image import open as pil_open
+        from datetime import datetime
         from io import BytesIO
-        from datetime import date, datetime
         from os.path import splitext
+
+        from PIL.Image import open as pil_open
 
         if isinstance(path, Path):
             path = str(path)
 
         time_str = datetime.now().strftime(time_fmt) if with_time else ""
         img_name, img_type = splitext(path)
-        img_path = f"{img_name}{divider if with_time else ''}{time_str}{img_type.lower()}"
+        img_path = (
+            f"{img_name}{divider if with_time else ''}{time_str}{img_type.lower()}"
+        )
         try:
-            with open(img_path, "wb") as f :
+            with open(img_path, "wb") as f:
                 screen_bytes = self.visa_handle.query_binary_values(
-                    f":DISPlay:DATA? {img_type.upper()[1:]}", # without .
+                    f":DISPlay:DATA? {img_type.upper()[1:]}",  # without .
                     # https://docs.python.org/3/library/struct.html#format-characters
-                    datatype='B', # Capitcal B for unsigned byte
-                    container=bytes
+                    datatype='B',  # Capitcal B for unsigned byte
+                    container=bytes,
                 )
-                f.write(screen_bytes)
+                f.write(screen_bytes) # type: ignore
             print(f"Screen image written to {img_path}")
-            return np.asarray(pil_open(BytesIO(screen_bytes)))
+            return np.asarray(pil_open(BytesIO(screen_bytes))) # type: ignore
         except Exception as e:
             self.log.error(f"Failed to save screenshot, Error occurred: \n{e}")
             return None
