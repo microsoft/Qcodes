780984c7956cdeed50879c5e956092aaebb7a96a
panasee <dongkai.pan@gmail.com>
2025-02-20 14:45:34 +0800

    pre-commit ruff-format changes

--- src/qcodes/instrument_drivers/oxford/MercuryiPS_VISA.py
+++ src/qcodes/instrument_drivers/oxford/MercuryiPS_VISA.py
@@ -64,6 +64,7 @@ def _signal_parser(our_scaling: float, response: str) -> float:
 
     return float(digits) * their_scaling * our_scaling
 
+
 def _temp_parser(response: str) -> float:
     """
     Parse a response string into a correct SI temperature value.
@@ -113,8 +114,8 @@ class OxfordMercuryWorkerPS(InstrumentChannel):
             self.psu_string = "PSU"
 
         self.heater_switch: Parameter = self.add_parameter(
-            'heater_switch',
-            label='Heater switch status/set',
+            "heater_switch",
+            label="Heater switch status/set",
             get_cmd=partial(self._param_getter, "SIG:SWHT"),
             set_cmd=partial(self._param_setter, "SIG:SWHT"),
             get_parser=_response_preparser,
@@ -358,37 +359,37 @@ class OxfordMercuryiPS(VisaInstrument):
             x=self.GRPX.field(), y=self.GRPY.field(), z=self.GRPZ.field()
         )
 
-        self._magnet_temp_addr = 'DEV:MB1.T1:TEMP'
-        self._pt1_temp_addr = 'DEV:DB8.T1:TEMP'
-        self._pt2_temp_addr = 'DEV:DB7.T1:TEMP'
+        self._magnet_temp_addr = "DEV:MB1.T1:TEMP"
+        self._pt1_temp_addr = "DEV:DB8.T1:TEMP"
+        self._pt2_temp_addr = "DEV:DB7.T1:TEMP"
 
         self.magnet_temp: Parameter = self.add_parameter(
-                           name='magnet_temp',
-                           label='Magnet Temperature',
-                           unit='K',
-                           docstring='Temperature of the magnet sensor',
-                           get_cmd="READ:" + self._magnet_temp_addr + ":SIG:TEMP?",
-                           get_parser=_temp_parser
-                           )
+            name="magnet_temp",
+            label="Magnet Temperature",
+            unit="K",
+            docstring="Temperature of the magnet sensor",
+            get_cmd="READ:" + self._magnet_temp_addr + ":SIG:TEMP?",
+            get_parser=_temp_parser,
+        )
         """Parameter magnet temperature"""
 
         self.pt1_temp: Parameter = self.add_parameter(
-                           name='pt1_temp',
-                           label='pt1 Temperature',
-                           unit='K',
-                           docstring='Temperature of the pt1',
-                           get_cmd="READ:" + self._pt1_temp_addr + ":SIG:TEMP?",
-                           get_parser=_temp_parser
-                           )
+            name="pt1_temp",
+            label="pt1 Temperature",
+            unit="K",
+            docstring="Temperature of the pt1",
+            get_cmd="READ:" + self._pt1_temp_addr + ":SIG:TEMP?",
+            get_parser=_temp_parser,
+        )
         """Parameter pt1 temperature"""
 
         self.pt2_temp: Parameter = self.add_parameter(
-            name='pt2_temp',
-            label='pt2 Temperature',
-            unit='K',
-            docstring='Temperature of the pt2',
+            name="pt2_temp",
+            label="pt2 Temperature",
+            unit="K",
+            docstring="Temperature of the pt2",
             get_cmd="READ:" + self._pt2_temp_addr + ":SIG:TEMP?",
-            get_parser=_temp_parser
+            get_parser=_temp_parser,
         )
         """Parameter pt2 temperature"""
 
