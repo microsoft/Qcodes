7ac158c9dd790aea036707b88f88c30d6eb7d469
Harriet Howard <harriet.howard@oxinst.com>
2025-03-18 13:46:25 +0000

    add test for parameters plotting

--- tests/dataset/test_plotting.py
+++ tests/dataset/test_plotting.py
@@ -10,13 +10,21 @@ from pytest import FixtureRequest
 import qcodes as qc
 from qcodes.dataset.descriptions.detect_shapes import detect_shape_of_measurement
 from qcodes.dataset.measurements import Measurement
+
+# temporarily need to import edited version of plotting.py -> plot_dataset function 
+# as it's not editted in the qcodes package itself
+import sys 
+sys.path.append('../../')
+from src.qcodes.dataset.plotting import plot_dataset
+
 from qcodes.dataset.plotting import (
     _appropriate_kwargs,
     _complex_to_real_preparser,
     _make_rescaled_ticks_and_units,
     plot_by_id,
-    plot_dataset,
+    #plot_dataset,
 )
+from qcodes.parameters import Parameter
 from qcodes.instrument_drivers.mock_instruments import DummyInstrument
 from qcodes.plotting.axis_labels import _ENGINEERING_PREFIXES, _UNITS_FOR_RESCALING
 
@@ -367,3 +375,59 @@ def test__complex_to_real_preparser_complex_setpoint() -> None:
     assert measured_param["label"] == "measured voltage"
     assert measured_param["unit"] == "V"
     assert all(measured_param["data"] == np.array([0, 1, 2]))
+
+@pytest.mark.parametrize("params", [None, 'y', 'y2', ['y', 'y2']])
+def test_plot_dataset_parameters(experiment, request: FixtureRequest, params) -> None: 
+    """
+    Test plotting of specified parameters with plot_dataset 
+    function.
+    """
+    # Generate some data
+    x = Parameter(name="x", label="Voltage", unit="V", set_cmd=None, get_cmd=None)
+    y = Parameter(name="y", label="Voltage", unit="V", set_cmd=None, get_cmd=None)
+    y2 = Parameter(name="y2", label="Current", unit="A", set_cmd=None, get_cmd=None)
+    
+    meas = Measurement()
+    meas.register_parameter(x)
+    meas.register_parameter(y, setpoints=[x])
+    meas.register_parameter(y2, setpoints=[x])
+
+    xvals = np.linspace(-5, 5, 250)
+
+    with meas.run() as datasaver:
+        for xnum in xvals:
+            datasaver.add_result((x, xnum), (y, xnum**2))
+            datasaver.add_result((x, xnum), (y2, -(xnum**2)))
+
+    dataset = datasaver.dataset
+
+    axes, cbaxes = plot_dataset(dataset=dataset, parameters=params)
+
+    # None is default for parameters 
+    # so if parameters is None all dependent parameters should be plotted
+    params = ['y', 'y2'] if params is None else params
+
+    if params == ['y', 'y2']: 
+        assert len(axes) == 2
+
+        for i, param in enumerate(['y', 'y2']): 
+            data = dataset.get_parameter_data()[param][param]
+            plotted = axes[i].get_children()[0].get_ydata()
+            # actual data and plotted not exactly equal (?)
+            # so check for very small diff 
+            for k in range(len(plotted)):
+                diff = (abs(plotted[k]-data[k]))/data[k]
+                assert diff < 0.0000000001 
+
+    # check only 'requested' parameter has been plotted
+    elif params == 'y' or 'y2': 
+        assert len(axes) == 1 
+
+        data = dataset.get_parameter_data()[params][params]
+        plotted = axes[0].get_children()[0].get_ydata()
+        # actual data and plotted not exactly equal (?)
+        # so check for very small diff 
+        for k in range(len(plotted)):
+            diff = (abs(plotted[k]-data[k]))/data[k]
+            assert diff < 0.0000000001
+
