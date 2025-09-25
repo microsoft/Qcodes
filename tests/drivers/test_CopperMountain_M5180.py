9b8b0e91c48745788450c88f56ae9748daff85dd
Thomas Lemon <v-tlemon@microsoft.com>
2025-09-25 11:28:43 -0700

    Update M5065 tests and add tests for M5180

--- tests/drivers/test_CopperMountain_M5180.py
+++ tests/drivers/test_CopperMountain_M5180.py
@@ -1,6 +1,6 @@
 import pytest
 
-from qcodes.instrument_drivers.CopperMountain import CopperMountainM5065
+from qcodes.instrument_drivers.CopperMountain import CopperMountainM5180
 
 
 class DummyM5065:
@@ -13,27 +13,27 @@ class DummyM5065:
 
 @pytest.fixture()
 def vna():
-    instance = CopperMountainM5065(
-        name="M5065",
+    instance = CopperMountainM5180(
+        name="M5180",
         address="TCPIP0::localhost::hislip0::INSTR",
-        pyvisa_sim_file="CopperMountain_M5065.yaml",
+        pyvisa_sim_file="CopperMountain_M5180.yaml",
     )
     instance.reset()
     yield instance
     instance.close()
 
 
-def test_m5065_instantiation(vna):
-    assert vna.name == "M5065"
+def test_m5180_instantiation(vna):
+    assert vna.name == "M5180"
     assert vna._address == "TCPIP0::localhost::hislip0::INSTR"
 
 
 def test_idn_command(vna):
     idn = vna.get_idn()
     assert idn["vendor"] == "CMT"
-    assert idn["model"] == "M5065"
+    assert idn["model"] == "M5180"
 
 
 def test_min_and_max_frequency_defaults(vna):
     assert vna.min_freq == 300e3
-    assert vna.max_freq == 6.5e9
+    assert vna.max_freq == 18e9
