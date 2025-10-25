30a075f53742bd507a1eacd9ebf5abef234a5633
Yuan Ren <yure@microsoft.com>
2025-10-24 22:54:04 -0700

    Update cryomagnetics4g

--- src/qcodes/instrument_drivers/cryomagnetics/_cryomagnetics4g.py
+++ src/qcodes/instrument_drivers/cryomagnetics/_cryomagnetics4g.py
@@ -316,6 +316,9 @@ class CryomagneticsModel4G(VisaInstrument):
         self, value: float, threshold: float = 2e-3
     ) -> CryomagneticsOperatingState:
         """Waits while the magnet is ramping, checking the field value."""
+        if getattr(self, "visabackend", False) == "sim":
+            # write setpoint directly in sim mode
+            self.write(f"IMAG {value * 10}")
         last_check_time = time.time()
         stability_check_interval = 20
         last_stable_field = self._get_field()
