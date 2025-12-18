909428f862fc9253a4b611fee7ff98190f55a690
Jens H. Nielsen <jenielse@microsoft.com>
2025-12-18 14:47:05 +0100

    Make use of generic parameters in Yokogawa driver

--- src/qcodes/instrument_drivers/yokogawa/Yokogawa_GS200.py
+++ src/qcodes/instrument_drivers/yokogawa/Yokogawa_GS200.py
@@ -1,5 +1,5 @@
 from functools import partial
-from typing import TYPE_CHECKING, Literal, cast
+from typing import TYPE_CHECKING, Literal
 
 from qcodes.instrument import (
     InstrumentBaseKWArgs,
@@ -328,12 +328,14 @@ class YokogawaGS200(VisaInstrument):
         )
         """Parameter output"""
 
-        self.source_mode: Parameter = self.add_parameter(
-            "source_mode",
-            label="Source Mode",
-            get_cmd=":SOUR:FUNC?",
-            set_cmd=self._set_source_mode,
-            vals=Enum("VOLT", "CURR"),
+        self.source_mode: Parameter[Literal["VOLT", "CURR"], YokogawaGS200] = (
+            self.add_parameter(
+                "source_mode",
+                label="Source Mode",
+                get_cmd=":SOUR:FUNC?",
+                set_cmd=self._set_source_mode,
+                vals=Enum("VOLT", "CURR"),
+            )
         )
         """Parameter source_mode"""
 
@@ -410,12 +412,13 @@ class YokogawaGS200(VisaInstrument):
         # We need to pass the source parameter for delegate parameters
         # (range and output_level) here according to the present
         # source_mode.
-        if self.source_mode() == "VOLT":
-            self.range.source = self.voltage_range
-            self.output_level.source = self.voltage
-        else:
-            self.range.source = self.current_range
-            self.output_level.source = self.current
+        match self.source_mode():
+            case "VOLT":
+                self.range.source = self.voltage_range
+                self.output_level.source = self.voltage
+            case "CURR":
+                self.range.source = self.current_range
+                self.output_level.source = self.current
 
         self.voltage_limit: Parameter = self.add_parameter(
             "voltage_limit",
@@ -429,7 +432,7 @@ class YokogawaGS200(VisaInstrument):
         )
         """Parameter voltage_limit"""
 
-        self.current_limit: Parameter = self.add_parameter(
+        self.current_limit: Parameter[float, YokogawaGS200] = self.add_parameter(
             "current_limit",
             label="Current Protection Limit",
             unit="I",
@@ -621,10 +624,11 @@ class YokogawaGS200(VisaInstrument):
                 )
         else:
             mode = self.source_mode.get_latest()
-            if mode == "CURR":
-                self_range = 200e-3
-            else:
-                self_range = 30.0
+            match mode:
+                case "CURR":
+                    self_range = 200e-3
+                case "VOLT":
+                    self_range = 30.0
 
         # Check we are not trying to set an out of range value
         if self.range() is None or abs(output_level) > abs(self_range):
@@ -670,7 +674,7 @@ class YokogawaGS200(VisaInstrument):
             # since the parameter is not generic in the data type this cannot
             # narrow None to ModeType even if that is the only valid values
             # for source_mode.
-            source_mode = cast("ModeType", self.source_mode.get_latest())
+            source_mode = self.source_mode.get_latest()
         # Get source range if auto-range is off
         if source_range is None and not self.auto_range():
             source_range = self.range()
