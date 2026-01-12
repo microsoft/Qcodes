588e2b247470911ef7c24f8560fe86f400a11514
Jens H. Nielsen <jenshnielsen@gmail.com>
2026-01-12 07:49:30 +0100

    Tests use type ignore for issue raised by more than one type checker

--- tests/dataset/measurement/test_measurement_context_manager.py
+++ tests/dataset/measurement/test_measurement_context_manager.py
@@ -297,7 +297,7 @@ def test_unregister_parameter(DAC, DMM) -> None:
     not_parameters = [DAC, DMM, 0.0, 1]
     for notparam in not_parameters:
         with pytest.raises(ValueError):
-            meas.unregister_parameter(notparam)  # pyright: ignore[reportArgumentType]
+            meas.unregister_parameter(notparam)  # type: ignore
 
     # unregistering something not registered should silently "succeed"
     meas.unregister_parameter("totes_not_registered")
