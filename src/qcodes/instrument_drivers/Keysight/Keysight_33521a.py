05699509f94652b27b188b5872de28839ef0d963
Fedor Parashchenko <f.parashchenko@module.ru>
2026-06-08 16:37:02 +0300

    Create classes for new instruments

---
+++ src/qcodes/instrument_drivers/Keysight/Keysight_33521a.py
@@ -0,0 +1,7 @@
+from .KeysightAgilent_33XXX import Keysight33xxxSingleChannel
+
+
+class Keysight33521A(Keysight33xxxSingleChannel):
+    """
+    QCoDeS driver for the Keysight 33521A waveform generator.
+    """
---
+++ src/qcodes/instrument_drivers/Keysight/Keysight_33612a.py
@@ -0,0 +1,7 @@
+from .KeysightAgilent_33XXX import Keysight33xxxDualChannels
+
+
+class Keysight33612A(Keysight33xxxDualChannels):
+    """
+    QCoDeS driver for the Keysight 33612A waveform generator.
+    """
