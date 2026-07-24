457c7e66c11bed508ea21092c139f94c701510ce
Jens H. Nielsen <jenshnielsen@gmail.com>
2026-07-24 07:43:52 +0200

    Implement B009

--- tests/drivers/test_lakeshore_372.py
+++ tests/drivers/test_lakeshore_372.py
@@ -62,9 +62,9 @@ class MockVisaInstrument:
                 f = getattr(self, func_name)
                 # only add for methods that have such an attribute
                 with suppress(AttributeError):
-                    self.queries[getattr(f, "query_name")] = f
+                    self.queries[f.query_name] = f
                 with suppress(AttributeError):
-                    self.cmds[getattr(f, "command_name")] = f
+                    self.cmds[f.command_name] = f
 
     def write_raw(self, cmd) -> None:
         cmd_parts = cmd.split(" ")
