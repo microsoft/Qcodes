d59f0b6d81243f2d93a7eea89d792ef3e18c6550
Jens H. Nielsen <jenshnielsen@gmail.com>
2026-07-27 07:46:42 +0200

    Ignore new type checking errors

--- src/qcodes/utils/json_utils.py
+++ src/qcodes/utils/json_utils.py
@@ -61,7 +61,7 @@ class NumpyJSONEncoder(json.JSONEncoder):
             }
         elif hasattr(o, "_JSONEncoder"):
             # Use object's custom JSON encoder
-            jsosencode = o._JSONEncoder
+            jsosencode = o._JSONEncoder  # pyright: ignore[reportAttributeAccessIssue]
             return jsosencode()
         else:
             try:
@@ -82,7 +82,7 @@ class NumpyJSONEncoder(json.JSONEncoder):
                 ):
                     return {
                         "__class__": type(o).__name__,
-                        "__args__": o.__getnewargs__(),
+                        "__args__": o.__getnewargs__(),  # pyright: ignore[reportAttributeAccessIssue]
                     }
                 else:
                     # we cannot convert the object to JSON, just take a string
