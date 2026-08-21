47c2e9934a5544b36375c6ae0837b079a3b8bb06
Jens H. Nielsen <jenshnielsen@gmail.com>
2026-08-21 07:36:25 +0200

    Suppress ty on the ChannelList setitem narrowing
    Narrowing the value with isinstance does not tell either checker that it
    is the element type of the list, because the element type is a TypeVar
    bound to InstrumentModule. Extend the existing mypy suppression to ty
    and move the explanatory comment above the line it applies to.

--- src/qcodes/instrument/channel.py
+++ src/qcodes/instrument/channel.py
@@ -731,9 +731,10 @@ class ChannelList[InstrumentModuleType: "InstrumentModule"](  #  pyright: ignore
         # asserts added to work around https://github.com/python/mypy/issues/7858
         if isinstance(index, int):
             assert isinstance(value, InstrumentModule)
-            self._channels[index] = value  # type: ignore[assignment]
-            # mypy does not know that InstrumentModuleType is a TypeVar bound to
-            # InstrumentModule so complains here
+            # neither mypy nor ty knows that InstrumentModuleType is a TypeVar
+            # bound to InstrumentModule, so narrowing value with the isinstance
+            # above does not give them the element type of the list
+            self._channels[index] = value  # type: ignore[assignment]  # ty: ignore[invalid-assignment]
         else:
             assert not isinstance(value, InstrumentModule)
             self._channels[index] = value
