faa1fd28aa2c8dafacf629e2bb713cfbc28d8c86
Jens H. Nielsen <jenielse@microsoft.com>
2026-09-16 09:25:38 +0200

    Better error mesage in multi channel set

--- src/qcodes/parameters/multi_channel_instrument_parameter.py
+++ src/qcodes/parameters/multi_channel_instrument_parameter.py
@@ -66,7 +66,9 @@ class MultiChannelInstrumentParameter[InstrumentModuleType: "InstrumentModule"](
                 # Catch wrong length of value before any setting is done
                 value_list = list(value)
                 if len(value_list) != len(self._channels):
-                    raise ValueError
+                    raise ValueError(
+                        "Length of value sequence does not match the number of channels"
+                    )
                 for chan, val in zip(self._channels, value_list):
                     getattr(chan, self._param_name).set(val)
             except (TypeError, ValueError):
